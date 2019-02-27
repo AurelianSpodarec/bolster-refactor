6e3757252f83211326eb413757341a07bf9f2573
Thomas Duthie <tom@silverchip.com>
2019-02-27 15:01:39 +0000

    login setting up

--- app-src/src/components/auth/login/containers/LoginContainer.js
+++ app-src/src/components/auth/login/containers/LoginContainer.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Login from '../presentational/Login';
+
+import clearFieldErrors from 'actions/generic/sync/fieldErrors/clearFieldErrors';
+import addFieldError from 'actions/generic/sync/fieldErrors/addFieldError';
 
 class LoginContainer extends Component {
     state = {
-        email:'',
-        password:''
+        email: '',
+        password: ''
     };
     render() {
         return (
-            
+            <Login
+                {...this.state}
+                handleInputChange={this.handleInputChange}
+                validateEmail={this.validateEmail}
+            />
         );
     }
-}
 
-export default LoginContainer
+    componentDidMount = () => {
+        this.props.clearFieldErrors();
+    };
+    handleInputChange = e => {
+        e.preventDefault();
+
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        });
+    };
+    validateEmail = value => {
+        if (!(value && value.includes('@')))
+            return 'This is not a valid email.';
+
+        if (value && value.length < 4) return 'This email is too short.';
+    };
+}
+const mapDispatchToProps = dispatch => ({
+    clearFieldErrors: () => {
+        dispatch(clearFieldErrors());
+    },
+    addFieldError: (fieldName, error) => {
+        dispatch(addFieldError(fieldName, error));
+    }
+});
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(LoginContainer);
--- app-src/src/components/auth/login/presentational/Login.js
+++ app-src/src/components/auth/login/presentational/Login.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
-const Login = () => <h1>Login</h1>;
+import TextInputContainer from '../../../generic/containers/TextInputContainer';
+
+const Login = (email, password) => (
+    <div className="size-lg-12">
+        <form>
+            <TextInputContainer
+                value={email}
+                name="email"
+                placeholder="email"
+                required
+            />
+            <TextInputContainer
+                value={password}
+                name="password"
+                placeholder="password"
+                required
+            />
+            <button type="submit">Login</button>
+        </form>
+    </div>
+);
 
 export default Login;
