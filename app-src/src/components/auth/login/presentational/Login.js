52a3874f4e246454841a51c9f9a9c14362a396f3
Thomas Duthie <tom@silverchip.com>
2019-02-27 17:05:13 +0000

    Layout styles as well as login styles

--- app-src/src/components/auth/login/presentational/Login.js
+++ app-src/src/components/auth/login/presentational/Login.js
@@ -4,27 +4,32 @@ import { Link } from 'react-router-dom';
 import TextInputContainer from '../../../generic/containers/TextInputContainer';
 
 const Login = ({ email, password, validateEmail, handleInputChange }) => (
-    <div className="size-lg-12">
-        <form>
-            <TextInputContainer
-                value={email}
-                name="email"
-                placeholder="email"
-                required
-                handleChange={handleInputChange}
-                validate={validateEmail}
-            />
-            <TextInputContainer
-                value={password}
-                name="password"
-                type="password"
-                placeholder="password"
-                handleChange={handleInputChange}
-                required
-            />
-            <Link to="/">Forgot password</Link>
-            <button type="submit">Login</button>
-        </form>
+    <div className="auth size-lg-12" id="page-area">
+        <div className="container">
+            <h1 className="title size-lg-12">Log in</h1>
+            <div className="cointent-area size-lg-12">
+                <form className="size-lg-12">
+                    <TextInputContainer
+                        value={email}
+                        name="email"
+                        placeholder="email"
+                        required
+                        handleChange={handleInputChange}
+                        validate={validateEmail}
+                    />
+                    <TextInputContainer
+                        value={password}
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                        handleChange={handleInputChange}
+                        required
+                    />
+                    <Link to="/">Forgot password</Link>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
     </div>
 );
 
