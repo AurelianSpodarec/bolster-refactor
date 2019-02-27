f973d7e39fea2163c7b64aeea631c8f23865c6b8
Oliver Theabould <oli@silverchip.com>
2019-02-27 14:17:46 +0000

    add `Dropdown` component

---
+++ app-src/src/components/generic/presentational/Dropdown.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Dropdown = ({
+    placeholder = '--- select an option ---',
+    name,
+    handleDropdownChange,
+    options,
+    selectedOption = { text: placeholder }
+}) => {
+    return (
+        <div className="custom-dropdown size-lg-12">
+            <label>
+                <input type="text" value={selectedOption.text} />
+                <i className="arrow far fa-chevron-down" />
+                <select name={name} onChange={handleDropdownChange}>
+                    <option value={0}>{placeholder}</option>
+                    {options.map(({ text, value }) => (
+                        <option
+                            key={value}
+                            value={value}
+                            selected={selectedOption.value === value}
+                        >
+                            {text}
+                        </option>
+                    ))}
+                </select>
+            </label>
+        </div>
+    );
+};
+
+export default Dropdown;
