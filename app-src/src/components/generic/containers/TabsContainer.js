91d19cbf54a4589d9b274e617782b4d1b7ea650e
Thomas Duthie <tom@silverchip.com>
2019-02-27 14:08:39 +0000

    Generics added back for form

---
+++ app-src/src/components/generic/containers/TabsContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import selectTab from 'actions/generic/sync/tabs/selectTab';
+
+import Tabs from '../presentational/Tabs';
+
+const TabsContainer = ({ dispatch, tabs, selectedTab }) => (
+    <Tabs
+        tabs={tabs}
+        selectedTab={selectedTab}
+        selectTab={(e, tab) => {
+            e.preventDefault();
+            dispatch(selectTab(tab));
+        }}
+    />
+);
+
+const mapStateToProps = state => state.genericReducers.tabs;
+
+export default connect(mapStateToProps)(TabsContainer);
---
+++ app-src/src/components/generic/presentational/TextInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TextInput = props => (
+    <div className="row">
+        <input
+            className="col s6"
+            type={props.type}
+            name={props.name}
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange={props.handleChange}
+            onBlur={props.handleBlur}
+        />
+        {!!props.error && (
+            <p className="error red-text text-accent-4">{props.error}</p>
+        )}
+    </div>
+);
+
+export default TextInput;
