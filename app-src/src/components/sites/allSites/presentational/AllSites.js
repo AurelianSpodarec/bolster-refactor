a4930d05b0da8de3b8c52c9ec13ad648cfeea418
Oliver Theabould <oli@silverchip.com>
2019-02-27 17:25:55 +0000

    add `fetchSites` action

--- app-src/src/components/sites/allSites/presentational/AllSites.js
+++ app-src/src/components/sites/allSites/presentational/AllSites.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import SitesFiltersContainer from '../containers/SitesFiltersContainer';
+import SitesListContainer from '../containers/SitesListContainer';
 
 const AllSites = () => (
     <div>
         <h1>All Sites</h1>
         <SitesFiltersContainer />
+        <SitesListContainer />
     </div>
 );
 
