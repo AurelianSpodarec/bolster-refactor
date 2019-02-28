8876b49fdeff8a5b66247b43f77dea1a3b8eca86
Oliver Theabould <oli@silverchip.com>
2019-02-28 13:14:21 +0000

    add sites list

--- app-src/src/components/sites/allSites/presentational/SitesList.js
+++ app-src/src/components/sites/allSites/presentational/SitesList.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SitesList = ({ sites }) =>
     sites.map(site => (
         <tr key={site.id}>
+            <td>{sites.length}</td>
             <td>{site.name}</td>
             <td>{site.ownedBy}</td>
             <td>{site.permissions}</td>
