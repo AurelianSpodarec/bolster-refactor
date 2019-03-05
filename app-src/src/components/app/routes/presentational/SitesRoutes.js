767530e48d794b7d4ff990079a0cd69ec11e2695
Bob <bob@silverchip.com>
2019-03-05 16:40:36 +0000

    App switch fix

--- app-src/src/components/app/routes/presentational/SitesRoutes.js
+++ app-src/src/components/app/routes/presentational/SitesRoutes.js
@@ -5,11 +5,11 @@ import SwitchWith404 from './SwitchWith404';
 import AllSights from 'components/sites/allSites/presentational/AllSites';
 import Site from 'components/sites/singleSite/presentational/Site';
 
-const SitesRoutes = () => (
-    <Switch>
-        <Route exact path="/sites" component={AllSights} />
-        <Route exact path="/site/single" component={Site} />
-    </Switch>
+const SitesRoutes = ({ match: { url: baseUrl } }) => (
+    <SwitchWith404>
+        <Route exact path={`${baseUrl}`} component={AllSights} />
+        <Route exact path={`${baseUrl}/:id`} component={Site} />
+    </SwitchWith404>
 );
 
 export default SitesRoutes;
