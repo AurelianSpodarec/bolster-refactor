eff44dab07bcc96f5b69ee18c31a39ca327f41e6
Oliver Theabould <oli@silverchip.com>
2019-03-06 11:49:41 +0000

    update `Breadcrumb` to accept children

--- app-src/src/components/layout/breadcrumb/presentational/Breadcrumb.js
+++ app-src/src/components/layout/breadcrumb/presentational/Breadcrumb.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Breadcrumb = ({ classNames = 'size-lg-12' }) => (
+const Breadcrumb = ({ classNames = 'size-lg-12', children }) => (
     <div className="content-container size-lg-12">
-        <div className={`content-area ${classNames}`}>
-            <h3 className="heading heading-3">Breadcrumb item 1 - item 2</h3>
+        <div className="content-area size-lg-12">
+            <div className={`breadcrumb ${classNames}`}>
+                <h3 className="heading heading-3">
+                    Breadcrumb item 1 - item 2
+                </h3>
+            </div>
+            {children}
         </div>
     </div>
 );
