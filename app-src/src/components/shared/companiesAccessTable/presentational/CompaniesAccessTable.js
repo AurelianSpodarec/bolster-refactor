d355eec0856330cf57c5fa784d2542e1de1a423f
Bob <bob@silverchip.com>
2019-03-06 16:55:48 +0000

    Added scaffolding buttons

--- app-src/src/components/shared/companiesAccessTable/presentational/CompaniesAccessTable.js
+++ app-src/src/components/shared/companiesAccessTable/presentational/CompaniesAccessTable.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
 
-const CompaniesAccess = () => (
+const CompaniesAccess = ({ location }) => (
     <div className="content-container size-lg-4">
         <div className="content-area size-lg-12">
             <h1 className="heading heading-3 size-lg-12">
                 CompaniesAccessTable
             </h1>
+            <Link className="button" to={`${location.pathname}/add-company`}>
+                Add company
+            </Link>
         </div>
     </div>
 );
 
-export default CompaniesAccess;
+export default withRouter(CompaniesAccess);
--- app-src/src/components/shared/operatives/presentational/OperativesTable.js
+++ app-src/src/components/shared/operatives/presentational/OperativesTable.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
 
-const OperativesTable = () => (
+const OperativesTable = ({ location }) => (
     <div className="content-container size-lg-4">
         <div className="content-area size-lg-12">
             <h1 className="heading heading-3 size-lg-12">Operatives</h1>
+            <Link
+                className="button"
+                to={`${location.pathname}/attatch-operative`}
+            >
+                Attatch operative
+            </Link>
         </div>
     </div>
 );
 
-export default OperativesTable;
+export default withRouter(OperativesTable);
