ed4978b4c34d6dc229be75a2e0198bc14bc2241a
Mark Baker <mark@silverchip.com>
2019-03-06 16:34:45 +0000

    updates to layouts in site single

--- app-src/src/components/shared/companiesAccessTable/presentational/CompaniesAccessTable.js
+++ app-src/src/components/shared/companiesAccessTable/presentational/CompaniesAccessTable.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
 const CompaniesAccess = () => (
-    <div className="content-container size-lg-4">
-        <div className="content-area size-lg-12">
-            <h1 className="heading heading-3 size-lg-12">
-                CompaniesAccessTable
-            </h1>
-        </div>
+    <div className="size-lg-12">
+        <h1 className="heading heading-3 size-lg-12">CompaniesAccessTable</h1>
     </div>
 );
 
--- app-src/src/components/shared/operatives/presentational/OperativesTable.js
+++ app-src/src/components/shared/operatives/presentational/OperativesTable.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const OperativesTable = () => (
-    <div className="content-container size-lg-4">
-        <div className="content-area size-lg-12">
-            <h1 className="heading heading-3 size-lg-12">Operatives</h1>
-        </div>
+    <div className="size-lg-12">
+        <h1 className="heading heading-3 size-lg-12">Operatives</h1>
     </div>
 );
 
