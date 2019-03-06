77970dbc3e75597f8a9c72d27aae32522ad16d95
Oliver Theabould <oli@silverchip.com>
2019-03-06 11:16:35 +0000

    add breadcrumb container

---
+++ app-src/src/components/sites/changeSiteOwnership/containers/ChangeSiteOwnership.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import BreadcrumbContainer from 'components/layout/breadcrumb/containers/BreadcrumbContainer';
+
+const ChangeSiteOwnership = () => (
+    <div>
+        <BreadcrumbContainer />
+    </div>
+);
+
+export default ChangeSiteOwnership;
