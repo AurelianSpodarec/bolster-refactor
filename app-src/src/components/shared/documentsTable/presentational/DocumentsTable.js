b4de931266b3c159db7539f77304c2f92b3cfd86
Bob <bob@silverchip.com>
2019-03-06 17:25:05 +0000

    Invite company added in shared

--- app-src/src/components/shared/documentsTable/presentational/DocumentsTable.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DocumentsTable = () => (
-    <div className="size-lg-12">
-        <h1 className="heading heading-3 size-lg-12">DocumentsTable</h1>
-    </div>
-);
-
-export default DocumentsTable;
