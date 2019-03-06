85e90f15d22b4eaaa749008617c50a0dbd57a0ba
Bob <bob@silverchip.com>
2019-03-06 19:33:37 +0000

    Added invite company

--- app-src/src/components/drawings/singleDrawing/containers/DrawingDocumentsContainer.js
+++ app-src/src/components/drawings/singleDrawing/containers/DrawingDocumentsContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import DocumentsTable from 'components/shared/documentsTable/presentational/DocumentsTable';
+import DocumentsTable from 'components/shared/documents/presentational/DocumentsTable';
 
 class DrawingDocumentsContainer extends Component {
     render() {
---
+++ app-src/src/components/sites/singleSite/containers/InviteCompanyContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import InviteCompany from 'components/shared/company/presentational/InviteCompany';
+
+class InviteCompanyContainer extends Component {
+    render() {
+        return <InviteCompany />;
+    }
+}
+
+export default InviteCompanyContainer;
