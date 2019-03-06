796f60e1eb644fdc3935b5e7446beeb7c892d37a
Mark Baker <mark@silverchip.com>
2019-03-06 11:49:51 +0000

    added breadcrumb

--- app-src/src/components/drawings/singleDrawing/presentational/SingleDrawing.js
+++ app-src/src/components/drawings/singleDrawing/presentational/SingleDrawing.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import BreadcrumbContainer from 'components/layout/breadcrumb/containers/BreadcrumbContainer';
 import TabsContainer from 'components/generic/tabs/containers/TabsContainer';
 import SingleDrawingRouteContainer from '../containers/SingleDrawingRouteContainer';
 
 const Drawing = () => (
     <div className="size-lg-12">
-        <TabsContainer />
+        <div className="content-container size-lg-12">
+            <div className="content-area size-lg-12">
+                <BreadcrumbContainer />
+                <TabsContainer />
+            </div>
+        </div>
+
         <SingleDrawingRouteContainer />
     </div>
 );
