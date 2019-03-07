ab29e2a1e8af1a8290c185f7a59c720d7747131c
Oliver Theabould <oli@silverchip.com>
2019-03-07 11:04:18 +0000

    update form to validate email and disable button on submit

--- app-src/src/components/auth/login/presentational/Login.js
+++ app-src/src/components/auth/login/presentational/Login.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
-import Form from 'components/shared/generic/form/containers/Form';
-import Field from 'components/shared/generic/form/presentational/Field';
+import LoginFormContainer from '../containers/LoginFormContainer';
 
-const Login = ({
-    email,
-    password,
-    validateEmail,
-    handleInputChange,
-    handleSubmit
-}) => (
+const Login = () => (
     <div className="auth size-lg-12">
         <div className="content-container size-lg-12">
             <h1 className="heading heading-1 size-lg-12">Log In</h1>
@@ -20,39 +11,7 @@ const Login = ({
                 <h3 className="heading heading-3 size-lg-12">
                     Enter your login details
                 </h3>
-                <Form className="size-lg-12" onSubmit={handleSubmit}>
-                    <Field name="Email" sizeClasses="size-lg-4">
-                        <TextInputContainer
-                            value={email}
-                            name="email"
-                            placeholder="Please enter your email"
-                            required
-                            handleChange={handleInputChange}
-                            validate={validateEmail}
-                        />
-                    </Field>
-
-                    <div className="size-lg-1" />
-
-                    <Field name="Password" sizeClasses="size-lg-4">
-                        <TextInputContainer
-                            value={password}
-                            name="password"
-                            type="password"
-                            placeholder="Please enter your password"
-                            handleChange={handleInputChange}
-                            required
-                        />
-                    </Field>
-
-                    <div className="button-area size-lg-12">
-                        <button type="submit">Login</button>
-
-                        <Link className="button" to="/forgot-password">
-                            Forgot password
-                        </Link>
-                    </div>
-                </Form>
+                <LoginFormContainer />
             </div>
         </div>
     </div>
