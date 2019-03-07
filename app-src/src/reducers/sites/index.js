835d168680006c86b2021f114c0beb2a75deb786
Bob <bob@silverchip.com>
2019-03-07 19:50:56 +0000

    Started client actions/reducers

--- app-src/src/reducers/sites/index.js
+++ app-src/src/reducers/sites/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import sites from './sites';
 import sitesFilters from './sitesFilters';
+import singleSiteClients from './singleSiteClients';
 
 export default combineReducers({
     sites,
+    singleSiteClients,
     sitesFilters
 });
