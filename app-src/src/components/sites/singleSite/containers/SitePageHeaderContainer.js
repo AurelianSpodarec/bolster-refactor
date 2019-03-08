70478f74dcc1ee1b2ace7f48798cab3397996d96
Bob <bob@silverchip.com>
2019-03-08 14:57:58 +0000

    Started container on single site

--- app-src/src/components/sites/singleSite/containers/SitePageHeaderContainer.js
+++ app-src/src/components/sites/singleSite/containers/SitePageHeaderContainer.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 
 class SitePageHeaderContainer extends Component {
     render() {
         return (
-            <PageHeading title={'Site:'}>
+            <PageHeading title={`Site: ${this.props.site.name}`}>
                 <Link to="/site">Change Ownership</Link>
             </PageHeading>
         );
     }
 }
 
-const mapStateToProps = state => ({});
-
-const mapDispatchToProps = dispatch => ({});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(SitePageHeaderContainer);
+export default withRouter(
+    connect(({ sitesReducers }, ownProps) => ({
+        site: sitesReducers.sites[ownProps.match.params.id] || {}
+    }))(SitePageHeaderContainer)
+);
