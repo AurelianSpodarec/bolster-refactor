956578275516b8e9cfbf54132e8633ce42033ca9
Mark Baker <mark@silverchip.com>
2019-03-11 15:04:45 +0000

    operatives with access proper stylings

--- app-src/src/components/shared/operatives/presentational/OperativesList.js
+++ app-src/src/components/shared/operatives/presentational/OperativesList.js
@@ -4,10 +4,17 @@ import { Link } from 'react-router-dom';
 const DocumentsList = ({ operatives }) =>
     [...operatives].map(operative => (
         <tr key={operative.id}>
-            <td>{operative.name}</td>
+            <td className="small-text">
+                {`${operative.name} - ${operative.id}`}
+                <br />
+                {operative.email}
+            </td>
             <td>
-                <Link to={`/pins/${operative.id}`} className="button">
-                    View
+                <Link to="#" className="button icon-only">
+                    <i className="far fa-pencil fa-fw" />
+                </Link>
+                <Link to="#" className="button icon-only">
+                    <i className="far fa-trash-alt fa-fw" />
                 </Link>
             </td>
         </tr>
