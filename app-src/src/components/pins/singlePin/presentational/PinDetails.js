202061cdb9f950c0a7e460d9a05514da62573d55
Oliver Theabould <oli@silverchip.com>
2019-03-12 17:18:36 +0000

    udpate pin details

--- app-src/src/components/pins/singlePin/presentational/PinDetails.js
+++ app-src/src/components/pins/singlePin/presentational/PinDetails.js
@@ -2,13 +2,23 @@ import React from 'react';
 
 import Block from 'components/shared/generic/block/presentational/Block';
 
-const PinDetails = () => (
-    <Block>
-        <h3 className="heading heading-3">Details</h3>
+const PinDetails = ({ pinHistory }) => (
+    <div>
+        <Block>
+            <h3 className="heading heading-3">Details</h3>
+            <p>ID</p>
+            <p>{pinHistory.id}</p>
+            <p>History</p>
+            <p>{pinHistory.id}</p>
+            <p>ID</p>
+            <p>{pinHistory.id}</p>
+            <p>ID</p>
+            <p>{pinHistory.id}</p>
+        </Block>
         <a className="button" href="#/">
             Edit this history
         </a>
-    </Block>
+    </div>
 );
 
 export default PinDetails;
