dfcf39ed20051467fbe35eef04855cc87bd3cd2e
Mark Baker <mark@silverchip.com>
2019-03-19 16:10:46 +0000

    heading for sites list

--- app-src/src/components/sites/allSites/presentational/AllSites.js
+++ app-src/src/components/sites/allSites/presentational/AllSites.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Breadcrumb from 'components/shared/generic/breadcrumb/presentational/Breadcrumb';
 import SitesFiltersContainer from '../containers/SitesFiltersContainer';
 import SitesTableContainer from '../containers/SitesTableContainer';
 import Block from 'components/shared/generic/block/presentational/Block';
+import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 
 const AllSites = () => (
-    <div>
-        <Breadcrumb breadcrumbs={[{ text: 'Sites' }]} />
-        <h1>All Sites</h1>
+    <>
+        <PageHeading title="Add Sites">
+            <Link to="/sites/add-site" className="button pull-right">
+                <i className="far fa-plus" /> Add site
+            </Link>
+        </PageHeading>
+
         <Block>
             <SitesFiltersContainer />
         </Block>
 
         <SitesTableContainer />
-        <Link className="button" to="/buildings/1">
-            View Building
-        </Link>
-        <Link className="button" to="/floors/1">
-            View Floor
-        </Link>
-    </div>
+    </>
 );
 
 export default AllSites;
