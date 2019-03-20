a43905a8467be3d7c5ded115b70e24b068487841
Bob <bob@silverchip.com>
2019-03-20 15:27:50 +0000

    Add buildings actions/reducer in progress

---
+++ app-src/src/actions/buildings/async/createBuilding.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { API_URL } from 'config/index';
+
+import setAPIFieldErrors from 'actions/generic/fieldErrors/sync/setAPIFieldErrors';
+import { getHeaders } from 'helpers/api';
+import {
+    CREATE_BUILDING_REQUEST,
+    CREATE_BUILDING_SUCCESS,
+    CREATE_BUILDING_FAILURE
+} from 'constants/actionTypes/buildings';
+
+export const createBuildingRequest = () => ({
+    type: CREATE_BUILDING_REQUEST
+});
+
+export const createBuildingSuccess = payload => ({
+    type: CREATE_BUILDING_SUCCESS,
+    payload
+});
+
+export const createBuildingFailure = error => ({
+    type: CREATE_BUILDING_FAILURE,
+    error
+});
+
+export default postBody => dispatch => {
+    dispatch(createBuildingRequest());
+
+    axios
+        .post(`${API_URL}/buildings`, postBody, getHeaders())
+        .then(result => dispatch(createBuildingSuccess(result.data)))
+        .catch(error => {
+            dispatch(createBuildingFailure(error));
+            if (error.response.status === 400)
+                dispatch(setAPIFieldErrors(error.response.data.errors));
+        });
+};
