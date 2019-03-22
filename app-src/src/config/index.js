36399c7657c1c00002c4c7d476a6d869c8be9a93
Bob <bob@silverchip.com>
2019-03-22 10:13:06 +0000

    API URL change

--- app-src/src/config/index.js
+++ app-src/src/config/index.js
@@ -1,2 +1,2 @@
 export const API_URL =
-    'http://bolsterdynamicstaging-api.69k2qimca3.eu-west-1.elasticbeanstalk.com/api';
+    'http://bolsterdynamicstaging-api.69k2qimca3.eu-west-1.elasticbeanstalk.com/api/company';
