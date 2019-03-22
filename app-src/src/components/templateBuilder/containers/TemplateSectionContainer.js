a92038b9b973f04c2d4a1354492807b98a72ffe2
Bob <bob@silverchip.com>
2019-03-22 16:19:46 +0000

    Delete section added

--- app-src/src/components/templateBuilder/containers/TemplateSectionContainer.js
+++ app-src/src/components/templateBuilder/containers/TemplateSectionContainer.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import deleteSection from 'actions/templateBuilder/sync/deleteSection';
+
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import TemplateSection from '../presentational/TemplateSection';
 
@@ -6,10 +10,29 @@ class TemplateSectionContainer extends Component {
     render() {
         return (
             <BlockContainer>
-                <TemplateSection section={this.props.section} />
+                <TemplateSection
+                    section={this.props.section}
+                    deleteSection={this.deleteSection}
+                />
             </BlockContainer>
         );
     }
+
+    deleteSection = e => {
+        const { deleteSection } = this.props;
+
+        e.preventDefault();
+
+        deleteSection(this.props.section.uuid);
+    };
 }
 
-export default TemplateSectionContainer;
+const mapDispatchToProps = dispatch => ({
+    deleteSection: uuid => {
+        dispatch(deleteSection(uuid));
+    }
+});
+export default connect(
+    null,
+    mapDispatchToProps
+)(TemplateSectionContainer);
--- app-src/src/reducers/templateBuilder.js
+++ app-src/src/reducers/templateBuilder.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
-import { updateObj } from 'helpers/generic';
-import { ADD_SECTION } from 'constants/actionTypes/templateBuilder';
+import { updateObj, removeObjItem } from 'helpers/generic';
+import {
+    ADD_SECTION,
+    DELETE_SECTION
+} from 'constants/actionTypes/templateBuilder';
 
 export default combineReducers({
     sections: sectionsReducer
@@ -11,6 +14,9 @@ function sectionsReducer(state = {}, action) {
     switch (action.type) {
         case ADD_SECTION:
             return updateObj(state, action.section.uuid, action.section);
+
+        case DELETE_SECTION:
+            return removeObjItem(state, action.uuid);
         default:
             return state;
     }
