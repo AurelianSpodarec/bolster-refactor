23a1f20ff2247ae2b2c31d72d2a49a906e73e7bf
Emma Randall <emma@silverchip.com>
2019-03-25 12:09:08 +0000

    change usermanagement route

--- app-src/src/components/app/routes/presentational/UserManagementRoutes.js
+++ app-src/src/components/app/routes/presentational/UserManagementRoutes.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import AllUsers from 'components/userManagement/allUsers/presentational/AllUsers';
 import AllAdmins from 'components/userManagement/allAdmins/presentational/AllAdmins';
 
 import SwitchWith404 from './SwitchWith404';
+import AllUsersContainer from 'components/userManagement/allUsers/containers/AllUsersContainer';
 
-const UserMannagementRoutes = ({ match: { url: baseUrl } }) => (
+const UserManagementRoutes = ({ match: { url: baseUrl } }) => (
     <SwitchWith404>
-        <Route exact path={`${baseUrl}/users`} component={AllUsers} />
+        <Route exact path={`${baseUrl}`} component={AllUsersContainer} />
         <Route exact path={`${baseUrl}/admins`} component={AllAdmins} />
     </SwitchWith404>
 );
 
-export default UserMannagementRoutes;
+export default UserManagementRoutes;
