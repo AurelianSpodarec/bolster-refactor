69f968f7d7df0508a4b0679cddec90c691a8e6ad
Mark Baker <mark@silverchip.com>
2019-03-25 11:29:37 +0000

    5000 pins output commented out

--- app-src/src/components/drawings/singleDrawing/containers/DrawingMapGeneralContainer.js
+++ app-src/src/components/drawings/singleDrawing/containers/DrawingMapGeneralContainer.js
@@ -19,21 +19,26 @@ class DrawingMapGeneralContainer extends Component {
         serviceTypeSelected: 'All services',
         pinLat: 51.505,
         pinLng: -0.09,
-        mapZoom: 13,
-        pins: [
-            {
-                id: 1,
-                latitude: 56.78696472965114,
-                longitude: -83.74763705103969,
-                title: 'Marker 1',
-                description: 'Description 1',
-                status: 'Action Required'
-            }
-        ]
+        mapZoom: 3
     };
 
     render() {
         const position = [this.state.pinLat, this.state.pinLng];
+
+        // let pins = [];
+
+        // for (let i = 0; i < 5000; i++) {
+        //     const newPin = {
+        //         id: i,
+        //         latitude: 56.78696472965114,
+        //         longitude: -83.74763705103969,
+        //         title: `Marker ${i}`,
+        //         description: `Description ${i}`,
+        //         status: 'Action Required'
+        //     };
+
+        //     pins.push(newPin);
+        // }
 
         return (
             <div className="size-lg-12">
@@ -45,7 +50,7 @@ class DrawingMapGeneralContainer extends Component {
                 <DrawingMapViewSimple
                     position={position}
                     zoom={this.state.mapZoom}
-                    pins={this.state.pins}
+                    pins={this.props.pins}
                     handleClick={this.handleClick}
                 />
             </div>
