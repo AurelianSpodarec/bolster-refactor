33d66d9a2dc64fc1f8113b79f018b286967c803f
Emma Randall <emma@silverchip.com>
2019-03-25 12:09:24 +0000

    refactor service table

--- app-src/src/components/services/shared/containers/ServiceTableContainer.js
+++ app-src/src/components/services/shared/containers/ServiceTableContainer.js
@@ -1,34 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import ServiceTable from '../presentational/ServiceTable';
-import fetchAllServices from 'actions/services/async/fetchAllServices';
 
-class ServiceTableContainer extends Component {
-    componentDidMount() {
-        this.props.fetchAllServices();
-    }
-    render() {
-        const { isFetching, error, services } = this.props;
-        return (
-            <ServiceTable
-                headers={['Service name', '']}
-                isFetching={isFetching}
-                error={error}
-                services={services}
-            />
-        );
-    }
-}
+const ServiceTableContainer = ({ isFetching, error, services }) => (
+    <ServiceTable
+        headers={['Service name', '']}
+        isFetching={isFetching}
+        error={error}
+        services={services}
+    />
+);
 
-export default connect(
-    ({ servicesReducer }) => ({
-        isFetching: servicesReducer.isFetching,
-        error: servicesReducer.error,
-        services: servicesReducer.services
-    }),
-    dispatch => ({
-        fetchAllServices: () => {
-            dispatch(fetchAllServices());
-        }
-    })
-)(ServiceTableContainer);
+export default connect(({ servicesReducer }) => ({
+    isFetching: servicesReducer.isFetching,
+    error: servicesReducer.error,
+    services: servicesReducer.services
+}))(ServiceTableContainer);
