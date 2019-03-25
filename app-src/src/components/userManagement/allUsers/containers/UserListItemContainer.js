9a298fd552edfd899115f6b437e82a65f8d105e1
Emma Randall <emma@silverchip.com>
2019-03-25 17:02:55 +0000

    showModal function in UserListItem Container

--- app-src/src/components/userManagement/allUsers/containers/UserListItemContainer.js
+++ app-src/src/components/userManagement/allUsers/containers/UserListItemContainer.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { showModal } from 'actions/generic/modals/sync/showModal';
+import { EDIT_USER_PASSWORD } from 'constants/modalTypes';
 import UserListItem from '../presentational/UserListItem';
-const UserListItemContainer = ({ user, colCount }) => {
-    return <UserListItem user={user} colCount={colCount} />;
+
+const UserListItemContainer = ({ user, colCount, showModal }) => {
+    const handleShowModal = user => {
+        showModal(EDIT_USER_PASSWORD, user);
+    };
+    return (
+        <UserListItem
+            user={user}
+            colCount={colCount}
+            handleShowModal={handleShowModal}
+        />
+    );
 };
 
-export default UserListItemContainer;
+const mapDispatchToProps = dispatch => ({
+    showModal: (modalType, modalProps) => {
+        dispatch(showModal(modalType, modalProps));
+    }
+});
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(UserListItemContainer);
