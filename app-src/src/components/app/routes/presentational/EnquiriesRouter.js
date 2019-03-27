68f7ff8d423599bd0456a8a2150227d00f6a7bf3
Emma Randall <emma@silverchip.com>
2019-03-27 14:50:27 +0000

    add enquiry to router

--- app-src/src/components/app/routes/presentational/EnquiriesRouter.js
+++ app-src/src/components/app/routes/presentational/EnquiriesRouter.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import SwitchWith404 from './SwitchWith404';
 import AllEnquiriesContainer from 'components/siteManagement/enquiries/shared/containers/AllEnquiriesContainer';
+import SingleEnquiryContainer from 'components/siteManagement/enquiries/singleEnquiry/containers/SingleEnquiryContainer';
 
 const EnquiriesRoutes = ({ match: { url: baseUrl } }) => (
     <SwitchWith404>
         <Route exact path={`${baseUrl}`} component={AllEnquiriesContainer} />
+        <Route path={`${baseUrl}/:id`} component={SingleEnquiryContainer} />
     </SwitchWith404>
 );
 
