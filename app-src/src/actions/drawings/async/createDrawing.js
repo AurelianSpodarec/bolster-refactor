06230e70f80bf31bec8c430559e18b9bd419955e
Oliver Theabould <oli@silverchip.com>
2019-03-28 09:40:36 +0000

    update drawing form

---
+++ app-src/src/actions/drawings/async/createDrawing.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+import { API_URL } from 'config/index';
+import setAPIFieldErrors from 'actions/generic/fieldErrors/sync/setAPIFieldErrors';
+import { getHeaders } from 'helpers/api';
+import {
+    CREATE_DRAWING_REQUEST,
+    CREATE_DRAWING_SUCCESS,
+    CREATE_DRAWING_FAILURE
+} from 'constants/actionTypes/drawings';
+
+export const createDrawingRequest = () => ({
+    type: CREATE_DRAWING_REQUEST
+});
+
+export const createDrawingSuccess = payload => ({
+    type: CREATE_DRAWING_SUCCESS,
+    payload
+});
+
+export const createDrawingFailure = error => ({
+    type: CREATE_DRAWING_FAILURE,
+    error
+});
+
+export default newDrawing => dispatch => {
+    dispatch(createDrawingRequest());
+    axios
+        .post(`${API_URL}/sites`, newDrawing, getHeaders())
+        .then(result => dispatch(createDrawingSuccess(result.data)))
+        .catch(error => {
+            dispatch(createDrawingFailure(error));
+            if (error.response.status === 400)
+                dispatch(setAPIFieldErrors(error.response.data.errors));
+        });
+};
