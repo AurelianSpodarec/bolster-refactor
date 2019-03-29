7a482e8dd16c0e42093986c6f640df08703e377c
Bob <bob@silverchip.com>
2019-03-29 11:57:20 +0000

    Breadcrumb container logic started - leaving for now

--- app-src/src/components/sites/allSites/presentational/AllSites.js
+++ app-src/src/components/sites/allSites/presentational/AllSites.js
@@ -5,11 +5,11 @@ import SitesFiltersContainer from '../containers/SitesFiltersContainer';
 import SitesTableContainer from '../containers/SitesTableContainer';
 import Block from 'components/shared/generic/block/presentational/Block';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
-import BreadcrumbContainer from 'components/pins/singlePin/containers/BreadcrumbContainer';
+import Breadcrumb from 'components/shared/generic/breadcrumb/presentational/Breadcrumb';
 
 const AllSites = () => (
     <>
-        <BreadcrumbContainer />
+        <Breadcrumb breadcrumbs={[{ text: 'Sites' }]} />
 
         <PageHeading title="Sites">
             <Link to="/sites/create" className="button">
