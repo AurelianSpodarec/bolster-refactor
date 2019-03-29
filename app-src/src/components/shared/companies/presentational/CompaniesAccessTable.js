566ec94833c6202a77605514a099b51c8f08af8e
Mark Baker <mark@silverchip.com>
2019-03-29 16:08:59 +0000

    companies with access list outputting

--- app-src/src/components/shared/companies/presentational/CompaniesAccessTable.js
+++ app-src/src/components/shared/companies/presentational/CompaniesAccessTable.js
@@ -4,28 +4,46 @@ import { withRouter, Link } from 'react-router-dom';
 import Table from 'components/shared/generic/tables/presentational/Table';
 import CompaniesAccessList from './CompaniesAccessList';
 
-const CompaniesAccess = ({ location, companies, isFetching, error }) => (
-    <div className="size-lg-12">
-        <h3 className="heading heading-3 size-lg-12">Companies with access</h3>
-        <Table
-            headers={['Name', 'Actions']}
-            isFetching={isFetching}
-            error={error}
-            noData={!companies.length}
-            noDataMessage="There are no companies to display."
-            withActions
-        >
-            <CompaniesAccessList companies={companies} />
-        </Table>
-        <div className="button-container table">
-            <Link
-                className="button pull-right"
-                to={`${location.pathname}/invite-company`}
+const CompaniesAccess = ({
+    location,
+    companies,
+    parentId,
+    isFetching,
+    isEmpty,
+    error
+}) => {
+    if (isEmpty) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <div className="size-lg-12">
+            <h3 className="heading heading-3 size-lg-12">
+                Companies with access
+            </h3>
+            <Table
+                headers={['Name', 'Actions']}
+                isFetching={isFetching}
+                error={error}
+                noData={!companies.length}
+                noDataMessage="There are no companies to display."
+                withActions
             >
-                <i className="fa fa-plus" /> Invite company
-            </Link>
+                <CompaniesAccessList
+                    companies={companies}
+                    parentId={parentId}
+                />
+            </Table>
+            <div className="button-container table">
+                <Link
+                    className="button pull-right"
+                    to={`${location.pathname}/invite-company`}
+                >
+                    <i className="fa fa-plus" /> Invite company
+                </Link>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default withRouter(CompaniesAccess);
