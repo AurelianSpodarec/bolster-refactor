dca39d8cafbcb297c78f82da14071271326495be
Mark Baker <mark@silverchip.com>
2019-04-01 11:43:45 +0100

    updated location of inspection log

--- app-src/src/components/drawings/singleDrawing/presentational/GeneralOverview.js
+++ app-src/src/components/drawings/singleDrawing/presentational/GeneralOverview.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Block from 'components/shared/generic/block/presentational/Block';
-
 import DrawingMapGeneralContainer from '../containers/DrawingMapGeneralContainer';
-import DrawingInspectionLogContainer from '../containers/DrawingInspectionLogContainer';
 import DrawingDocumentsContainer from '../containers/DrawingDocumentsContainer';
 import DrawingClientAccessContainer from '../containers/DrawingClientAccessContainer';
 import DrawingCompanyAccessContainer from '../containers/DrawingCompanyAccessContainer';
 import DrawingOperativesAccessContainer from '../containers/DrawingOperativesAccessContainer';
 
 const GeneralOverview = () => (
-    <div className="size-lg-12">
-        <div className="size-lg-8">
+    <>
+        <div className="size-lg-12">
             <div className="size-lg-12">
                 <DrawingMapGeneralContainer />
             </div>
+        </div>
 
-            <div className="size-lg-6">
+        <div className="size-lg-12">
+            <div className="size-lg-3">
                 <DrawingClientAccessContainer />
             </div>
 
-            <div className="size-lg-6">
+            <div className="size-lg-3">
                 <DrawingCompanyAccessContainer />
+            </div>
+
+            <div className="size-lg-3">
+                <DrawingDocumentsContainer />
+            </div>
+
+            <div className="size-lg-3">
+                <DrawingOperativesAccessContainer />
             </div>
 
             <div className="content-container size-lg-12">
@@ -31,21 +38,7 @@ const GeneralOverview = () => (
                 </Link>
             </div>
         </div>
-
-        <div className="size-lg-4">
-            <div className="size-lg-12">
-                <DrawingInspectionLogContainer />
-            </div>
-
-            <div className="size-lg-12">
-                <DrawingDocumentsContainer />
-            </div>
-
-            <div className="size-lg-12">
-                <DrawingOperativesAccessContainer />
-            </div>
-        </div>
-    </div>
+    </>
 );
 
 export default GeneralOverview;
