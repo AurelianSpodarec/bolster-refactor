6110fdf2058c087782245308e02f403526d5db2a
Mark Baker <mark@silverchip.com>
2019-04-02 16:56:56 +0100

    cancel button working

--- app-src/src/components/shared/operatives/presentational/AttachOperativeForm.js
+++ app-src/src/components/shared/operatives/presentational/AttachOperativeForm.js
@@ -4,14 +4,17 @@ import { withRouter, Link } from 'react-router-dom';
 import Form from 'components/shared/generic/form/containers/Form';
 import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 
-const AttachOperativeForm = ({ handleSubmit }) => (
+const AttachOperativeForm = ({ location, handleSubmit }) => (
     <Form className="size-lg-12" onSubmit={handleSubmit}>
         <BlockButtonWrapper>
             <button className="button green">
                 <i className="fa fa-plus" />
                 Add Operative
             </button>
-            <Link to={'/'} className="button">
+            <Link
+                to={location.pathname.replace('/add-operative', '')}
+                className="button"
+            >
                 <i className="fa fa-times" /> Cancel
             </Link>
         </BlockButtonWrapper>
