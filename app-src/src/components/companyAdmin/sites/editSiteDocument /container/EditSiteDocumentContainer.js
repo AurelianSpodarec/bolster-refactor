33b3dd5f1d0fdf0bde1bbfdeb21a52d4ba1b6eaa
Oliver Theabould <oli@silverchip.com>
2019-04-02 16:31:56 +0100

    comment our edit site document

--- app-src/src/components/companyAdmin/sites/editSiteDocument /container/EditSiteDocumentContainer.js
+++ app-src/src/components/companyAdmin/sites/editSiteDocument /container/EditSiteDocumentContainer.js
@@ -8,38 +8,38 @@ class EditSiteDocumentContainer extends Component {
     render() {
         const { siteID, documentID } = this.props.match.params;
 
-        return (
-            <EditSiteDocument
-                handleSubmit={this.handleSubmit}
-                siteID={siteID}
-                documentID={documentID}
-            />
+        return (null
+            // <EditSiteDocument
+            //     handleSubmit={this.handleSubmit}
+            //     siteID={siteID}
+            //     documentID={documentID}
+            // />
         );
     }
 
-    componentDidUpdate({ postSuccess: prevSuccess }) {
-        const { postSuccess, history, match } = this.props;
-        const { siteID } = match.params;
-        if (postSuccess && !prevSuccess) {
-            // ? what redirect route?
-        }
-    }
+//     componentDidUpdate({ postSuccess: prevSuccess }) {
+//         const { postSuccess, history, match } = this.props;
+//         const { siteID } = match.params;
+//         if (postSuccess && !prevSuccess) {
+//             // ? what redirect route?
+//         }
+//     }
 
-    handleSubmit = postBody => {
-        const { siteID, documentID } = this.props.match.params;
-        editDocument(1, siteID, documentID, postBody);
-    };
-}
+//     handleSubmit = postBody => {
+//         const { siteID, documentID } = this.props.match.params;
+//         editDocument(1, siteID, documentID, postBody);
+//     };
+// }
 
-const mapStateToProps = ({ documentsReducer }) => ({
-    postSuccess: documentsReducer.postSuccess
-});
+// const mapStateToProps = ({ documentsReducer }) => ({
+//     postSuccess: documentsReducer.postSuccess
+// });
 
-const mapDispatchToProps = dispatch => ({
-    editDocument: (type, siteID, documentID, postBody) => {
-        dispatch(editDocument(type, siteID, documentID, postBody));
-    }
-});
+// const mapDispatchToProps = dispatch => ({
+//     editDocument: (type, siteID, documentID, postBody) => {
+//         dispatch(editDocument(type, siteID, documentID, postBody));
+//     }
+// });
 
 export default withRouter(
     connect(
