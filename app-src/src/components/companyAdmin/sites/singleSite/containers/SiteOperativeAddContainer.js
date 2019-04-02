c0705f49524443360393b6cfeb7c795cc74f374c
Mark Baker <mark@silverchip.com>
2019-04-02 15:15:53 +0100

    client and operative box updates

--- app-src/src/components/companyAdmin/sites/singleSite/containers/SiteOperativeAddContainer.js
+++ app-src/src/components/companyAdmin/sites/singleSite/containers/SiteOperativeAddContainer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
-import ClientInvite from 'components/shared/clients/presentational/ClientInvite';
+import OperativesAdd from 'components/shared/operatives/presentational/OperativesAdd';
 
 class SiteClientInviteContainer extends Component {
     render() {
         return (
             <BlockContainer>
-                <ClientInvite type="site" />
+                <OperativesAdd type="site" />
             </BlockContainer>
         );
     }
