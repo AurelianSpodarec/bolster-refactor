2f499ffc3b67a7a5678a116551dd0107f717b6fb
Rob Fairclough <rob@silverchip.com>
2019-04-02 10:51:19 +0100

    actions, reducers for fetch subscriptions

---
+++ app-src/src/reducers/subscriptions.js
@@ -0,0 +1,45 @@
+import { combineReducers } from 'redux';
+import {
+    FETCH_ALL_SUBSCRIPTIONS_REQUEST,
+    FETCH_ALL_SUBSCRIPTIONS_SUCCESS,
+    FETCH_ALL_SUBSCRIPTIONS_FAILURE
+} from 'constants/actionTypes/subscriptions';
+import { convertArrToObj } from 'helpers/generic';
+
+export default combineReducers({
+    isFetching: isFetchingReducer,
+    error: errorReducer,
+    subscriptions: subscriptionsReducer
+});
+
+function isFetchingReducer(state = false, action) {
+    switch (action.type) {
+        case FETCH_ALL_SUBSCRIPTIONS_REQUEST:
+            return true;
+        case FETCH_ALL_SUBSCRIPTIONS_SUCCESS:
+        case FETCH_ALL_SUBSCRIPTIONS_FAILURE:
+            return false;
+        default:
+            return state;
+    }
+}
+
+function errorReducer(state = null, action) {
+    switch (action.type) {
+        case FETCH_ALL_SUBSCRIPTIONS_REQUEST:
+            return null;
+        case FETCH_ALL_SUBSCRIPTIONS_FAILURE:
+            return action.error;
+        default:
+            return state;
+    }
+}
+
+function subscriptionsReducer(state = {}, action) {
+    switch (action.type) {
+        case FETCH_ALL_SUBSCRIPTIONS_SUCCESS:
+            return convertArrToObj(action.payload);
+        default:
+            return state;
+    }
+}
