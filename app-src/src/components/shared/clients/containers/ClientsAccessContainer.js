b65288581874b691a1c368e5b5f9c27be46d3064
Mark Baker <mark@silverchip.com>
2019-04-02 15:57:33 +0100

    clients access page

---
+++ app-src/src/components/shared/clients/containers/ClientsAccessContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import ClientsAccess from '../presentational/ClientsAccess';
+
+class ClientsAccessContainer extends Component {
+    render() {
+        return <ClientsAccess />;
+    }
+}
+
+export default ClientsAccessContainer;
