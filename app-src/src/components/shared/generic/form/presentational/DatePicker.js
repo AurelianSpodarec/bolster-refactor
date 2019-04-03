1eebbd48ff2ff72b98260a40ac24fb6ab4d0564e
Emma Randall <emma@silverchip.com>
2019-04-03 18:02:56 +0100

    remove label from datepicker

--- app-src/src/components/shared/generic/form/presentational/DatePicker.js
+++ app-src/src/components/shared/generic/form/presentational/DatePicker.js
@@ -3,10 +3,9 @@ import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
-const DatePickerPresentational = ({ selected, onChange, name, label }) => {
+const DatePickerPresentational = ({ selected, onChange, name }) => {
     return (
         <div className="size-lg-4">
-            <label htmlFor={name}>{label}</label>
             <DatePicker
                 id={name}
                 selected={selected}
