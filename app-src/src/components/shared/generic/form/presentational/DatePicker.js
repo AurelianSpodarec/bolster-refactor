db16a73d327d0fe2afa8f01184ddbdb38d702ee8
Emma Randall <emma@silverchip.com>
2019-04-03 17:53:17 +0100

    fix id in edite site container

--- app-src/src/components/shared/generic/form/presentational/DatePicker.js
+++ app-src/src/components/shared/generic/form/presentational/DatePicker.js
@@ -3,10 +3,10 @@ import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
-const DatePickerPresentational = ({ selected, onChange, name }) => {
+const DatePickerPresentational = ({ selected, onChange, name, label }) => {
     return (
         <div className="size-lg-4">
-            <label htmlFor={name}>{name}</label>
+            <label htmlFor={name}>{label}</label>
             <DatePicker
                 id={name}
                 selected={selected}
