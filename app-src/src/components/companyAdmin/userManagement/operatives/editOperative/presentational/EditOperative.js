7a64152a0420d54e3811cdb1a1f347e4297b068f
Emma Randall <emma@silverchip.com>
2019-04-04 15:56:59 +0100

    file structure refactor

--- app-src/src/components/companyAdmin/userManagement/operatives/editOperative/presentational/EditOperative.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/editOperative/presentational/EditOperative.js
@@ -3,7 +3,7 @@ import React from 'react';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import Breadcrumb from 'components/shared/generic/breadcrumb/presentational/Breadcrumb';
-import EditOperativeFormContainer from '../containers/CreateOperativeFormContainer';
+import EditOperativeFormContainer from '../../createOperative/containers/CreateOperativeFormContainer';
 
 const EditOperative = () => (
     <>
--- app-src/src/components/shared/app/routes/presentational/companyAdmin/UserManagementRoutes.js
+++ app-src/src/components/shared/app/routes/presentational/companyAdmin/UserManagementRoutes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import AllCompanyAdminsContainer from 'components/companyAdmin/userManagement/companyAdmins/containers/AllCompanyAdminsContainer';
+import AllCompanyAdminsContainer from 'components/companyAdmin/userManagement/companyAdmins/allCompanyAdmins/containers/AllCompanyAdminsContainer';
 
 import SwitchWith404 from '../SwitchWith404';
-import AllOperativesContainer from 'components/companyAdmin/userManagement/operatives/containers/AllOperativesContainer';
-import CreateOperative from 'components/companyAdmin/userManagement/operatives/presentational/CreateOperative';
-import CreateCompanyAdmin from 'components/companyAdmin/userManagement/companyAdmins/presentational/CreateCompanyAdmin';
-import EditOperative from 'components/companyAdmin/userManagement/operatives/containers/EditOperativeFormContainer';
+import AllOperativesContainer from 'components/companyAdmin/userManagement/operatives/allOperatives/containers/AllOperativesContainer';
+import CreateOperative from 'components/companyAdmin/userManagement/operatives/createOperative/presentational/CreateOperative';
+import CreateCompanyAdmin from 'components/companyAdmin/userManagement/companyAdmins/createCompanyAdmin/presentational/CreateCompanyAdmin';
+import EditOperative from 'components/companyAdmin/userManagement/operatives/editOperative/presentational/EditOperative';
 
 const UserManagementRoutes = ({ match: { url: baseUrl } }) => (
     <SwitchWith404>
