bd1b1339a766627b3cf331b59b117ee8a3832cb5
Oliver Theabould <oli@silverchip.com>
2019-04-04 17:32:20 +0100

    begin file upload test

---
+++ app-src/src/components/test/fileUpload/presentational/TestFileUploadForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Field from 'components/shared/generic/form/presentational/Field';
+import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
+import Form from 'components/shared/generic/form/containers/Form';
+import TestFileUploadContainer from '../containers/TestFileUploadContainer';
+
+const TestFileUploadForm = ({ handleSubmit, handleFileChange }) => (
+    <Form onSubmit={handleSubmit} className="generic-form size-lg-12">
+        <div className="size-lg-12">
+            <div className="size-lg-6">
+                <Field name="Upload plan">
+                    <TestFileUploadContainer
+                        required
+                        name="file"
+                        handleChange={handleFileChange}
+                    />
+                </Field>
+            </div>
+        </div>
+
+        <BlockButtonWrapper>
+            <button className="button green" type="submit">
+                <i className="fa fa-plus" /> Submit
+            </button>
+        </BlockButtonWrapper>
+    </Form>
+);
+export default TestFileUploadForm;
