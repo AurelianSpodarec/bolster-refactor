bcf6c50059d5bc71836d7c1ae88032f87cba0104
Mark Baker <mark@silverchip.com>
2019-04-04 15:18:55 +0100

    refactored building document deletion

--- app-src/src/components/companyAdmin/sites/singleSite/containers/SiteDocumentsTableContainer.js
+++ app-src/src/components/companyAdmin/sites/singleSite/containers/SiteDocumentsTableContainer.js
@@ -5,12 +5,11 @@ import { withRouter } from 'react-router-dom';
 import DocumentsTable from 'components/shared/documents/presentational/DocumentsTable';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 
-import { DELETE_DOCUMENT, DELETION_ERROR } from 'constants/shared/modalTypes';
 import { showModal } from 'actions/shared/generic/modals/sync/showModal';
 
 class SiteDocumentsTableContainer extends Component {
     render() {
-        const { error, isFetching, parent } = this.props;
+        const { error, isFetching } = this.props;
 
         return (
             <BlockContainer error={error}>
@@ -37,19 +36,9 @@ const mapStateToProps = (
     parent: sitesReducer.sites[match.params.id] || { documentIDs: [] },
     documents: Object.values(documentsReducer.documents),
     isFetching: documentsReducer.isFetching || sitesReducer.isFetching,
-    error: documentsReducer.error,
-    deletionError: documentsReducer.deletionError
-});
-
-const mapDispatchToProps = dispatch => ({
-    showModal: (modalType, modalProps) => {
-        dispatch(showModal(modalType, modalProps));
-    }
+    error: documentsReducer.error
 });
 
 export default withRouter(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(SiteDocumentsTableContainer)
+    connect(mapStateToProps)(SiteDocumentsTableContainer)
 );
