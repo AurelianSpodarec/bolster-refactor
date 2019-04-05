456830511f5efbaea1c2944ae0f61bf170ad695a
Oliver Theabould <oli@silverchip.com>
2019-04-05 10:34:41 +0100

    add admin/company apps

--- app-src/src/reducers/superAdmin/templateQuestions.js
+++ app-src/src/reducers/superAdmin/templateQuestions.js
@@ -8,63 +8,11 @@ import {
     SWAP_QUESTION_SORTS
 } from 'constants/actionTypes/templateBuilder';
 
-// dummy data
-const defaultQuestions = {
-    'aa22ec20-52e3-11e9-8633-45ed325a6f1e': {
-        isHidden: true,
-        isPrefill: true,
-        isRequired: true,
-        name: 'Question 1',
-        prereqUuid: '',
-        prereqVal: '',
-        questionType: 'DROPDOWN',
-        sectionUuid: '9d707ec0-52e3-11e9-8633-45ed325a6f1e',
-        sort: 1,
-        uuid: 'aa22ec20-52e3-11e9-8633-45ed325a6f1e'
-    },
-    '273ab210-52e4-11e9-ac26-8590a0785f92': {
-        isHidden: true,
-        isPrefill: false,
-        isRequired: true,
-        name: 'Question 2',
-        prereqUuid: 'aa22ec20-52e3-11e9-8633-45ed325a6f1e',
-        prereqVal: 'prereq val',
-        questionType: 'SINGLE_LINE',
-        sectionUuid: '9d707ec0-52e3-11e9-8633-45ed325a6f1e',
-        sort: 2,
-        uuid: '273ab210-52e4-11e9-ac26-8590a0785f92'
-    },
-    '273ab210-52e4-11e9-ac26-8590a0785f23': {
-        isHidden: true,
-        isPrefill: false,
-        isRequired: false,
-        name: 'Question 3',
-        prereqUuid: '',
-        prereqVal: '',
-        questionType: 'SINGLE_LINE',
-        sectionUuid: '9d707ec0-52e3-11e9-8633-45ed325a6f1e',
-        sort: 3,
-        uuid: '273ab210-52e4-11e9-ac26-8590a0785f23'
-    },
-    '273ab210-52e4-11e9-ac26-8590a0785f34': {
-        isHidden: true,
-        isPrefill: false,
-        isRequired: false,
-        name: 'Question 4',
-        prereqUuid: '',
-        prereqVal: '',
-        questionType: 'SINGLE_LINE',
-        sectionUuid: '9d707ec0-52e3-11e9-8633-45ed325a6f2f',
-        sort: 1,
-        uuid: '273ab210-52e4-11e9-ac26-8590a0785f34'
-    }
-};
-
 export default combineReducers({
     questions: questionsReducer
 });
 
-function questionsReducer(state = defaultQuestions, action) {
+function questionsReducer(state = {}, action) {
     switch (action.type) {
         case SET_QUESTION:
             return updateObj(state, action.question.uuid, action.question);
--- app-src/src/reducers/superAdmin/templates.js
+++ app-src/src/reducers/superAdmin/templates.js
@@ -12,23 +12,12 @@ import {
     RESET_SAVE_REQUIRED
 } from 'constants/actionTypes/templateBuilder';
 
-const defaultTemplates = {
-    '9d707ec0-52e3-11e9-8633-45ed325a6f22': {
-        name: 'Template 1',
-        uuid: '9d707ec0-52e3-11e9-8633-45ed325a6f22'
-    },
-    '33707ec0-52e3-11e9-8633-45ed325a6f2f': {
-        name: 'Template 2',
-        uuid: '33707ec0-52e3-11e9-8633-45ed325a6f2f'
-    }
-};
-
 export default combineReducers({
     templates: templatesReducer,
     saveRequired: saveRequiredReducer
 });
 
-function templatesReducer(state = defaultTemplates, action) {
+function templatesReducer(state = {}, action) {
     switch (action.type) {
         case SET_TEMPLATE:
             return updateObj(state, action.template.uuid, action.template);
