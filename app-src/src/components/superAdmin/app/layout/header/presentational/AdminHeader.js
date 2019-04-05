07b0223eabce0d4ab0c6611b4fa5c76d3390d5a5
Oliver Theabould <oli@silverchip.com>
2019-04-05 16:39:02 +0100

    add jamies face

--- app-src/src/components/superAdmin/app/layout/header/presentational/AdminHeader.js
+++ app-src/src/components/superAdmin/app/layout/header/presentational/AdminHeader.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import BlackLogo from '_content/images/footer/logo-black.png';
+const bolsterLogo =
+    'https://pbs.twimg.com/profile_images/720922614332592128/PMbyPQNk_400x400.jpg';
 
 const AdminHeader = () => (
     <header id="page-header" style={{ borderColor: 'red' }}>
         <div className="container">
             <div className="logo">
                 <Link to="/admin">
-                    <img alt="bolster logo" src={BlackLogo} />
+                    <img alt="bolster logo" src={bolsterLogo} />
                 </Link>
             </div>
 
