0a09a61426e9078575bd44d863750e003f7fe2ae
Oliver Theabould <oli@silverchip.com>
2019-04-06 12:09:15 +0100

    fix pin route

--- app-src/src/components/companyAdmin/app/routes/presentational/PinRoutes.js
+++ app-src/src/components/companyAdmin/app/routes/presentational/PinRoutes.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 import SwitchWith404 from 'components/appRoute/routes/presentational/SwitchWith404';
 import SinglePinContainer from 'components/companyAdmin/pins/singlePin/containers/SinglePinContainer';
 
-const PinRoutes = ({ base = 'company/pins' }) => (
+const PinRoutes = ({ base = '/company/pins' }) => (
     <SwitchWith404>
         <Route exact path={`${base}/:id`} component={SinglePinContainer} />
     </SwitchWith404>
