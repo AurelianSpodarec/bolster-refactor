16e257d40af6064879eab3554e8f49b928604dcd
Mark Baker <mark@silverchip.com>
2019-04-08 15:30:35 +0100

    single pin page tidied up a bit

--- app-src/src/components/companyAdmin/pins/singlePin/containers/PinHistoriesListContainer.js
+++ app-src/src/components/companyAdmin/pins/singlePin/containers/PinHistoriesListContainer.js
@@ -1,44 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 import PinHistoriesList from '../presentational/PinHistoriesList';
-import BlockHeadingWControls from 'components/shared/generic/blockHeadingWControls/presentational/BlockHeadingWControls';
+import PinHistoriesActions from '../presentational/PinHistoriesActions';
 
-const PinHistoriesListContainer = ({ histories, selectedHistoryId }) => {
-    return (
-        <>
-            <BlockHeadingWControls title="Other pin histories">
-                <button className="button red">
-                    <i className="fa fa-trash" />
-                    Delete all
-                </button>
-                <button className="button">
-                    <i className="fa fa-plus" />
-                    Add new history
-                </button>
-            </BlockHeadingWControls>
-            <PinHistoriesList
-                histories={histories.filter(
-                    hist => hist.id !== selectedHistoryId
-                )}
-                historyCount={histories.length}
-            />
-        </>
-    );
+class PinHistoriesListContainer extends Component {
+    render() {
+        return (
+            <>
+                <PinHistoriesActions location={this.props.location} />
+                <PinHistoriesList
+                    histories={this.props.histories.filter(
+                        hist => hist.id !== this.props.selectedHistoryId
+                    )}
+                    historyCount={this.props.histories.length}
+                />
+            </>
+        );
+    }
+}
+
+const mapStateToProps = ({ companyAdmin: { pinsReducer } }, { match }) => {
+    const pin = pinsReducer.pins[match.params.id];
+
+    return {
+        isFetching: pinsReducer.isFetching,
+        error: pinsReducer.error,
+        histories: Object.values(pin.histories),
+        selectedHistoryId: pin.pin.latestHistoryID
+    };
 };
 
-export default withRouter(
-    connect(
-        ({ companyAdmin: { pinsReducer, pinHistoriesReducer } }, { match }) => {
-            return {
-                isFetching: pinHistoriesReducer.isFetching,
-                error: pinHistoriesReducer.error,
-                histories: Object.values(
-                    pinsReducer.pins[match.params.id].histories
-                ),
-                selectedHistoryId: pinHistoriesReducer.selectedHistoryId
-            };
-        }
-    )(PinHistoriesListContainer)
-);
+export default withRouter(connect(mapStateToProps)(PinHistoriesListContainer));
