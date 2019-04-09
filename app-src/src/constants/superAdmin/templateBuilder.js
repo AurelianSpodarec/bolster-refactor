cd07d96950dd0d167cc7f60858c61105dd173451
Oliver Theabould <oli@silverchip.com>
2019-04-09 13:32:37 +0100

    create uestion specific forms for template builder

--- app-src/src/constants/superAdmin/templateBuilder.js
+++ app-src/src/constants/superAdmin/templateBuilder.js
@@ -4,7 +4,22 @@ export const QUESTION_TYPES = {
     '3': 'Number',
     '4': 'Dropdown',
     '5': 'Checkbox',
-    '6': 'Photo'
+    '6': 'Photo',
+    '7': 'Radio',
+    '8': 'Signiture',
+    '9': 'Multi select'
+};
+
+export const QUESTION_TYPE_VALUES = {
+    SINGLE_LINE: '1',
+    MULTI_LINE: '2',
+    NUMBER: '3',
+    DROPDOWN: '4',
+    CHECKBOX: '5',
+    PHOTO: '6',
+    RADIO: '7',
+    SIGNITURE: '8',
+    MULTI_SELECT: '9'
 };
 
 export const PREREQ_TYPES = ['4', '5'];
