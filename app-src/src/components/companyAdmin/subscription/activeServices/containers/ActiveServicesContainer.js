e4902163f86399b8341a324598fece22bd9523c3
Rob Fairclough <rob@silverchip.com>
2019-04-09 10:25:46 +0100

    fix routes to subscription

--- app-src/src/components/companyAdmin/subscription/activeServices/containers/ActiveServicesContainer.js
+++ app-src/src/components/companyAdmin/subscription/activeServices/containers/ActiveServicesContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { isObjEmpty } from 'helpers/generic';
-import ActiveServices from 'components/companyAdmin/sites/subscription/activeServices/presentational/ActiveServices';
+import ActiveServices from 'components/companyAdmin/subscription/activeServices/presentational/ActiveServices';
 
 class ActiveServicesContainer extends Component {
     state = {
--- app-src/src/components/companyAdmin/subscription/pendingInvoices/containers/PendingInvoicesContainer.js
+++ app-src/src/components/companyAdmin/subscription/pendingInvoices/containers/PendingInvoicesContainer.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
-import PendingInvoices from '../presentational/PendingInvoices';
+import { connect } from 'react-redux';
 
-export default class PendingInvoicesContainer extends Component {
-    render() {
-        return <PendingInvoices />;
-    }
+import PendingInvoices from '../presentational/PendingInvoices';
+import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
+import fetchAllInvoices from 'actions/companyAdmin/invoices/async/fetchAllInvoices';
+import fetchAllInvoiceItems from 'actions/companyAdmin/invoices/async/fetchAllInvoiceItems';
+
+class PendingInvoicesContainer extends Component {
+    render = () => (
+        <BlockContainer>
+            <PendingInvoices />
+        </BlockContainer>
+    );
+
+    componentDidMount = () => {
+        const { fetchInvoiceData } = this.props;
+        fetchInvoiceData();
+    };
 }
+
+const mapDispatchToProps = dispatch => ({
+    fetchInvoiceData: () => {
+        dispatch(fetchAllInvoices());
+        dispatch(fetchAllInvoiceItems());
+    }
+});
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(PendingInvoicesContainer);
