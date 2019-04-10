04fd8c88ad130e6eca3655beb3afa255b677bbf9
Emma Randall <emma@silverchip.com>
2019-04-10 11:36:20 +0100

    start edit profile

--- app-src/src/components/companyAdmin/app/routes/presentational/ProfilesRoutes.js
+++ app-src/src/components/companyAdmin/app/routes/presentational/ProfilesRoutes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import SwitchWith404 from 'components/appRoute/routes/presentational/SwitchWith404';
-import ProfileContainer from 'components/companyAdmin/profile/containers/ProfileContainer';
+import ProfileContainer from 'components/companyAdmin/profile/profilePage/containers/ProfileContainer';
+import EditProfileContainer from 'components/companyAdmin/profile/editProfile/containers/EditProfileContainer';
 
 const ProfilesRoutes = ({ base = '/company/profile' }) => (
     <SwitchWith404>
         <Route exact path={base} component={ProfileContainer} />
+        <Route exact path={`${base}/edit`} component={EditProfileContainer} />
     </SwitchWith404>
 );
 
---
+++ app-src/src/components/companyAdmin/profile/editProfile/containers/EditProfileContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class EditProfileContainer extends Component {
+    render() {
+        return <div>hello</div>;
+    }
+}
+
+export default EditProfileContainer;
