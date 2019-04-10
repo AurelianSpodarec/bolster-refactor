1519b8f500660548ab781db1f305d483a9fb855d
Bob <bob@silverchip.com>
2019-04-10 17:30:09 +0100

    Breadcrumb for building single done

---
+++ app-src/src/components/companyAdmin/buildings/singleBuilding/containers/BuildingBreadcrumbContainer.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import fetchSingleSite from 'actions/companyAdmin/sites/async/fetchSingleSite';
+
+import Breadcrumb from 'components/shared/generic/breadcrumb/presentational/Breadcrumb';
+
+class BuildingBreadcrumbContainer extends Component {
+    // const breadcrumbs = [
+    //     { text: 'Site one', link: 'sites/1' },
+    //     { text: 'Building one', link: 'buildings/1' },
+    //     { text: 'Floor one', link: 'floors/1' },
+    //     { text: 'Drawing one', link: 'drawings/1' },
+    //     { text: '00067:34' }
+    // ];
+    //needs to be array to map out each link
+    //how do i know if is a neext
+    state = {
+        siteName: '',
+        siteID: 0
+    };
+
+    render() {
+        const breadcrumbsArray = [
+            {
+                text: 'Sites',
+                link: '/company/sites/'
+            },
+            {
+                text: this.state.siteName,
+                link: `/company/sites/${this.state.siteID}`
+            },
+            { text: this.props.building.name }
+        ];
+        return <Breadcrumb breadcrumbs={breadcrumbsArray} />;
+    }
+
+    _setSiteDetails = siteID => {
+        const { sites } = this.props;
+
+        const site = sites[siteID];
+
+        this.setState({
+            siteName: site.name,
+            siteID: siteID
+        });
+    };
+
+    componentDidMount = () => {
+        const { sites, building } = this.props;
+
+        if (Object.values(sites).length) {
+            this._setSiteDetails(building.siteID);
+        }
+    };
+
+    componentDidUpdate = prevProps => {
+        const { sites, building, fetchSingleSite } = this.props;
+
+        if (!prevProps.building.id && !!building.id) {
+            fetchSingleSite(building.siteID);
+        }
+
+        if (
+            !Object.values(prevProps.sites).length &&
+            Object.values(sites).length
+        ) {
+            this._setSiteDetails(building.siteID);
+        }
+    };
+}
+
+const mapStateToProps = (
+    {
+        companyAdmin: {
+            buildingsReducer: { buildings },
+            sitesReducer: { sites }
+        }
+    },
+    { match }
+) => ({
+    building: buildings[match.params.id] || {},
+    sites: sites
+});
+const mapDispatchToProps = dispatch => ({
+    fetchSingleSite: siteID => {
+        dispatch(fetchSingleSite(siteID));
+    }
+});
+export default withRouter(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(BuildingBreadcrumbContainer)
+);
--- app-src/src/components/companyAdmin/floors/singleFloor/presentational/SingleFloorHeader.js
+++ app-src/src/components/companyAdmin/floors/singleFloor/presentational/SingleFloorHeader.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 
