49b48da9bdfbef3105685328288012d15b9d5a27
Mark Baker <mark@silverchip.com>
2019-04-10 12:28:45 +0100

    console errors removed

--- app-src/src/components/companyAdmin/pins/singlePin/presentational/PinQuestion.js
+++ app-src/src/components/companyAdmin/pins/singlePin/presentational/PinQuestion.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PinQuestion = ({ questions }) =>
     questions.map(question => (
-        <div className="pin-details-section size-lg-12">
+        <div className="pin-details-section size-lg-12" key={question.id}>
             <h4 className="title">{question.name}</h4>
             <p>Question...</p>
         </div>
--- app-src/src/components/companyAdmin/pins/singlePin/presentational/PinSection.js
+++ app-src/src/components/companyAdmin/pins/singlePin/presentational/PinSection.js
@@ -3,7 +3,7 @@ import PinQuestionsContainer from '../containers/PinQuestionsContainer';
 
 const PinSection = ({ sections }) =>
     sections.map(section => (
-        <div className="pin-details-section size-lg-12">
+        <div className="pin-details-section size-lg-12" key={section.id}>
             <h4 className="title">{section.name}</h4>
             <PinQuestionsContainer sectionID={section.id} />
         </div>
