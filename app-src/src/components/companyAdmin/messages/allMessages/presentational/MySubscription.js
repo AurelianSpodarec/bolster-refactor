25fa9be64a8ee7d18a7f35ac858ae1f2225e6b8b
Rob Fairclough <rob@silverchip.com>
2019-04-11 14:38:02 +0100

    update button in message centre

--- app-src/src/components/companyAdmin/messages/allMessages/presentational/MySubscription.js
+++ app-src/src/components/companyAdmin/messages/allMessages/presentational/MySubscription.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
-const MySubscription = ({ services, daysleft, location }) => (
+const MySubscription = ({ services, daysleft }) => (
     <div className="size-lg-12">
         <h1 className="heading heading-3 size-lg-12">My Subscription</h1>
 
         <p className="generic-text size-lg-12">
-            {`Expire in ${daysleft} days`}
+            {daysleft > 1
+                ? `Expires in ${daysleft} days.`
+                : daysleft
+                ? `Expires in ${daysleft} day.`
+                : 'Expired'}
         </p>
 
         {services.map(service => (
@@ -14,11 +18,8 @@ const MySubscription = ({ services, daysleft, location }) => (
         ))}
 
         <div className="button-container">
-            <Link
-                className="button pull-right"
-                to={`${location.pathname}/invite-client`}
-            >
-                <i className="fa fa-plus" /> Invite
+            <Link className="button pull-right" to="/company/subscription">
+                Manage My Subscription
             </Link>
         </div>
     </div>
