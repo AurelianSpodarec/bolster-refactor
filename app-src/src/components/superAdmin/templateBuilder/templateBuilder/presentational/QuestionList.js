cc5af26211c397dc7525a6942a86b6cdec01edda
Bob <bob@silverchip.com>
2019-04-11 14:51:36 +0100

    Question styling in progress

--- app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/QuestionList.js
+++ app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/QuestionList.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 import QuestionContainer from '../containers/QuestionContainer';
 
-const QuestionList = ({ sectionUUID, questions, moveQuestion }) =>
-    questions.map((question, i) => (
-        <QuestionContainer
-            key={question.uuid}
-            index={i}
-            sectionUUID={sectionUUID}
-            question={question}
-            moveQuestion={moveQuestion}
-        />
-    ));
+const QuestionList = ({ sectionUUID, questions, moveQuestion }) => (
+    <div className="question-list size-lg-12">
+        <div className="question-header size-lg-12">
+            <p className="size-lg-3">Name</p>
+            <p className="size-lg-3">Type</p>
+            <p className="size-lg-2">Required</p>
+            <p className="size-lg-2">Show</p>
+        </div>
+        {questions.map((question, i) => (
+            <QuestionContainer
+                key={question.uuid}
+                index={i}
+                sectionUUID={sectionUUID}
+                question={question}
+                moveQuestion={moveQuestion}
+            />
+        ))}
+    </div>
+);
 
 export default QuestionList;
