986551fdf182d18b76131d451ed8d76e2d529c59
Bob <bob@silverchip.com>
2019-04-13 11:16:19 +0100

    New subscribed service item component  on subscription page

--- app-src/src/components/companyAdmin/subscription/activeServices/presentational/CompanyServicesList.js
+++ app-src/src/components/companyAdmin/subscription/activeServices/presentational/CompanyServicesList.js
@@ -1,34 +1,14 @@
 import React from 'react';
-
-import Checkbox from 'components/shared/generic/form/presentational/Checkbox';
-import StatusIcon from 'components/shared/generic/statusIcon/presentationl/StatusIcon';
+import SubscribedServicesItem from './SubscribedServicesItem';
 
 const CompanyServicesList = ({ subscriptions, handleChange, children }) => (
     <div className="subscriptions-list size-lg-12">
         {subscriptions.map(subscription => (
-            <div
-                key={`subscription-${subscription.id}`}
-                className="subscription-item size-lg-6"
-            >
-                <div className="field-name size-lg-6">
-                    <StatusIcon />
-                    <label
-                        className=""
-                        htmlFor={`subscription-id-${subscription.id}`}
-                    >
-                        {subscription.name}
-                    </label>
-                </div>
-                <Checkbox
-                    classes="small-text"
-                    checked={subscription.isAutoRenew}
-                    name={subscription.name}
-                    value={subscription.serviceID}
-                    id={`subscription-id-${subscription.id}`}
-                    handleChange={handleChange}
-                    text="Renew?"
-                />
-            </div>
+            <SubscribedServicesItem
+                key={subscription.id}
+                subscription={subscription}
+                handleChange={handleChange}
+            />
         ))}
         {children}
     </div>
---
+++ app-src/src/components/companyAdmin/subscription/activeServices/presentational/SubscribedServicesItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Checkbox from 'components/shared/generic/form/presentational/Checkbox';
+import StatusIcon from 'components/shared/generic/statusIcon/presentationl/StatusIcon';
+
+const SubscribedServicesItem = ({ key, subscription, handleChange }) => (
+    <div key={`subscription-${key}`} className="subscription-item size-lg-6">
+        <div className="field-name size-lg-6">
+            <StatusIcon />
+            <label className="" htmlFor={`subscription-id-${subscription.id}`}>
+                {subscription.name}
+            </label>
+        </div>
+        <Checkbox
+            classes="small-text"
+            checked={subscription.isAutoRenew}
+            name={subscription.name}
+            value={subscription.serviceID}
+            id={`subscription-id-${subscription.id}`}
+            handleChange={handleChange}
+            text="Renew?"
+        />
+    </div>
+);
+
+export default SubscribedServicesItem;
