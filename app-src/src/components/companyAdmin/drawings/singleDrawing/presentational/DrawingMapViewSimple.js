4dfa213eb423021ca7567e9b2286e2c06e9c1aaa
Liam <liam@liambateman.net>
2019-04-15 16:29:47 +0100

    Validation on map upload.

--- app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/DrawingMapViewSimple.js
+++ app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/DrawingMapViewSimple.js
@@ -17,43 +17,50 @@ const DrawingMapViewSimple = ({
     history
 }) => (
     <>
-        {addMode ? (
+        {drawing.tilesetS3Key ? (
             <>
-                <Link
-                    to={`${drawing.id}/add-pin`}
-                    className="button pull-right"
-                >
-                    <i className="fa fa-times" /> Confirm position
-                </Link>
+            {addMode ? (
+                <>
+                    <Link
+                        to={`${drawing.id}/add-pin`}
+                        className="button pull-right"
+                    >
+                        <i className="fa fa-times" /> Confirm position
+                    </Link>
+                    <button className="button pull-right" onClick={toggleAddMode}>
+                        <i className="fa fa-plus" /> Stop
+                    </button>
+                </>
+            ) : (
                 <button className="button pull-right" onClick={toggleAddMode}>
-                    <i className="fa fa-plus" /> Stop
+                    <i className="fa fa-plus" /> Add pin
                 </button>
-            </>
-        ) : (
-            <button className="button pull-right" onClick={toggleAddMode}>
-                <i className="fa fa-plus" /> Add pin
-            </button>
-        )}
+            )}
 
-        <Map
-            center={position}
-            zoom={zoom}
-            minZoom={0}
-            maxZoom={5}
-            onClick={e => handleClick(e)}
-        >
-            <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url={`${FILE_STORAGE_URL}/${
-                    drawing.tilesetS3Key
-                }/{z}/{x}/{y}.jpg`}
-                noWrap={true}
-            />
-            {pins.map(pin => (
-                <MapPin key={pin.id} pin={pin} history={history} />
-            ))}
-            {addMode && <Marker position={addPinPosition} />}
-        </Map>
+
+            <Map
+                center={position}
+                zoom={zoom}
+                minZoom={0}
+                maxZoom={5}
+                onClick={e => handleClick(e)}
+            >
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url={`${FILE_STORAGE_URL}/${
+                        drawing.tilesetS3Key
+                        }/{z}/{x}/{y}.jpg`}
+                    noWrap={true}
+                />
+                {pins.map(pin => (
+                    <MapPin key={pin.id} pin={pin} history={history}/>
+                ))}
+                {addMode && <Marker position={addPinPosition}/>}
+            </Map>
+            </>
+
+        ) : (<div>Please wait for your tileset to load</div>)
+        }
     </>
 );
 
