e98e514ff4b4f240721b694552ab06f7dc585f2c
Rob Fairclough <rob@silverchip.com>
2019-04-15 14:48:46 +0100

    show archived text at top of hierarchy pages

--- app-src/src/components/companyAdmin/buildings/singleBuilding/presentational/BuildingPageHeader.js
+++ app-src/src/components/companyAdmin/buildings/singleBuilding/presentational/BuildingPageHeader.js
@@ -3,7 +3,13 @@ import React from 'react';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 
 const BuildingPageHeader = ({ building, children }) => (
-    <PageHeading title={`Building: ${building.name}`}>{children}</PageHeading>
+    <PageHeading
+        title={`Building: ${building.name} ${
+            building.isArchived ? '(ARCHIVED)' : ''
+        }`}
+    >
+        {children}
+    </PageHeading>
 );
 
 export default BuildingPageHeader;
--- app-src/src/components/companyAdmin/floors/singleFloor/presentational/SingleFloorHeader.js
+++ app-src/src/components/companyAdmin/floors/singleFloor/presentational/SingleFloorHeader.js
@@ -3,7 +3,13 @@ import React from 'react';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 
 const SingleFloorHeader = ({ floor, children }) => (
-    <PageHeading title={`Floor: ${floor.name || ''}`}>{children}</PageHeading>
+    <PageHeading
+        title={`Floor: ${floor.name || ''} ${
+            floor.isArchived ? '(ARCHIVED)' : ''
+        }`}
+    >
+        {children}
+    </PageHeading>
 );
 
 export default SingleFloorHeader;
