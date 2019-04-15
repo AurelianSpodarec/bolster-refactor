e9bc77c80ab10230b842f2fa911c3614ec6b7bf3
Tom <bob@silverchip.com>
2019-04-15 19:02:26 +0100

    Unused variables removed

--- app-src/src/components/companyAdmin/pins/addPin/presentational/AddPin.js
+++ app-src/src/components/companyAdmin/pins/addPin/presentational/AddPin.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Breadcrumb from 'components/shared/generic/breadcrumb/presentational/Breadcrumb';
 import AddPinFormContainer from '../containers/AddPinFormContainer';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import BackButtonContainer from 'components/shared/generic/backButton/containers/BackButtonContainer';
--- app-src/src/components/shared/pins/presentational/CustomPin.js
+++ app-src/src/components/shared/pins/presentational/CustomPin.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 const CustomPin = ({ pinColour, pinCode, pinID, history }) => {
     const imageLocation = require(`_content/images/map-markers/${pinColour}-pin2x.png`);
