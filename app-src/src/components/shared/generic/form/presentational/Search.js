3e0838e4321afaaa395ef3ad7ee4a17544e98b6b
Rob Fairclough <rob@silverchip.com>
2019-04-16 12:12:37 +0100

    remove autofill from search bar

--- app-src/src/components/shared/generic/form/presentational/Search.js
+++ app-src/src/components/shared/generic/form/presentational/Search.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Search = ({ name, placeholder, value, handleChange }) => (
+const Search = ({
+    name,
+    placeholder,
+    value,
+    handleChange,
+    autoComplete = 'off'
+}) => (
     <div className="generic-search">
         <i className="search-icon far fa-search" />
         <input
+            autoComplete={autoComplete}
             type="text"
             name={name}
             placeholder={placeholder}
