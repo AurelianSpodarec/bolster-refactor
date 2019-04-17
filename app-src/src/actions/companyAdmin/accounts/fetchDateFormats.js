bdf8bf2ea860d58d74e147d16030f578dd875ced
Rob Fairclough <rob@silverchip.com>
2019-04-17 14:58:05 +0100

    actions and reducers for fetch dateformats and timezones

---
+++ app-src/src/actions/companyAdmin/accounts/fetchDateFormats.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+import {
+    FETCH_DATE_FORMATS_REQUEST,
+    FETCH_DATE_FORMATS_SUCCESS,
+    FETCH_DATE_FORMATS_FAILURE
+} from 'constants/actionTypes/accounts';
+import { getHeaders } from 'helpers/api';
+import { ACCOUNTS_API_URL } from 'config';
+
+export const fetchDateFormatsRequest = () => ({
+    type: FETCH_DATE_FORMATS_REQUEST
+});
+
+export const fetchDateFormatsSuccess = payload => ({
+    type: FETCH_DATE_FORMATS_SUCCESS,
+    payload
+});
+
+export const fetchDateFormatsFailure = error => ({
+    type: FETCH_DATE_FORMATS_FAILURE,
+    error
+});
+
+export default () => dispatch => {
+    dispatch(fetchDateFormatsRequest());
+
+    axios
+        .get(`${ACCOUNTS_API_URL}/localisation/dateformats`, getHeaders())
+        .then(res => dispatch(fetchDateFormatsSuccess(res.data)))
+        .catch(err => dispatch(fetchDateFormatsFailure(err.message)));
+};
