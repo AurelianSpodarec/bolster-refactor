0d07ae82dcdd41dd1c6918e9b8cd4724a976408a
Rob Fairclough <rob@silverchip.com>
2019-04-17 11:27:37 +0100

    add twitter feed to transfer requests page

--- app-src/src/components/companyAdmin/transferRequests/presentational/TransferRequests.js
+++ app-src/src/components/companyAdmin/transferRequests/presentational/TransferRequests.js
@@ -4,18 +4,30 @@ import TransferRequestsTableContainer from '../containers/TransferRequestsTableC
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import PendingInvitesTableContainer from '../containers/PendingInvitesTableContainer';
+import MySubscriptionContainer from 'components/companyAdmin/messages/allMessages/containers/MySubscriptionContainer';
+import TwitterFeed from 'components/shared/twitterFeed/presentational/TwitterFeed';
 
 const TransferRequests = () => (
     <>
         <PageHeading title="Pending Requests" />
-        <BlockContainer>
-            <BlockHeading title="Pending Invites" />
-            <PendingInvitesTableContainer />
-        </BlockContainer>
-        <BlockContainer>
-            <BlockHeading title="Owner Requests" />
-            <TransferRequestsTableContainer />
-        </BlockContainer>
+        <div className="size-lg-8">
+            <BlockContainer>
+                <BlockHeading title="Pending Invites" />
+                <PendingInvitesTableContainer />
+            </BlockContainer>
+        </div>
+        <div className="size-lg-4">
+            <MySubscriptionContainer />
+        </div>
+        <div className="size-lg-8">
+            <BlockContainer>
+                <BlockHeading title="Owner Requests" />
+                <TransferRequestsTableContainer />
+            </BlockContainer>
+        </div>
+        <div className="size-lg-4">
+            <TwitterFeed />
+        </div>
     </>
 );
 
