69850989d425133abb342d09f38a90e63aea17b8
Rob Fairclough <rob@silverchip.com>
2019-04-18 09:52:09 +0100

    use correct import for auth-api-url for dateformats and timezones

--- app-src/src/actions/companyAdmin/accounts/fetchTimezones.js
+++ app-src/src/actions/companyAdmin/accounts/fetchTimezones.js
@@ -6,7 +6,7 @@ import {
     FETCH_TIMEZONES_FAILURE
 } from 'constants/actionTypes/accounts';
 import { getHeaders } from 'helpers/api';
-import { ACCOUNTS_API_URL } from 'config';
+import { AUTH_API_URL } from 'config';
 
 export const fetchTimezonesRequest = () => ({
     type: FETCH_TIMEZONES_REQUEST
@@ -26,7 +26,7 @@ export default () => dispatch => {
     dispatch(fetchTimezonesRequest());
 
     axios
-        .get(`${ACCOUNTS_API_URL}/localisation/timezones`, getHeaders())
+        .get(`${AUTH_API_URL}/localisation/timezones`, getHeaders())
         .then(res => dispatch(fetchTimezonesSuccess(res.data)))
         .catch(err => dispatch(fetchTimezonesFailure(err.message)));
 };
