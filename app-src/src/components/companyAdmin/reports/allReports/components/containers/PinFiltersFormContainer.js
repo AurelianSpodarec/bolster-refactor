a80dd861805de75bb00ae0abf1d1bff8f541c705
Tom <bob@silverchip.com>
2019-04-19 17:02:10 +0100

    Sites filter on pin reports

--- app-src/src/components/companyAdmin/reports/allReports/components/containers/PinFiltersFormContainer.js
+++ app-src/src/components/companyAdmin/reports/allReports/components/containers/PinFiltersFormContainer.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
+import updateSitesFilters from 'actions/companyAdmin/sites/sync/updateSitesFilters';
+
 import PinFiltersForm from '../presentational/PinFiltersForm';
 
 export class PinFiltersFormContainer extends Component {
     render() {
         return <PinFiltersForm />;
     }
-
-    _getFilteredSites = () => {
-        const { sites, filters } = this.props;
-        const { status } = filters;
-        const name = filters.name.toLowerCase();
-
-        return sites
-            .filter(site => site.name.toLowerCase().includes(name))
-            .filter(
-                ({ accessType }) =>
-                    !status.length || status + '' === accessType + ''
-            );
-    };
 }
 
 const mapStateToProps = ({
