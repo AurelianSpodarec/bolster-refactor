6d5aeddcbd2b1961f284d8ebf897bad11de7aee0
Tom <bob@silverchip.com>
2019-04-23 14:19:28 +0100

    Page heading on headquarters companies

--- app-src/src/components/companyAdmin/headquarters/presentational/HeadquartersCompanies.js
+++ app-src/src/components/companyAdmin/headquarters/presentational/HeadquartersCompanies.js
@@ -3,22 +3,26 @@ import { Link } from 'react-router-dom';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import HeadquartersCompaniesTableContainer from '../containers/HeadquartersCompaniesTableContainer';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
+import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 
 const HeadquartersCompanies = ({ handleCancelImpersonation }) => (
-    <BlockContainer heading="Headquarters Companies">
-        <BlockHeading title="Companies Table" classes="w-table">
-            <button className="button" onClick={handleCancelImpersonation}>
-                Cancel impersonation
-            </button>
-            <Link
-                to="/company/headquarters/companies/create"
-                className="button green"
-            >
-                <i className="fa fa-plus" /> Add Company
-            </Link>
-        </BlockHeading>
-        <HeadquartersCompaniesTableContainer />
-    </BlockContainer>
+    <>
+        <PageHeading title="Headquarters Companies" />
+        <BlockContainer>
+            <BlockHeading title="Companies Table" classes="w-table">
+                <Link
+                    to="/company/headquarters/companies/create"
+                    className="button green"
+                >
+                    <i className="fa fa-plus" /> Add Company
+                </Link>
+                <button className="button" onClick={handleCancelImpersonation}>
+                    Cancel impersonation
+                </button>
+            </BlockHeading>
+            <HeadquartersCompaniesTableContainer />
+        </BlockContainer>{' '}
+    </>
 );
 
 export default HeadquartersCompanies;
