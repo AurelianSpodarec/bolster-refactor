a60e4700fbc85370440f4e674ce32e4627da50ab
Mark Baker <mark@silverchip.com>
2019-04-23 22:37:10 +0100

    created containers for dashboard / pin feed stuff

---
+++ app-src/src/components/companyAdmin/dashboard/containers/DashboardPinFeedContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import DashboardPinFeed from '../presentational/DashboardPinFeed';
+
+class DashboardPinFeedContainer extends Component {
+    render() {
+        return <DashboardPinFeed />;
+    }
+}
+
+export default DashboardPinFeedContainer;
