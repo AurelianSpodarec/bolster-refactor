da2ad961f3c053f418873972606b5b7be44a84f3
Rob Fairclough <rob@silverchip.com>
2019-04-25 14:16:42 +0100

    add back button to template builder page

--- app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/TemplateBuilderHeader.js
+++ app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/TemplateBuilderHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
+import BackButtonContainer from 'components/shared/generic/backButton/containers/BackButtonContainer';
 
 const TemplateBuilderHeader = ({
     name = '',
@@ -8,17 +9,20 @@ const TemplateBuilderHeader = ({
     children,
     serviceName
 }) => (
-    <PageHeading
-        title={`Template builder : ${name} ${
-            serviceName ? `(${serviceName})` : ''
-        }`}
-    >
-        <button className="button yellow" onClick={showTemplateForm}>
-            <i className="far fa-pencil" /> {name.length ? 'Edit' : 'Add'}{' '}
-            template
-        </button>
-        {children}
-    </PageHeading>
+    <>
+        <PageHeading
+            title={`Template builder : ${name} ${
+                serviceName ? `(${serviceName})` : ''
+            }`}
+        >
+            <button className="button yellow" onClick={showTemplateForm}>
+                <i className="far fa-pencil" /> {name.length ? 'Edit' : 'Add'}{' '}
+                template
+            </button>
+            {children}
+            <BackButtonContainer />
+        </PageHeading>
+    </>
 );
 
 export default TemplateBuilderHeader;
