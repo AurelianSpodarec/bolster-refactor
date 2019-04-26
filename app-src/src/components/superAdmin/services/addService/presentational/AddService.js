138ad15bb0ec59e656985877df71b61a50934820
Emma Randall <emma@silverchip.com>
2019-04-26 15:29:16 +0100

    remove console warnings

--- app-src/src/components/superAdmin/services/addService/presentational/AddService.js
+++ app-src/src/components/superAdmin/services/addService/presentational/AddService.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Breadcrumb from 'components/shared/generic/breadcrumb/presentational/Breadcrumb';
 import AddServiceFormContainer from '../containers/AddServiceFormContainer';
 import Block from 'components/shared/generic/block/presentational/Block';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
--- app-src/src/components/superAdmin/services/shared/presentational/AllServices.js
+++ app-src/src/components/superAdmin/services/shared/presentational/AllServices.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Breadcrumb from 'components/shared/generic/breadcrumb/presentational/Breadcrumb';
+
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import ServiceTableContainer from '../containers/ServiceTableContainer';
