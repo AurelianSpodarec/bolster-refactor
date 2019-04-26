246a1c17b778de177fdcb10e80bb196af9794b51
Rob Fairclough <rob@silverchip.com>
2019-04-26 11:43:33 +0100

    create duplicate template from existing

--- app-src/src/components/superAdmin/templateBuilder/templateBuilder/containers/TemplateBuilderHeaderContainer.js
+++ app-src/src/components/superAdmin/templateBuilder/templateBuilder/containers/TemplateBuilderHeaderContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import newUUID from 'uuid/v1';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import { ADD_TEMPLATE, EDIT_TEMPLATE } from 'constants/shared/modalTypes';
 import { showModal } from 'actions/shared/generic/modals/sync/showModal';
 import TemplateBuilderHeader from '../presentational/TemplateBuilderHeader';
+import postTemplate from 'actions/superAdmin/templateBuilder/async/postTemplate';
 
 const TemplateBuilderHeaderContainer = ({
     showAddTemplateForm,
@@ -14,7 +16,10 @@ const TemplateBuilderHeaderContainer = ({
     template,
     isExisting,
     showAddSectionModal,
-    serviceName
+    serviceName,
+    templateSections,
+    templateSectionQuestions,
+    postTemplate
 }) => {
     return (
         <TemplateBuilderHeader
@@ -23,9 +28,21 @@ const TemplateBuilderHeaderContainer = ({
             serviceName={serviceName}
         >
             {isExisting && (
-                <button onClick={showAddSectionModal} className="button blue">
-                    <i className="fa fa-plus" /> Add Section
-                </button>
+                <>
+                    <button
+                        onClick={addTemplateFromExisting}
+                        className="button green"
+                    >
+                        <i className="fa fa-plus" /> Create new from this
+                        template
+                    </button>
+                    <button
+                        onClick={showAddSectionModal}
+                        className="button blue"
+                    >
+                        <i className="fa fa-plus" /> Add Section
+                    </button>
+                </>
             )}
         </TemplateBuilderHeader>
     );
@@ -35,12 +52,50 @@ const TemplateBuilderHeaderContainer = ({
             ? showAddTemplateForm(uuid, companyID)
             : showEditTemplateForm(template, companyID);
     }
+
+    function addTemplateFromExisting() {
+        const newTemplateUUID = newUUID();
+        const newTemplate = {
+            ...template,
+            name: `${template.name}(copy)`,
+            uuid: newTemplateUUID
+        };
+        const newSections = [];
+        const newQuestions = [];
+
+        templateSections.forEach(template => {
+            const newSectionUUID = newUUID();
+            newSections.push({
+                ...template,
+                uuid: newSectionUUID,
+                newTemplateUUID
+            });
+            templateSectionQuestions.forEach(({ sectionUUID, ...question }) => {
+                if (sectionUUID === template.uuid) {
+                    const newQuestionUUID = newUUID();
+                    newQuestions.push({
+                        ...question,
+                        sectionUUID: newSectionUUID,
+                        uuid: newQuestionUUID
+                    });
+                }
+            });
+        });
+        const postBody = {
+            template: newTemplate,
+            sections: newSections,
+            questions: newQuestions
+        };
+        postTemplate(postBody);
+    }
 };
 
 const mapStateToProps = (
     {
         superAdmin: {
             templatesReducer: { templates },
+            templateSectionsReducer: { sections },
+            templateQuestionsReducer: { questions },
             servicesReducer: { services }
         }
     },
@@ -51,9 +106,18 @@ const mapStateToProps = (
     }
 ) => {
     const template = templates[uuid] || { serviceID: '' };
+    const templateSections = Object.values(sections).filter(
+        ({ templateUUID }) => templateUUID === uuid
+    );
+    const sectionIDs = templateSections.map(({ uuid }) => uuid);
+    const templateSectionQuestions = Object.values(questions).filter(
+        ({ sectionUUID }) => sectionIDs.includes(sectionUUID)
+    );
     const service = services[template.serviceID] || {};
     return {
         template,
+        templateSections,
+        templateSectionQuestions,
         uuid,
         companyID,
         serviceName: services && template ? service.name : ''
@@ -66,7 +130,8 @@ const mapDispatchToProps = dispatch => ({
     },
     showEditTemplateForm: (template, companyID) => {
         dispatch(showModal(EDIT_TEMPLATE, { template, companyID }));
-    }
+    },
+    postTemplate: postBody => dispatch(postTemplate(postBody))
 });
 
 const HeaderWithConnect = connect(
