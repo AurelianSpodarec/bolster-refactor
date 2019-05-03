cc1af0477a1fe0c1955df5f127fb685313f47ff0
Tom Duthie <tom@silverchip.com>
2019-05-03 10:56:13 +0100

    Moved button in template button headers

--- app-src/src/components/shared/generic/backButton/presentational/BackButton.js
+++ app-src/src/components/shared/generic/backButton/presentational/BackButton.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ButtonContainer from '../../button/containers/ButtonContainer';
 
-const BackButton = ({ handleClick }) => (
-    <ButtonContainer className="button back" handleClick={handleClick}>
+const BackButton = ({ handleClick, classes = '' }) => (
+    <ButtonContainer
+        className={`button back ${classes}`}
+        handleClick={handleClick}
+    >
         <i className="fa fa-chevron-double-left" /> Back
     </ButtonContainer>
 );
--- app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/TemplateBuilderHeader.js
+++ app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/TemplateBuilderHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
+import BackButtonContainer from 'components/shared/generic/backButton/containers/BackButtonContainer';
 
 const TemplateBuilderHeader = ({
     name = '',
@@ -15,9 +16,9 @@ const TemplateBuilderHeader = ({
         title={`Template builder : ${name} ${
             serviceName ? `(${serviceName})` : ''
         }`}
-        withBackButton
     >
         <div className="button-holder">
+            <BackButtonContainer />
             <button className="button yellow" onClick={showTemplateForm}>
                 <i className="far fa-pencil" /> {name.length ? 'Edit' : 'Add'}{' '}
                 template
@@ -25,7 +26,7 @@ const TemplateBuilderHeader = ({
             {isExisting && (
                 <>
                     <button
-                        onClick={addTemplateFromExisting()}
+                        onClick={addTemplateFromExisting}
                         className="button green"
                     >
                         <i className="fa fa-plus" /> Copy Template
