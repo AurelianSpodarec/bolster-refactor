fa9901243dfd67a0148e2d23d5b12a40422c2065
Emma Randall <emma@silverchip.com>
2019-05-03 15:35:23 +0100

    actions and reducers for edit dropdown options

---
+++ app-src/src/actions/companyAdmin/dropdownOptions/async/editDropdownOption.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+import { API_URL } from 'config/index';
+import setAPIFieldErrors from 'actions/shared/generic/fieldErrors/sync/setAPIFieldErrors';
+import { getHeaders } from 'helpers/api';
+import {
+    EDIT_DROPDOWN_OPTION_REQUEST,
+    EDIT_DROPDOWN_OPTION_SUCCESS,
+    EDIT_DROPDOWN_OPTION_FAILURE
+} from 'constants/actionTypes/dropdownOptions';
+
+export const editDropdownOptionRequest = () => ({
+    type: EDIT_DROPDOWN_OPTION_REQUEST
+});
+
+export const editDropdownOptionSuccess = payload => ({
+    type: EDIT_DROPDOWN_OPTION_SUCCESS,
+    payload
+});
+
+export const editDropdownOptionFailure = error => ({
+    type: EDIT_DROPDOWN_OPTION_FAILURE,
+    error
+});
+
+export default (id, type, postBody) => dispatch => {
+    dispatch(editDropdownOptionRequest());
+
+    axios
+        .post(
+            `${API_URL}/dropdownoptions/${type}/${id}`,
+            postBody,
+            getHeaders()
+        )
+        .then(result => dispatch(editDropdownOptionSuccess(result.data)))
+        .catch(error => {
+            dispatch(editDropdownOptionFailure(error));
+            if (error.response.status === 400)
+                dispatch(setAPIFieldErrors(error.response.data.errors));
+        });
+};
--- app-src/src/reducers/companyAdmin/dropdownOptions.js
+++ app-src/src/reducers/companyAdmin/dropdownOptions.js
@@ -7,7 +7,10 @@ import {
     FETCH_ALL_DROPDOWN_OPTIONS_FAILURE,
     CREATE_DROPDOWN_OPTION_REQUEST,
     CREATE_DROPDOWN_OPTION_SUCCESS,
-    CREATE_DROPDOWN_OPTION_FAILURE
+    CREATE_DROPDOWN_OPTION_FAILURE,
+    EDIT_DROPDOWN_OPTION_REQUEST,
+    EDIT_DROPDOWN_OPTION_SUCCESS,
+    EDIT_DROPDOWN_OPTION_FAILURE
 } from 'constants/actionTypes/dropdownOptions';
 
 export default combineReducers({
@@ -32,8 +35,10 @@ function isFetchingReducer(state = false, action) {
 function postSuccessReducer(state = false, action) {
     switch (action.type) {
         case CREATE_DROPDOWN_OPTION_REQUEST:
+        case EDIT_DROPDOWN_OPTION_REQUEST:
             return false;
         case CREATE_DROPDOWN_OPTION_SUCCESS:
+        case EDIT_DROPDOWN_OPTION_SUCCESS:
             return true;
         default:
             return state;
@@ -65,9 +70,11 @@ function errorReducer(state = null, action) {
     switch (action.type) {
         case FETCH_ALL_DROPDOWN_OPTIONS_REQUEST:
         case CREATE_DROPDOWN_OPTION_REQUEST:
+        case EDIT_DROPDOWN_OPTION_REQUEST:
             return null;
         case FETCH_ALL_DROPDOWN_OPTIONS_FAILURE:
         case CREATE_DROPDOWN_OPTION_FAILURE:
+        case EDIT_DROPDOWN_OPTION_FAILURE:
             return action.error;
         default:
             return state;
@@ -79,6 +86,7 @@ function dropdownOptionsReducer(state = {}, action) {
         case FETCH_ALL_DROPDOWN_OPTIONS_SUCCESS:
             return convertArrToObj(action.payload);
         case CREATE_DROPDOWN_OPTION_SUCCESS:
+        case EDIT_DROPDOWN_OPTION_SUCCESS:
             return updateObj(state, action.payload.id, action.payload);
         default:
             return state;
