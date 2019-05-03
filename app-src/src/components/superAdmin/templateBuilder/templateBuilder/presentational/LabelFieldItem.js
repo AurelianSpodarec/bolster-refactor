469518d8b9dbeb046a49dbbc0d540252b0336d9d
Tom Duthie <tom@silverchip.com>
2019-05-03 12:06:48 +0100

    Final touches label fields modal and table

--- app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/LabelFieldItem.js
+++ app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/LabelFieldItem.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LabelFieldItem = ({ source, title, content }) => (
     <div className="label-field-item  size-lg-12">
-        <p className="size-lg-3">{source || '{ empty }'}</p>
-        <p className="size-lg-3">{title || '{ empty }'}</p>
+        <p className="size-lg-4">{source || '{ empty }'}</p>
+        <p className="size-lg-4">{title || '{ empty }'}</p>
         <p className="size-lg-4">{content || '{ empty }'}</p>
     </div>
 );
--- app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/LabelFieldList.js
+++ app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/LabelFieldList.js
@@ -4,8 +4,8 @@ import LabelFieldItemContainer from '../containers/LabelFieldItemContainer';
 const LabelFieldList = ({ fields }) => (
     <div className="label-field-list">
         <div className="label-field-header size-lg-12">
-            <p className="size-lg-3">Source</p>
-            <p className="size-lg-3">Title</p>
+            <p className="size-lg-4">Source</p>
+            <p className="size-lg-4">Title</p>
             <p className="size-lg-4">Content</p>
         </div>
 
