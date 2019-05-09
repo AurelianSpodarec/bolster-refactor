7274355960350a3edbef3cf2438bf657c07063d4
Emma Randall <emma@silverchip.com>
2019-05-09 13:43:08 +0100

    put pins on separate lines

--- app-src/src/components/shared/pins/map/presentational/CustomPin.js
+++ app-src/src/components/shared/pins/map/presentational/CustomPin.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const CustomPin = ({ pinColour = 'red', pinCode = '' }) => {
     const imageLocation = require(`_content/images/map-markers/${pinColour}-pin2x.png`);
+    const topCode = pinCode.slice(0, 4);
+    const bottomCode = pinCode.slice(5);
 
     return (
         <div className="custom-pin">
             <img alt={`${pinColour} pin`} src={imageLocation} />
-            <span className="code">{pinCode}</span>
+            <div className="code">
+                <p className="code-half">{topCode}</p>
+                <p className="code-half">{bottomCode}</p>
+            </div>
         </div>
     );
 };
