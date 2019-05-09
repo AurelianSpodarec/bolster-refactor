8a1295e8c79159bd680fb65b33ea68bd1cac6904
Rob Fairclough <rob@silverchip.com>
2019-05-09 14:16:24 +0100

    add loading image to enlarge image mmodal

--- app-src/src/components/shared/generic/modals/presentational/PinPhotoModal.js
+++ app-src/src/components/shared/generic/modals/presentational/PinPhotoModal.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ModalOuterContainer from '../containers/ModalOuterContainer';
+import loading from '../../../../../_content/images/layout/loading.gif';
 
-const PinPhotoModal = ({ image }) => (
-    <ModalOuterContainer>
-        <img alt="pin" src={image} />
-    </ModalOuterContainer>
-);
+class PinPhotoModal extends Component {
+    state = { isLoading: true };
+    render = () => (
+        <ModalOuterContainer>
+            {this.state.isLoading && (
+                <img
+                    alt="loading"
+                    src={loading}
+                    style={{ width: '10em', height: '10em' }}
+                />
+            )}
+            <img
+                alt="pin"
+                src={this.props.image}
+                // set the loading variable to false on load or error
+                onLoad={() => this.setState({ isLoading: false })}
+                onError={() => this.setState({ isLoading: false })}
+                style={{
+                    padding: '1em'
+                }}
+            />
+        </ModalOuterContainer>
+    );
+}
 
 export default PinPhotoModal;
