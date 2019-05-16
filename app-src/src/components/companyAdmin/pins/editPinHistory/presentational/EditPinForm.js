db5ce165390dbcc0079abce95669efb74da09e24
Oliver <oli@silverchip.com>
2019-05-16 10:43:01 +0100

    update edit history to use status

--- app-src/src/components/companyAdmin/pins/editPinHistory/presentational/EditPinForm.js
+++ app-src/src/components/companyAdmin/pins/editPinHistory/presentational/EditPinForm.js
@@ -3,33 +3,17 @@ import { Prompt } from 'react-router-dom';
 
 import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 import Form from 'components/shared/generic/form/containers/Form';
-import Field from 'components/shared/generic/form/presentational/Field';
-import DropdownContainer from 'components/shared/generic/form/containers/DropdownContainer';
 import EditPinVersionsContainer from '../containers/EditPinVersionsContainer';
 import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
 
 const EditPinForm = ({
     location,
-    handleChange,
     handleSubmit,
-    statuses,
-    selectedStatus,
     filesUploading,
     confirmLeave,
     selectedHistory
 }) => (
     <Form onSubmit={handleSubmit} className="generic-form size-lg-12">
-        <Field name="Select a status" sizeClasses="size-lg-6" required>
-            <DropdownContainer
-                placeholder="-- select --"
-                name="status"
-                options={statuses}
-                selectedOption={selectedStatus}
-                handleChange={handleChange}
-                required
-            />
-        </Field>
-
         <EditPinVersionsContainer
             templateVersionID={selectedHistory.templateVersionID}
         />
