d0fef7d44efdf062687739f1588672b59e030205
Emma Randall <emma@silverchip.com>
2019-05-20 15:01:39 +0100

    basic structure of client area site page

--- app-src/src/components/clientArea/sites/singleSite/presentational/SingleSite.js
+++ app-src/src/components/clientArea/sites/singleSite/presentational/SingleSite.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 import SiteDetailsContainer from '../containers/SiteDetailsContainer';
-import SiteDocumentsTableContainer from '../containers/SiteDocumentsTableContainer';
 import SiteBuildingsTableContainer from '../containers/SiteBuildingsTableContainer';
-import SiteOperativeAddContainer from '../containers/SiteOperativeAddContainer';
-import SiteClientInviteContainer from '../containers/SiteClientInviteContainer';
-import SiteCompaniesAccessTableContainer from '../containers/SiteCompaniesAccessTableContainer';
 import SitePageHeaderContainer from '../containers/SitePageHeaderContainer';
 
 const SingleSite = () => (
@@ -13,28 +9,11 @@ const SingleSite = () => (
         <div className="size-lg-12">
             <SitePageHeaderContainer />
         </div>
-        <div className="flex-container size-lg-12">
-            <div className="flex-item size-lg-8">
-                <SiteDetailsContainer />
-            </div>
-            <div className="flex-item size-lg-4">
-                <SiteDocumentsTableContainer />
-            </div>
+        <div className="size-lg-12">
+            <SiteDetailsContainer />
         </div>
-
         <div className="size-lg-12">
             <SiteBuildingsTableContainer />
-        </div>
-        <div className="flex-container size-lg-12">
-            <div className="flex-item size-lg-4">
-                <SiteClientInviteContainer />
-            </div>
-            <div className="flex-item size-lg-4">
-                <SiteOperativeAddContainer />
-            </div>
-            <div className="flex-item size-lg-4">
-                <SiteCompaniesAccessTableContainer />
-            </div>
         </div>
     </>
 );
--- app-src/src/components/clientArea/sites/singleSite/presentational/SiteStats.js
+++ app-src/src/components/clientArea/sites/singleSite/presentational/SiteStats.js
@@ -2,39 +2,12 @@ import React from 'react';
 
 import PieChart from 'components/shared/stats/presentational/PieChart';
 import SiteDetails from './SiteDetails';
-import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
-const SiteStats = ({
-    site,
-    stats,
-    handleDelete,
-    handleArchive,
-    handleEditSiteModal
-}) => (
+
+const SiteStats = ({ site, stats }) => (
     <div className="stats size-lg-12">
         <div className="flex-item size-lg-12">
             <SiteDetails site={site} stats={stats} />
             <PieChart stats={stats} hierarchyType="site" />
-        </div>
-
-        <div className="button-container">
-            <button className="button red" onClick={handleDelete} type="button">
-                <i className="far fa-trash-alt fa-fw" /> Delete
-            </button>
-            <button className="button yellow" onClick={handleEditSiteModal}>
-                <i className="far fa-pencil fa-fw" /> Edit
-            </button>
-
-            <button
-                className="button blue"
-                onClick={handleArchive}
-                type="button"
-            >
-                <i className="fa fa-archive" />
-                {site.isArchived ? 'Un-Archive' : 'Archive'}
-            </button>
-            <ButtonContainer to={`/company/sites/${site.id}/change-ownership`}>
-                <i className="fa fa-exchange" /> Change Ownership
-            </ButtonContainer>
         </div>
     </div>
 );
