87569565fc45836c567036057344d04315417fed
Emma Randall <emma@silverchip.com>
2019-05-20 15:16:48 +0100

    single floor container

--- app-src/src/components/clientArea/buildings/singleBuilding/containers/BuildingFloorsTableContainer.js
+++ app-src/src/components/clientArea/buildings/singleBuilding/containers/BuildingFloorsTableContainer.js
@@ -1,94 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-import { showModal } from 'actions/shared/generic/modals/sync/showModal';
-import { hideModal } from 'actions/shared/generic/modals/sync/hideModal';
-
-import {
-    ADD_FLOOR,
-    SUCCESS_MODAL,
-    ERROR_MODAL
-} from 'constants/shared/modalTypes';
-
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
-import FloorTableContainer from 'components/companyAdmin/floors/shared/containers/FloorTableContainer';
+import FloorTableContainer from 'components/clientArea/floors/shared/containers/FloorTableContainer';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 
-class BuildingsFloorsTableContainer extends Component {
-    render() {
-        const { building } = this.props;
-        return (
-            <BlockContainer>
-                <BlockHeading title="Floors" classes="w-table">
-                    <button
-                        className="button green"
-                        onClick={this.handleAddFloorModal}
-                    >
-                        <i className="fa fa-plus" /> Add floor
-                    </button>
-                </BlockHeading>
-                <FloorTableContainer ids={building.floorIDs || []} />
-            </BlockContainer>
-        );
-    }
-
-    componentDidUpdate = prevProps => {
-        const { postSuccess, error, showModal, hideModal } = this.props;
-
-        if (!prevProps.postSuccess && postSuccess) {
-            showModal(SUCCESS_MODAL, {
-                hideModal,
-                message: 'Floor added successfully.'
-            });
-        }
-
-        if (error && !prevProps.error) {
-            showModal(ERROR_MODAL, {
-                hideModal,
-                title: 'Error',
-                message:
-                    error.message ||
-                    '##There was an error processing your request, please try again later.##'
-            });
-        }
-    };
-
-    handleAddFloorModal = () => {
-        const { showModal, buildingID } = this.props;
-        showModal(ADD_FLOOR, { buildingID });
-    };
-}
+const BuildingsFloorsTableContainer = ({ building }) => (
+    <BlockContainer>
+        <BlockHeading title="Floors" classes="w-table" />
+        <FloorTableContainer ids={building.floorIDs || []} />
+    </BlockContainer>
+);
 
 const mapStateToProps = (
     {
         companyAdmin: {
             buildingsReducer,
-            floorsReducer: { postSuccess, updatedFloorID, error }
+            floorsReducer: { error }
         }
     },
     { match }
 ) => ({
     error,
-    postSuccess,
-    updatedFloorID,
     building: buildingsReducer.buildings[match.params.id] || {},
     isFetching: buildingsReducer.isFetching,
     buildingID: match.params.id
 });
 
-const mapDispatchToProps = dispatch => ({
-    showModal: (type, props) => {
-        dispatch(showModal(type, props));
-    },
-    hideModal: () => {
-        dispatch(hideModal());
-    }
-});
-
 export default withRouter(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(BuildingsFloorsTableContainer)
+    connect(mapStateToProps)(BuildingsFloorsTableContainer)
 );
--- app-src/src/components/clientArea/buildings/singleBuilding/presentational/SingleBuilding.js
+++ app-src/src/components/clientArea/buildings/singleBuilding/presentational/SingleBuilding.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 import BuildingDetailsContainer from '../containers/BuildingDetailsContainer';
-import BuildingDocumentsTableContainer from '../containers/BuildingDocumentsTableContainer';
 import BuildingFloorsTableContainer from '../containers/BuildingFloorsTableContainer';
-import BuildingOperativeAddContainer from '../containers/BuildingOperativeAddContainer';
-import BuildingInviteClientContainer from '../containers/BuildingInviteClientContainer';
-import BuildingCompaniesAccessTableContainer from '../containers/BuildingCompaniesAccessTableContainer';
 import BuildingPageHeaderContainer from '../containers/BuildingPageHeaderContainer';
 
 const SingleBuilding = () => (
@@ -13,31 +9,13 @@ const SingleBuilding = () => (
         <div className="size-lg-12">
             <BuildingPageHeaderContainer />
         </div>
-        <div className="flex-container size-lg-12">
-            <div className="flex-item size-lg-8">
-                <BuildingDetailsContainer />
-            </div>
 
-            <div className="flex-item size-lg-4">
-                <BuildingDocumentsTableContainer />
-            </div>
+        <div className=" size-lg-12">
+            <BuildingDetailsContainer />
         </div>
+
         <div className="size-lg-12">
             <BuildingFloorsTableContainer />
-        </div>
-
-        <div className="flex-container size-lg-12">
-            <div className="flex-item size-lg-4">
-                <BuildingInviteClientContainer />
-            </div>
-
-            <div className="flex-item size-lg-4">
-                <BuildingOperativeAddContainer />
-            </div>
-
-            <div className="flex-item size-lg-4">
-                <BuildingCompaniesAccessTableContainer />
-            </div>
         </div>
     </>
 );
