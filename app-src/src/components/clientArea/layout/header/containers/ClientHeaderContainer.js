502d0cc4c2c741644682e5f148da5bd910cf808e
Emma Randall <emma@silverchip.com>
2019-05-21 14:53:32 +0100

    remove unwanted buttons from client area header

--- app-src/src/components/clientArea/layout/header/containers/ClientHeaderContainer.js
+++ app-src/src/components/clientArea/layout/header/containers/ClientHeaderContainer.js
@@ -3,40 +3,22 @@ import { connect } from 'react-redux';
 
 import ClientHeader from '../presentational/ClientHeader';
 
-import { showModal } from 'actions/shared/generic/modals/sync/showModal';
-import { BUY_CREDITS } from 'constants/shared/modalTypes';
-import { MESSAGE_TYPES } from 'constants/companyAdmin/enums';
-
 const ClientHeaderContainer = ({
     profile,
     companySettings,
-    unreadMessageCount,
-    totalCredits,
-    totalRequests,
-    isImpersonating,
-    showModal
+
+    isImpersonating
 }) => (
     <ClientHeader
         profile={profile}
         company={companySettings}
-        unreadMessageCount={unreadMessageCount}
-        totalCredits={totalCredits}
-        totalRequests={totalRequests}
         isImpersonating={isImpersonating}
-        showModal={e => {
-            e.preventDefault();
-            showModal(BUY_CREDITS);
-        }}
     />
 );
 
 const mapStateToProps = ({
     companyAdmin: {
-        companySettingsReducer: { companySettings },
-        messagesReducer: { messages },
-        creditsReducer: { credits, isFetching, costOfCredits },
-        transferRequestsReducer: { incomingTransferRequests },
-        pendingInvitesReducer: { pendingInvites }
+        companySettingsReducer: { companySettings }
     },
     shared: {
         profileReducer: { profile },
@@ -47,34 +29,11 @@ const mapStateToProps = ({
 }) => {
     const isImpersonating = headquartersCompanyID !== companyID;
 
-    const unreadMessageCount = Object.values(messages).filter(
-        ({ type, isRead }) => type === MESSAGE_TYPES.SYSTEM && !isRead
-    ).length;
-    const totalCredits = Object.values(credits).reduce(
-        (a, b) => a + b.quantity,
-        0
-    );
-    const totalRequests =
-        Object.values(incomingTransferRequests).length +
-        Object.values(pendingInvites).length;
-
     return {
         profile: profile,
         companySettings,
-        unreadMessageCount,
-        totalCredits,
-        totalRequests,
-        isImpersonating,
-        isFetching,
-        costOfCredits
+        isImpersonating
     };
 };
 
-const mapDispatchToProps = dispatch => ({
-    showModal: (type, props) => dispatch(showModal(type, props))
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(ClientHeaderContainer);
+export default connect(mapStateToProps)(ClientHeaderContainer);
--- app-src/src/components/clientArea/layout/header/presentational/ClientHeader.js
+++ app-src/src/components/clientArea/layout/header/presentational/ClientHeader.js
@@ -6,16 +6,10 @@ import defaultStyles from 'constants/defaultStyles';
 
 import SearchContainer from '../containers/SearchContainer';
 import HeaderProfileContainer from '../containers/HeaderProfileContainer';
-import HeaderNotificationsContainer from '../containers/HeaderNotificationsContainer';
-import { PARENTAL_TYPES } from 'constants/companyAdmin/enums';
+// import HeaderNotificationsContainer from '../containers/HeaderNotificationsContainer';
+// import { PARENTAL_TYPES } from 'constants/companyAdmin/enums';
 
-const ClientHeader = ({
-    company,
-    unreadMessageCount,
-    totalCredits,
-    totalRequests,
-    showModal
-}) => (
+const ClientHeader = ({ company }) => (
     <header
         id="page-header"
         style={{ borderColor: company.colourCode || defaultStyles.colourCode }}
@@ -46,30 +40,8 @@ const ClientHeader = ({
             <div className="account-area">
                 {/*** notifications ***/}
                 <div className="notifications">
-                    <button className="item main" onClick={showModal}>
-                        {company.parentalType === PARENTAL_TYPES.NONE && (
-                            <span className="number green">{totalCredits}</span>
-                        )}
-                        <i className="far fa-money-bill-alt fa-fw" />
-                    </button>
-                    <HeaderNotificationsContainer />
-                    {/* <Link to="/company/message-centre" className="item main">
-                        {!!unreadMessageCount && (
-                            <span className="number">{unreadMessageCount}</span>
-                        )}
-                        <i className="far fa-envelope fa-fw" />
-                    </Link>
-                    <Link
-                        to="/company/tools/transfer-requests"
-                        className="item main"
-                    >
-                        {!!totalRequests && (
-                            <span className="number">{totalRequests}</span>
-                        )}
-                        <i className="far fa-exchange-alt fa-fw" />
-                    </Link> */}
                     {/* Modal to select a different company */}
-                    <button className="item main">
+                    <button className="item main large">
                         ##Current Company: Silverchip## ##Change Company##
                     </button>
                 </div>
