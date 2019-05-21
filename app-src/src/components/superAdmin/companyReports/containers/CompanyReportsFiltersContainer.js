6babbb6a4cdeb8a7d275a1bcc239cd5aadcd81b2
Rob Fairclough <rob@silverchip.com>
2019-05-21 17:29:21 +0100

    fix bug in admin company reports filters

---
+++ app-src/src/components/superAdmin/companyReports/containers/CompanyReportsFiltersContainer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import CompanyReportsFilters from '../presentational/CompanyReportsFilters';
+import updateCompanyReportsSort from 'actions/superAdmin/companyReports/sync/updateCompanyReportsSort';
+import { connect } from 'react-redux';
+
+const CompanyReportsFiltersContainer = ({
+    updateCompanyReportsSort,
+    sortString
+}) => {
+    const sortOptions = {
+        'createdOn asc': {
+            text: 'Date (asc)',
+            value: 'createdOn asc'
+        },
+        'createdOn desc': {
+            text: 'Date (desc)',
+            value: 'createdOn desc'
+        }
+    };
+    return (
+        <CompanyReportsFilters
+            sortOptions={Object.values(sortOptions)}
+            selectedOption={sortOptions[sortString]}
+            handleChange={handleChange}
+        />
+    );
+
+    function handleChange(_, value) {
+        updateCompanyReportsSort(value);
+    }
+};
+
+const mapDispatchToProps = dispatch => ({
+    updateCompanyReportsSort: sortString =>
+        dispatch(updateCompanyReportsSort(sortString))
+});
+
+const mapStateToProps = ({ superAdmin: { companyReportsReducer } }) => ({
+    sortString: companyReportsReducer.sort.sortString
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CompanyReportsFiltersContainer);
