d4fe87cc8450c352f101bae34ae2de5bcdd5aadd
Emma Randall <emma@silverchip.com>
2019-05-21 15:51:45 +0100

    change folder name for clientArea to client

---
+++ app-src/src/components/client/floors/shared/presentational/FloorListItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import DrawingTableContainer from 'components/client/drawings/shared/containers/DrawingTableContainer';
+import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
+
+const FloorListItem = ({
+    floor,
+    isExpanded,
+    colCount,
+    toggleExpanded,
+    permissions
+}) => (
+    <>
+        <tr
+            onClick={toggleExpanded}
+            className={`expandable ${isExpanded ? 'open' : ''}`}
+        >
+            <td>
+                {isExpanded ? (
+                    <i className="fa fa-chevron-down" />
+                ) : (
+                    <i className="fa fa-chevron-right" />
+                )}{' '}
+                {floor.name}
+            </td>
+            <td>{permissions}</td>
+            <td>
+                <ButtonContainer
+                    to={`/client/floors/${floor.id}`}
+                    handleClick={e => e.stopPropagation()}
+                >
+                    View
+                </ButtonContainer>
+            </td>
+        </tr>
+        {isExpanded && (
+            <tr>
+                <td colSpan={colCount} className="table-container drawing">
+                    <DrawingTableContainer ids={floor.drawingIDs} />
+                </td>
+            </tr>
+        )}
+    </>
+);
+
+export default FloorListItem;
