296c4688d6c5c32e5de8b1b37b8177592f246a0f
Tom Duthie <tom@silverchip.com>
2019-05-22 15:06:44 +0100

    Terms and conditions page added and linked to on register form

--- app-src/src/components/auth/app/routes/presentational/index.js
+++ app-src/src/components/auth/app/routes/presentational/index.js
@@ -4,11 +4,13 @@ import { Route } from 'react-router-dom';
 import SwitchWith404 from 'components/appRoute/routes/presentational/SwitchWith404';
 import Login from 'components/auth/login/presentational/Login';
 import Register from 'components/auth/registration/presentational/Register';
+import Terms from 'components/auth/terms/presentational/Terms';
 
 const AuthRoutes = ({ base = '/auth' }) => (
     <SwitchWith404>
         <Route exact path={`${base}/login`} component={Login} />
         <Route exact path={`${base}/register`} component={Register} />
+        <Route exact path={`${base}/terms`} component={Terms} />
     </SwitchWith404>
 );
 
---
+++ app-src/src/components/shared/generic/wysiwyg/presentational/WysiwygBlock.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WysiwygBlock = ({ children }) => (
+    <div className="wysiwyg size-lg-12">{children}</div>
+);
+
+export default WysiwygBlock;
