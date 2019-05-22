345c0e984d96dd2edda188d85472a84c7b76358d
Emma Randall <emma@silverchip.com>
2019-05-22 13:39:33 +0100

    client services reducer

---
+++ app-src/src/reducers/client/clientServicesReducer.js
@@ -0,0 +1,46 @@
+import { combineReducers } from 'redux';
+import {
+    CLIENT_FETCH_ALL_SERVICES_REQUEST,
+    CLIENT_FETCH_ALL_SERVICES_SUCCESS,
+    CLIENT_FETCH_ALL_SERVICES_FAILURE
+} from 'constants/client/actionTypes/clientServices';
+import { convertArrToObj } from 'helpers/generic';
+
+export default combineReducers({
+    error: errorReducer,
+    isFetching: isFetchingReducer,
+    services: servicesReducer
+});
+
+function isFetchingReducer(state = false, action) {
+    switch (action.type) {
+        case CLIENT_FETCH_ALL_SERVICES_REQUEST:
+            return true;
+        case CLIENT_FETCH_ALL_SERVICES_SUCCESS:
+        case CLIENT_FETCH_ALL_SERVICES_FAILURE:
+            return false;
+        default:
+            return state;
+    }
+}
+
+function errorReducer(state = null, action) {
+    switch (action.type) {
+        case CLIENT_FETCH_ALL_SERVICES_REQUEST:
+            return null;
+
+        case CLIENT_FETCH_ALL_SERVICES_FAILURE:
+            return action.error;
+        default:
+            return state;
+    }
+}
+
+function servicesReducer(state = {}, action) {
+    switch (action.type) {
+        case CLIENT_FETCH_ALL_SERVICES_SUCCESS:
+            return convertArrToObj(action.payload);
+        default:
+            return state;
+    }
+}
