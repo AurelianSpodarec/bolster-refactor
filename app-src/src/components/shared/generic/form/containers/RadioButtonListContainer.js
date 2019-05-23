45d15ef57d141910dc7f7b6be872ad152a738f9b
Mark Baker <mark@silverchip.com>
2019-05-23 10:58:03 +0100

    required fix for radio buttons

--- app-src/src/components/shared/generic/form/containers/RadioButtonListContainer.js
+++ app-src/src/components/shared/generic/form/containers/RadioButtonListContainer.js
@@ -54,10 +54,11 @@ class RadioButtonListContainer extends Component {
             error,
             addFieldError,
             removeFieldError,
-            selectedOption
+            selectedOption,
+            required
         } = this.props;
 
-        if (!selectedOption) {
+        if (!selectedOption && required) {
             addFieldError(name, 'This is a required field.');
         } else if (error) {
             removeFieldError(name);
