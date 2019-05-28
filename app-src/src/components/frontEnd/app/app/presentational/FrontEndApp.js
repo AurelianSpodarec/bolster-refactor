e4bf0e081e37cbfd30948d84381b3b51cb867c93
Tom Duthie <tom@silverchip.com>
2019-05-28 12:36:43 +0100

    menu added in

--- app-src/src/components/frontEnd/app/app/presentational/FrontEndApp.js
+++ app-src/src/components/frontEnd/app/app/presentational/FrontEndApp.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import MenuContainer from 'components/shared/layout/menu/containers/MenuContainer';
 import AdminFooter from 'components/superAdmin/layout/footer/presentational/AdminFooter';
 import FrontEndHeader from 'components/frontEnd/layout/header/presentational/FrontEndHeader';
+import FrontEndMenu from 'components/frontEnd/layout/navigation/presentational/FrontEndMenu';
 import FrontEndRoutes from '../../routes/presentational';
 
 const FrontEndApp = () => (
     <div id="frontend-site">
         <FrontEndHeader />
+        <FrontEndMenu />
+
         <div className="full-container container">
-            <MenuContainer />
             <div id="page-area">
                 <FrontEndRoutes />
             </div>
---
+++ app-src/src/components/frontEnd/layout/navigation/presentational/FrontEndMenu.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import HomeIcon from '_content/images/frontend/buttons/home-icon.png';
+
+const FrontEndMenu = () => (
+    <div className="frontend-menu">
+        <div className="container">
+            <ul>
+                <li className="home active">
+                    <Link to="/home">
+                        <img src={HomeIcon} />
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to="/How">How it works</Link>
+                </li>
+
+                <li>
+                    <Link to="/About">About</Link>
+                </li>
+                <li>
+                    <Link to="/Request">About</Link>
+                </li>
+                <li>
+                    <Link to="/Contact">Contact</Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+);
+
+export default FrontEndMenu;
