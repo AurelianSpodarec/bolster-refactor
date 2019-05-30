9e0a4e91e375f456c54fa1f08d58952c1d93fedc
Rob Fairclough <rob@silverchip.com>
2019-05-30 10:38:20 +0100

    conditional check on window.twttr as firefox content blocking crashes the app without check

--- app-src/src/components/shared/twitterFeed/presentational/TwitterFeed.js
+++ app-src/src/components/shared/twitterFeed/presentational/TwitterFeed.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
-class TwitterFeed extends Component {
-    render() {
-        return (
-            <div className="twitter size-lg-12">
-                <a
-                    className="twitter-timeline"
-                    data-height="421"
-                    href="https://twitter.com/bolstersystems?ref_src=twsrc%5Etfw"
-                >
-                    Tweets by bolstersystems
-                </a>
-            </div>
-        );
-    }
+const TwitterFeed = () => {
+    // Browsers with content blocking will crash when trying to access window.twttr.widgets
+    // as they have blocked window.twttr, added this conditional check to prevent a crash
+    // will also allow for loading the tweets again without refresh if the twttr window prop becomes available
+    useEffect(() => {
+        if (window.twttr) window.twttr.widgets.load();
+    }, [window.twttr]);
 
-    componentDidMount = () => {
-        window.twttr.widgets.load();
-    };
-}
+    return (
+        <div className="twitter size-lg-12">
+            <a
+                className="twitter-timeline"
+                data-height="421"
+                href="https://twitter.com/bolstersystems?ref_src=twsrc%5Etfw"
+            >
+                Tweets by bolstersystems
+            </a>
+        </div>
+    );
+};
 
 export default TwitterFeed;
