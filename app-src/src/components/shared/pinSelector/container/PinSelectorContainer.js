43ef535562548c452e76ef9d9689651e04d0091a
Rob Fairclough <rob@silverchip.com>
2019-05-31 11:48:20 +0100

    udpate pinfilters when updating selected service

--- app-src/src/components/shared/pinSelector/container/PinSelectorContainer.js
+++ app-src/src/components/shared/pinSelector/container/PinSelectorContainer.js
@@ -84,13 +84,13 @@ class PinSelectorContainer extends Component {
             included:
                 option.included || selectedPinOptions.includes(option.value)
         }));
-        const pinIDs = oldOptions
-            .filter(({ included }) => !included)
+        const pinIDs = Object.values(pinOptions)
+            .filter(({ included }) => included)
             .map(({ value }) => value);
         this.setState({
             pinOptions,
-            selectedPinOptions: selectedPinOptions.filter(
-                id => !pinIDs.includes(id)
+            selectedPinOptions: selectedPinOptions.filter(id =>
+                pinIDs.includes(id)
             )
         });
 
@@ -107,7 +107,7 @@ class PinSelectorContainer extends Component {
             included:
                 option.included && !selectedPinOptions.includes(option.value)
         }));
-        const pinIDs = oldOptions
+        const pinIDs = pinOptions
             .filter(({ included }) => included)
             .map(({ value }) => value);
         this.setState({
@@ -156,11 +156,12 @@ class PinSelectorContainer extends Component {
             filters: { pinIDs: oldPinIDs },
             handleChange
         } = this.props;
+
         if (prevProps.customFilters.pins.length !== pins.length) {
             this._setPinOptions();
             const pinIDs = pins.map(({ id }) => id);
             const newSelectedPins = oldPinIDs.filter(id => pinIDs.includes(id));
-            handleChange('pinIDs', newSelectedPins);
+            // handleChange('pinIDs', newSelectedPins);
         }
     };
 
@@ -172,7 +173,9 @@ class PinSelectorContainer extends Component {
             }),
             {}
         );
+        const pinIDs = Object.values(pinOptions).map(({ value }) => value);
         this.setState({ pinOptions });
+        this.props.handleChange('pinIDs', pinIDs);
     };
 }
 
