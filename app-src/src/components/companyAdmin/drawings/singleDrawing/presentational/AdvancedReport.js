d9461d851c7cdc8430991ef1246fd481144cdc70
Rob Fairclough <rob@silverchip.com>
2019-05-31 16:24:15 +0100

    progress on advanced report

--- app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/AdvancedReport.js
+++ app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/AdvancedReport.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Block from 'components/shared/generic/block/presentational/Block';
 
-import DrawingMapAdvancedContainer from '../containers/DrawingMapAdvancedContainer';
 import Block1FiltersContainer from 'components/companyAdmin/reports/createReport/components/containers/Block1FiltersContainer';
 import BasicFiltersContainer from 'components/companyAdmin/reports/createReport/components/containers/BasicFiltersContainer';
 import FurtherFiltrationContainer from 'components/companyAdmin/reports/createReport/components/containers/FurtherFiltrationContainer';
@@ -18,29 +17,12 @@ const AdvancedReport = () => (
         </div>
 
         <div className="flex-container size-lg-12">
-            {/* update to account for selected drawing */}
             <Block1FiltersContainer blockName="hierarchyFilters" advanced />
             <BasicFiltersContainer blockName="basicFilters" />
         </div>
 
         <FurtherFiltrationContainer />
         <OutputSettingsContainer />
-
-        {/* <div className="size-lg-8">
-            <Block>
-                <DrawingPinSelectorContainer />
-            </Block>
-
-            <Block>
-                <DrawingReportOptionsContainer />
-            </Block>
-        </div>
-
-        <div className="size-lg-4">
-            <Block>
-                <DrawingPinOptionsContainer />
-            </Block>
-        </div> */}
     </div>
 );
 
