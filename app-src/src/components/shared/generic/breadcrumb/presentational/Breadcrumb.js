357be57627c115d7d0aabe7472630abaeb78b185
Rob Fairclough <rob@silverchip.com>
2019-05-31 14:12:39 +0100

    fix unique key prop on breadcrumb

--- app-src/src/components/shared/generic/breadcrumb/presentational/Breadcrumb.js
+++ app-src/src/components/shared/generic/breadcrumb/presentational/Breadcrumb.js
@@ -11,12 +11,12 @@ const Breadcrumb = ({
             <p>
                 {breadcrumbs.map((item, i) =>
                     item.link ? (
-                        <>
-                            <Link key={item.link + i} to={item.link}>
+                        <React.Fragment key={item.link}>
+                            <Link key={item.link} to={item.link}>
                                 <sub>{item.text}</sub>
                             </Link>
                             {i < breadcrumbs.length - 1 ? ' / ' : ''}
-                        </>
+                        </React.Fragment>
                     ) : (
                         <span key={item.text + i}>
                             {item.text}
