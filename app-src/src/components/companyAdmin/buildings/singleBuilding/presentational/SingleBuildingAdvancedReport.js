27739873e667fadef8fba58d4667be71867e389b
Rob Fairclough <rob@silverchip.com>
2019-06-03 14:35:06 +0100

    remove import bug

--- app-src/src/components/companyAdmin/buildings/singleBuilding/presentational/SingleBuildingAdvancedReport.js
+++ app-src/src/components/companyAdmin/buildings/singleBuilding/presentational/SingleBuildingAdvancedReport.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import FilterMapContainer from 'components/companyAdmin/reports/createReport/components/containers/FilterMapContainer';
-import { Block1FiltersContainer } from 'components/companyAdmin/reports/createReport/components/containers/Block1FiltersContainer';
+import Block1FiltersContainer from 'components/companyAdmin/reports/createReport/components/containers/Block1FiltersContainer';
 import BasicFiltersContainer from 'components/companyAdmin/reports/createReport/components/containers/BasicFiltersContainer';
 import FurtherFiltrationContainer from 'components/companyAdmin/reports/createReport/components/containers/FurtherFiltrationContainer';
 import OutputSettingsContainer from 'components/companyAdmin/reports/createReport/components/containers/OutputSettingsContainer';
