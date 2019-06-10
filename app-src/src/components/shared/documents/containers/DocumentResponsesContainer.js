e2583080a4afcca04879b78eb5b2b869548722c8
Rob Fairclough <rob@silverchip.com>
2019-06-10 15:21:22 +0100

    set up for document responses

--- app-src/src/components/shared/documents/containers/DocumentResponsesContainer.js
+++ app-src/src/components/shared/documents/containers/DocumentResponsesContainer.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import DocumentResponses from '../presentational/DocumentResponses';
+import fetchDocumentResponses from 'actions/companyAdmin/documents/async/fetchDocumentResponses';
+import fetchSingleDocument from 'actions/documents/async/fetchSingleDocument';
+import { componentDidMount } from 'helpers/generic';
 
-class DocumentResponsesContainer extends Component {
-    render = () => {
-        const sampleDocument = {
-            agreeanceEveryXDays: 0,
-            createdByCompanyUserID: 1,
-            createdOn: '2019-06-10T10:52:14',
-            endOn: '2019-07-05T23:00:00',
-            fileS3Key:
-                'd031e1ab-0aed-4f49-b7bc-2facff143396/60mm-ablative-coated-batt.pdf',
-            id: 3,
-            isFileViewRequired: false,
-            isPhotoRequired: false,
-            isSignatureRequired: false,
-            isUpsyncForced: false,
-            name: '60mm Ablative Coated Batt',
-            serviceIDs: Array[1],
-            startOn: '2019-06-09T23:00:00',
-            type: 1
-        };
-        return <DocumentResponses document={sampleDocument} />;
-    };
-}
+const DocumentResponsesContainer = ({
+    document,
+    fetchDocumentResponses,
+    fetchSingleDocument,
+    documentID
+}) => {
+    componentDidMount(() => {
+        fetchDocumentResponses(documentID);
+        fetchSingleDocument(documentID);
+    });
 
-const mapDispatchToProps = {};
+    return <DocumentResponses document={document} />;
+};
+
+const mapStateToProps = (
+    { companyAdmin: { documentsReducer } },
+    { match: { params } }
+) => ({
+    document: documentsReducer.documents[params.documentID] || {},
+    documentID: params.documentID
+});
+
+const mapDispatchToProps = { fetchDocumentResponses, fetchSingleDocument };
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(DocumentResponsesContainer);
