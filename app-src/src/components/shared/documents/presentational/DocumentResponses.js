916712f5cfe129dede9d344a22a456070a3c74be
Tom Duthie <tom@silverchip.com>
2019-06-11 19:31:31 +0100

    Documents response table styled

--- app-src/src/components/shared/documents/presentational/DocumentResponses.js
+++ app-src/src/components/shared/documents/presentational/DocumentResponses.js
@@ -2,11 +2,16 @@ import React from 'react';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import DocumentResponsesTableContainer from '../containers/DocumentResponsesTableContainer';
+import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
+import BackButtonContainer from 'components/shared/generic/backButton/containers/BackButtonContainer';
 
 const DocumentResponses = ({ document }) => (
     <>
-        <PageHeading title="Document Responses" />
-        <BlockContainer heading={document.name}>
+        <PageHeading title="Document Responses" leftChildren={true}>
+            <BackButtonContainer />
+        </PageHeading>
+        <BlockContainer>
+            <BlockHeading title={document.name} />
             <DocumentResponsesTableContainer />
         </BlockContainer>
         {/* list of responses, split by operative */}
--- app-src/src/components/shared/documents/presentational/DocumentResponsesTable.js
+++ app-src/src/components/shared/documents/presentational/DocumentResponsesTable.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import Table from 'components/shared/generic/tables/presentational/Table';
 import DocumentResponsesTableItemContainer from '../containers/DocumentResponsesTableItemContainer';
 
 const DocumentResponsesTable = ({ responses, users, isFetching }) => (
-    <BlockContainer>
-        <Table
-            headers={['Agreed By', 'Agreed On', 'Upsynced On']}
-            isFetching={isFetching}
-            noData={!responses.length}
-            noDataMessage="No responses to display for this document."
-        >
-            {responses.map(response => (
-                <DocumentResponsesTableItemContainer
-                    key={response.id}
-                    response={response}
-                    user={users[response.createdByCompanyUserID] || {}}
-                />
-            ))}
-        </Table>
-    </BlockContainer>
+    <Table
+        headers={['Agreed By', 'Agreed On', 'Upsynced On']}
+        isFetching={isFetching}
+        noData={!responses.length}
+        noDataMessage="No responses to display for this document."
+    >
+        {responses.map(response => (
+            <DocumentResponsesTableItemContainer
+                key={response.id}
+                response={response}
+                user={users[response.createdByCompanyUserID] || {}}
+            />
+        ))}
+    </Table>
 );
 
 export default DocumentResponsesTable;
