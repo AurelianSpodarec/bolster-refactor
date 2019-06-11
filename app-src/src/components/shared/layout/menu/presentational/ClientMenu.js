2524e14057ef25009c0085c3af8fb1e6cc3704da
Mark Baker <mark@silverchip.com>
2019-06-11 11:42:53 +0100

    html fix for client menu (style issues)

--- app-src/src/components/shared/layout/menu/presentational/ClientMenu.js
+++ app-src/src/components/shared/layout/menu/presentational/ClientMenu.js
@@ -7,11 +7,13 @@ const ClientMenu = () => (
         <div className="menu-bg" />
         <div className="menu">
             <MenuItemContainer link="/client" base>
-                <i className="fa fa-home icon" /> Dashboard
+                <i className="fa fa-home icon" />{' '}
+                <span className="menu-text">Dashboard</span>
             </MenuItemContainer>
 
             <MenuItemContainer link="/client/sites">
-                <i className="fa fa-building icon" /> Sites
+                <i className="fa fa-building icon" />{' '}
+                <span className="menu-text">Sites</span>
             </MenuItemContainer>
         </div>
     </>
