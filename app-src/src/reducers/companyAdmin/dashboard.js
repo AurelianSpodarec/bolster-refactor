34a4e79dfe4347aa1385e606c48c9eb06cf98593
Tom Duthie <tom@silverchip.com>
2019-06-12 12:10:52 +0100

    Action and reducer for pin stats - dashboard

--- app-src/src/reducers/companyAdmin/dashboard.js
+++ app-src/src/reducers/companyAdmin/dashboard.js
@@ -6,17 +6,34 @@ import {
     FETCH_HISTORY_FEED_REQUEST,
     FETCH_HISTORY_FEED_SUCCESS,
     FETCH_HISTORY_FEED_FAILURE,
-    UPDATE_DASHBOARD_SETTING
+    UPDATE_DASHBOARD_SETTING,
+    FETCH_DASH_PINS_STATS_REQUEST,
+    FETCH_DASH_PINS_STATS_SUCCESS,
+    FETCH_DASH_PINS_STATS_FAILURE
 } from 'constants/actionTypes/dashboard';
-import { updateObj } from 'helpers/generic';
+import { updateObj, convertArrToObj } from 'helpers/generic';
 
 export default combineReducers({
     error: errorReducer,
+    isFetchingDashPinsStats: isFetchingDashPinsStatsReducer,
     isInitialFetching: isInitialFetchingReducer,
     isLiveFetching: isLiveFetchingReducer,
     liveHistories: liveHistoriesReducer,
+    dashPinsStats: dashPinsStatsReducer,
     settings: settingsReducer
 });
+
+function isFetchingDashPinsStatsReducer(state = false, action) {
+    switch (action.type) {
+        case FETCH_DASH_PINS_STATS_REQUEST:
+            return true;
+        case FETCH_DASH_PINS_STATS_FAILURE:
+        case FETCH_DASH_PINS_STATS_SUCCESS:
+            return false;
+        default:
+            return state;
+    }
+}
 
 function isInitialFetchingReducer(state = false, action) {
     switch (action.type) {
@@ -41,14 +58,26 @@ function isLiveFetchingReducer(state = false, action) {
             return state;
     }
 }
+
 function errorReducer(state = null, action) {
     switch (action.type) {
         case FETCH_HISTORY_FEED_REQUEST:
         case FETCH_LIVE_HISTORIES_REQUEST:
+        case FETCH_DASH_PINS_STATS_REQUEST:
             return null;
         case FETCH_HISTORY_FEED_FAILURE:
         case FETCH_LIVE_HISTORIES_FAILURE:
+        case FETCH_DASH_PINS_STATS_FAILURE:
             return action.error;
+        default:
+            return state;
+    }
+}
+
+function dashPinsStatsReducer(state = {}, action) {
+    switch (action.type) {
+        case FETCH_DASH_PINS_STATS_SUCCESS:
+            return convertArrToObj(action.payload);
         default:
             return state;
     }
