be0f933a4f8bf52a6c92320bcb9a1637ab200da2
Rob Fairclough <rob@silverchip.com>
2019-06-12 14:11:01 +0100

    on floor grey out add client and add operative buttons if no drawings present

--- app-src/src/components/companyAdmin/floors/singleFloor/containers/FloorClientAccessContainer.js
+++ app-src/src/components/companyAdmin/floors/singleFloor/containers/FloorClientAccessContainer.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import FloorClientAccess from '../presentational/FloorClientAccess';
 
-class FloorClientAccessContainer extends Component {
+export default class FloorClientAccessContainer extends Component {
     render() {
         return <FloorClientAccess />;
     }
 }
-
-const mapStateToProps = state => ({});
-
-const mapDispatchToProps = dispatch => ({});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(FloorClientAccessContainer);
