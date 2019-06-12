5bc5137d1c449234e433a2d4bc3d65b16e13342d
Rob Fairclough <rob@silverchip.com>
2019-06-12 12:52:29 +0100

    edge case where all pins are deselected, then change to another filter option

--- app-src/src/components/companyAdmin/reports/createReport/components/hocs/withUpdateOnChange.js
+++ app-src/src/components/companyAdmin/reports/createReport/components/hocs/withUpdateOnChange.js
@@ -127,7 +127,7 @@ export default function(ProtectedComponent) {
                 companyUserIDs,
                 serviceID: serviceID || null,
                 status: status || null,
-                pinIDs: pinIDs.length ? pinIDs : null,
+                pinIDs: pinIDs || null,
                 questionFilters,
                 showHidden,
                 sortBy
