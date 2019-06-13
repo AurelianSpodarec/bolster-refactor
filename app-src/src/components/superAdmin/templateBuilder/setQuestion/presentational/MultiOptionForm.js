c31c26fa91d0e241f2f4dbf1681da64b3c80428e
Rob Fairclough <rob@silverchip.com>
2019-06-13 10:38:17 +0100

    add defaultvalue to templatebuilder radio buttons

--- app-src/src/components/superAdmin/templateBuilder/setQuestion/presentational/MultiOptionForm.js
+++ app-src/src/components/superAdmin/templateBuilder/setQuestion/presentational/MultiOptionForm.js
@@ -3,38 +3,40 @@ import React from 'react';
 import Field from 'components/shared/generic/form/presentational/Field';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
 import CheckboxContainer from 'components/shared/generic/form/containers/CheckboxContainer';
+import Select from 'components/shared/generic/form/presentational/Select';
 
 const MultiOptionForm = ({
     addOption,
     removeOption,
     updateOption,
     options,
+    optionsForSelect,
     canCompanyEdit,
-    handleInputChange
+    handleInputChange,
+    defaultValue,
+    radio
 }) => (
     <div className="dropdown-create size-lg-12">
-        {options.map((option, i) => {
-            return (
-                <Field key={option.id} name={`Option ${i + 1}`} required>
-                    {options.length > 1 && (
-                        <button
-                            className="button red icon-only delete-question"
-                            onClick={e => removeOption(e, option.id)}
-                            type="button"
-                        >
-                            <i className="far fa-trash-alt" />
-                        </button>
-                    )}
-                    <TextInputContainer
-                        placeholder="Type the option here"
-                        name={option.id}
-                        required
-                        handleChange={updateOption}
-                        value={option.text}
-                    />
-                </Field>
-            );
-        })}
+        {options.map((option, i) => (
+            <Field key={option.id} name={`Option ${i + 1}`} required>
+                {options.length > 1 && (
+                    <button
+                        className="button red icon-only delete-question"
+                        onClick={() => removeOption(option.id)}
+                        type="button"
+                    >
+                        <i className="far fa-trash-alt" />
+                    </button>
+                )}
+                <TextInputContainer
+                    placeholder="Type the option here"
+                    name={option.id}
+                    required
+                    handleChange={updateOption}
+                    value={option.text}
+                />
+            </Field>
+        ))}
 
         <div className="field-intro size-lg-12">
             <div className="size-lg-12">
@@ -55,6 +57,16 @@ const MultiOptionForm = ({
                 name="canCompanyEdit"
             />
         </Field>
+        {radio && (
+            <Field name="Default value">
+                <Select
+                    options={optionsForSelect}
+                    onChange={handleInputChange}
+                    value={defaultValue}
+                    name="defaultValue"
+                />
+            </Field>
+        )}
     </div>
 );
 
