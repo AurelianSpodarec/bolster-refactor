832ad8af5de79ced55a40515256e92236c251288
Rob Fairclough <rob@silverchip.com>
2019-06-14 10:51:41 +0100

    add ishistory checks to addpin

--- app-src/src/components/shared/pins/addPin/containers/AddPinVersionsContainer.js
+++ app-src/src/components/shared/pins/addPin/containers/AddPinVersionsContainer.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import AddPinVersions from '../presentational/AddPinVersions';
 
-const AddPinVersionsContainer = ({ selectedVersion }) => {
-    return <AddPinVersions selectedVersion={selectedVersion} />;
+const AddPinVersionsContainer = ({ selectedVersion, isHistory }) => {
+    return (
+        <AddPinVersions
+            selectedVersion={selectedVersion}
+            isHistory={isHistory}
+        />
+    );
 };
 
 const mapStateToProps = (
