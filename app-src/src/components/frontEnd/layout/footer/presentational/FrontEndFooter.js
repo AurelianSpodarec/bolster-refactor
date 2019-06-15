b74ade41c162a535269e1beda03ecabda460fd92
Jamie McMullan <jamie@silverchip.com>
2019-06-15 13:24:43 +0100

    updated main website footer icon

--- app-src/src/components/frontEnd/layout/footer/presentational/FrontEndFooter.js
+++ app-src/src/components/frontEnd/layout/footer/presentational/FrontEndFooter.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-import WhiteLogo from '_content/images/frontend/footer-logo.png';
+import WhiteLogo from '_content/images/footer/logo-footer-white.png';
 
 const FrontEndFooter = () => (
     <div id="footer-alt">
         <div className="container">
             <div className="logo">
-                <span>powered by</span>
-
                 <img
                     alt="logo of Bolster Systems"
                     src={WhiteLogo}
-                    width="93"
-                    height="40"
+                    width="107"
+                    height="51"
                 />
             </div>
         </div>
