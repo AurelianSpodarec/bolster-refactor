ceacf5dbcb74c2b8d86c195565a14db887ca5cf5
Jamie McMullan <jamie@silverchip.com>
2019-06-15 13:49:07 +0100

    main site footer logo and default logo fix

--- app-src/src/constants/defaultStyles/index.js
+++ app-src/src/constants/defaultStyles/index.js
@@ -2,7 +2,7 @@ import bolsterLogo from '_content/images/bolster_logo.png';
 export default {
     id: 1,
     name: 'Bolster Systems',
-    logoFile: { bolsterLogo },
+    logoFile: bolsterLogo,
     colourCode: '#E10512',
     colorSchemeText: 'white'
 };
