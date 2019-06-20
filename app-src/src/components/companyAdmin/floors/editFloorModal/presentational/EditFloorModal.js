fcfe506fccfb6c7d96e1369e52351131e97291e8
Emma Randall <emma@silverchip.com>
2019-06-20 14:28:32 +0100

    plug in label example to add and edit floor

--- app-src/src/components/companyAdmin/floors/editFloorModal/presentational/EditFloorModal.js
+++ app-src/src/components/companyAdmin/floors/editFloorModal/presentational/EditFloorModal.js
@@ -17,8 +17,14 @@ const EditFloorModal = ({ floor, isUsingBolsterLabels }) => (
     </ModalOuterContainer>
 );
 
-const mapStateToProps = ({ companyAdmin: { companySettingsReducer } }) => ({
-    isUsingBolsterLabels: companySettingsReducer.isUsingBolsterLabels
+const mapStateToProps = ({
+    companyAdmin: {
+        companySettingsReducer: {
+            companySettings: { isUsingBolsterLabels }
+        }
+    }
+}) => ({
+    isUsingBolsterLabels
 });
 
 export default connect(mapStateToProps)(EditFloorModal);
