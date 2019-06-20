13d830c02aab4d07b950a2696157d94d6b1e30a0
Emma Randall <emma@silverchip.com>
2019-06-20 12:50:52 +0100

    company settings

--- app-src/src/components/appRoute/app/presentational/App.js
+++ app-src/src/components/appRoute/app/presentational/App.js
@@ -6,6 +6,7 @@ import Routes from '../../routes/presentational';
 const App = () => (
     <div className="App">
         <Routes />
+        {/* hidden field potentially for version no. */}
         <div className="hidden">hidden</div>
         <div className="clear" />
         <ModalRoute />
