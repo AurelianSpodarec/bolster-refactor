0aef699011cce805374f611019f4db9c14debb5b
Mark Baker <mark@silverchip.com>
2019-06-26 12:28:27 +0100

    operatives / drawing on dashboard can now take you to generate report with prefilled data

--- app-src/src/components/companyAdmin/dashboard/presentational/DashboardDataByOperativeListItem.js
+++ app-src/src/components/companyAdmin/dashboard/presentational/DashboardDataByOperativeListItem.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import moment from 'moment';
 
-const DrawingDataByOperativeListItem = ({ operative }) => (
-    <tr>
-        <td>{operative.name}</td>
-        <td>
-            {operative.lastLogin
-                ? moment(operative.lastLogin).format('DD-MM-YYYY HH:mm')
-                : 'Not logged in'}
-        </td>
-        <td>
-            {operative.lastSync
-                ? moment(operative.lastSync).format('DD-MM-YYYY HH:mm')
-                : 'Not synced'}
-        </td>
-        <td>{operative.pinsUpdated}</td>
-        <td />
-    </tr>
-);
+const DrawingDataByOperativeListItem = ({ operative, history }) => {
+    return (
+        <tr>
+            <td>{operative.name}</td>
+            <td>
+                {operative.lastLogin
+                    ? moment(operative.lastLogin).format('DD-MM-YYYY HH:mm')
+                    : 'Not logged in'}
+            </td>
+            <td>
+                {operative.lastSync
+                    ? moment(operative.lastSync).format('DD-MM-YYYY HH:mm')
+                    : 'Not synced'}
+            </td>
+            <td>{operative.pinsUpdated}</td>
+            <td>
+                {/* <button className="button blue" onClick={generateReport}>
+                    Generate Report
+                </button> */}
+            </td>
+        </tr>
+    );
 
-export default DrawingDataByOperativeListItem;
+    function generateReport() {
+        //NEEDS TO BE UPDATED ONCE API IS
+        history.push({
+            pathname: '/company/tools/create-report',
+            state: {
+                operativeID: 6
+            }
+        });
+    }
+};
+
+export default withRouter(DrawingDataByOperativeListItem);
