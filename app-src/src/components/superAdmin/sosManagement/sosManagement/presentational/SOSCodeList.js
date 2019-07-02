0f6b21670f4a4164b12673487fd770912079bd7e
Emma Randall <emma@silverchip.com>
2019-07-02 16:52:31 +0100

    include fetch all sos codes in sos management container

--- app-src/src/components/superAdmin/sosManagement/sosManagement/presentational/SOSCodeList.js
+++ app-src/src/components/superAdmin/sosManagement/sosManagement/presentational/SOSCodeList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import moment from 'moment';
+import { DATE_TIMES } from 'constants/companyAdmin/enums';
 
 const SOSCodeList = ({ sosCodes }) =>
     sosCodes.map(code => (
         <tr key={code.id}>
-            <td>{`${code.date}`}</td>
+            <td>{`${moment(code.timestamp).format(DATE_TIMES[1])}`}</td>
             <td>{code.code}</td>
             <td>{code.description}</td>
-            <td>{code.hasSynced ? 'Yes' : 'No'}</td>
+            <td>{code.hasUserSynced ? 'Yes' : 'No'}</td>
         </tr>
     ));
 export default SOSCodeList;
