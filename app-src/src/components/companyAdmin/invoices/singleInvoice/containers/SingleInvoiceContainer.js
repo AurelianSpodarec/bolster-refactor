e24e32dd2485f907648ad4755a3d252398acd32b
Tom Duthie <tom@silverchip.com>
2019-07-03 10:37:26 +0100

    Single invoice styles and payment button

--- app-src/src/components/companyAdmin/invoices/singleInvoice/containers/SingleInvoiceContainer.js
+++ app-src/src/components/companyAdmin/invoices/singleInvoice/containers/SingleInvoiceContainer.js
@@ -4,12 +4,18 @@ import { withRouter } from 'react-router-dom';
 
 import fetchSingleInvoice from 'actions/companyAdmin/invoices/async/fetchSingleInvoiceItems';
 import fetchSingleInvoiceItems from 'actions/companyAdmin/invoices/async/fetchSingleInvoice';
-
+import { showModal } from 'actions/shared/generic/modals/sync/showModal';
 import SingleInvoice from '../presentational/SingleInvoice';
 
 class SingleInvoiceContainer extends Component {
     render() {
-        return <SingleInvoice id={this.props.id} />;
+        return (
+            <SingleInvoice
+                id={this.props.id}
+                showModal={this.props.showModal}
+                hasPayed={!!this.props.invoicePayments.length}
+            />
+        );
     }
 
     componentDidMount = () => {
@@ -17,15 +23,26 @@ class SingleInvoiceContainer extends Component {
     };
 }
 
-const mapStateToProps = (_, { match }) => ({
-    id: match.params.id
+const mapStateToProps = (
+    {
+        companyAdmin: {
+            invoicesReducer: { invoicePayments }
+        }
+    },
+    { match }
+) => ({
+    id: match.params.id,
+    invoicePayments: Object.values(invoicePayments).filter(
+        payment => +payment.invoiceID === +match.params.id
+    )
 });
 
 const mapDispatchToProps = dispatch => ({
     fetchInvoiceAndItems: id => {
         dispatch(fetchSingleInvoice(id));
         dispatch(fetchSingleInvoiceItems(id));
-    }
+    },
+    showModal: (type, props) => dispatch(showModal(type, props))
 });
 
 export default withRouter(
