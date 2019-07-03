539f432029d93e9637017d7d25a37af141f45af9
Tom Duthie <tom@silverchip.com>
2019-07-03 16:36:44 +0100

    Account overview

--- app-src/src/components/companyAdmin/messages/allMessages/presentational/MySubscribedServiceListItem.js
+++ app-src/src/components/companyAdmin/messages/allMessages/presentational/MySubscribedServiceListItem.js
@@ -2,7 +2,10 @@ import React from 'react';
 import StatusIcon from 'components/shared/generic/statusIcon/presentationl/StatusIcon';
 
 const SubscribedServiceListItem = ({ key, serviceName }) => (
-    <div key={`subscription-${key}`} className="subscription-item size-lg-6">
+    <div
+        key={`subscription-${key}`}
+        className="subscription-item size-lg-6 size-md-12"
+    >
         <div className="field-name size-lg-12">
             <label>{serviceName}</label>
             <StatusIcon />
--- app-src/src/components/companyAdmin/subscription/activeServices/presentational/AddServiceItem.js
+++ app-src/src/components/companyAdmin/subscription/activeServices/presentational/AddServiceItem.js
@@ -4,8 +4,11 @@ import { ADD_SERVICE_TO_SUBSCRIPTION } from 'constants/shared/modalTypes';
 import StatusIcon from 'components/shared/generic/statusIcon/presentationl/StatusIcon';
 
 const AddServiceItem = ({ service, key, showModal }) => (
-    <div key={key} className="subscription-item add-service size-lg-6">
-        <div className="field-name size-lg-6">
+    <div
+        key={key}
+        className="subscription-item add-service size-lg-6 size-md-12"
+    >
+        <div className="field-name size-lg-6 size-md-8">
             <StatusIcon classes="add" iconClass="fa fa-plus" />
             <label>{service.name}</label>
         </div>
