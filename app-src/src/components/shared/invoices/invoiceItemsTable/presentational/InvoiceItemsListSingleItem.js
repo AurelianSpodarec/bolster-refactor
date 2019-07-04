7640656ba0cce9eac74a0c8c67e955261055a620
Tom Duthie <tom@silverchip.com>
2019-07-04 15:18:19 +0100

    more mobile tables

--- app-src/src/components/shared/invoices/invoiceItemsTable/presentational/InvoiceItemsListSingleItem.js
+++ app-src/src/components/shared/invoices/invoiceItemsTable/presentational/InvoiceItemsListSingleItem.js
@@ -2,15 +2,49 @@ import React from 'react';
 import { formatCurrency } from 'helpers/generic';
 
 const InvoiceItemsListSingleItem = ({
-    item: { description, customName, quantity, itemPrice, itemVAT, total }
+    item: { description, customName, quantity, itemPrice, itemVAT, total },
+    onMobile,
+    headers
 }) => (
     <tr>
-        <td>{`${description || ''}`}</td>
-        <td>{customName || 'N/A'}</td>
-        <td>{quantity}</td>
-        <td>{`£${formatCurrency(itemPrice)}`}</td>
-        <td>{`£${formatCurrency(itemVAT)}`}</td>
-        <td>{`£${formatCurrency(total * quantity)}`}</td>
+        <td>
+            {' '}
+            {onMobile && (
+                <span className="mobile-table-heading">{headers[0]}</span>
+            )}
+            {`${description || ''}`}
+        </td>
+        <td>
+            {' '}
+            {onMobile && (
+                <span className="mobile-table-heading">{headers[1]}</span>
+            )}
+            {customName || 'N/A'}
+        </td>
+        <td>
+            {onMobile && (
+                <span className="mobile-table-heading">{headers[2]}</span>
+            )}
+            {quantity}
+        </td>
+        <td>
+            {onMobile && (
+                <span className="mobile-table-heading">{headers[3]}</span>
+            )}
+            {`£${formatCurrency(itemPrice)}`}
+        </td>
+        <td>
+            {onMobile && (
+                <span className="mobile-table-heading">{headers[4]}</span>
+            )}
+            {`£${formatCurrency(itemVAT)}`}
+        </td>
+        <td>
+            {onMobile && (
+                <span className="mobile-table-heading">{headers[5]}</span>
+            )}
+            {`£${formatCurrency(total * quantity)}`}
+        </td>
     </tr>
 );
 
