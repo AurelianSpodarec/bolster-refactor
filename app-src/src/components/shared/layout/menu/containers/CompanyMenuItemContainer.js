2088e4fd0040473fc7ca6a260f060f38ec755b69
Tom Duthie <tom@silverchip.com>
2019-07-06 15:58:55 +0100

    removed old code

--- app-src/src/components/shared/layout/menu/containers/CompanyMenuItemContainer.js
+++ app-src/src/components/shared/layout/menu/containers/CompanyMenuItemContainer.js
@@ -67,7 +67,6 @@ class MenuItemContainer extends Component {
         e.preventDefault();
         if (logout) {
             localStorage.setItem('token', '');
-            localStorage.setItem('justLoggedOut', true);
 
             history.replace('/auth/login');
         }
