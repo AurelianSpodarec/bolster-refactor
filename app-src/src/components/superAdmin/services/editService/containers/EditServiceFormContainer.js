672850c4edde8394aa56f1b63a682926cdeb9581
Mark Baker <mark@silverchip.com>
2019-07-06 17:54:21 +0100

    fetching templates update on add / edit services

--- app-src/src/components/superAdmin/services/editService/containers/EditServiceFormContainer.js
+++ app-src/src/components/superAdmin/services/editService/containers/EditServiceFormContainer.js
@@ -7,9 +7,9 @@ import EditServiceForm from '../presentational/EditServiceForm';
 import fetchSingleService from 'actions/superAdmin/services/async/fetchSingleService';
 import editService from 'actions/superAdmin/services/async/editService';
 import fetchTemplateForService from 'actions/superAdmin/services/async/fetchTemplateForService';
-import fetchTemplates from 'actions/superAdmin/templateBuilder/async/fetchTemplates';
 import postTemplatesForService from 'actions/superAdmin/services/async/postTemplatesForService';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
+import fetchTemplatesSimple from 'actions/superAdmin/templateBuilder/async/fetchTemplatesSimple';
 
 class EditServiceFormContainer extends Component {
     state = {
@@ -40,7 +40,7 @@ class EditServiceFormContainer extends Component {
     }
 
     componentDidMount = () => {
-        this.props.fetchTemplates();
+        this.props.fetchTemplatesSimple();
 
         this.props.fetchSingleService(this.props.id);
         this.props.fetchTemplateForService(this.props.id);
@@ -152,8 +152,8 @@ const mapDispatchToProps = dispatch => ({
     fetchTemplateForService: serviceID => {
         return dispatch(fetchTemplateForService(serviceID));
     },
-    fetchTemplates: () => {
-        dispatch(fetchTemplates());
+    fetchTemplatesSimple: () => {
+        dispatch(fetchTemplatesSimple());
     },
     editService: (id, name, showOnCompanySite) => {
         dispatch(editService(id, name, showOnCompanySite));
