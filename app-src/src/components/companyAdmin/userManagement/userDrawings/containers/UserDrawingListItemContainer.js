e26b62d8dcf91ceecaf4c7993b3ad6568f640b3d
Tom Duthie <tom@silverchip.com>
2019-07-06 14:48:52 +0100

    Drawings access breadcrumb

--- app-src/src/components/companyAdmin/userManagement/userDrawings/containers/UserDrawingListItemContainer.js
+++ app-src/src/components/companyAdmin/userManagement/userDrawings/containers/UserDrawingListItemContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import { showModal } from 'actions/shared/generic/modals/sync/showModal';
@@ -6,26 +6,108 @@ import { showModal } from 'actions/shared/generic/modals/sync/showModal';
 import { hideModal } from 'actions/shared/generic/modals/sync/hideModal';
 import editCompanyUserType from 'actions/companyAdmin/userManagement/async/editCompanyUserType';
 import UserDrawingListItem from '../presentational/UserDrawingListItem';
+import fetchSingleFloor from 'actions/companyAdmin/floors/async/fetchSingleFloor';
+import fetchSingleBuilding from 'actions/companyAdmin/buildings/async/fetchSingleBuilding';
+import fetchSingleSite from 'actions/companyAdmin/sites/async/fetchSingleSite';
+import { componentDidMount, componentDidUpdate } from 'helpers/generic';
 
 const UserDrawingListItemContainer = ({
     drawing,
     colCount,
     checkedDrawings,
-    handleDrawingIDs
+    handleDrawingIDs,
+    fetchSingleSite,
+    fetchSingleFloor,
+    fetchSingleBuilding,
+    sites,
+    buildings,
+    floors,
+    fetchingDrawings,
+    isFetching
 }) => {
+    const [siteName, setSiteName] = useState('Unkown');
+    const [floorName, setFloorName] = useState('Unkown');
+    const [buildingName, setBuildingName] = useState('Unkown');
+
+    componentDidMount(() => {
+        fetchSingleSite(drawing.siteID)
+            .then(({ payload }) => {
+                if (payload.name) {
+                    setSiteName(payload.name);
+                }
+            })
+            .catch(error => console.log(error));
+
+        fetchSingleBuilding(drawing.buildingID)
+            .then(({ payload }) => {
+                if (payload.name) {
+                    setBuildingName(payload.name);
+                }
+            })
+            .catch(error => console.log(error));
+
+        fetchSingleFloor(drawing.floorID)
+            .then(({ payload }) => {
+                if (payload.name) {
+                    setFloorName(payload.name);
+                }
+            })
+            .catch(error => console.log(error));
+    });
     return (
         <UserDrawingListItem
             drawing={drawing}
             colCount={colCount}
             checkedDrawings={checkedDrawings}
             handleDrawingIDs={handleDrawingIDs}
+            siteName={siteName}
+            buildingName={buildingName}
+            floorName={floorName}
         />
     );
 };
 
-const mapDispatchToProps = { showModal, hideModal, editCompanyUserType };
-
+const mapDispatchToProps = dispatch => ({
+    showModal: (modalType, modalProps) => {
+        dispatch(showModal(modalType, modalProps));
+    },
+    hideModal: () => {
+        dispatch(hideModal());
+    },
+    editCompanyUserType: (companyUserID, postBody) => {
+        dispatch(editCompanyUserType(companyUserID, postBody));
+    },
+    fetchSingleSite: id => {
+        return dispatch(fetchSingleSite(id));
+    },
+    fetchSingleBuilding: id => {
+        return dispatch(fetchSingleBuilding(id));
+    },
+    fetchSingleFloor: id => {
+        return dispatch(fetchSingleFloor(id));
+    }
+});
+const mapStateToProps = ({
+    companyAdmin: {
+        sitesReducer: { sites, isFetching: fetchingSites },
+        buildingsReducer: { buildings, isFetching: fetchingBuildings },
+        floorsReducer: { floors, isFetching: fetchingFloors },
+        userDrawingsReducer: { isFetching: fetchingDrawings }
+    }
+}) => {
+    // const drawing = drawings[match.params.id] || {};
+    // const floor = floors[drawing.floorID] || {};
+    // const building = buildings[floor.buildingID] || {};
+    // const site = sites[building.siteID] || {};
+    return {
+        sites: sites || {},
+        buildings: buildings || {},
+        floors: floors || {},
+        fetchingDrawings,
+        isFetching: fetchingFloors || fetchingBuildings || fetchingSites
+    };
+};
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(UserDrawingListItemContainer);
