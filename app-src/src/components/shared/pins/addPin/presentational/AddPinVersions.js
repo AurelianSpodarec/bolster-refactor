9ec204a1537580e9ed384eb4c1fc95971d4097fe
Tom Duthie <tom@silverchip.com>
2019-07-08 16:54:35 +0100

    front end mobile button

--- app-src/src/components/shared/pins/addPin/presentational/AddPinVersions.js
+++ app-src/src/components/shared/pins/addPin/presentational/AddPinVersions.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import AddPinSectionsContainer from '../containers/AddPinSectionsContainer';
 
-const AddPinVersions = ({ selectedVersion }) => (
+const AddPinVersions = ({ selectedVersion, isHistory }) => (
     <div className="size-lg-12">
         {selectedVersion && (
             <AddPinSectionsContainer
                 selectedVersionID={selectedVersion.id}
                 selectedVersion={selectedVersion}
+                isHistory={isHistory}
             />
         )}
     </div>
