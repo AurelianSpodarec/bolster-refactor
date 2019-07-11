b00edad84ef655a5ff5eddb56a57350a3600c75b
Emma Randall <emma@silverchip.com>
2019-07-11 11:17:28 +0100

    mirror lazy loading functionality in client area pin single page

--- app-src/src/reducers/client/clientPinHistories.js
+++ app-src/src/reducers/client/clientPinHistories.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
 import { convertArrToObj } from 'helpers/generic';
-import { CLIENT_FETCH_SINGLE_PIN_SUCCESS } from 'constants/client/actionTypes/clientPins';
+import {
+    CLIENT_FETCH_SINGLE_PIN_SUCCESS,
+    CLIENT_FETCH_SINGLE_PIN_REQUEST
+} from 'constants/client/actionTypes/clientPins';
 
 export default combineReducers({
     histories: historiesReducer
@@ -9,6 +12,8 @@ export default combineReducers({
 
 function historiesReducer(state = {}, action) {
     switch (action.type) {
+        case CLIENT_FETCH_SINGLE_PIN_REQUEST:
+            return {};
         case CLIENT_FETCH_SINGLE_PIN_SUCCESS:
             return convertArrToObj(action.payload.histories);
         default:
