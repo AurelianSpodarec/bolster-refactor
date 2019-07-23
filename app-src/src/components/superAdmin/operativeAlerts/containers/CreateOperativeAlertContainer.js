0e1fa3b2e664ebb2b6d4cf8b916ae5705181dde6
Rob Fairclough <rob@silverchip.com>
2019-07-23 17:14:43 +0100

    create admin operative alert

--- app-src/src/components/superAdmin/operativeAlerts/containers/CreateOperativeAlertContainer.js
+++ app-src/src/components/superAdmin/operativeAlerts/containers/CreateOperativeAlertContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CreateOperativeAlert from '../presentational/CreateOperativeAlert';
@@ -6,20 +7,21 @@ import adminCreateOperativeAlert from 'actions/superAdmin/operativeAlerts/async/
 import { showModal } from 'actions/shared/generic/modals/sync/showModal';
 
 class CreateOperativeAlertContainer extends Component {
-    state = { message: '' };
+    state = { message: '', sending: false };
 
     render = () => (
         <CreateOperativeAlert
             message={this.state.message}
             handleChange={this.handleChange}
             handleSubmit={this.handleSubmit}
+            sending={this.state.sending}
         />
     );
 
     componentDidUpdate = prevProps => {
-        const { postSuccess, error } = this.props;
+        const { postSuccess, error, history } = this.props;
         if (postSuccess && !prevProps.postSuccess) {
-            // todo handle success
+            history.push('/admin/operative-alerts');
         }
         if (error && !prevProps.error) {
             //  todo handle error
@@ -29,18 +31,32 @@ class CreateOperativeAlertContainer extends Component {
     handleChange = (name, value) => this.setState({ [name]: value });
 
     handleSubmit = () => {
-        const { message } = this.state;
-        const { adminCreateOperativeAlert } = this.props;
+        const { message, sending } = this.state;
+        if (!sending) {
+            this.setState({ sending: true });
+            const { adminCreateOperativeAlert } = this.props;
 
-        const postBody = { message };
+            const postBody = { message };
 
-        adminCreateOperativeAlert(postBody);
+            adminCreateOperativeAlert(postBody);
+        }
     };
 }
 
+const mapStateToProps = ({
+    superAdmin: {
+        operativeAlertsReducer: { postSuccess, error }
+    }
+}) => ({
+    postSuccess,
+    error
+});
+
 const mapDispatchToProps = { adminCreateOperativeAlert, showModal };
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(CreateOperativeAlertContainer);
+export default withRouter(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(CreateOperativeAlertContainer)
+);
