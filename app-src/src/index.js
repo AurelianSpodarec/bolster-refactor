865730d9c037de6f95fa1c3f9928005694486d42
Tom Duthie <tom@silverchip.com>
2019-07-25 16:02:02 +0100

    polyfill fix attempt again

--- app-src/src/index.js
+++ app-src/src/index.js
@@ -1,5 +1,6 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
+import 'core-js';
+// polyfill only stable `core-js` features - ES and web standards:
+import 'core-js/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
