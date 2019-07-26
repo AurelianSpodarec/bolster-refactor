bcee3bd8f8def2f5a0ee8bc94da27fb4e3e38a8b
Tom Duthie <tom@silverchip.com>
2019-07-26 12:38:20 +0100

    made polyfill file with new object entries fix

--- app-src/src/index.js
+++ app-src/src/index.js
@@ -1,9 +1,4 @@
-import 'react-app-polyfill/ie11';
-import 'core-js/features/array/find';
-import 'core-js/features/array/includes';
-import 'core-js/features/number/is-nan';
-import 'es7-object-polyfill';
-import 'airbnb-js-shims';
+import 'config/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
