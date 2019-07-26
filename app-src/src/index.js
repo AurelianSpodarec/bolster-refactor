0c3b276d6151fb0ef777f711910a8c9ba9b203ad
Tom Duthie <tom@silverchip.com>
2019-07-26 09:37:12 +0100

    pollyfill fix proper branching

--- app-src/src/index.js
+++ app-src/src/index.js
@@ -1,3 +1,8 @@
+import 'react-app-polyfill/ie11';
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';
+import 'es7-object-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
