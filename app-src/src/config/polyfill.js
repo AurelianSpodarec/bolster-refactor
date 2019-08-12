c5bacf558b2be0d41ea16c16ea4985e06a300676
Tom Duthie <tom@silverchip.com>
2019-08-12 14:54:44 +0100

    new polyfill again

--- app-src/src/config/polyfill.js
+++ app-src/src/config/polyfill.js
@@ -1,16 +1,6 @@
+import 'config/objectEntriesIE';
+import 'es7-object-polyfill';
 import 'react-app-polyfill/ie11';
 import 'core-js/features/array/find';
 import 'core-js/features/array/includes';
 import 'core-js/features/number/is-nan';
-import 'es7-object-polyfill';
-
-if (!Object.entries) {
-    Object.entries = function(obj) {
-        var ownProps = Object.keys(obj),
-            i = ownProps.length,
-            resArray = new Array(i); // preallocate the Array
-        while (i--) resArray[i] = [ownProps[i], obj[ownProps[i]]];
-
-        return resArray;
-    };
-}
