8932d525c7b314bbcc450fe5ce8fc2aaa6a3245f
Jake Juluch <jake@silverchip.com>
2019-09-04 14:21:34 +0100

    Config flip

--- app-src/src/config/index.js
+++ app-src/src/config/index.js
@@ -1,29 +1,29 @@
 // - Staging
-// export const API_URL = 'https://api.company.bolster-staging.com';
-// export const CLIENT_API_URL = 'https://api.client.bolster-staging.com';
-// export const FILE_API_URL = 'https://api.media.bolster-staging.com/files';
-// export const ADMIN_API_URL = 'https://api.admin.bolster-staging.com';
-// export const AUTH_API_URL = 'https://api.accounts.bolster-staging.com';
-// export const FRONTEND_API_URL = 'http://api.front-end.bolster-staging.com';
-// export const RENEWALS_API_URL = 'http://api.renewals.bolster-staging.com';
-//
-// export const FILE_STORAGE_URL = 'https://dizelaxol0ewg.cloudfront.net';
-// export const RAW_S3_STORAGE_URL = 'https://s3-eu-west-1.amazonaws.com/bolster-media';
-//
-// export const REPORT_VIEWER_URL =
-//     'http://bolsterdynamiclive-report-viewer.eu-west-1.elasticbeanstalk.com/1';
-
-// - Local Dev
-export const API_URL = 'https://localhost:5021';
-export const CLIENT_API_URL = 'https://localhost:5051';
-export const FILE_API_URL = 'https://localhost:5031/files';
-export const ADMIN_API_URL = 'https://localhost:5011';
-export const AUTH_API_URL = 'https://localhost:5001';
-export const FRONTEND_API_URL = 'https://localhost:5091';
-export const RENEWALS_API_URL = 'https://localhost:5081';
+export const API_URL = 'https://api.company.bolster-staging.com';
+export const CLIENT_API_URL = 'https://api.client.bolster-staging.com';
+export const FILE_API_URL = 'https://api.media.bolster-staging.com/files';
+export const ADMIN_API_URL = 'https://api.admin.bolster-staging.com';
+export const AUTH_API_URL = 'https://api.accounts.bolster-staging.com';
+export const FRONTEND_API_URL = 'http://api.front-end.bolster-staging.com';
+export const RENEWALS_API_URL = 'http://api.renewals.bolster-staging.com';
 
 export const FILE_STORAGE_URL = 'https://dizelaxol0ewg.cloudfront.net';
 export const RAW_S3_STORAGE_URL = 'https://s3-eu-west-1.amazonaws.com/bolster-media';
 
 export const REPORT_VIEWER_URL =
-    'https://localhost:6001/1';
+    'http://bolsterdynamiclive-report-viewer.eu-west-1.elasticbeanstalk.com/1';
+
+// - Local Dev
+// export const API_URL = 'https://localhost:5021';
+// export const CLIENT_API_URL = 'https://localhost:5051';
+// export const FILE_API_URL = 'https://localhost:5031/files';
+// export const ADMIN_API_URL = 'https://localhost:5011';
+// export const AUTH_API_URL = 'https://localhost:5001';
+// export const FRONTEND_API_URL = 'https://localhost:5091';
+// export const RENEWALS_API_URL = 'https://localhost:5081';
+//
+// export const FILE_STORAGE_URL = 'https://dizelaxol0ewg.cloudfront.net';
+// export const RAW_S3_STORAGE_URL = 'https://s3-eu-west-1.amazonaws.com/bolster-media';
+//
+// export const REPORT_VIEWER_URL =
+//     'https://localhost:6001/1';
