620b802542967bbe7120b257f66b2ee57fff69af
Mark Baker <mark@silverchip.com>
2019-09-16 11:10:41 +0100

    Created initial routes and page stuff for recently deleted area

---
+++ app-src/src/components/companyAdmin/recentlyDeleted/recentlyDeleted/containers/RecentlyDeletedContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import RecentlyDeleted from '../presentational/RecentlyDeleted';
+
+class RecentlyDeletedContainer extends Component {
+    render() {
+        return <RecentlyDeleted />;
+    }
+}
+
+export default RecentlyDeletedContainer;
---
+++ app-src/src/components/companyAdmin/recentlyDeleted/recentlyDeleted/presentational/RecentlyDeleted.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
+
+const RecentlyDeleted = () => (
+    <>
+        <PageHeading title="Recently Deleted" />
+    </>
+);
+
+export default RecentlyDeleted;
