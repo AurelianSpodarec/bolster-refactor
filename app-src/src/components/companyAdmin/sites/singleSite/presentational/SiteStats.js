93d39116951e7040d6369d0364d4b1e707e24e98
Mark Baker <mark@silverchip.com>
2019-09-23 16:29:42 +0100

    Updated archive button on hierarchies to always be visible

--- app-src/src/components/companyAdmin/sites/singleSite/presentational/SiteStats.js
+++ app-src/src/components/companyAdmin/sites/singleSite/presentational/SiteStats.js
@@ -5,14 +5,7 @@ import PieChart from 'components/shared/stats/presentational/PieChart';
 import SiteDetails from './SiteDetails';
 import { ACCESS_TYPES_VALUES } from 'constants/companyAdmin/enums';
 
-const SiteStats = ({
-    site,
-    stats,
-    handleDelete,
-    handleArchive,
-    onMobile,
-    handleEditSiteModal
-}) => (
+const SiteStats = ({ site, stats, handleDelete, handleArchive, onMobile, handleEditSiteModal }) => (
     <div className="stats size-lg-12">
         <div className="flex-item size-lg-12">
             <SiteDetails site={site} stats={stats} />
@@ -24,35 +17,26 @@ const SiteStats = ({
             />
         </div>
 
-        {site.accessType === ACCESS_TYPES_VALUES.OWNER && (
-            <div className="button-container">
-                <button
-                    className="button red"
-                    onClick={handleDelete}
-                    type="button"
-                >
-                    <i className="far fa-trash-alt fa-fw" /> Delete
-                </button>
-                <button className="button yellow" onClick={handleEditSiteModal}>
-                    <i className="far fa-pencil fa-fw" /> Edit
-                </button>
+        <div className="button-container">
+            {site.accessType === ACCESS_TYPES_VALUES.OWNER && (
+                <>
+                    <button className="button red" onClick={handleDelete} type="button">
+                        <i className="far fa-trash-alt fa-fw" /> Delete
+                    </button>
+                    <button className="button yellow" onClick={handleEditSiteModal}>
+                        <i className="far fa-pencil fa-fw" /> Edit
+                    </button>
+                    <Link className="button" to={`/company/sites/${site.id}/change-ownership`}>
+                        <i className="fa fa-exchange" /> Change Ownership
+                    </Link>
+                </>
+            )}
 
-                <button
-                    className="button blue"
-                    onClick={handleArchive}
-                    type="button"
-                >
-                    <i className="fa fa-archive" />
-                    {site.isArchived ? 'Un-Archive' : 'Archive'}
-                </button>
-                <Link
-                    className="button"
-                    to={`/company/sites/${site.id}/change-ownership`}
-                >
-                    <i className="fa fa-exchange" /> Change Ownership
-                </Link>
-            </div>
-        )}
+            <button className="button blue" onClick={handleArchive} type="button">
+                <i className="fa fa-archive" />
+                {site.isArchived ? 'Un-Archive' : 'Archive'}
+            </button>
+        </div>
     </div>
 );
 
