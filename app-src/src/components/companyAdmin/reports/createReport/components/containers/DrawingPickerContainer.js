4ffbe8e0d82aa8518ca277471455e7ee7b2c7f03
Tom <bob@silverchip.com>
2019-09-27 16:49:11 +0100

    Drawings picker in progress

--- app-src/src/components/companyAdmin/reports/createReport/components/containers/DrawingPickerContainer.js
+++ app-src/src/components/companyAdmin/reports/createReport/components/containers/DrawingPickerContainer.js
@@ -1,29 +1,67 @@
-import React, { Component, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+
+import { HIERARCHY_IDS } from 'constants/companyAdmin/enums';
 
 import DrawingPicker from '../presentational/DrawingPicker';
 
 import withUpdateOnChange from '../hocs/withUpdateOnChange';
 
 const DrawingPickerContainer = ({ siteID, buildingID, floorID, drawings }) => {
-    function availableDrawings() {
+    const [includedDrawings, setIncludeDrawing] = useState([]);
+    const [excludedDrawings, setExcludeDrawing] = useState([]);
+
+    function usePrevious(value) {
+        const ref = useRef();
+        useEffect(() => {
+            ref.current = value;
+        });
+        return ref.current;
+    }
+
+    const prevHierachyID = usePrevious({ siteID, buildingID, floorID });
+
+    useEffect(() => {
+        setExcludeDrawing(availableDrawings());
+    }, [siteID, buildingID, floorID]);
+
+    return (
+        <DrawingPicker
+            handleIncludeDrawing={handleIncludeDrawing}
+            excludedDrawings={excludedDrawings}
+            includedDrawings={includedDrawings}
+            handleExcludeDrawing={handleExcludeDrawing}
+        />
+    );
+
+    function availableDrawings(hierarchyID, hierarchyTypeID) {
         const allDrawings = Object.values(drawings);
 
-        if (siteID) {
-            return allDrawings.filter(drawing => drawing.siteID.toString() === siteID);
+        if (hierarchyTypeID === HIERARCHY_IDS.SITE) {
+            return allDrawings.filter(drawing => drawing.siteID.toString() === hierarchyID);
         }
-        if (buildingID) {
-            return allDrawings.filter(drawing => drawing.buildingID.toString() === buildingID);
+        if (hierarchyTypeID === HIERARCHY_IDS.BUILDING) {
+            return allDrawings.filter(drawing => drawing.buildingID.toString() === hierarchyID);
         }
-        if (floorID) {
-            return allDrawings.filter(drawing => drawing.floorID.toString() === floorID);
+        if (hierarchyTypeID === HIERARCHY_IDS.FLOOR) {
+            return allDrawings.filter(drawing => drawing.floorID.toString() === hierarchyID);
         }
 
         return [];
     }
 
-    return <DrawingPicker drawings={availableDrawings()} />;
+    //get drawing and select the proper hierarchyID to get the correct floor.
+
+    function handleIncludeDrawing(e, drawingID) {
+        e.preventDefault();
+        setIncludeDrawing(drawingID);
+    }
+
+    function handleExcludeDrawing(e, drawingID, hierarchyTypeID) {
+        e.preventDefault();
+        setExcludeDrawing(drawingID);
+    }
 };
 
 //get each drawing for each level
--- app-src/src/components/companyAdmin/reports/createReport/components/presentational/DrawingPicker.js
+++ app-src/src/components/companyAdmin/reports/createReport/components/presentational/DrawingPicker.js
@@ -2,13 +2,31 @@ import React from 'react';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 
-const DrawingPicker = ({ drawings }) => (
+const DrawingPicker = ({
+    excludedDrawings,
+    includedDrawings,
+    handleExcludeDrawing,
+    handleIncludeDrawing
+}) => (
     <>
         <BlockContainer>
             <BlockHeading title="Drawing Container" />
-            {drawings.map(drawing => (
-                <p key={drawing.id}>{drawing.name}</p>
-            ))}
+            <div className="size-lg-6">
+                <p>Excluded</p>
+                {excludedDrawings.map(drawing => (
+                    <p onClick={e => handleExcludeDrawing(e)} key={drawing.id}>
+                        {drawing.name}
+                    </p>
+                ))}
+            </div>
+            <div className="size-lg-6">
+                <p>Included</p>
+                {includedDrawings.map(drawing => (
+                    <p key={drawing.id} onClick={e => handleIncludeDrawing(e)}>
+                        {drawing.name}
+                    </p>
+                ))}
+            </div>
         </BlockContainer>
     </>
 );
