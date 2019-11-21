a2bbef82034c8a8b09001cc573b7ce56c6428488
Rob Fairclough <rob@silverchip.com>
2019-11-21 13:38:38 +0000

    fix handlechange naming

--- app-src/src/components/shared/pins/addPin/fieldTypes/MultiPhoto.js
+++ app-src/src/components/shared/pins/addPin/fieldTypes/MultiPhoto.js
@@ -2,18 +2,15 @@ import React from 'react';
 import FileUploadContainer from 'components/shared/generic/form/containers/FileUploadContainer';
 import { RAW_S3_STORAGE_URL } from 'config';
 
-
-
-
 const MultiPhoto = ({
     isRequired,
     question: { maxPhotos, id },
     answers,
-    handleFileChange,
+    hanldeChange,
     handleImageClick,
     edit
-}) => {
-    return edit ? (
+}) => 
+    edit ? (
         <div>
             {(answers[id] || []).map(src => (
                 <img
@@ -34,10 +31,9 @@ const MultiPhoto = ({
             required={isRequired}
             acceptedTypes={['image/*']}
             maxFiles={maxPhotos ? maxPhotos : 25}
-            handleChange={handleFileChange}
+            handleChange={hanldeChange}
             value={answers[id]}
         />
     );
-};
 
 export default MultiPhoto;
