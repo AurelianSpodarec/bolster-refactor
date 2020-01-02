2db06d5d5c4f50bdde2974e6d486c9ac59259fa6
Tom <bob@silverchip.com>
2020-01-02 14:40:25 +0000

    IE script attempt

--- app-src/src/components/appRoute/app/containers/AppContainer.js
+++ app-src/src/components/appRoute/app/containers/AppContainer.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import _ from 'lodash';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
+import { withRouter } from "react-router-dom";
 
-import App from '../presentational/App';
+import App from "../presentational/App";
 
-import setMobileWidth from 'actions/shared/generic/mobile/sync/setMobileWidth';
+import setMobileWidth from "actions/shared/generic/mobile/sync/setMobileWidth";
 
 class AppContainer extends Component {
     render() {
@@ -13,7 +13,12 @@ class AppContainer extends Component {
     }
     componentDidMount = () => {
         this.checkMobileWidth();
-        window.addEventListener('resize', this.checkMobileWidth);
+        window.addEventListener("resize", this.checkMobileWidth);
+        console.log(this.isIE10());
+        console.log(this.isIE10());
+        console.log(this.isIE10());
+        console.log(this.isIE10());
+        console.log(this.isIE10());
     };
 
     //effects onMobile reducer
@@ -26,15 +31,21 @@ class AppContainer extends Component {
             setMobileWidth(false);
         }
     }, 1000);
+
+    isIE10 = () => {
+        let isIE10 = false;
+        /*@cc_on
+            if (/^10/.test(@_jscript_version)) {
+                isIE10 = true;
+            }
+        @*/
+
+        return isIE10;
+    };
 }
 
 const mapDispatchToProps = dispatch => ({
     setMobileWidth: isMobile => dispatch(setMobileWidth(isMobile))
 });
 
-export default withRouter(
-    connect(
-        null,
-        mapDispatchToProps
-    )(AppContainer)
-);
+export default withRouter(connect(null, mapDispatchToProps)(AppContainer));
