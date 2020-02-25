403948f2d9c77dd036f60be6000d9208b871c048
Mark Baker <mark@silverchip.com>
2020-02-25 16:55:26 +0000

    qr code added to pin single

--- app-src/src/components/companyAdmin/pins/singlePin/presentational/PinQRCode.js
+++ app-src/src/components/companyAdmin/pins/singlePin/presentational/PinQRCode.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import QRCode from 'qrcode.react';
+
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 
@@ -7,6 +9,14 @@ const PinQRCode = ({ pin }) => {
 
     return <BlockContainer>
         <BlockHeading classes="underline-full" title='QR Code' />
+        <div className="qr-container">
+            <QRCode value={pin.qrCode} size={90} />
+            <p>
+                This pin has a QR code attached.
+                <br /><br />
+                Scan this code on the Bolster app to view it on there.
+            </p>
+        </div>
     </BlockContainer>;
 };
 
