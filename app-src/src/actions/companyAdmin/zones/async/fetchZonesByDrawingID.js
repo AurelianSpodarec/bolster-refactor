e7fe483dfd2104af7ec93f6ad7b2b451e9e8afbb
Oliver <oli@silverchip.com>
2020-03-13 14:12:49 +0000

    add action to fetch zones

---
+++ app-src/src/actions/companyAdmin/zones/async/fetchZonesByDrawingID.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+import {
+    FETCH_DRAWING_ZONES_REQUEST,
+    FETCH_DRAWING_ZONES_SUCCESS,
+    FETCH_DRAWING_ZONES_FAILURE
+} from 'constants/actionTypes/zones';
+
+import { API_URL } from 'config';
+import { getHeaders, handleErrors } from 'helpers/api';
+
+export const createDrawingZoneRequest = () => ({
+    type: FETCH_DRAWING_ZONES_REQUEST
+});
+
+export const createDrawingZoneSuccess = payload => ({
+    type: FETCH_DRAWING_ZONES_SUCCESS,
+    payload
+});
+
+export const createDrawingZoneFailure = error => ({
+    type: FETCH_DRAWING_ZONES_FAILURE,
+    error
+});
+
+export default (drawingID, postBody) => dispatch => {
+    dispatch(createDrawingZoneRequest());
+
+    return axios
+        .post(`${API_URL}/drawings/${drawingID}/zones`, postBody, getHeaders())
+        .then(({ data }) => dispatch(createDrawingZoneSuccess(data)))
+        .catch(err => dispatch(handleErrors(createDrawingZoneFailure)(err)));
+};
