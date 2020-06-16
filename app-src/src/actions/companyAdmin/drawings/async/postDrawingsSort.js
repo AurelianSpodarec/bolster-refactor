fd6a2d12614de1392aebbffd6cf162f45dd48d17
Mark Baker <mark@silverchip.com>
2020-06-16 10:38:47 +0100

    fix for drawings sort

--- app-src/src/actions/companyAdmin/drawings/async/postDrawingsSort.js
+++ app-src/src/actions/companyAdmin/drawings/async/postDrawingsSort.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-import { POST_DRAWINGS_SORT } from 'constants/actionTypes/floors';
+import { POST_DRAWINGS_SORT } from 'constants/actionTypes/drawings';
 import { API_URL } from 'config/index';
 import { getHeaders } from 'helpers/api';
 import fetchAllDrawings from './fetchAllDrawings';
 
-export default floors => dispatch => {
+export default drawings => dispatch => {
     dispatch({ type: POST_DRAWINGS_SORT });
 
     axios
         .post(
-            `${API_URL}/floors/sort`,
-            { items: floors.map(({ id, sort }) => ({ id, sort })) },
-            getHeaders()
+            `${API_URL}/drawings/sort`,
+            { items: drawings.map(({ id, sort }) => ({ id, sort })) },
+            getHeaders(),
         )
         .catch(() => dispatch([fetchAllDrawings]()));
 };
