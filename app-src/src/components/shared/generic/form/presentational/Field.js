37873b74b8f9b2996f64983bb8bb26286c434ef9
Mark Baker <mark@silverchip.com>
2020-06-16 11:00:06 +0100

    added message for gb vat codes on register page

--- app-src/src/components/shared/generic/form/presentational/Field.js
+++ app-src/src/components/shared/generic/form/presentational/Field.js
@@ -3,19 +3,18 @@ import React from 'react';
 const Field = ({
     children,
     name = '',
+    smallDesc = null,
     sizeClasses = 'size-lg-12',
     classes = '',
     required = false,
     htmlFor,
-    styles = {}
+    styles = {},
 }) => (
-    <div
-        className={`form-field ${sizeClasses} ${classes} `}
-        style={{ ...styles }}
-    >
+    <div className={`form-field ${sizeClasses} ${classes} `} style={{ ...styles }}>
         {name && name.length && (
             <label className="title" htmlFor={htmlFor}>
-                {name} {required && <sub>*</sub>}
+                {name} <span className="small">{smallDesc ? smallDesc : ''}</span>{' '}
+                {required && <sub>*</sub>}
             </label>
         )}
 
