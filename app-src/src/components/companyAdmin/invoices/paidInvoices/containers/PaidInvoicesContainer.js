1c788311de161f7e47c45afe7ce1ba96ee2e529b
Oluseun Adesina <seun@silverchip.com>
2020-07-24 11:58:10 +0100

    sorted paid invoices by latest date

--- app-src/src/components/companyAdmin/invoices/paidInvoices/containers/PaidInvoicesContainer.js
+++ app-src/src/components/companyAdmin/invoices/paidInvoices/containers/PaidInvoicesContainer.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import InvoicesTableContainer from 'components/companyAdmin/invoices/shared/invoiceListTable/containers/InvoicesTableContainer';
+import { sortArrayByField } from 'helpers/generic';
 
 const PaidInvoicesContainer = ({ error, isFetching, paidInvoices }) => (
     <BlockContainer heading="Payments">
@@ -11,7 +12,10 @@ const PaidInvoicesContainer = ({ error, isFetching, paidInvoices }) => (
 );
 
 const mapStateToProps = ({ companyAdmin: { invoicesReducer } }) => ({
-    paidInvoices: Object.values(invoicesReducer.invoices).filter(({ isPaid }) => isPaid)
+    paidInvoices: sortArrayByField(
+        Object.values(invoicesReducer.invoices).filter(({ isPaid }) => isPaid),
+        'createdOn',
+    ),
 });
 
 export default connect(mapStateToProps)(PaidInvoicesContainer);
