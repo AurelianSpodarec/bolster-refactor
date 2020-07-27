e88c3329939b7e68eb58e5d8fa8fbb0f62f7caf3
Rob Fairclough <rob@silverchip.com>
2020-07-27 09:29:18 +0100

    remove logs

--- app-src/src/components/shared/dateTime/containers/DateTimeContainer.js
+++ app-src/src/components/shared/dateTime/containers/DateTimeContainer.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import moment from 'moment';
 import Moment from 'react-moment';
 import 'moment-timezone';
 import { connect } from 'react-redux';
 import { DATE_TIMES, DATE_TIME_IDS, DATE_TIME_DEFAULTS } from 'constants/companyAdmin/enums';
 
 const DateTimeContainer = ({ date, timeZone, dateFormat, className = '' }) => {
-    let dateTime = moment(date).tz(timeZone);
-    if (dateTime.isDST()) {
-        console.error('hi');
-    } else {
-        console.error('ho');
-    }
     return (
         <Moment tz={timeZone} format={dateFormat} date={date + '+00:00'} className={className} />
     );
