634d1c1418ab95613b3bb10760def5657e41ada4
Mark Baker <mark@silverchip.com>
2020-07-27 11:54:27 +0100

    fix height for qr code box

--- app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/ViewZonesModal.js
+++ app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/ViewZonesModal.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import QRCode from 'qrcode.react';
+import React from "react";
+import QRCode from "qrcode.react";
 
-import ModalOuterContainer from 'components/shared/generic/modals/containers/ModalOuterContainer';
-import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
-import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
-import ZoneListItem from './ZoneListItem';
-import ModalOuter from 'components/shared/generic/modals/presentational/ModalOuter';
+import ModalOuterContainer from "components/shared/generic/modals/containers/ModalOuterContainer";
+import BlockHeading from "components/shared/generic/blockHeading/presentational/BlockHeading";
+import BlockButtonWrapper from "components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper";
+import ZoneListItem from "./ZoneListItem";
+import ModalOuter from "components/shared/generic/modals/presentational/ModalOuter";
 
-const ViewZonesModal = ({ hideModal, addZone, zonesArr, selectedQR, selectQR }) => (
-    <ModalOuterContainer>
+const ViewZonesModal = ({
+    hideModal,
+    addZone,
+    zonesArr,
+    selectedQR,
+    selectQR,
+}) => (
+    <ModalOuterContainer extraClasses="zone-modal">
         <BlockHeading title="View zones"></BlockHeading>
 
-        {zonesArr.length ?
+        {zonesArr.length ? (
             <div className="zones-list size-lg-12">
                 <div className="headings size-lg-12">
                     <p className="item size-lg-6">
@@ -25,17 +31,31 @@ const ViewZonesModal = ({ hideModal, addZone, zonesArr, selectedQR, selectQR }) 
                     </p>
                 </div>
 
-                {zonesArr.map(zone => <ZoneListItem key={zone.id} zone={zone} selectQR={selectQR} />)}
-            </div> : <p className="generic-text no-data size-lg-12">No zones were found</p>
-        }
+                {zonesArr.map((zone) => (
+                    <ZoneListItem
+                        key={zone.id}
+                        zone={zone}
+                        selectQR={selectQR}
+                    />
+                ))}
+            </div>
+        ) : (
+            <p className="generic-text no-data size-lg-12">
+                No zones were found
+            </p>
+        )}
 
-        {selectedQR && <ModalOuter hideCloseButton extraClasses='qr-view'>
-            <QRCode value={selectedQR} size={200} />
-            <button className='button grey pull-right' onClick={() => selectQR(null)}>
-                Close
-            </button>
-        </ModalOuter>}
-
+        {selectedQR && (
+            <ModalOuter hideCloseButton extraClasses="qr-view">
+                <QRCode value={selectedQR} size={200} />
+                <button
+                    className="button grey pull-right"
+                    onClick={() => selectQR(null)}
+                >
+                    Close
+                </button>
+            </ModalOuter>
+        )}
 
         <BlockButtonWrapper>
             <button className="button green" onClick={addZone}>
