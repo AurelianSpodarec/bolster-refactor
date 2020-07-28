088ba1a0acf9da9674d72b2dc1628e2e3d29f351
Mark Baker <mark@silverchip.com>
2020-07-28 15:53:35 +0100

    add field text for sort options dropdown

--- app-src/src/components/companyAdmin/dropdownOptions/allDropdownOptions/presentational/DropdownListSortOrder.js
+++ app-src/src/components/companyAdmin/dropdownOptions/allDropdownOptions/presentational/DropdownListSortOrder.js
@@ -3,16 +3,21 @@ import React from 'react';
 import Dropdown from 'components/shared/generic/form/presentational/Dropdown';
 
 const DropdownListSortOrder = ({ options, selectedValue, handleChangeSortOrder }) => (
-    <div className="pull-right" style={{ width: '200px', marginRight: '10px' }}>
-        <Dropdown
-            name="sortOrder"
-            value={selectedValue}
-            options={Object.values(options)}
-            selectedOption={options[selectedValue]}
-            handleChange={handleChangeSortOrder}
-            withoutPlaceholder
-        />
-    </div>
+    <>
+        <div className="pull-right" style={{ width: '200px', marginRight: '10px' }}>
+            <Dropdown
+                name="sortOrder"
+                value={selectedValue}
+                options={Object.values(options)}
+                selectedOption={options[selectedValue]}
+                handleChange={handleChangeSortOrder}
+                withoutPlaceholder
+            />
+        </div>
+        <p className="pull-right" style={{ padding: '7px 5px 0 0' }}>
+            Sort by:
+        </p>
+    </>
 );
 
 export default DropdownListSortOrder;
