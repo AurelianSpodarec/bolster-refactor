87bfd29c173527b40f8e5c38c91216b3a6ead862
Rob Fairclough <rob@silverchip.com>
2020-08-18 15:54:35 +0100

    send log post
    Former-commit-id: bf939d8d585a1c4f9fb5f0ba5d6793ab03dd7acd

--- app-src/src/components/shared/generic/modals/containers/OAndMTsAndCsModalContainer.js
+++ app-src/src/components/shared/generic/modals/containers/OAndMTsAndCsModalContainer.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import OAndMTsAndCsModal from '../presentational/OAndMTsAndCsModal';
 import { hideOAndMTsAndCsModal } from 'actions/shared/generic/modals/sync/hideOAndMTsAndCsModal';
+import { API_URL } from 'config';
+import { getHeaders } from 'helpers/api';
 
 // ! This works seperately to the regular modals.
 
@@ -13,9 +16,15 @@ const OAndMTsAndCsModalContainer = () => {
     }
     return null;
 
-    function handleAccept() {
-        // todo api call
-        const message = modalReducer.oAndMTsAndCsMessage;
+    async function handleAccept() {
+        const logMessage = modalReducer.oAndMTsAndCsMessage;
+        const postBody = { logMessage };
+        try {
+            await axios.post(`${API_URL}/users/company/0`, postBody, getHeaders());
+        } catch {
+            // ignore
+        }
+
         dispatch(hideOAndMTsAndCsModal());
     }
 };
