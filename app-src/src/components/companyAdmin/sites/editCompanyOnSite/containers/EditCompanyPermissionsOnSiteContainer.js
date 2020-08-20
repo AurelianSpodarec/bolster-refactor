22b565ea151cdb54e55b5dec9d9d47d199e62134
Mark Baker <mark@silverchip.com>
2020-08-20 11:20:19 +0100

    add fetch for company permissions

--- app-src/src/components/companyAdmin/sites/editCompanyOnSite/containers/EditCompanyPermissionsOnSiteContainer.js
+++ app-src/src/components/companyAdmin/sites/editCompanyOnSite/containers/EditCompanyPermissionsOnSiteContainer.js
@@ -4,6 +4,7 @@ import { withRouter } from 'react-router-dom';
 
 import EditCompanyPermissionsOnSite from '../presentational/EditCompanyPermissionsOnSite';
 import fetchSingleSite from 'actions/companyAdmin/sites/async/fetchSingleSite';
+import fetchPermissionsForCompanyPermission from 'actions/companyAdmin/companiesPermissions/async/fetchPermissionsForCompanyPermission';
 
 class EditCompanyPermissionsOnSiteContainer extends Component {
     render() {
@@ -13,17 +14,24 @@ class EditCompanyPermissionsOnSiteContainer extends Component {
     }
 
     componentDidMount = () => {
-        const { siteID, fetchSingleSite } = this.props;
+        const {
+            siteID,
+            fetchSingleSite,
+            fetchPermissionsForCompanyPermission,
+            companyID,
+        } = this.props;
         fetchSingleSite(siteID);
+        fetchPermissionsForCompanyPermission('site', siteID, companyID);
     };
 }
 
 const mapStateToProps = ({ companyAdmin: { sitesReducer } }, { match }) => ({
     siteID: match.params.id,
     site: sitesReducer.sites[match.params.id] || {},
+    companyID: match.params.companyID,
 });
 
-const mapDispatchToProps = { fetchSingleSite };
+const mapDispatchToProps = { fetchSingleSite, fetchPermissionsForCompanyPermission };
 export default withRouter(
     connect(mapStateToProps, mapDispatchToProps)(EditCompanyPermissionsOnSiteContainer),
 );
