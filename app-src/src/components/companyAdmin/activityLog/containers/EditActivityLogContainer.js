77cac13db263cf26065ecbf66d71b22710c99edb
Mark Baker <mark@silverchip.com>
2020-08-24 14:52:27 +0100

    add sections and setting items to edit activity log page

--- app-src/src/components/companyAdmin/activityLog/containers/EditActivityLogContainer.js
+++ app-src/src/components/companyAdmin/activityLog/containers/EditActivityLogContainer.js
@@ -1,30 +1,64 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
-import fetchActivityLog from 'actions/companyAdmin/activityLog/async/fetchActivityLog';
+import fetchActivityLogSettings from 'actions/companyAdmin/activityLog/async/fetchActivityLogSettings';
 
 import EditActivityLog from '../presentational/EditActivityLog';
+import { ACTIVITY_LOG_REFERENCE_VALUES } from 'constants/companyAdmin/enums';
 
-const EditActivityLogContainer = ({ fetchActivityLog, logs, isFetching, error }) => {
+const EditActivityLogContainer = ({ fetchActivityLogSettings, settings, isFetching, error }) => {
+    const [form, handleChange] = useState({
+        test: false,
+    });
+
     useEffect(() => {
-        fetchActivityLog();
+        fetchActivityLogSettings();
     }, []);
 
-    return <EditActivityLog logs={logs} isFetching={isFetching} error={error} />;
+    return (
+        <EditActivityLog
+            settings={settings}
+            isFetching={isFetching}
+            error={error}
+            form={form}
+            handleFormChange={handleFormChange}
+            sections={getSections()}
+        />
+    );
+
+    function getSections() {
+        const sections = Object.keys(ACTIVITY_LOG_REFERENCE_VALUES).map(val => {
+            const name = ACTIVITY_LOG_REFERENCE_VALUES[val];
+
+            return {
+                id: +val,
+                name,
+            };
+        });
+
+        return sections;
+    }
+
+    function handleFormChange(name, value) {
+        handleChange({
+            ...form,
+            [name]: value,
+        });
+    }
 };
 
 const mapStateToProps = ({
     companyAdmin: {
-        activityLogReducer: { activityLog, isFetching, error },
+        activityLogReducer: { settings, isFetching, error },
     },
 }) => ({
-    logs: activityLog,
+    settings,
     isFetching,
     error,
 });
 
 const mapDispatchToProps = {
-    fetchActivityLog,
+    fetchActivityLogSettings,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditActivityLogContainer);
--- app-src/src/reducers/companyAdmin/activityLog.js
+++ app-src/src/reducers/companyAdmin/activityLog.js
@@ -4,12 +4,16 @@ import {
     FETCH_ACTIVITY_LOG_REQUEST,
     FETCH_ACTIVITY_LOG_SUCCESS,
     FETCH_ACTIVITY_LOG_FAILURE,
+    FETCH_ACTIVITY_LOG_SETTINGS_REQUEST,
+    FETCH_ACTIVITY_LOG_SETTINGS_SUCCESS,
+    FETCH_ACTIVITY_LOG_SETTINGS_FAILURE,
 } from 'constants/actionTypes/activityLog';
 
 import { convertArrToObj } from 'helpers/generic';
 
 export default combineReducers({
     activityLog: activityLogReducer,
+    settings: settingsReducer,
     isFetching: isFetchingReducer,
     error: errorReducer,
     success: successReducer,
@@ -18,9 +22,12 @@ export default combineReducers({
 function isFetchingReducer(state = false, action) {
     switch (action.type) {
         case FETCH_ACTIVITY_LOG_REQUEST:
+        case FETCH_ACTIVITY_LOG_SETTINGS_REQUEST:
             return true;
         case FETCH_ACTIVITY_LOG_SUCCESS:
         case FETCH_ACTIVITY_LOG_FAILURE:
+        case FETCH_ACTIVITY_LOG_SETTINGS_SUCCESS:
+        case FETCH_ACTIVITY_LOG_SETTINGS_FAILURE:
             return false;
         default:
             return state;
@@ -30,8 +37,10 @@ function isFetchingReducer(state = false, action) {
 function errorReducer(state = null, action) {
     switch (action.type) {
         case FETCH_ACTIVITY_LOG_REQUEST:
+        case FETCH_ACTIVITY_LOG_SETTINGS_REQUEST:
             return null;
         case FETCH_ACTIVITY_LOG_FAILURE:
+        case FETCH_ACTIVITY_LOG_SETTINGS_FAILURE:
             return action.error;
         default:
             return state;
@@ -41,8 +50,10 @@ function errorReducer(state = null, action) {
 function successReducer(state = false, action) {
     switch (action.type) {
         case FETCH_ACTIVITY_LOG_REQUEST:
+        case FETCH_ACTIVITY_LOG_SETTINGS_REQUEST:
             return false;
         case FETCH_ACTIVITY_LOG_SUCCESS:
+        case FETCH_ACTIVITY_LOG_SETTINGS_SUCCESS:
             return true;
         default:
             return state;
@@ -53,6 +64,15 @@ function activityLogReducer(state = {}, action) {
     switch (action.type) {
         case FETCH_ACTIVITY_LOG_SUCCESS:
             return convertArrToObj(action.payload);
+        default:
+            return state;
+    }
+}
+
+function settingsReducer(state = [], action) {
+    switch (action.type) {
+        case FETCH_ACTIVITY_LOG_SETTINGS_SUCCESS:
+            return action.payload;
         default:
             return state;
     }
