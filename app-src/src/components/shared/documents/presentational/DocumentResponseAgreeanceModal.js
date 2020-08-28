e5989b6e7fb6ceac6853c33ec48ceb4ebb5dca4a
Tom <bob@silverchip.com>
2020-08-28 13:53:08 +0100

    doc response pdf file location corrected

--- app-src/src/components/shared/documents/presentational/DocumentResponseAgreeanceModal.js
+++ app-src/src/components/shared/documents/presentational/DocumentResponseAgreeanceModal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ModalOuterContainer from 'components/shared/generic/modals/containers/ModalOuterContainer';
 import FieldOutput from 'components/shared/generic/fieldOutput/presentational/FieldOutput';
-import { FILE_STORAGE_URL } from 'config';
+import { FILE_STORAGE_URL, RAW_S3_STORAGE_URL } from 'config';
 import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
 import DateTimeContainer from 'components/shared/dateTime/containers/DateTimeContainer';
@@ -33,7 +33,7 @@ const DocumentResponseAgreeanceModal = ({ response, hideModal }) => (
                       response.signatureS3Key.endsWith('.pdf') ? (
                         <FieldOutput title="Signature">
                             <ButtonContainer
-                                to={`${FILE_STORAGE_URL}/${response.signatureS3Key}`}
+                                to={`${RAW_S3_STORAGE_URL}/${response.signatureS3Key}`}
                                 isAnchor
                                 className="btn blue"
                                 openNewTab
