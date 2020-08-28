f991781a5751012f227adb10451d9da36a98091a
Rob Fairclough <rob@silverchip.com>
2020-08-28 14:08:01 +0100

    show all user guide versions

--- app-src/src/reducers/superAdmin/userGuide.js
+++ app-src/src/reducers/superAdmin/userGuide.js
@@ -7,6 +7,9 @@ import {
     ADMIN_FETCH_USER_GUIDE_FAILURE,
     ADMIN_FETCH_USER_GUIDE_REQUEST,
     ADMIN_FETCH_USER_GUIDE_SUCCESS,
+    ADMIN_FETCH_ALL_USER_GUIDE_VERSIONS_REQUEST,
+    ADMIN_FETCH_ALL_USER_GUIDE_VERSIONS_FAILURE,
+    ADMIN_FETCH_ALL_USER_GUIDE_VERSIONS_SUCCESS,
 } from 'constants/actionTypes/userGuide';
 
 export default combineReducers({
@@ -14,6 +17,7 @@ export default combineReducers({
     error: errorReducer,
     isFetching: isFetchingReducer,
     userGuide: userGuideReducer,
+    versions: versionsReducer,
 });
 
 function postSuccessReducer(state = false, action) {
@@ -31,9 +35,11 @@ function errorReducer(state = null, action) {
     switch (action.type) {
         case ADMIN_UPLOAD_USER_GUIDE_REQUEST:
         case ADMIN_FETCH_USER_GUIDE_REQUEST:
+        case ADMIN_FETCH_ALL_USER_GUIDE_VERSIONS_REQUEST:
             return null;
         case ADMIN_UPLOAD_USER_GUIDE_FAILURE:
         case ADMIN_FETCH_USER_GUIDE_FAILURE:
+        case ADMIN_FETCH_ALL_USER_GUIDE_VERSIONS_FAILURE:
             return action.error;
         default:
             return state;
@@ -43,9 +49,12 @@ function errorReducer(state = null, action) {
 function isFetchingReducer(state = false, action) {
     switch (action.type) {
         case ADMIN_FETCH_USER_GUIDE_REQUEST:
+        case ADMIN_FETCH_ALL_USER_GUIDE_VERSIONS_REQUEST:
             return true;
         case ADMIN_FETCH_USER_GUIDE_SUCCESS:
         case ADMIN_FETCH_USER_GUIDE_FAILURE:
+        case ADMIN_FETCH_ALL_USER_GUIDE_VERSIONS_SUCCESS:
+        case ADMIN_FETCH_ALL_USER_GUIDE_VERSIONS_FAILURE:
             return false;
         default:
             return state;
@@ -55,6 +64,15 @@ function isFetchingReducer(state = false, action) {
 function userGuideReducer(state = {}, action) {
     switch (action.type) {
         case ADMIN_FETCH_USER_GUIDE_SUCCESS:
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+function versionsReducer(state = [], action) {
+    switch (action.type) {
+        case ADMIN_FETCH_ALL_USER_GUIDE_VERSIONS_SUCCESS:
             return action.payload;
         default:
             return state;
