4818ea3772892819c84ffa28836855198b93de50
Seb Smith <seb@silverchip.com>
2020-09-08 14:33:14 +0100

    Displays last updated pin on the drawings

--- app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/DrawingDetails.js
+++ app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/DrawingDetails.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import DateTimeContainer from 'components/shared/dateTime/containers/DateTimeContainer';
 
-const DrawingDetails = ({ stats: { lastUpdatedOn } }) => (
+const DrawingDetails = ({ stats: { lastUpdatedOn, lastUpdatedByName } }) => (
     <p className="size-lg-12">
         Last Updated:{' '}
         {lastUpdatedOn ? (
-            <span>
-                <DateTimeContainer date={lastUpdatedOn} />
-            </span>
+            <>
+                <span>
+                    <DateTimeContainer date={lastUpdatedOn} />
+                </span>{' '}
+                <span>{`${lastUpdatedByName}`}</span>
+            </>
         ) : (
             <span>Not Updated</span>
         )}
