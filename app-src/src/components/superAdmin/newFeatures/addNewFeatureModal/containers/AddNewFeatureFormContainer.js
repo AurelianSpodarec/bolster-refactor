3c986cc96a9359c431d222217f4802acd178cdb5
Seb Smith <seb@silverchip.com>
2020-09-09 17:00:57 +0100

    Adds a form to add a new feature

--- app-src/src/components/superAdmin/newFeatures/addNewFeatureModal/containers/AddNewFeatureFormContainer.js
+++ app-src/src/components/superAdmin/newFeatures/addNewFeatureModal/containers/AddNewFeatureFormContainer.js
@@ -4,9 +4,17 @@ import { connect } from 'react-redux';
 
 import AddNewFeatureForm from '../presentational/AddNewFeatureForm';
 import { showModal } from 'actions/shared/generic/modals/sync/showModal';
+import { useForm } from 'helpers/hooks';
 
 function AddNewFeatureFormContainer() {
-    return <AddNewFeatureForm />;
+    const [formData, handleChange] = useForm({
+        title: '',
+        shortDescription: '',
+        fullDescription: '',
+        publishDate: '',
+    });
+    console.log(formData);
+    return <AddNewFeatureForm handleChange={handleChange} form={formData} />;
 }
 
 const mapDispatchToProps = {
--- app-src/src/components/superAdmin/newFeatures/addNewFeatureModal/presentational/AddNewFeatureForm.js
+++ app-src/src/components/superAdmin/newFeatures/addNewFeatureModal/presentational/AddNewFeatureForm.js
@@ -1,7 +1,49 @@
 import React from 'react';
 
 import Form from 'components/shared/generic/form/containers/Form';
+import Field from 'components/shared/generic/form/presentational/Field';
+import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
+import DatePickerContainer from 'components/shared/generic/form/containers/DatePickerContainer';
 
-export default function AddNewFeatureForm() {
-    return <Form className="generic-form"></Form>;
+export default function AddNewFeatureForm({ handleChange, form }) {
+    return (
+        <Form className="generic-form">
+            <Field name="Enter Title" required>
+                <TextInputContainer
+                    placeholder="Enter title of the new feature"
+                    name="title"
+                    value={form.title}
+                    handleChange={handleChange}
+                    required
+                />
+            </Field>
+            <Field name="Enter Short Description" required>
+                <TextInputContainer
+                    placeholder="Enter a short description of the new feature"
+                    name="shortDescription"
+                    value={form.shortDescription}
+                    handleChange={handleChange}
+                    required
+                />
+            </Field>
+            <Field name="Enter Full Description" required>
+                <TextInputContainer
+                    placeholder="Enter a long description of the new feature"
+                    name="fullDescription"
+                    value={form.fullDescription}
+                    handleChange={handleChange}
+                    required
+                />
+            </Field>
+            <Field name="Date range" sizeClasses="w-dates size-lg-12">
+                <DatePickerContainer
+                    name="publishDate"
+                    selected={form.publishDate}
+                    onChange={val => handleChange('publishDate', val)}
+                    placeholderText="Publish Date"
+                    // onBlur={() => handleDateBlur(true)}
+                />
+            </Field>
+        </Form>
+    );
 }
