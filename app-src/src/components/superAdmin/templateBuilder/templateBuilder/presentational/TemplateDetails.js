fb23c015f6ac2df9a879c1bc88203e3584ca2c16
Mark Baker <mark@silverchip.com>
2020-09-10 15:26:03 +0100

    add outputs for date created and last updated

--- app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/TemplateDetails.js
+++ app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/TemplateDetails.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import moment from 'moment';
 
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 import FieldOutput from 'components/shared/generic/fieldOutput/presentational/FieldOutput';
+import DateTimeContainer from 'components/shared/dateTime/containers/DateTimeContainer';
 
-const TemplateDetails = ({ template }) => (
+const TemplateDetails = ({ template: { name, dateCreated, lastUpdated } }) => (
     <>
         <BlockHeading title="Template Details" />
-        <FieldOutput title="Name" description={template.name} fieldClass="no-h-padding" />
+        <FieldOutput title="Name" description={name} fieldClass="no-h-padding" />
         <FieldOutput
             title="Date created"
-            description={`##${moment().format('DD/MM/YYYY HH:mm')}##`}
+            description={dateCreated ? <DateTimeContainer date={dateCreated} /> : '--'}
             fieldClass="no-h-padding"
         />
         <FieldOutput
             title="Last updated"
-            description={`##${moment().format('DD/MM/YYYY HH:mm')}##`}
+            description={lastUpdated ? <DateTimeContainer date={lastUpdated} /> : '--'}
             fieldClass="no-h-padding"
         />
     </>
