13ddb5376320a489e47c7ba2458b086216499aae
Mark Baker <mark@silverchip.com>
2020-09-10 17:17:42 +0100

    add list for recent updates

--- app-src/src/components/companyAdmin/layout/header/presentational/RecentUpdates.js
+++ app-src/src/components/companyAdmin/layout/header/presentational/RecentUpdates.js
@@ -5,6 +5,21 @@ const RecentUpdates = () => (
         <button className="icon" type="button">
             <i className="fas fa-bullhorn"></i>
         </button>
+
+        <div className="list">
+            <a className="item">
+                <h2>Announcement</h2>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+            </a>
+            <a className="item">
+                <h2>Announcement</h2>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+            </a>
+            <a className="item">
+                <h2>Announcement</h2>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+            </a>
+        </div>
     </div>
 );
 
