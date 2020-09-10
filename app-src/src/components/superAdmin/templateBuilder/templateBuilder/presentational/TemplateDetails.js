814c5d54745beb33657df9cd57d051eaf5ed4552
Mark Baker <mark@silverchip.com>
2020-09-10 16:33:47 +0100

    add formatting for dates

--- app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/TemplateDetails.js
+++ app-src/src/components/superAdmin/templateBuilder/templateBuilder/presentational/TemplateDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 import FieldOutput from 'components/shared/generic/fieldOutput/presentational/FieldOutput';
@@ -10,12 +11,24 @@ const TemplateDetails = ({ template: { name, dateCreated, lastUpdated } }) => (
         <FieldOutput title="Name" description={name} fieldClass="no-h-padding" />
         <FieldOutput
             title="Date created"
-            description={dateCreated ? <DateTimeContainer date={dateCreated} /> : '--'}
+            description={
+                dateCreated ? (
+                    <DateTimeContainer date={moment(dateCreated).format('YYYY-MM-DDTHH:mm:ss')} />
+                ) : (
+                    '--'
+                )
+            }
             fieldClass="no-h-padding"
         />
         <FieldOutput
             title="Last updated"
-            description={lastUpdated ? <DateTimeContainer date={lastUpdated} /> : '--'}
+            description={
+                lastUpdated ? (
+                    <DateTimeContainer date={moment(lastUpdated).format('YYYY-MM-DDTHH:mm:ss')} />
+                ) : (
+                    '--'
+                )
+            }
             fieldClass="no-h-padding"
         />
     </>
