810ecfb34ae63cb5fcf81d27fc9ebcfaa40c15fb
Mark Baker <mark@silverchip.com>
2020-09-10 16:51:44 +0100

    add button for recent updates

---
+++ app-src/src/components/companyAdmin/layout/header/containers/RecentUpdatesContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import RecentUpdates from '../presentational/RecentUpdates';
+
+const RecentUpdatesContainer = () => {
+    return <RecentUpdates />;
+};
+
+export default RecentUpdatesContainer;
