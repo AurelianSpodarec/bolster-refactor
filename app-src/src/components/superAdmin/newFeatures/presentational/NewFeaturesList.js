7229020fdc5359edd8a040318433e3ec0774c05c
Seb Smith <seb@silverchip.com>
2020-09-11 11:41:10 +0100

    Sorts the features list

--- app-src/src/components/superAdmin/newFeatures/presentational/NewFeaturesList.js
+++ app-src/src/components/superAdmin/newFeatures/presentational/NewFeaturesList.js
@@ -5,31 +5,33 @@ import DateTimeContainer from 'components/shared/dateTime/containers/DateTimeCon
 import { Link } from 'react-router-dom';
 
 const NewFeaturesList = ({ newFeatures, showDeleteModal, showEditModal }) => {
-    return newFeatures.map(newFeature => (
-        <tr key={newFeature.id}>
-            <td>{newFeature.title}</td>
-            <td>{newFeature.shortDescription}</td>
-            <td>
-                {' '}
-                <DateTimeContainer
-                    date={moment(newFeature.publishDate).format('YYYY-MM-DDTHH:mm:ss')}
-                />
-            </td>
-            <td>
-                <Link to={`/admin/new-features/${newFeature.id}`} className="button blue">
-                    <i className="fa fa-eye fa-fw" />
-                    View
-                </Link>
-                <button className="button yellow" onClick={() => showEditModal(newFeature)}>
-                    <i className="fa fa-edit fa-fw" />
-                    Edit
-                </button>
-                <button className="button red" onClick={() => showDeleteModal(newFeature.id)}>
-                    <i className="fa fa-times fa-fw" />
-                    Delete
-                </button>
-            </td>
-        </tr>
-    ));
+    return [...newFeatures]
+        .sort((a, b) => new Date(b.publishDate) - new Date(a.publishDate))
+        .map(newFeature => (
+            <tr key={newFeature.id}>
+                <td>{newFeature.title}</td>
+                <td>{newFeature.shortDescription}</td>
+                <td>
+                    {' '}
+                    <DateTimeContainer
+                        date={moment(newFeature.publishDate).format('YYYY-MM-DDTHH:mm:ss')}
+                    />
+                </td>
+                <td>
+                    <Link to={`/admin/new-features/${newFeature.id}`} className="button blue">
+                        <i className="fa fa-eye fa-fw" />
+                        View
+                    </Link>
+                    <button className="button yellow" onClick={() => showEditModal(newFeature)}>
+                        <i className="fa fa-edit fa-fw" />
+                        Edit
+                    </button>
+                    <button className="button red" onClick={() => showDeleteModal(newFeature.id)}>
+                        <i className="fa fa-times fa-fw" />
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        ));
 };
 export default NewFeaturesList;
