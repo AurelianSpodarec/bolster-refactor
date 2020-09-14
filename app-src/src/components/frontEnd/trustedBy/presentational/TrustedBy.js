a4489386676de79d731603ad330af6ef70d73c3d
Oluseun Adesina <seun@silverchip.com>
2020-09-14 16:56:32 +0100

    going to make shared header title component

---
+++ app-src/src/components/frontEnd/trustedBy/presentational/TrustedBy.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TrustedBy = () => (
+    <div className="trusted-by-container">
+        <Header>
+            <h1>TrustedBy by industry leaders</h1>
+        </Header>
+    </div>
+);
