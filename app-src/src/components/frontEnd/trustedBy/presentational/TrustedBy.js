ba49e89126e63d85737faed955ec3bbda444d451
Oluseun Adesina <seun@silverchip.com>
2020-09-15 09:30:50 +0100

    trusted by

--- app-src/src/components/frontEnd/trustedBy/presentational/TrustedBy.js
+++ app-src/src/components/frontEnd/trustedBy/presentational/TrustedBy.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Header from 'components/frontEnd/shared/header/presentational/Header';
+import { TrustedByList } from 'constants/frontEnd/trustedBy';
 
 const TrustedBy = () => (
     <div className="trusted-by-container">
-        <Header>
-            <h1>TrustedBy by industry leaders</h1>
+        <Header className="trusted-by-header">
+            <h1 className="line">Trusted by industry leaders</h1>
         </Header>
+        <div className="trusted-by-grid">
+            {TrustedByList.map((item, index) => (
+                <div key={index} className="item">
+                    <img src={item} alt="item-1" />
+                </div>
+            ))}
+        </div>
     </div>
 );
+
+export default TrustedBy;
---
+++ app-src/src/constants/frontEnd/trustedBy.js
@@ -0,0 +1,8 @@
+import Sodexo from '_content/images/frontend-new/trusted-by/sodexo.png';
+import Engie from '_content/images/frontend-new/trusted-by/engie.png';
+import ISG from '_content/images/frontend-new/trusted-by/isg.png';
+import Bam from '_content/images/frontend-new/trusted-by/bam.png';
+import NHS from '_content/images/frontend-new/trusted-by/nhs.png';
+import ASFP from '_content/images/frontend-new/trusted-by/asfp.png';
+
+export const TrustedByList = [Sodexo, Engie, ISG, Bam, NHS, ASFP, Sodexo, Engie, ISG, Bam];
