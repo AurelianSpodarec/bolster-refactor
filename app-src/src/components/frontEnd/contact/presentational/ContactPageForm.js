0dee54806292be397173bb65748283843d09c324
Seb Smith <seb@silverchip.com>
2020-09-15 14:31:25 +0100

    Adds functionality to post a form

--- app-src/src/components/frontEnd/contact/presentational/ContactPageForm.js
+++ app-src/src/components/frontEnd/contact/presentational/ContactPageForm.js
@@ -4,83 +4,46 @@ import Field from 'components/shared/generic/form/presentational/Field';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
 import FrontEndButton from 'components/frontEnd/shared/buttons/presentational/FrontEndButton';
 import Error from 'components/shared/generic/form/presentational/Error';
+import Form from 'components/shared/generic/form/containers/Form';
 
-const ContactPageForm = ({
-    name,
-    email,
-    contactNumber,
-    companyName,
-    handleChange,
-    handleSubmit,
-    sent,
-    error,
-}) => (
-    <div id="contact-form-content" className="contact-form">
-        <form className="form">
-            <>
-                {sent ? (
-                    <p>Thanks! A member of our team will be in touch.</p>
-                ) : (
-                    <>
-                        <div className="row">
-                            <Field name="Name" required>
-                                <TextInputContainer
-                                    name="name"
-                                    required
-                                    value={name}
-                                    handleChange={handleChange}
-                                />
-                            </Field>
-                        </div>
-                        <div className="row">
-                            <Field name="Email" required>
-                                <TextInputContainer
-                                    name="email"
-                                    required
-                                    value={email}
-                                    type="email"
-                                    handleChange={handleChange}
-                                />
-                            </Field>
-                        </div>
-                        <div className="row">
-                            <Field name="Company">
-                                <TextInputContainer
-                                    name="companyName"
-                                    required
-                                    value={companyName}
-                                    handleChange={handleChange}
-                                />
-                            </Field>
-                        </div>
-                        <div className="row">
-                            <Field name="Contact Number" required>
-                                <TextInputContainer
-                                    name="contactNumber"
-                                    required
-                                    value={contactNumber}
-                                    handleChange={handleChange}
-                                />
-                            </Field>
-                        </div>
-                        {error && (
-                            <Error>
-                                Something went wrong submitting your request, please try again
-                                later.
-                            </Error>
-                        )}
-                        <FrontEndButton
-                            type="submit"
-                            classes="gray right spacing-right"
-                            handleClick={handleSubmit}
-                        >
-                            Submit
-                        </FrontEndButton>
-                    </>
-                )}
-            </>
-        </form>
-    </div>
+const ContactPageForm = ({ form, handleChange, handleSubmit, error }) => (
+    <Form id="contact-form-content" className="contact-form" onSubmit={handleSubmit}>
+        <Field name="Name" required>
+            <TextInputContainer
+                name="name"
+                value={form.name}
+                handleChange={handleChange}
+                required
+            />
+        </Field>
+        <Field name="Email" required>
+            <TextInputContainer
+                name="email"
+                value={form.email}
+                handleChange={handleChange}
+                required
+            />
+        </Field>
+        <Field name="Company" required>
+            <TextInputContainer
+                name="companyName"
+                value={form.companyName}
+                handleChange={handleChange}
+                required
+            />
+        </Field>
+        <Field name="Contact Number" required>
+            <TextInputContainer
+                name="contactNumber"
+                value={form.contactNumber}
+                handleChange={handleChange}
+                required
+            />
+        </Field>
+        <FrontEndButton classes="gray right spacing-right" handleClick={handleSubmit} type="submit">
+            Submit
+        </FrontEndButton>
+    </Form>
 );
 
 export default ContactPageForm;
