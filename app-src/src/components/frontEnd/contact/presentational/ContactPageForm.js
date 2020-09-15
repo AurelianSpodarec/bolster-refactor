09f472dc92fbc83a1cc8d7e50e4184b927b92a43
Seb Smith <seb@silverchip.com>
2020-09-15 15:22:32 +0100

    Adds ReCAPTCHA skeleton

--- app-src/src/components/frontEnd/contact/presentational/ContactPageForm.js
+++ app-src/src/components/frontEnd/contact/presentational/ContactPageForm.js
@@ -1,49 +1,65 @@
 import React from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 import Field from 'components/shared/generic/form/presentational/Field';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
 import FrontEndButton from 'components/frontEnd/shared/buttons/presentational/FrontEndButton';
-import Error from 'components/shared/generic/form/presentational/Error';
 import Form from 'components/shared/generic/form/containers/Form';
+import Error from 'components/shared/generic/form/presentational/Error';
 
-const ContactPageForm = ({ form, handleChange, handleSubmit, error }) => (
-    <Form id="contact-form-content" className="contact-form" onSubmit={handleSubmit}>
-        <Field name="Name" required>
-            <TextInputContainer
-                name="name"
-                value={form.name}
-                handleChange={handleChange}
-                required
-            />
-        </Field>
-        <Field name="Email" required>
-            <TextInputContainer
-                name="email"
-                value={form.email}
-                handleChange={handleChange}
-                required
-            />
-        </Field>
-        <Field name="Company" required>
-            <TextInputContainer
-                name="companyName"
-                value={form.companyName}
-                handleChange={handleChange}
-                required
-            />
-        </Field>
-        <Field name="Contact Number" required>
-            <TextInputContainer
-                name="contactNumber"
-                value={form.contactNumber}
-                handleChange={handleChange}
-                required
-            />
-        </Field>
-        <FrontEndButton classes="gray right spacing-right" handleClick={handleSubmit} type="submit">
-            Submit
-        </FrontEndButton>
-    </Form>
-);
+const ContactPageForm = ({ form, handleChange, handleSubmit, error }) => {
+    const recaptchaRef = React.createRef();
+
+    return error ? (
+        <Error>{error}</Error>
+    ) : (
+        <Form id="contact-form-content" className="contact-form" onSubmit={handleSubmit}>
+            <Field name="Name" required>
+                <TextInputContainer
+                    name="name"
+                    value={form.name}
+                    handleChange={handleChange}
+                    required
+                />
+            </Field>
+            <Field name="Email" required>
+                <TextInputContainer
+                    name="email"
+                    value={form.email}
+                    handleChange={handleChange}
+                    required
+                />
+            </Field>
+            <Field name="Company" required>
+                <TextInputContainer
+                    name="companyName"
+                    value={form.companyName}
+                    handleChange={handleChange}
+                    required
+                />
+            </Field>
+            <Field name="Contact Number" required>
+                <TextInputContainer
+                    name="contactNumber"
+                    value={form.contactNumber}
+                    handleChange={handleChange}
+                    required
+                />
+            </Field>
+            <Field>
+                <div className="recaptcha-wrapper">
+                    <ReCAPTCHA ref={recaptchaRef} sitekey="Key" />
+                </div>
+            </Field>
+            <FrontEndButton
+                classes="gray right spacing-right-sm"
+                handleClick={handleSubmit}
+                type="submit"
+            >
+                Submit
+            </FrontEndButton>
+        </Form>
+    );
+};
 
 export default ContactPageForm;
