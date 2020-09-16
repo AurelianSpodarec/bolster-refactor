8e03f40439b91c77820b8422d5c405d04f135882
Mark Baker <mark@silverchip.com>
2020-09-16 17:00:43 +0100

    add prep for timeline scrolling
    Former-commit-id: f7a9015b8e6595b37becf92015e59f6a431160a7

--- app-src/src/components/frontEnd/how/containers/HowItWorksTimelineContainer.js
+++ app-src/src/components/frontEnd/how/containers/HowItWorksTimelineContainer.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import items from 'constants/frontEnd/timeline';
 
 import HowItWorksTimeline from '../presentational/HowItWorksTimeline';
 
 const HowItWorksTimelineContainer = () => {
+    useEffect(() => {
+        const sections = document.querySelector('.how-it-works-sections');
+
+        sections.addEventListener('scroll', setActiveTimelineItem);
+
+        return () => sections.removeEventListener('scroll', setActiveTimelineItem);
+    }, []);
+
     return <HowItWorksTimeline items={items} />;
+
+    function setActiveTimelineItem() {
+        console.log('scroll yo');
+    }
 };
 
 export default HowItWorksTimelineContainer;
