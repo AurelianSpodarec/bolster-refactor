12a37b60436a35bcba144c352d7488188ec9a9c3
Mark Baker <mark@silverchip.com>
2020-09-16 15:42:46 +0100

    add scroll to how it works sections depending on where you are on page
    Former-commit-id: cb01fe30db7f7c23d751cc5341b05ee580f429d2

--- app-src/src/components/frontEnd/how/presentational/HowItWorksSections.js
+++ app-src/src/components/frontEnd/how/presentational/HowItWorksSections.js
@@ -3,8 +3,8 @@ import React from 'react';
 import HowItWorksTimelineContainer from '../containers/HowItWorksTimelineContainer';
 import HowItWorksHeadings from './HowItWorksHeadings';
 
-const HowItWorksSections = () => (
-    <div className="how-it-works-sections">
+const HowItWorksSections = ({ innerScrollable }) => (
+    <div className={`how-it-works-sections ${innerScrollable ? 'scrollable' : ''}`}>
         <HowItWorksHeadings />
         <HowItWorksTimelineContainer />
     </div>
