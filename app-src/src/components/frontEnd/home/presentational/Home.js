6c939c772c7326706662841a660cd8ff7a6568de
Oluseun Adesina <seun@silverchip.com>
2020-09-16 16:36:53 +0100

    dealing with last part of home page and adding controls
    Former-commit-id: 7926ce1e79c0f239d259f66a232c8adab4c0d87e

--- app-src/src/components/frontEnd/home/presentational/Home.js
+++ app-src/src/components/frontEnd/home/presentational/Home.js
@@ -3,10 +3,11 @@ import HomeSlidesContainer from '../containers/HomeSlidesContainer';
 import TrustedBy from 'components/frontEnd/trustedBy/presentational/TrustedBy';
 
 const Home = () => (
-    <>
+    <div className="home-container">
         <HomeSlidesContainer />
         <TrustedBy />
-    </>
+        <TrustedBy />
+    </div>
 );
 
 export default Home;
