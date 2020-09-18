2e77bcd1af7fd1fb9f44dff94352d922b67a87cf
Mark Baker <mark@silverchip.com>
2020-09-18 10:42:27 +0100

    add publish date to feature view on company side

--- app-src/src/components/companyAdmin/layout/header/presentational/RecentUpdateModal.js
+++ app-src/src/components/companyAdmin/layout/header/presentational/RecentUpdateModal.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 import { hideModal } from 'actions/shared/generic/modals/sync/hideModal';
 
 import ModalOuterContainer from 'components/shared/generic/modals/containers/ModalOuterContainer';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
+import FieldOutput from 'components/shared/generic/fieldOutput/presentational/FieldOutput';
+import DateTimeContainer from 'components/shared/dateTime/containers/DateTimeContainer';
 
-const RecentUpdateModal = ({ update: { title, fullDescription }, hideModal }) => (
+const RecentUpdateModal = ({ update: { title, fullDescription, publishDate }, hideModal }) => (
     <ModalOuterContainer>
         <BlockHeading title={title} />
-        <p className="generic-text intro-text size-lg-12">{fullDescription}</p>
+        <FieldOutput title="Description" description={fullDescription} />
+        <FieldOutput title="Publish Date">
+            <p>
+                <DateTimeContainer date={moment.utc(publishDate).format('YYYY-MM-DDTHH:mm:ss')} />
+            </p>
+        </FieldOutput>
         <BlockButtonWrapper>
             <button className="button" onClick={hideModal}>
                 Close
