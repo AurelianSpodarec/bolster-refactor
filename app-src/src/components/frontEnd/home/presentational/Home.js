a0dfedc2ab3885b365274ddbfbbb76cbaa9bdc43
Oluseun Adesina <seun@silverchip.com>
2020-09-18 15:52:38 +0100

    light promixity snapping for home page
    Former-commit-id: 1558e9f9f8be7707632f1baa1e098dd0cf6f57b5

--- app-src/src/components/frontEnd/home/presentational/Home.js
+++ app-src/src/components/frontEnd/home/presentational/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import HomeSlidesContainer from '../containers/HomeSlidesContainer';
-import TrustedBy from 'components/frontEnd/trustedBy/presentational/TrustedBy';
 
 const Home = () => (
     <div className="home-container">
         <HomeSlidesContainer />
-        <TrustedBy />
     </div>
 );
 
