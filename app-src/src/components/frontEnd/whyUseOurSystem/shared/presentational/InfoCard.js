c86ed1f8a9fb6859e37cedace936cf619c0ce82e
Mark Baker <mark@silverchip.com>
2020-09-21 16:53:06 +0100

    add cloud area to our system page
    Former-commit-id: 736bae4cbd257854484d2a982cdee86e7fe4fabf

--- app-src/src/components/frontEnd/whyUseOurSystem/shared/presentational/InfoCard.js
+++ app-src/src/components/frontEnd/whyUseOurSystem/shared/presentational/InfoCard.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import PlusIcon from '_content/images/frontend-new/why-use-our-system/plus-icon.png';
 
-const InfoCard = ({ title, icon, handleClick }) => {
+const InfoCard = ({ title = '', icon, hidePlusIcon = false, handleClick = () => {} }) => {
     return (
-        <div className="info-card">
-            <div className="icon-wrapper">
-                <img src={icon} className="icon" />
-            </div>
-            <img src={PlusIcon} className="plus-icon" onClick={handleClick} />
+        <div className={`info-card ${!hidePlusIcon ? 'clickable' : ''}`} onClick={handleClick}>
+            {icon && (
+                <div className="icon-wrapper">
+                    <img src={icon} className="icon" />
+                </div>
+            )}
+            {!hidePlusIcon && <img src={PlusIcon} className="plus-icon" />}
             <div className="title-wrapper">
                 <h2>{title}</h2>
                 <div className="divider"></div>
