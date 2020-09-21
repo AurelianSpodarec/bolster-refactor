1fd937469a653323f6cc81f460ca5b26bafa1b9f
Mark Baker <mark@silverchip.com>
2020-09-21 17:03:31 +0100

    add arrow to page banners
    Former-commit-id: 3b2de94399e2c92eb40e5eba97dbe4e66eff647a

--- app-src/src/components/frontEnd/shared/banners/presentational/FrontEndBanner.js
+++ app-src/src/components/frontEnd/shared/banners/presentational/FrontEndBanner.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import ArrowIcon from '_content/images/frontend-new/banners/banner-arrow.png';
+
 const FrontEndBanner = ({ video = '', heading = '', description = '' }) => (
     <div className="frontend-banner">
         <video className="video" autoPlay="autoplay" loop muted>
             <source src={video} type="video/mp4" />
         </video>
+
         <div className="content">
             <h1>{heading}</h1>
             <p>{description}</p>
         </div>
+
+        <img className="arrow" alt="arrow" src={ArrowIcon} />
     </div>
 );
 
