e204127a6ab0c9e2fdc6c099435a240836c4c4d9
Mark Baker <mark@silverchip.com>
2020-09-21 14:19:14 +0100

    update banner component to take video prop
    Former-commit-id: e515f8712b5fef18c099bcea0ea875653bac8eb8

--- app-src/src/components/frontEnd/how/presentational/HowItWorks.js
+++ app-src/src/components/frontEnd/how/presentational/HowItWorks.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { useBannerScroll } from 'helpers/frontEndHooks';
 import { useWindowDimensions } from 'helpers/hooks';
 
+import PinsVideo from '_content/videos/frontend/05_Pins.mp4';
+
 import HowItWorksSections from './HowItWorksSections';
 import FrontEndBanner from 'components/frontEnd/shared/banners/presentational/FrontEndBanner';
 
@@ -15,6 +17,7 @@ const HowItWorks = () => {
             <FrontEndBanner
                 heading="So why use Bolster?"
                 description="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet?"
+                video={PinsVideo}
             />
             <HowItWorksSections />
         </div>
--- app-src/src/components/frontEnd/shared/banners/presentational/FrontEndBanner.js
+++ app-src/src/components/frontEnd/shared/banners/presentational/FrontEndBanner.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import PinsVideo from '_content/videos/frontend/05_Pins.mp4';
-
-const FrontEndBanner = ({ background = '', heading = '', description = '' }) => (
-    <div className="frontend-banner" style={{ backgroundImage: background }}>
+const FrontEndBanner = ({ video = '', heading = '', description = '' }) => (
+    <div className="frontend-banner">
         <video className="video" autoPlay="autoplay" loop muted>
-            <source src={PinsVideo} type="video/mp4" />
+            <source src={video} type="video/mp4" />
         </video>
         <div className="content">
             <h1>{heading}</h1>
