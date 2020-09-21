0d3a28ec42030c90fcb915ee04fc317bc9f74c52
Mark Baker <mark@silverchip.com>
2020-09-21 15:13:48 +0100

    add banner to why use bolster page
    Former-commit-id: c64a114212cf011f641308f8de2cb7d5a94d54d2

--- app-src/src/components/frontEnd/whyUseOurSystem/presentational/WhyUseOurSystem.js
+++ app-src/src/components/frontEnd/whyUseOurSystem/presentational/WhyUseOurSystem.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import { useWindowDimensions } from 'helpers/hooks';
+import { useBannerScroll } from 'helpers/frontEndHooks';
+
+import PinsVideo from '_content/videos/frontend/05_Pins.mp4';
+
 import OurSystemListContainer from '../containers/OurSystemListContainer';
+import FrontEndBanner from 'components/frontEnd/shared/banners/presentational/FrontEndBanner';
 
 const WhyUseOurSystem = () => {
+    const { width } = useWindowDimensions();
+    useBannerScroll(width);
+
     return (
         <div id="why-use-our-system">
+            <FrontEndBanner
+                heading="So why use Bolster?"
+                description="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet?"
+                video={PinsVideo}
+            />
             <div className="page-divider">
                 <h2>Lorem Ipsum</h2>
                 <div className="divider"></div>
