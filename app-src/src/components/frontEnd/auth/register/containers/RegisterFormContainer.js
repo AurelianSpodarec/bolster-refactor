b184099659424e778ddb8a6744d2c012dcd15f2e
Seb Smith <seb@silverchip.com>
2020-09-22 13:39:57 +0100

    Adds first two skeleton pages for the register section
    Former-commit-id: ef84631e705d6647778f9f0b66d0d2feebecce6f

--- app-src/src/components/frontEnd/auth/register/containers/RegisterFormContainer.js
+++ app-src/src/components/frontEnd/auth/register/containers/RegisterFormContainer.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import RegisterForm from '../presentational/RegisterForm';
 
 const RegisterFormContainer = () => {
-    return <RegisterForm />;
+    const [page, setPage] = useState(2);
+    return <RegisterForm activePage={page} />;
 };
 
 export default RegisterFormContainer;
--- app-src/src/components/frontEnd/auth/register/presentational/RegisterForm.js
+++ app-src/src/components/frontEnd/auth/register/presentational/RegisterForm.js
@@ -1,33 +1,14 @@
 import React from 'react';
 
-import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
-import Form from 'components/shared/generic/form/containers/Form';
-import Field from 'components/shared/generic/form/presentational/Field';
-import FrontEndButton from 'components/frontEnd/shared/buttons/presentational/FrontEndButton';
-import FrontEndFormHeading from 'components/frontEnd/shared/forms/presentational/FrontEndFormHeading';
+import RegisterPageContainer from '../containers/RegisterPageContainer';
 
-const RegisterForm = ({ handleSubmit, handleChange }) => {
+const RegisterForm = ({ handleSubmit, handleChange, activePage }) => {
     return (
-        <div className="auth-form-wrapper register">
-            <FrontEndFormHeading title="Register" subtitle="User Details" />
-            <Form onSubmit={handleSubmit}>
-                <Field name="First Name" classes="auth-form-field">
-                    <TextInputContainer
-                        value={''}
-                        name="firstName"
-                        type="firstName"
-                        required
-                        handleChange={handleChange}
-                        classes="auth-text-input-container"
-                    />
-                </Field>
-                <Field classes="auth-form-field right">
-                    <FrontEndButton classes="gray" type="submit">
-                        Submit
-                    </FrontEndButton>
-                </Field>
-            </Form>
-        </div>
+        <RegisterPageContainer
+            activePage={activePage}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+        />
     );
 };
 
