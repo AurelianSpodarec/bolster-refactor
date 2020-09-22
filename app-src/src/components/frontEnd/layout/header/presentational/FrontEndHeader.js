0620168be261bbe82975733800516f0ff7853767
Mark Baker <mark@silverchip.com>
2020-09-22 14:49:26 +0100

    split header component into desktop and mobile
    Former-commit-id: a4d3bacfeeb641da32eac63e8e32f40395ebe3ff

--- app-src/src/components/frontEnd/layout/header/presentational/FrontEndHeader.js
+++ app-src/src/components/frontEnd/layout/header/presentational/FrontEndHeader.js
@@ -1,82 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '_content/images/frontend-new/logo.png';
-import GoogleAppStore from '_content/images/frontend-new/google-play-badge.png';
-import AppleAppStore from '_content/images/frontend-new/apple-store.svg';
-import Container from 'components/frontEnd/shared/container/presentational/Container';
-import FrontEndButton from 'components/frontEnd/shared/buttons/presentational/FrontEndButton';
-import navItems from 'constants/frontEnd/navItems';
 
-const FrontEndHeader = ({
-    isSuperAdmin,
-    isCompanyAdmin,
-    isClientAccess,
-    onClick,
-    logout,
-    hideHeader,
-    curRoute,
-}) => (
-    <Container className="frontend-header">
-        <div className="frontend-header-left">
-            <div className="frontend-logo">
-                <Link to="/">
-                    <img src={Logo} alt="Logo of Bolster Systems" />
-                </Link>
-            </div>
-            <div className="frontend-header-navlinks-container">
-                <ul>
-                    {navItems
-                        .filter(({ name }) => name !== 'Register')
-                        .map(({ name, slug }) => (
-                            <li key={name}>
-                                <Link to={slug} className={curRoute === slug ? 'active' : ''}>
-                                    {name}
-                                </Link>
-                            </li>
-                        ))}
-                </ul>
-            </div>
-        </div>
-        <div className="frontend-header-right">
-            {isSuperAdmin || isCompanyAdmin || isClientAccess ? (
-                <div className="logged-in-button-container">
-                    {isSuperAdmin && (
-                        <FrontEndButton to="admin" classes="gray spacing-right">
-                            Super Admin
-                        </FrontEndButton>
-                    )}
-                    {isCompanyAdmin && (
-                        <FrontEndButton to="company" classes="gray spacing-right">
-                            Company Admin
-                        </FrontEndButton>
-                    )}
-                    {isClientAccess && (
-                        <FrontEndButton to="client/companies" classes="gray spacing-right">
-                            Client Access
-                        </FrontEndButton>
-                    )}
-                    <FrontEndButton type="button" classes="red spacing-right" handleClick={logout}>
-                        Logout
-                    </FrontEndButton>
-                </div>
-            ) : (
-                <div className="logged-out-button-container">
-                    <div className="mobile-app-button-container">
-                        <img src={GoogleAppStore} alt="Google App Store" />
-                        <img src={AppleAppStore} alt="Apple App Store" />
-                    </div>
-                    <div>
-                        <FrontEndButton to="auth/register" classes="gray spacing-right">
-                            Register
-                        </FrontEndButton>
-                        <FrontEndButton to="auth/login" classes="red">
-                            Login
-                        </FrontEndButton>
-                    </div>
-                </div>
-            )}
-        </div>
-    </Container>
-);
+import { useWindowDimensions } from 'helpers/hooks';
+import FrontEndHeaderDesktop from './FrontEndHeaderDesktop';
+import FrontEndHeaderMobile from './FrontEndHeaderMobile';
+
+const FrontEndHeader = ({ isSuperAdmin, isCompanyAdmin, isClientAccess, logout, curRoute }) => {
+    const { width } = useWindowDimensions();
+
+    if (width <= 1100)
+        return (
+            <FrontEndHeaderMobile
+                isSuperAdmin={isSuperAdmin}
+                isCompanyAdmin={isCompanyAdmin}
+                isClientAccess={isClientAccess}
+                curRoute={curRoute}
+                logout={logout}
+            />
+        );
+
+    return (
+        <FrontEndHeaderDesktop
+            isSuperAdmin={isSuperAdmin}
+            isCompanyAdmin={isCompanyAdmin}
+            isClientAccess={isClientAccess}
+            curRoute={curRoute}
+            logout={logout}
+        />
+    );
+};
 
 export default FrontEndHeader;
---
+++ app-src/src/components/frontEnd/layout/header/presentational/FrontEndHeaderDesktop.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '_content/images/frontend-new/logo.png';
+import GoogleAppStore from '_content/images/frontend-new/google-play-badge.png';
+import AppleAppStore from '_content/images/frontend-new/apple-store.svg';
+import Container from 'components/frontEnd/shared/container/presentational/Container';
+import FrontEndButton from 'components/frontEnd/shared/buttons/presentational/FrontEndButton';
+import navItems from 'constants/frontEnd/navItems';
+
+const FrontEndHeaderDesktop = ({
+    isSuperAdmin,
+    isCompanyAdmin,
+    isClientAccess,
+    logout,
+    curRoute,
+}) => (
+    <Container className="frontend-header">
+        <div className="frontend-header-left">
+            <div className="frontend-logo">
+                <Link to="/">
+                    <img src={Logo} alt="Logo of Bolster Systems" />
+                </Link>
+            </div>
+            <div className="frontend-header-navlinks-container">
+                <ul>
+                    {navItems
+                        .filter(({ name }) => name !== 'Register')
+                        .map(({ name, slug }) => (
+                            <li key={name}>
+                                <Link to={slug} className={curRoute === slug ? 'active' : ''}>
+                                    {name}
+                                </Link>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+        </div>
+        <div className="frontend-header-right">
+            {isSuperAdmin || isCompanyAdmin || isClientAccess ? (
+                <div className="logged-in-button-container">
+                    {isSuperAdmin && (
+                        <FrontEndButton to="admin" classes="gray spacing-right">
+                            Super Admin
+                        </FrontEndButton>
+                    )}
+                    {isCompanyAdmin && (
+                        <FrontEndButton to="company" classes="gray spacing-right">
+                            Company Admin
+                        </FrontEndButton>
+                    )}
+                    {isClientAccess && (
+                        <FrontEndButton to="client/companies" classes="gray spacing-right">
+                            Client Access
+                        </FrontEndButton>
+                    )}
+                    <FrontEndButton type="button" classes="red spacing-right" handleClick={logout}>
+                        Logout
+                    </FrontEndButton>
+                </div>
+            ) : (
+                <div className="logged-out-button-container">
+                    <div className="mobile-app-button-container">
+                        <img src={GoogleAppStore} alt="Google App Store" />
+                        <img src={AppleAppStore} alt="Apple App Store" />
+                    </div>
+                    <div>
+                        <FrontEndButton to="auth/register" classes="gray spacing-right">
+                            Register
+                        </FrontEndButton>
+                        <FrontEndButton to="auth/login" classes="red">
+                            Login
+                        </FrontEndButton>
+                    </div>
+                </div>
+            )}
+        </div>
+    </Container>
+);
+
+export default FrontEndHeaderDesktop;
