cb0ae7694cba6bc84724c0b32b22de47bf7eae51
Mark Baker <mark@silverchip.com>
2020-09-22 14:59:51 +0100

    add menu button for header mobile
    Former-commit-id: 49b234e9e0ff2e55c7829a42d3b063f7daeddbac

--- app-src/src/components/frontEnd/layout/header/presentational/FrontEndHeaderMobile.js
+++ app-src/src/components/frontEnd/layout/header/presentational/FrontEndHeaderMobile.js
@@ -15,67 +15,17 @@ const FrontEndHeaderMobile = ({
     logout,
     curRoute,
 }) => (
-    <Container className="frontend-header">
-        <div className="frontend-header-left">
+    <>
+        <Container className="frontend-header mobile">
             <div className="frontend-logo">
                 <Link to="/">
                     <img src={Logo} alt="Logo of Bolster Systems" />
                 </Link>
             </div>
-            <div className="frontend-header-navlinks-container">
-                <ul>
-                    {navItems
-                        .filter(({ name }) => name !== 'Register')
-                        .map(({ name, slug }) => (
-                            <li key={name}>
-                                <Link to={slug} className={curRoute === slug ? 'active' : ''}>
-                                    {name}
-                                </Link>
-                            </li>
-                        ))}
-                </ul>
-            </div>
-        </div>
-        <div className="frontend-header-right">
-            {isSuperAdmin || isCompanyAdmin || isClientAccess ? (
-                <div className="logged-in-button-container">
-                    {isSuperAdmin && (
-                        <FrontEndButton to="admin" classes="gray spacing-right">
-                            Super Admin
-                        </FrontEndButton>
-                    )}
-                    {isCompanyAdmin && (
-                        <FrontEndButton to="company" classes="gray spacing-right">
-                            Company Admin
-                        </FrontEndButton>
-                    )}
-                    {isClientAccess && (
-                        <FrontEndButton to="client/companies" classes="gray spacing-right">
-                            Client Access
-                        </FrontEndButton>
-                    )}
-                    <FrontEndButton type="button" classes="red spacing-right" handleClick={logout}>
-                        Logout
-                    </FrontEndButton>
-                </div>
-            ) : (
-                <div className="logged-out-button-container">
-                    <div className="mobile-app-button-container">
-                        <img src={GoogleAppStore} alt="Google App Store" />
-                        <img src={AppleAppStore} alt="Apple App Store" />
-                    </div>
-                    <div>
-                        <FrontEndButton to="auth/register" classes="gray spacing-right">
-                            Register
-                        </FrontEndButton>
-                        <FrontEndButton to="auth/login" classes="red">
-                            Login
-                        </FrontEndButton>
-                    </div>
-                </div>
-            )}
-        </div>
-    </Container>
+
+            <i className="menu-button fa fa-bars" />
+        </Container>
+    </>
 );
 
 export default FrontEndHeaderMobile;
