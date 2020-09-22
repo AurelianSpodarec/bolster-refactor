7a783804bd1051400e0c8aea7b6ee51595940531
Mark Baker <mark@silverchip.com>
2020-09-22 13:33:42 +0100

    add active class to nav items in header
    Former-commit-id: beba08214da32c1be6144a7664987eaee9d23329

---
+++ app-src/src/constants/frontEnd/navItems.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: 'About us',
+        slug: '/about-us',
+    },
+    {
+        name: 'Our system',
+        slug: '/our-system',
+    },
+    {
+        name: 'How it works',
+        slug: '/how-it-works',
+    },
+    {
+        name: 'Contact',
+        slug: '/contact',
+    },
+    {
+        name: 'Register',
+        slug: '/auth/register',
+    },
+];
