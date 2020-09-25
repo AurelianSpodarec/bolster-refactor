359a176c1382b8f9c2661942370358766c3e6682
Seb Smith <seb@silverchip.com>
2020-09-25 13:46:28 +0100

    Styles Ts&Cs and privacy policy area
    Former-commit-id: aa40e3b9645e252da17c8ce994a2d0c5bdd86997

--- app-src/src/components/shared/terms/Terms.js
+++ app-src/src/components/shared/terms/Terms.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
-import BackButtonContainer from 'components/shared/generic/backButton/containers/BackButtonContainer';
 import PrivacyPolicy from './PrivacyPolicy';
 import EULA from './EULA';
 import TsAndCs from './TsAndCs';
@@ -10,9 +8,6 @@ import Helmet from 'components/frontEnd/shared/meta/presentational/Helmet';
 const Terms = () => (
     <>
         <Helmet title="Terms &amp; Conditions" />
-        <PageHeading leftChildren={true} title="Terms &amp; Conditions">
-            <BackButtonContainer />
-        </PageHeading>
         <TsAndCs />
         <EULA />
         <PrivacyPolicy />
