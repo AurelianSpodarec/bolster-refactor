506e459d688fa1085b89ccad838cb87b82fa2e46
Mark Baker <mark@silverchip.com>
2020-09-28 11:36:30 +0100

    remove unused import
    Former-commit-id: 956de6a3c2a1bcbf33c84e65b4211c15421f4408

--- app-src/src/components/superAdmin/contactSubmissions/shared/containers/ContactSubmissionsTableContainer.js
+++ app-src/src/components/superAdmin/contactSubmissions/shared/containers/ContactSubmissionsTableContainer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ContactSubmissionsTable from '../presentational/ContactSubmissionsTable';
-import moment from 'moment';
 
 const ContactSubmissionsTableContainer = ({ isFetching, fetchingError, contactSubmissions }) => (
     <ContactSubmissionsTable
