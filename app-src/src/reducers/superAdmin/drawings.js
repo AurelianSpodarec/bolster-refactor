edf15309f895bf41cdfb57265936090f86adb306
Seb Smith <seb@silverchip.com>
2020-10-01 16:36:36 +0100

    Adds action types and reducer

--- app-src/src/reducers/superAdmin/drawings.js
+++ app-src/src/reducers/superAdmin/drawings.js
@@ -3,7 +3,10 @@ import { combineReducers } from 'redux';
 import {
     ADMIN_FETCH_DRAWINGS_FOR_COMPANY_REQUEST,
     ADMIN_FETCH_DRAWINGS_FOR_COMPANY_SUCCESS,
-    ADMIN_FETCH_DRAWINGS_FOR_COMPANY_FAILURE
+    ADMIN_FETCH_DRAWINGS_FOR_COMPANY_FAILURE,
+    ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_REQUEST,
+    ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_SUCCESS,
+    ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_FAILURE,
 } from 'constants/actionTypes/companies';
 
 import { ADMIN_MOVE_DRAWING_SUCCESS } from 'constants/actionTypes/moveTool';
@@ -13,7 +16,10 @@ import { convertArrToObj, removeObjItem } from 'helpers/generic';
 export default combineReducers({
     drawings: drawingsReducer,
     isFetching: isFetchingReducer,
-    error: errorReducer
+    error: errorReducer,
+    isPosting: isPostingReducer,
+    postSuccess: postSuccessReducer,
+    postError: postErrorReducer,
 });
 
 function isFetchingReducer(state = false, action) {
@@ -45,6 +51,40 @@ function drawingsReducer(state = {}, action) {
             return { ...state, ...convertArrToObj(action.payload) };
         case ADMIN_MOVE_DRAWING_SUCCESS:
             return removeObjItem(state, action.drawingID);
+        default:
+            return state;
+    }
+}
+
+function isPostingReducer(state = false, action) {
+    switch (action.type) {
+        case ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_REQUEST:
+            return true;
+        case ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_SUCCESS:
+        case ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_FAILURE:
+            return false;
+        default:
+            return state;
+    }
+}
+
+function postErrorReducer(state = null, action) {
+    switch (action.type) {
+        case ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_REQUEST:
+            return null;
+        case ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_FAILURE:
+            return action.error;
+        default:
+            return state;
+    }
+}
+
+function postSuccessReducer(state = false, action) {
+    switch (action.type) {
+        case ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_REQUEST:
+            return false;
+        case ADMIN_EDIT_DRAWING_EXIPIRATION_DATE_SUCCESS:
+            return true;
         default:
             return state;
     }
