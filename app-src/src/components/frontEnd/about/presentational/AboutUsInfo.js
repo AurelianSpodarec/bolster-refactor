8b5959553f2433026815b2bd3281ad54ccb3986b
Seb Smith <seb@silverchip.com>
2020-10-06 14:25:16 +0100

    Changes copy on 'About Us' page
    Former-commit-id: 081a588a312cad77353e3a32ca7f2aec617ff6b8

--- app-src/src/components/frontEnd/about/presentational/AboutUsInfo.js
+++ app-src/src/components/frontEnd/about/presentational/AboutUsInfo.js
@@ -3,9 +3,8 @@ import React from 'react';
 const AboutUsInfo = () => {
     return (
         <div className="content-page wysiwyg">
-            <h2>About Us</h2>
+            <h2>bol•ster (ˈboʊl stər) Support: Reinforce: Strengthen</h2>
             <div className="divider"></div>
-            <h3>Neque porro quisquam est qui do lorem amet?</h3>
             <p>
                 Founded in 2014, following a contractor need to comply with industry standard
                 compliance documentation the system was initially created to cover fire stopping
@@ -14,21 +13,18 @@ const AboutUsInfo = () => {
                 worldwide currently utilising the system. Bolster Systems mission is to create
                 simple applications that allow trade professionals to be more productive by
                 automating compliance documentation.
-            </p>
-            <h2>About Bolster</h2>
-            <div className="divider"></div>
-            <h3>Neque porro quisquam est qui do lorem amet?</h3>
-            <p>
+                <br />
+                <br />
                 Our dedicated team offer a mix of both construction and technology experience
                 allowing us to create software that is both at the forefront of current advancements
                 of available technology whilst also simple and effective to use in its
-                implementation. <br />
+                implementation.
                 <br />
-                We are Association for Specialist Fire Protection (ASFP) associate members. Our aim
-                is to better the industry through the medium of technology. <br />
                 <br />
-                We are Cyber Essential’s accredited meaning we always keep the system in line with
-                the latest industry standards for security and data protection.
+                Bolster Systems are Association for Specialist Fire Protection (ASFP) associate
+                members. Our aim is to better the industry through the medium of technology. We are
+                Cyber Essential’s accredited meaning we always keep the system in line with the
+                latest industry standards for security and data protection.
             </p>
         </div>
     );
