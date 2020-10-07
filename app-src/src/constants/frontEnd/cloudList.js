e3a8ad5ee38b03b89ef1cf7c632076f1a290e0eb
Mark Baker <mark@silverchip.com>
2020-10-07 13:48:10 +0100

    update content for our system page
    Former-commit-id: 8f4f850dc0b8ee75faac8fad242f314be4f4a66e

--- app-src/src/constants/frontEnd/cloudList.js
+++ app-src/src/constants/frontEnd/cloudList.js
@@ -1,58 +1,58 @@
-import PassiveFire from '_content/images/frontend-new/why-use-our-system/passive-fire-protection.png';
+import BolsterB from '_content/images/frontend-new/why-use-our-system/bolster-b.png';
 
 export const topList = [
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title:
             'Save time and money with PDF, spreadsheet and location drawing generation at the click of a button',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title:
             'Upload floorplan drawings of buildings to our bespoke pin drop system to accurately locate your findings',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title: 'Custom template creation to tailor the system to your compliance needs',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title: 'Standardise documentation throughout your business',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title: 'Multi service documentation through one portal',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title: 'Secure cloud-based data storage accessible from any location',
     },
 ];
 
 export const bottomList = [
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title: 'Track project progress in real time',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title: 'Offer your clients free access to monitor works',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title:
             'Sign on site required documentation (method statements, RAM’s, Safe Starts, Permits) directly through the app',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title: 'Schedule maintenance alerts of required upcoming works',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title: 'Ability for contractors to win projects through the system',
     },
     {
-        icon: PassiveFire,
+        icon: BolsterB,
         title: 'Ability for clients to tender projects through the system',
     },
 ];
--- app-src/src/constants/frontEnd/infoCard.js
+++ app-src/src/constants/frontEnd/infoCard.js
@@ -16,52 +16,86 @@ export const InfoCardList = [
     {
         title: 'Passive fire protection',
         icon: PassiveFire,
-        description: `<p>Bolster Systems offers the only fully integrated fire stopping app that records the whole survey, installation and inspection process.  
-        The system can be utilised on both newbuild construction sites and existing refurbishment or maintained buildings to ensure full compliance with fire regulations.<br><br>
-        Photographic reports of surveys and installations are available at the touch of a button.  
-        Clients have real time access to survey works and remedial works, giving all parties peace of mind that budgets are being utilised correctly. 
-        Standard templates for penetration sealing, collars, wraps and intumescent paint are available or users can request customised templates to meet their specific needs.</p>`,
+        description: `<p>Bolster Systems offers the only fully integrated fire stopping app that records the whole survey and fire stopping installation process, even down to printing 
+        labels with a specially coded mobile printer.
+        <br /><br />
+        Bolster Systems maps the location of all walls and floors, schedules each fire-stopped area (along with a picture of the installation), records when it was installed, what 
+        materials were used and its reference number and printed label provide a permanent record of fire-stopped locations for contractors carrying out renovation work.
+        <br /><br />
+        The system not only provides evidence of fire-stop compliance when a building is completed, it also provides building owners with a system they can use to maintain and manage 
+        an inventory of the impact of maintenance works on post-occupancy fire-barrier integrity.
+        <br /><br />
+        Drop pins have their own unique reference with a photographic record (before and after). Clients have real time access to survey works and remedial works, giving all parties 
+        peace of mind that budgets are being utilised correctly.</p>`,
+    },
+    {
+        title: 'Fire doors',
+        icon: FireDoor,
+        description: `<p>Bolster Systems Fire Door is a mobile app that assists installers and surveyors to easily adhere to Industry standards, speed up work, and automate fire 
+        door reporting. The system can track every part of a fire doors history from manufacture through to installation and future maintenance. QR codes can be added by the manufacturer 
+        that can be utilised throughout the lifespan of the door. Photos can be added at every stage to create a complete record of the door’s history. The systems templates can be 
+        customised to meet specific project requirements.</p>`,
+    },
+    {
+        title: 'Dampers / risers / ducts',
+        icon: Dampers,
+        description: `<p>Evidence of Fire Damper testing; BS 9999:2017 states: ‘... all fire dampers to be tested by a competent person on completion of the installation and at annual 
+        intervals.Completing a testing routine is insufficient for compliance. You need to maintain accurate records of when testing was carried out.
+        <br /><br />
+        Bolster Systems provides an industry-leading fire damper testing system, which includes photographic evidence of testing, to satisfy authorities and auditors that testing has 
+        been conducted.
+        <br /><br />
+        Bolster Surveyors can work on client’s drawings that are uploaded to Bolster, keeping all your compliance in one place.
+        <br /><br />
+        Our system printers can be utilised for printing inspection labels (Bolster QR labels can also be utilised). Along with a drawing generated with all inspection drop pins.</p>`,
     },
     {
         title: 'Electrical',
         icon: Electrical,
-        description: `<p>Electrical installations should be inspected and tested at regular intervals to check whether they are in a satisfactory condition for continued use as per BS 7671. The standard systems templates make installations and inspections an easy process that can be viewed and reported on by contractors’ and clients.<br><br>
-        Bespoke templates are also available to cover any other electrical items that a client may need to be periodically inspected.</p>`,
-    },
-    {
-        title: 'Project Management',
-        icon: ProjectManagement,
-        description: `<p>Bolster system can be utilised as a project management tool during a construction project.  
-        From daily inspections, area handovers and snagging our standard templates can dramatically reduce end of scheme snagging periods and cost.  
-        Any operative or supervisor can log a specific snag found including a photograph, location and who would be liable for the repair. 
-        From the desktop a specific trade by trade report can then be downloaded for issue to the relevant contractor. Areas can be photographed and logged for handover with a client including the ability to electronically sign off completed works.</p>`,
-    },
-    {
-        title: 'Fire Doors',
-        icon: FireDoor,
-        description: `<p>Track every parta fire doors history from manufacture through to installation and future maintenance using Bolster Systems mobile app. QR codes can be added by the door 
-        manufacturer highlighting installation instructions, maintenance requirements and fire test evidence.<br><br>
-        The systems templates can be customised to meet specific project requirements meaning new door installation and future maintenance can then be tracked by clients ensuring compliance.</p>`,
+        description: `<p>All electrical installations deteriorate with age and use. They should therefore be inspected and tested at regular intervals to check whether they are in a 
+        satisfactory condition for continued use. Such safety checks are commonly referred to as 'periodic inspection and testing'. Electrical testing can be carried out using Bolster 
+        Systems and our fully customisable templates complying with electrical industry standards all integrated into a fully integrated compliance solution from Bolster Systems.</p>`,
     },
     {
         title: 'Water management',
         icon: WaterManagement,
-        description: `<p>The system has standard templates that allow users to periodically test water for contaminants such as legionella.  
-        This can be used in buildings that require a water flushing regime as part of their day to day maintenance such as hospitals. 
-         Using our custom template service bespoke templates can also be created to suit you and your client’s needs.</p>`,
+        description: `<p>Hospitals need to have assurance that the quality of water used in patient treatment or in the general hospital environment is safe if public health risks are 
+        to be prevented or successfully managed. Bolster Systems not only provides a photographic record of where tests were carried out, it includes site specific test documentation 
+        along with a marked-up drawing showing locations. Bolster Systems alleviates the burden of back office report generation.</p>`,
     },
     {
-        title: 'Health and Safety',
+        title: 'Health and safety',
         icon: HealthAndSafety,
-        description: `<p>Bolster system can be utilised as part of a wider company health and safety procedure.  
-        The system can be used to create electronic safe start forms for operatives to read and sign along with method statement, permit to work and health and safety data sheet storage on the operative’s phone or tablet.  
-        The system can also be used for daily, weekly and months site safety visits either by site manager or external safety auditor.</p>`,
+        description: `<p>Bolster system can be utilised as part of a wider company health and safety procedure. The system can be used to create electronic safe start forms for operatives 
+        to read and sign along with method statement, permit to work and health and safety data sheet storage on the operative’s phone or tablet. The system can also be used for daily, 
+        weekly and months site safety visits either by site manager or external safety auditor.  All information logged.</p>`,
     },
     {
-        title: 'Windows',
-        icon: Windows,
-        description: `<p>External windows and doors can be logged and photographed at time of install allowing a contractor to issue a robust handover pack to their client on completion of the install works.<br><br>
-        A client can then use this information as part of its ongoing maintenance plan to confirm replacement parts or glazing. The system could also be used to log internal and external cleaning.</p>`,
+        title: 'Project management',
+        icon: ProjectManagement,
+        description: `<p>Bolster system can be utilised as a snagging tool during a construction project. Snagging a project from beginning to end can dramatically reduce end of scheme 
+        snagging periods and cost.  Any operative or supervisor can log a specific snag found including a photograph, location and who would be liable for the repair. From the desktop a 
+        specific trade by trade report can then be downloaded for issue to the relevant contractor.</p>`,
+    },
+    {
+        title: 'Internal walls',
+        icon: InternalWalls,
+        description: `<p>Survey existing walls for compliance or log all works completed as part of a new project. The system allows the user to document wall type, construction method, 
+        date completed, operative name and location along with photographic evidence of specific items like head details, insulation and letter boxes. Once complete the system will 
+        collate all information to allow issue of certificates or O&M information as part of a handover pack.</p>`,
+    },
+    {
+        title: 'Externals',
+        icon: ExternalWalls,
+        description:
+            '<p>From external brickwork to cladding our standard templates allow a user to log and photograph their works at installation stage giving clients piece of mind that crucial details such as ties or fire breaks have been installed correctly. </p>',
+    },
+    {
+        title: 'Asset management',
+        icon: AssetManagement,
+        description: `<p>Bolster systems can be utilised to log and periodically inspect any asset within a building.
+        Current users use if for items such as furniture and fire extinguishers.  
+        Drawings and reports can then be automatically generated to highlight location and show if replacements or servicing is required.  We can also make a bespoke template to suit your individual clients needs.</p>`,
     },
     {
         title: 'Joinery',
@@ -70,34 +104,15 @@ export const InfoCardList = [
          Bespoke templates can cover individual parts of a joinery item meaning areas susceptible to more damage and wear can be inspected and highlighted for any remedial works required.</p>`,
     },
     {
-        title: 'Dampers',
-        icon: Dampers,
-        description: `<p>Existing or newly installed dampers can be tagged with either Bolster systems pre-printed labels or QR codes enabling users to locate and inspect a buildings fire dampers as requested by British Standards BS 9999:2017.<br><br>
-        Clients have live access to view and report on inspections meaning that maintaining accurate inspection evidence becomes paperless.  The systems standard damper templates available make compliance easy, Custom templates are also available if required.</p>`,
+        title: 'Windows',
+        icon: Windows,
+        description: `<p>External windows and doors can be logged and photographed at time of install allowing a contractor to issue a robust handover pack to their client on completion of the install works.<br><br>
+        A client can then use this information as part of its ongoing maintenance plan to confirm replacement parts or glazing. The system could also be used to log internal and external cleaning.</p>`,
     },
     {
-        title: 'External Walls',
-        icon: ExternalWalls,
-        description: `<p>From external brickwork to cladding our standard templates allow a user to log and photograph their works at installation stage giving clients piece of mind that crucial details such as ties or fire breaks have been installed correctly. </p>`,
-    },
-    {
-        title: 'Internal Walls',
-        icon: InternalWalls,
-        description: `<p>Survey existing walls for compliance or log all works completed as part of a new project. 
-        The system allows the user to document wall type, construction method, date completed, operative name and location along with photographic evidence of specific items like head details, insulation and letter boxes. 
-        Once complete the system will collate all information to allow issue of certificates or O&M information as part of a handover pack.</p>`,
-    },
-    {
-        title: 'Asset Management',
-        icon: AssetManagement,
-        description: `<p>Bolster systems can be utilised to log and periodically inspect any asset within a building.
-        Current users use if for items such as furniture and fire extinguishers.  
-        Drawings and reports can then be automatically generated to highlight location and show if replacements or servicing is required.  We can also make a bespoke template to suit your individual clients needs.</p>`,
-    },
-    {
-        title: 'Any other Compliance Documentation',
+        title: 'Any other compliance documentation',
         icon: AnyOtherDocs,
-        description: `<p>The Bolster system is fully dynamic and so can be individually tailored to our client’s needs.  We can therefore offer bespoke templates for additional services not currently mentioned in our other services. Please contact the Bolster team for further information.
-        </p>`,
+        description: `<p>The Bolster system is fully dynamic and so can be individually tailored to our client’s needs.  We can therefore offer bespoke templates for additional services 
+        not currently mentioned in our other services. Please contact the Bolster team for further information.</p>`,
     },
 ];
