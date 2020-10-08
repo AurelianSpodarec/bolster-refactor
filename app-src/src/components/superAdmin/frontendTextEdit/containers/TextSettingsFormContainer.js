26bad8e08558c909624e36e1a2985abeb47eb2fc
Seb Smith <seb@silverchip.com>
2020-10-08 16:29:51 +0100

    Creates basic skeleton for editing frontend web text from super admin
    Former-commit-id: ad236281b68d43e04f109c08a9b471f8ff9931d2

--- app-src/src/components/superAdmin/frontendTextEdit/containers/TextSettingsFormContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import TextSettingsForm from '../presentational/TextSettingsForm';
-
-const TextSettingsFormContainer = () => {
-    return <TextSettingsForm />;
-};
-
-export default TextSettingsFormContainer;
