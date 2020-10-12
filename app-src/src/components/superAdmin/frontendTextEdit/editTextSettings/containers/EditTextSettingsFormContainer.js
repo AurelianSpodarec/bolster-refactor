7692ee687757b2874c6e997ce1b14df26fc48b4a
Seb Smith <seb@silverchip.com>
2020-10-12 16:33:48 +0100

    Plugs in api to display current text in settings display
    Former-commit-id: 7c84bade43b55d7188b031f86c29f92fa81844b7

--- app-src/src/components/superAdmin/frontendTextEdit/editTextSettings/containers/EditTextSettingsFormContainer.js
+++ app-src/src/components/superAdmin/frontendTextEdit/editTextSettings/containers/EditTextSettingsFormContainer.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { useForm, usePrevious } from 'helpers/hooks';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import fetchFrontendText from 'actions/superAdmin/frontendSite/textSettings/async/fetchFrontendText';
 import updateFrontendText from 'actions/superAdmin/frontendSite/textSettings/async/updateFrontendText';
+import { showModal } from 'actions/shared/generic/modals/sync/showModal';
+import { SUCCESS_MODAL, ERROR_MODAL } from 'constants/shared/modalTypes';
 
 import EditTextSettingsForm from '../presentational/EditTextSettingsForm';
 
@@ -14,6 +17,9 @@ const EditTextSettingsFormContainer = ({
     isFetching,
     frontendText,
     fetchFrontendText,
+    postSuccess,
+    history,
+    showModal,
 }) => {
     const { loginText, registerText } = frontendText;
 
@@ -22,24 +28,46 @@ const EditTextSettingsFormContainer = ({
         registerText: registerText || '',
     });
 
-    const prevProps = usePrevious({ isFetching });
+    const prevProps = usePrevious({ isFetching, postSuccess, error });
 
     useEffect(() => {
         fetchFrontendText();
     }, []);
 
     useEffect(() => {
-        if (!isFetching && isFetching !== prevProps.isFetching) {
+        if (!isFetching && prevProps.isFetching) {
             handleChange('loginText', loginText);
             handleChange('registerText', registerText);
         }
-    }, [isFetching, prevProps.isFetching]);
+
+        if (postSuccess && !prevProps.postSuccess) {
+            showModal(SUCCESS_MODAL, {
+                message: 'The text has been successfully changed',
+            });
+            return history.replace('/admin/text-settings');
+        }
+
+        if (error && !prevProps.error) {
+            showModal(ERROR_MODAL, {
+                title: error.title || 'Error',
+                message: error.message,
+            });
+        }
+    }, [
+        isFetching,
+        prevProps.isFetching,
+        postSuccess,
+        prevProps.postSuccess,
+        error,
+        prevProps.error,
+    ]);
 
     return (
         <EditTextSettingsForm
             {...formData}
             handleChange={handleChange}
             handleSubmit={handleSubmit}
+            isPosting={isPosting}
         />
     );
 
@@ -51,15 +79,18 @@ const EditTextSettingsFormContainer = ({
 
 const mapStateToProps = ({
     superAdmin: {
-        frontendTextSettingsReducer: { isPosting, error, frontendText, isFetching },
+        frontendTextSettingsReducer: { isPosting, error, frontendText, isFetching, postSuccess },
     },
 }) => ({
     isPosting,
     error,
     isFetching,
     frontendText,
+    postSuccess,
 });
 
-const mapDispatchToProps = { updateFrontendText, fetchFrontendText };
+const mapDispatchToProps = { updateFrontendText, fetchFrontendText, showModal };
 
-export default connect(mapStateToProps, mapDispatchToProps)(EditTextSettingsFormContainer);
+export default withRouter(
+    connect(mapStateToProps, mapDispatchToProps)(EditTextSettingsFormContainer),
+);
