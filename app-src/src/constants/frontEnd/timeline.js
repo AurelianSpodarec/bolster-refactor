976afedc39743b73f6cd9549a667cf91ef1e3fcf
Seb Smith <seb@silverchip.com>
2020-10-13 14:13:11 +0100

    Adds icons to the 'How it works' timeline
    Former-commit-id: 311aaf1acb54453701e4987a1a449c03a7cfb25f

--- app-src/src/constants/frontEnd/timeline.js
+++ app-src/src/constants/frontEnd/timeline.js
@@ -1,11 +1,20 @@
 import BespokeTemplatesIcon from '_content/images/frontend-new/how-it-works/timeline/bespoke-templates.png';
+import CreateUsersIcon from '_content/images/frontend-new/how-it-works/timeline/Create-Users.png';
+import CreateSitesAndUploadIcon from '_content/images/frontend-new/how-it-works/timeline/Create–sites-&-upload-floorplan-drawings.png';
+import UploadAndSignIcon from '_content/images/frontend-new/how-it-works/timeline/Attach-site-required-documentation.png';
+import DropPinsIcon from '_content/images/frontend-new/how-it-works/timeline/Drop-pins-on-drawings.png';
+import UpsyncWorkIcon from '_content/images/frontend-new/how-it-works/timeline/Up-sync-work–to-the-cloud.png';
+import GrantsClientsFreeAccessIcon from '_content/images/frontend-new/how-it-works/timeline/Grant-clients-free-access.png';
+import GenerateUnlimitedPDFIcon from '_content/images/frontend-new/how-it-works/timeline/Generate-PDF-CSV-&-Floorplan-reports.png';
+import TransferRecieveIcon from '_content/images/frontend-new/how-it-works/timeline/Transfer-receive-works-from-other-Bolster-companies-free–of-charge.png';
+import ManageJobsIcon from '_content/images/frontend-new/how-it-works/timeline/Schedule-alerts-on-sites-for-future-works.png';
 
 export default [
     {
         title: 'Create Users',
         description:
             '<p>Create an unlimited number of Admins/Operatives through the Bolster desktop.</p>',
-        icon: BespokeTemplatesIcon,
+        icon: CreateUsersIcon,
     },
     {
         title: 'Create bespoke templates',
@@ -17,50 +26,50 @@ export default [
         title: 'Create sites & upload floorplan drawings',
         description:
             '<p>Upload PDF or JPEG floorplan drawings for sites you are working on to locate any defects or remedial works.</p>',
-        icon: BespokeTemplatesIcon,
+        icon: CreateSitesAndUploadIcon,
     },
     {
         title: 'Upload and sign on site required documentation',
         description: `<p>Upload PDF or JPEG documents such as Data Sheets, Method Statements, RAM’s, permits & Safe Starts to sites to ensure all essential documentation is available to 
         operatives.</p>
         <p>Operatives will then have the ability to view and digitally sign documents directly through the Bolster Systems app ensuring your company is compliant with industry standards</p>`,
-        icon: BespokeTemplatesIcon,
+        icon: UploadAndSignIcon,
     },
     {
         title: 'Drop pins on drawings to document works',
         description:
             '<p>Use the colour coded pin system and bespoke templates to locate and document any survey, remedial or maintenance works being carried out on site.</p>',
-        icon: BespokeTemplatesIcon,
+        icon: DropPinsIcon,
     },
     {
         title: 'Up-sync work to the cloud & track real time through the company dashboard',
         description: `<p>Send any data documented on site to the Bolster Systems dedicated cloud-based servers to be accessed by company admins through the desktop.</p>
         <p>Through the desktop admins can keep track of their company’s progress across all live projects.</p>`,
-        icon: BespokeTemplatesIcon,
+        icon: UpsyncWorkIcon,
     },
     {
         title: 'Grant client’s free access',
         description:
             '<p>Invite client’s free of charge for read-only access to required sites to view progress ensuring transparency between all parties</p>',
-        icon: BespokeTemplatesIcon,
+        icon: GrantsClientsFreeAccessIcon,
     },
     {
         title: 'Generate unlimited PDF, CSV, Floorplan reports & O&M Manuals',
         description: `<p>Bolster Systems report generator will drastically reduce your time spent collating data allowing you to generate PDF, Spreadsheet and Floorplan reports in a 
         matter of seconds.</p>
         <p>Download Data Sheets for all materials used within a project to include in your O & M upon completion of works.</p>`,
-        icon: BespokeTemplatesIcon,
+        icon: GenerateUnlimitedPDFIcon,
     },
     {
         title: 'Transfer/receive works from other Bolster companies free of charge',
         description: `<p>Work alongside other Bolster Approved Companies on the same drawings enabling all stages of work are documented in one place to ensure complete transparency 
         between all parties.</p>`,
-        icon: BespokeTemplatesIcon,
+        icon: TransferRecieveIcon,
     },
     {
         title: 'Manage jobs and set up alerts for scheduled future works',
         description: `<p>Manage on-going or completed projects on a daily basis through the desktop admin area.</p>
         <p>Schedule email and system alerts for future survey or remedial actions required across sites.</p>`,
-        icon: BespokeTemplatesIcon,
+        icon: ManageJobsIcon,
     },
 ];
