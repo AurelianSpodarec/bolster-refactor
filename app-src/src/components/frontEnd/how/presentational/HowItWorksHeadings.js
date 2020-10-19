dd76eeeb797c5f2bc2631d9f295e327419814f23
Mark Baker <mark@silverchip.com>
2020-10-19 11:06:15 +0100

    revert changes to icons on how it works header area
    Former-commit-id: d06211212cc6b1b70796c72c279561040d6de973

--- app-src/src/components/frontEnd/how/presentational/HowItWorksHeadings.js
+++ app-src/src/components/frontEnd/how/presentational/HowItWorksHeadings.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import InspectIcon from '_content/images/frontend-new/how-it-works/inspect.png';
-import LocateIcon from '_content/images/frontend-new/how-it-works/locate.png';
-import DocumentIcon from '_content/images/frontend-new/how-it-works/document.png';
-import ManageIcon from '_content/images/frontend-new/how-it-works/manage.png';
+import InspectIcon from '_content/images/frontend-new/how-it-works/inspect2.png';
+import LocateIcon from '_content/images/frontend-new/how-it-works/locate2.png';
+import DocumentIcon from '_content/images/frontend-new/how-it-works/document2.png';
+import ManageIcon from '_content/images/frontend-new/how-it-works/manage2.png';
 
 const HowItWorksHeadings = () => (
     <div className="section how-it-works-headings">
@@ -12,20 +12,36 @@ const HowItWorksHeadings = () => (
                 <div className="icon">
                     <img alt="Inspect" src={InspectIcon} />
                 </div>
+                <div className='text'>
+                    <h3>Inspect</h3>
+                    <div className='divider' />
+                </div>
             </div>
             <div className="item">
                 <div className="icon">
                     <img alt="Locate" src={LocateIcon} />
+                </div>
+                <div className='text'>
+                    <h3>Locate</h3>
+                    <div className='divider' />
                 </div>
             </div>
             <div className="item">
                 <div className="icon">
                     <img alt="Document" src={DocumentIcon} />
                 </div>
+                <div className='text'>
+                    <h3>Document</h3>
+                    <div className='divider' />
+                </div>
             </div>
             <div className="item">
                 <div className="icon">
                     <img alt="Manage" src={ManageIcon} />
+                </div>
+                <div className='text'>
+                    <h3>Manage</h3>
+                    <div className='divider' />
                 </div>
             </div>
         </div>
