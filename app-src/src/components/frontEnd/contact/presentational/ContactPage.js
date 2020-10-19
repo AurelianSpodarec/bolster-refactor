c61d6b5ed0d2cd5dcb0cc5f5eba7e71f96ee91f9
Mark Baker <mark@silverchip.com>
2020-10-19 17:25:06 +0100

    update contact page to look lime login and register
    Former-commit-id: e6e745ae2236c53b46d841eed01595a40fae061c

--- app-src/src/components/frontEnd/contact/presentational/ContactPage.js
+++ app-src/src/components/frontEnd/contact/presentational/ContactPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ContactInfoContainer from '../containers/ContactInfoContainer';
+import ContactInfo from './ContactInfo';
 import ContactPageFormContainer from '../containers/ContactPageFormContainer';
 import ServerRoomBackgroundVideo from '_content/videos/frontend/Server_Room_Long.mp4';
 import Helmet from 'components/frontEnd/shared/meta/presentational/Helmet';
@@ -19,9 +19,13 @@ const ContactPage = () => {
                 >
                     <source src={ServerRoomBackgroundVideo} type="video/mp4" />
                 </video>
-                <div className="contact-form-wrapper">
-                    <div className="heading">
-                        <h1>Contact</h1>
+                <div className="auth-wrapper">
+                    <div className="heading-body-wrapper">
+                        <div className="auth-heading">
+                            <heading>
+                            <h1>Contact</h1>
+                            </heading>
+                        </div>
                         <p>
                             Bolster Systems offer a full demo exploring both the
                             desktop and application platforms of the system, its functionality, ease
@@ -29,14 +33,13 @@ const ContactPage = () => {
                             <br />
                             <br />
                             If you would like any further information on our system or to arrange a
-                            demo, please fill in the contact form below and a member of the Bolster
+                            demo, please fill in the contact form and a member of the Bolster
                             Systems team will get back to you.
                         </p>
+                        <ContactInfo />
                     </div>
-                    <div className="contact-section">
-                        <ContactPageFormContainer />
-                        <ContactInfoContainer />
-                    </div>
+                        
+                    <ContactPageFormContainer />
                 </div>
             </div>
         </>
--- app-src/src/components/frontEnd/contact/presentational/ContactPageForm.js
+++ app-src/src/components/frontEnd/contact/presentational/ContactPageForm.js
@@ -7,66 +7,78 @@ import TextInputContainer from 'components/shared/generic/form/containers/TextIn
 import FrontEndButton from 'components/frontEnd/shared/buttons/presentational/FrontEndButton';
 import Form from 'components/shared/generic/form/containers/Form';
 import LoadingIcon from 'components/shared/generic/misc/presentational/LoadingIcon';
+import FrontEndFormHeading from 'components/frontEnd/shared/forms/presentational/FrontEndFormHeading';
 
 const ContactPageForm = ({ form, handleChange, handleSubmit, sent, isPosting }) => {
     if (sent) {
         return (
-            <div className="sent-message-wrapper">
-                <p>Thank you! We will be in touch shortly.</p>
+            <div className="auth-form-wrapper contact">
+                <FrontEndFormHeading title="Success" subtitle='Thank you! We will be in touch shortly.' />
             </div>
         );
     }
 
     return (
-        <Form id="contact-form-content" className="contact-form" onSubmit={handleSubmit}>
-            <Field name="Name" required>
-                <TextInputContainer
-                    name="name"
-                    value={form.name}
-                    handleChange={handleChange}
-                    required
-                />
-            </Field>
-            <Field name="Email" required>
-                <TextInputContainer
-                    name="email"
-                    value={form.email}
-                    handleChange={handleChange}
-                    required
-                />
-            </Field>
-            <Field name="Company" required>
-                <TextInputContainer
-                    name="companyName"
-                    value={form.companyName}
-                    handleChange={handleChange}
-                    required
-                />
-            </Field>
-            <Field name="Contact Number" required>
-                <TextInputContainer
-                    name="contactNumber"
-                    value={form.contactNumber}
-                    handleChange={handleChange}
-                    required
-                />
-            </Field>
-            <Field classes="submit-wrapper">
-                <ReCaptcha
-                    name="reCaptchaToken"
-                    value={form.reCaptchaToken}
-                    onChange={handleChange}
-                    required
-                />
-                <FrontEndButton
-                    classes={`gray ${isPosting ? 'disabled' : ''}`}
-                    type="submit"
-                    disabled={isPosting}
-                >
-                    {isPosting ? <LoadingIcon /> : 'Submit'}
-                </FrontEndButton>
-            </Field>
-        </Form>
+        <div className="auth-form-wrapper contact">
+            <FrontEndFormHeading title="Enter your details" />
+            <Form id="contact-form-content" className="contact-form" onSubmit={handleSubmit}>
+                <Field name="Name" classes="auth-form-field" required>
+                    <TextInputContainer
+                        name="name"
+                        value={form.name}
+                        handleChange={handleChange}
+                        classes="auth-text-input-container"
+                        required
+                    />
+                </Field>
+                <Field name="Email" classes="auth-form-field" required>
+                    <TextInputContainer
+                        name="email"
+                        value={form.email}
+                        handleChange={handleChange}
+                        classes="auth-text-input-container"
+                        required
+                    />
+                </Field>
+                <Field name="Company" classes="auth-form-field" required>
+                    <TextInputContainer
+                        name="companyName"
+                        value={form.companyName}
+                        handleChange={handleChange}
+                        classes="auth-text-input-container"
+                        required
+                    />
+                </Field>
+                <Field name="Contact Number" classes="auth-form-field" required>
+                    <TextInputContainer
+                        name="contactNumber"
+                        value={form.contactNumber}
+                        handleChange={handleChange}
+                        classes="auth-text-input-container"
+                        required
+                    />
+                </Field>
+                <Field classes="submit-wrapper auth-form-field row between">
+                    <div>
+                        <ReCaptcha
+                            name="reCaptchaToken"
+                            value={form.reCaptchaToken}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    
+                    <FrontEndButton
+                        classes={`gray ${isPosting ? 'disabled' : ''}`}
+                        type="submit"
+                        disabled={isPosting}
+                    >
+                        {isPosting ? <LoadingIcon /> : 'Submit'}
+                    </FrontEndButton>
+                </Field>
+            </Form>
+        </div>
+        
     );
 };
 
