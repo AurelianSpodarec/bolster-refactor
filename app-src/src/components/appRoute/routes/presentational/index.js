177ccfa0eebb4c98d259b96d47b257cb662b0901
Mark Baker <mark@silverchip.com>
2020-10-20 13:27:22 +0100

    add better google analytics
    Former-commit-id: e0fbe390f04d3047c52b8e2d66f6a2c69d8faa1b

--- app-src/src/components/appRoute/routes/presentational/index.js
+++ app-src/src/components/appRoute/routes/presentational/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
+import { usePrevious } from 'helpers/hooks';
 
 import NotFound from 'components/shared/notFound/presentational/NotFound';
 import SwitchWith404 from './SwitchWith404';
@@ -11,15 +14,32 @@ import CompanyAppContainer from 'components/companyAdmin/app/app/containers/Comp
 import ClientAppContainer from 'components/client/app/app/containers/ClientAppContainer';
 import FrontEndAppContainer from 'components/frontEnd/app/app/containers/FrontEndAppContainer';
 
-const Routes = () => (
-    <SwitchWith404>
-        <Route exact path="/404" component={NotFound} />
-        <Route path="/auth" component={FrontEndAppContainer} />
-        <Route path="/admin" component={AdminAppContainer} />
-        <Route path="/company" component={CompanyAppContainer} />
-        <Route path="/client" component={ClientAppContainer} />
-        <Route path="/" component={FrontEndAppContainer} />
-    </SwitchWith404>
-);
+const Routes = () => {
+    const location = useLocation();
+    const prevProps = usePrevious({ location });
+
+    useEffect(() => {
+        ReactGA.initialize('UA-132047777-1');
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+
+    useEffect(() => {
+        if (location.pathname !== prevProps.location.pathname) {
+            ReactGA.set({ page: location.pathname });
+            ReactGA.pageview(location.pathname + location.search);
+        }
+    }, [location.pathname, prevProps.location]);
+
+    return (
+        <SwitchWith404>
+            <Route exact path="/404" component={NotFound} />
+            <Route path="/auth" component={FrontEndAppContainer} />
+            <Route path="/admin" component={AdminAppContainer} />
+            <Route path="/company" component={CompanyAppContainer} />
+            <Route path="/client" component={ClientAppContainer} />
+            <Route path="/" component={FrontEndAppContainer} />
+        </SwitchWith404>
+    );
+};
 
 export default Routes;
