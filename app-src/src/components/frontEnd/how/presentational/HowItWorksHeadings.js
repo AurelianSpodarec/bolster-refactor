fdfe7edaa5d63f104189f0bdfa5f309403dd952a
Seb Smith <seb@silverchip.com>
2020-10-28 09:47:42 +0000

    Removes text from how it works header area
    Former-commit-id: 7c606577de89056d6883b7deae18ead3ef60dff3

--- app-src/src/components/frontEnd/how/presentational/HowItWorksHeadings.js
+++ app-src/src/components/frontEnd/how/presentational/HowItWorksHeadings.js
@@ -12,36 +12,20 @@ const HowItWorksHeadings = () => (
                 <div className="icon">
                     <img alt="Inspect" src={InspectIcon} />
                 </div>
-                <div className='text'>
-                    <h3>Inspect</h3>
-                    <div className='divider' />
-                </div>
             </div>
             <div className="item">
                 <div className="icon">
                     <img alt="Locate" src={LocateIcon} />
-                </div>
-                <div className='text'>
-                    <h3>Locate</h3>
-                    <div className='divider' />
                 </div>
             </div>
             <div className="item">
                 <div className="icon">
                     <img alt="Document" src={DocumentIcon} />
                 </div>
-                <div className='text'>
-                    <h3>Document</h3>
-                    <div className='divider' />
-                </div>
             </div>
             <div className="item">
                 <div className="icon">
                     <img alt="Manage" src={ManageIcon} />
-                </div>
-                <div className='text'>
-                    <h3>Manage</h3>
-                    <div className='divider' />
                 </div>
             </div>
         </div>
