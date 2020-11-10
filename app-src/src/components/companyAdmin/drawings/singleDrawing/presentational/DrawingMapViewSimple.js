e9f966ae87965de9773d1eef15b2534e8f8c4b07
Seb Smith <seb@silverchip.com>
2020-11-10 09:10:22 +0000

    Hides areas when drawing start date not started

--- app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/DrawingMapViewSimple.js
+++ app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/DrawingMapViewSimple.js
@@ -9,10 +9,7 @@ import { CRS } from 'leaflet';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 import CustomPin from 'components/shared/pins/map/presentational/CustomPin';
 import Loading from 'components/shared/generic/misc/presentational/Loading';
-import {
-    ACCESS_TYPES_VALUES,
-    FLOORPLAN_STATES,
-} from 'constants/companyAdmin/enums';
+import { ACCESS_TYPES_VALUES, FLOORPLAN_STATES } from 'constants/companyAdmin/enums';
 import { EDIT_DRAWING } from 'constants/shared/modalTypes';
 import MapPinContainer from 'components/shared/pins/map/containers/MapPinContainer';
 import RedX from 'components/shared/pins/map/presentational/RedX';
@@ -22,7 +19,7 @@ import AddCreditsToDrawingButtonContainer from '../../addCreditsToDrawing/contai
 import DrawingMapAddZone from './DrawingMapAddZone';
 import DrawingMapViewZones from './DrawingMapViewZones';
 
-const getDataUrl = (src) => `${FILE_STORAGE_URL}/${src}/{z}/{x}/{y}.jpg`;
+const getDataUrl = src => `${FILE_STORAGE_URL}/${src}/{z}/{x}/{y}.jpg`;
 
 const DrawingMapViewSimple = ({
     position,
@@ -60,18 +57,17 @@ const DrawingMapViewSimple = ({
     hasZoneCoords,
     handleZoomChange,
     curZoom,
-    shouldRestrictPayments
+    shouldRestrictPayments,
+    drawingNotStarted,
 }) => {
     const mapRef = useRef();
 
     const newPinIcon = L.divIcon({
         className: '',
-        html: ReactDOMServer.renderToString(
-            <CustomPin pinColour="red" history={history} />
-        ),
+        html: ReactDOMServer.renderToString(<CustomPin pinColour="red" history={history} />),
         iconSize: [30, 50],
         iconAnchor: [15, 50],
-        popupAnchor: [0, -50]
+        popupAnchor: [0, -50],
     });
 
     const cornerClickedIcon = L.divIcon({
@@ -79,7 +75,7 @@ const DrawingMapViewSimple = ({
         html: ReactDOMServer.renderToString(<RedX />),
         iconSize: [30, 50],
         iconAnchor: [15, 50],
-        popupAnchor: [0, -50]
+        popupAnchor: [0, -50],
     });
     const shouldShowFloorplan = !!drawing.tilesetS3Key && !updating;
 
@@ -100,15 +96,9 @@ const DrawingMapViewSimple = ({
                             drawing.accessType === ACCESS_TYPES_VALUES.OWNER &&
                             !shouldRestrictPayments && (
                                 <>
-                                    <AddCreditsToDrawingButtonContainer
-                                        drawing={drawing}
-                                    />
-                                    <button
-                                        onClick={() => {}}
-                                        className="button red pull-right"
-                                    >
-                                        <i className="far fa-times" /> Drawing
-                                        expired
+                                    <AddCreditsToDrawingButtonContainer drawing={drawing} />
+                                    <button onClick={() => {}} className="button red pull-right">
+                                        <i className="far fa-times" /> Drawing expired
                                     </button>
                                 </>
                             )
@@ -122,8 +112,7 @@ const DrawingMapViewSimple = ({
                                                 to={`${drawing.id}/add-pin`}
                                                 className="button green pull-right"
                                             >
-                                                <i className="fa fa-check" />{' '}
-                                                Confirm position
+                                                <i className="fa fa-check" /> Confirm position
                                             </button>
                                             <button
                                                 className="button red pull-right"
@@ -133,30 +122,28 @@ const DrawingMapViewSimple = ({
                                             </button>
                                         </>
                                     ) : (
-                                        <button
-                                            className="button green pull-right"
-                                            onClick={toggleAddMode}
-                                        >
-                                            <i className="fa fa-plus" /> Add pin
-                                        </button>
+                                        !drawingNotStarted && (
+                                            <button
+                                                className="button green pull-right"
+                                                onClick={toggleAddMode}
+                                            >
+                                                <i className="fa fa-plus" /> Add pin
+                                            </button>
+                                        )
                                     )}
-                                    {drawing.accessType ===
-                                        ACCESS_TYPES_VALUES.OWNER &&
+                                    {drawing.accessType === ACCESS_TYPES_VALUES.OWNER &&
                                         !shouldRestrictPayments && (
                                             <>
                                                 <button
                                                     className="button yellow"
                                                     onClick={() =>
-                                                        showModal(
-                                                            EDIT_DRAWING,
-                                                            {
-                                                                drawing
-                                                            }
-                                                        )
+                                                        showModal(EDIT_DRAWING, {
+                                                            drawing,
+                                                        })
                                                     }
                                                 >
-                                                    <i className="far fa-pencil fa-fw" />{' '}
-                                                    Edit drawing
+                                                    <i className="far fa-pencil fa-fw" /> Edit
+                                                    drawing
                                                 </button>
 
                                                 <AddCreditsToDrawingButtonContainer
@@ -174,13 +161,9 @@ const DrawingMapViewSimple = ({
                         zoom={zoom}
                         minZoom={0}
                         maxZoom={8}
-                        onClick={(e) => handleClick(e)}
+                        onClick={e => handleClick(e)}
                         crs={CRS.Simple}
-                        onzoomend={() =>
-                            handleZoomChange(
-                                mapRef.current.leafletElement.getZoom()
-                            )
-                        }
+                        onzoomend={() => handleZoomChange(mapRef.current.leafletElement.getZoom())}
                         className={!showZones ? 'hide-tooltips' : ''}
                     >
                         <TileLayer
@@ -195,48 +178,30 @@ const DrawingMapViewSimple = ({
                         ) : (
                             <>
                                 {showZones && (
-                                    <DrawingMapViewZones
-                                        curZoom={curZoom}
-                                        zones={zones}
-                                    />
+                                    <DrawingMapViewZones curZoom={curZoom} zones={zones} />
                                 )}
-                                {pins.map((pin) => (
+                                {pins.map(pin => (
                                     <MapPinContainer
                                         updateCurTooltip={updateCurTooltip}
-                                        tooltipVisible={
-                                            currentTooltip === pin.id
-                                        }
+                                        tooltipVisible={currentTooltip === pin.id}
                                         urlStart="company"
                                         key={pin.id}
                                         pin={pin}
-                                        withLink={
-                                            !shouldShowPinSelectorOptions &&
-                                            !addMode
-                                        }
+                                        withLink={!shouldShowPinSelectorOptions && !addMode}
                                         withTooltip={!isExcluding}
                                         isExcluding={isExcluding}
                                     />
                                 ))}
-                                {addMode && (
-                                    <Marker
-                                        position={addPinPosition}
-                                        icon={newPinIcon}
-                                    />
-                                )}
+                                {addMode && <Marker position={addPinPosition} icon={newPinIcon} />}
                                 {cornerClicked && (
-                                    <Marker
-                                        position={cornerClicked}
-                                        icon={cornerClickedIcon}
-                                    />
+                                    <Marker position={cornerClicked} icon={cornerClickedIcon} />
                                 )}
 
-                                {rectangles.map((rectangle) => (
+                                {rectangles.map(rectangle => (
                                     <Rectangle
                                         key={rectangle.id}
                                         rectangle={rectangle}
-                                        onClick={() =>
-                                            handleDelete(rectangle.id)
-                                        }
+                                        onClick={() => handleDelete(rectangle.id)}
                                     />
                                 ))}
                             </>
@@ -256,28 +221,18 @@ const DrawingMapViewSimple = ({
                                             }`}
                                             onClick={showAddZoneModal}
                                         >
-                                            <i className="far fa-check fa-fw" />{' '}
-                                            Finish
+                                            <i className="far fa-check fa-fw" /> Finish
                                         </button>
-                                        <button
-                                            className="button grey"
-                                            onClick={cancelZoneAdd}
-                                        >
+                                        <button className="button grey" onClick={cancelZoneAdd}>
                                             Cancel
                                         </button>
                                     </>
                                 ) : (
                                     <>
-                                        <button
-                                            className="button blue"
-                                            onClick={handleZoneAdd}
-                                        >
+                                        <button className="button blue" onClick={handleZoneAdd}>
                                             View Zones
                                         </button>
-                                        <button
-                                            className="button blue"
-                                            onClick={toggleZones}
-                                        >
+                                        <button className="button blue" onClick={toggleZones}>
                                             Toggle zones on/off
                                         </button>
                                         {showZones && (
@@ -289,10 +244,8 @@ const DrawingMapViewSimple = ({
                                                     max="1"
                                                     value={zonesOpacity}
                                                     step="0.1"
-                                                    onChange={(e) =>
-                                                        handleOpacityChange(
-                                                            e.target.value
-                                                        )
+                                                    onChange={e =>
+                                                        handleOpacityChange(e.target.value)
                                                     }
                                                 />
                                             </div>
@@ -302,8 +255,7 @@ const DrawingMapViewSimple = ({
                             </div>
                         )}
                 </div>
-            ) : drawing.latestFloorplanState ===
-              FLOORPLAN_STATES.FAILEDCANCELLED ? (
+            ) : drawing.latestFloorplanState === FLOORPLAN_STATES.FAILEDCANCELLED ? (
                 <button
                     className="button yellow"
                     onClick={() => showModal(EDIT_DRAWING, { drawing })}
