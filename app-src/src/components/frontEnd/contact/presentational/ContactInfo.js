32b799f372557775af8e0901cd9c7e7e3b836a04
Seb Smith <seb@silverchip.com>
2020-11-13 09:50:10 +0000

    Adds mobile responsiveness to contact form
    Former-commit-id: 472ceff3a4c74324cfa94f1ac015b7abb4a5a63f [formerly 89c53b149ab264a31e1fefe625205ea0c60b6aef] [formerly 6eb924a1bb82803fcb0c5155c8b5bd1224d3f377 [formerly fa76fae5d0f860dc45e2ad09a34bc9c354285ba3]]
    Former-commit-id: 41301947bc505a5668f8a8528b9d7f7590ffb522 [formerly 3215ac53be4c5994bcc1734be4881a3dadf171de]
    Former-commit-id: f883f228b541bfa84fdcd76cee0ac4f560e14f00

--- app-src/src/components/frontEnd/contact/presentational/ContactInfo.js
+++ app-src/src/components/frontEnd/contact/presentational/ContactInfo.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CustomLineBreak from 'components/shared/generic/linebreak/CustomLineBreak';
+
 const ContactInfo = () => {
     return (
         <div className="contact-info-wrapper">
@@ -7,6 +9,7 @@ const ContactInfo = () => {
                 <p className="full">
                     <strong>Company Address</strong>
                     <br />
+                    <CustomLineBreak sizeClasses="small" />
                     The Studio, The Schoolhouse, 2nd Avenue
                     <br />
                     Trafford Park
@@ -20,11 +23,13 @@ const ContactInfo = () => {
                 <p>
                     <strong>Phone</strong>
                     <br />
+                    <CustomLineBreak sizeClasses="small" />
                     <a href="tel:01618737679">0161 873 7679</a>
                 </p>
                 <p>
                     <strong>Email</strong>
                     <br />
+                    <CustomLineBreak sizeClasses="small" />
                     <a href="mailto:info@bolstersystems.com">info@bolstersystems.com</a>
                 </p>
             </div>
