c2f8a656dfe0680c54dddeb39e1c552cab014c93
Rob Fairclough <rob@silverchip.com>
2020-11-17 16:23:21 +0000

    re-add fragment to avoid merge conflicts

--- app-src/src/components/shared/layout/menu/presentational/SuperAdminMenu.js
+++ app-src/src/components/shared/layout/menu/presentational/SuperAdminMenu.js
@@ -5,73 +5,80 @@ import MenuItemContainer from '../containers/MenuItemContainer';
 import AdminPinOptionsMenuItems from './AdminPinOptionsMenuItems';
 
 const SuperAdminMenu = ({ logout, unreadRequests }) => (
-    <div className="menu">
-        <MenuItemContainer link="/admin" base>
-            <i className="fa fa-home icon" /> <span className="menu-text">Dashboard</span>
-        </MenuItemContainer>
+    <>
+        <div className="menu">
+            <MenuItemContainer link="/admin" base>
+                <i className="fa fa-home icon" /> <span className="menu-text">Dashboard</span>
+            </MenuItemContainer>
 
-        <MenuItemContainer link="/admin/company-reports">
-            <i className="far fa-file-export icon" />{' '}
-            <span className="menu-text">Company Reports</span>
-        </MenuItemContainer>
+            <MenuItemContainer link="/admin/company-reports">
+                <i className="far fa-file-export icon" />{' '}
+                <span className="menu-text">Company Reports</span>
+            </MenuItemContainer>
 
-        <MenuItemContainer link="/admin/invoices">
-            <i className="fa fa-file icon" /> <span className="menu-text">Invoices</span>
-        </MenuItemContainer>
+            <MenuItemContainer link="/admin/invoices">
+                <i className="fa fa-file icon" /> <span className="menu-text">Invoices</span>
+            </MenuItemContainer>
 
-        <MenuItemContainer link="/admin/companies">
-            <i className="fa fa-users icon" /> <span className="menu-text">Companies</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/users">
-            <i className="fa fa-user icon" /> <span className="menu-text">Users</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/move-tool">
-            <i className="fa fa-layer-group icon" /> <span className="menu-text">Move Tool</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/merge-tool">
-            <i className="fa fa-code-merge icon" /> <span className="menu-text">Merge Tool</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/expiry-tool">
-            <i className="fa fa-clock icon" /> <span className="menu-text">Expiry Tool</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/recently-extended">
-            <i className="fa fa-history icon" />{' '}
-            <span className="menu-text">Recently Extended Drawings</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/services">
-            <i className="fa fa-folder-open icon" /> <span className="menu-text">Services</span>
-        </MenuItemContainer>
+            <MenuItemContainer link="/admin/companies">
+                <i className="fa fa-users icon" /> <span className="menu-text">Companies</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/users">
+                <i className="fa fa-user icon" /> <span className="menu-text">Users</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/move-tool">
+                <i className="fa fa-layer-group icon" />{' '}
+                <span className="menu-text">Move Tool</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/merge-tool">
+                <i className="fa fa-code-merge icon" />{' '}
+                <span className="menu-text">Merge Tool</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/expiry-tool">
+                <i className="fa fa-clock icon" /> <span className="menu-text">Expiry Tool</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/recently-extended">
+                <i className="fa fa-history icon" />{' '}
+                <span className="menu-text">Recently Extended Drawings</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/services">
+                <i className="fa fa-folder-open icon" /> <span className="menu-text">Services</span>
+            </MenuItemContainer>
 
-        <AdminPinOptionsMenuItems />
-        <MenuItemContainer link="/admin/enquiries">
-            <i className="far fa-phone icon" /> <span className="menu-text">User Enquiries</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/demo-requests">
-            {!!unreadRequests && <span className="number">{unreadRequests}</span>}
-            <i className="far fa-video icon" />{' '}
-            <span className={`menu-text ${unreadRequests ? 'large' : ''}`}>User Demo Requests</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/sos-management">
-            <i className="fas fa-hands-helping icon" />
-            <span className="menu-text">SOS Management</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/operative-alerts">
-            <i className="fas fa-envelope icon" />
-            <span className="menu-text">Operative Alerts</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/user-guides">
-            <i className="far fa-book fa-fw icon" />
-            <span className="menu-text">User guides</span>
-        </MenuItemContainer>
-        <MenuItemContainer link="/admin/new-features">
-            <i className="fas fa-file-medical icon" />
-            <span className="menu-text">New Features</span>
-        </MenuItemContainer>
-        <Link className="item" to="#" link="/admin/users" onClick={logout}>
-            <i className="icon fas fa-sign-out" />
-            <span className="menu-text">Logout</span>
-        </Link>
-    </div>
+            <AdminPinOptionsMenuItems />
+            <MenuItemContainer link="/admin/enquiries">
+                <i className="far fa-phone icon" />{' '}
+                <span className="menu-text">User Enquiries</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/demo-requests">
+                {!!unreadRequests && <span className="number">{unreadRequests}</span>}
+                <i className="far fa-video icon" />{' '}
+                <span className={`menu-text ${unreadRequests ? 'large' : ''}`}>
+                    User Demo Requests
+                </span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/sos-management">
+                <i className="fas fa-hands-helping icon" />
+                <span className="menu-text">SOS Management</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/operative-alerts">
+                <i className="fas fa-envelope icon" />
+                <span className="menu-text">Operative Alerts</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/user-guides">
+                <i className="far fa-book fa-fw icon" />
+                <span className="menu-text">User guides</span>
+            </MenuItemContainer>
+            <MenuItemContainer link="/admin/new-features">
+                <i className="fas fa-file-medical icon" />
+                <span className="menu-text">New Features</span>
+            </MenuItemContainer>
+            <Link className="item" to="#" link="/admin/users" onClick={logout}>
+                <i className="icon fas fa-sign-out" />
+                <span className="menu-text">Logout</span>
+            </Link>
+        </div>
+    </>
 );
 
 export default SuperAdminMenu;
