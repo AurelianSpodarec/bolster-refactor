efe7d15d7d891f526cd6754518db814f502900b9
Rob Fairclough <rob@silverchip.com>
2020-11-18 09:38:30 +0000

    add button to download csv

--- app-src/src/components/superAdmin/recentlyExtended/containers/RecentlyExtendedTableContainer.js
+++ app-src/src/components/superAdmin/recentlyExtended/containers/RecentlyExtendedTableContainer.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { connect, useDispatch } from 'react-redux';
+import axios from 'axios';
+import fileDownload from 'js-file-download';
 
 import RecentlyExtendedTable from '../presentational/RecentlyExtendedTable';
 import fetchRecentlyExtended from 'actions/superAdmin/recentlyExtended/async/fetchRecentlyExtended';
 import { componentDidMount } from 'helpers/generic';
+import { ADMIN_API_URL } from 'config';
+import { getHeaders } from 'helpers/api';
 
 const RecentlyExtendedTableContainer = ({ isFetching, error, recentlyExtended }) => {
+    const csvURL = `${ADMIN_API_URL}/drawings/extend/csv`;
+
     const dispatch = useDispatch();
     componentDidMount(() => dispatch(fetchRecentlyExtended()));
     return (
@@ -20,8 +26,15 @@ const RecentlyExtendedTableContainer = ({ isFetching, error, recentlyExtended })
             isFetching={isFetching}
             error={error}
             recentlyExtended={recentlyExtended}
+            downloadCSV={downloadCSV}
         />
     );
+
+    function downloadCSV() {
+        axios.get(csvURL, getHeaders()).then(res => {
+            fileDownload(res.data, 'RecentlyExtendedDrawings.csv');
+        });
+    }
 };
 
 const mapStateToProps = ({
