a8b4f2edde71be1c2f35f9322644c6b82ff61eb6
Mark Baker <mark@silverchip.com>
2020-11-19 15:22:17 +0000

    update banners to 'play' video manually
    Former-commit-id: 8635457b22a661037f3869872ea297a3b98d0b04 [formerly e3436819e39b83bb837b0beea112ec3fa87088ee] [formerly 9ac3ec3af08e1ec3fe6efe2ffefec39e87edfccc [formerly 168c351a2c6689de36247a10f040710bf765b09e]]
    Former-commit-id: 9a438e5fe3a0148f8dd348ef7a6413de9632bc5b [formerly f41e93253f9209ee6f070979a3335d0bb78dd48f]
    Former-commit-id: 7d87beb90ad75a0865575babcf019ec13ff94b0a

--- app-src/src/components/frontEnd/shared/banners/presentational/FrontEndBanner.js
+++ app-src/src/components/frontEnd/shared/banners/presentational/FrontEndBanner.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ArrowIcon from '_content/images/frontend-new/banners/banner-arrow.png';
 
-const FrontEndBanner = ({ video = '', heading = '', description = '' }) => (
-    <div className="frontend-banner">
-        <video className="video" autoPlay playsInline loop muted>
-            <source src={video} type="video/mp4" />
-        </video>
+const FrontEndBanner = ({ video = '', heading = '', description = '' }) => {
+    useEffect(() => {
+        const video = document.getElementById('page-video');
 
-        <div className="content">
-            <h1>{heading}</h1>
-            <p>{description}</p>
+        video.play();
+    }, []);
+
+    return (
+        <div className="frontend-banner">
+            <video id="page-video" className="video" playsInline loop muted>
+                <source src={video} type="video/mp4" />
+            </video>
+
+            <div className="content">
+                <h1>{heading}</h1>
+                <p>{description}</p>
+            </div>
+
+            <img className="arrow" alt="arrow" src={ArrowIcon} />
         </div>
-
-        <img className="arrow" alt="arrow" src={ArrowIcon} />
-    </div>
-);
+    );
+};
 
 export default FrontEndBanner;
