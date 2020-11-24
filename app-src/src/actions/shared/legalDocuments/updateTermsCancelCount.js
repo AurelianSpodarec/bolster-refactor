3df501ae47cd9d2249f832159ab492c977b459c6
Seb Smith <seb@silverchip.com>
2020-11-24 15:01:08 +0000

    Sets up reducers ready to be plugged in once BE is ready
    Former-commit-id: 457e079bda4645fd99ee9f7acce14183271755b0 [formerly 761c1a0ac3d4194d5f30be6adf2e2a971c8925a6] [formerly 9a81747bd2425566ad8131c21f4bd5cb961e771d [formerly 77e3a05c0438810bdcd28cc627e97a865aa20293]]
    Former-commit-id: e3d893b10f71169804b7582a6a50c0f936a9dfd1 [formerly abbe649d736b8780402cb78438d5dad379ef03ba]
    Former-commit-id: a518baa1e55695c96c92116622ac1b082a66c3f4

---
+++ app-src/src/actions/shared/legalDocuments/updateTermsCancelCount.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+import {
+    UPDATE_TERMS_CANCEL_COUNT_REQUEST,
+    UPDATE_TERMS_CANCEL_COUNT_SUCCESS,
+    UPDATE_TERMS_CANCEL_COUNT_FAILURE,
+} from 'constants/actionTypes/legalDocuments';
+import { getHeaders } from 'helpers/api';
+import { FRONTEND_API_URL } from 'config';
+
+const updateTermsCancelCountRequest = () => ({
+    type: UPDATE_TERMS_CANCEL_COUNT_REQUEST,
+});
+
+const updateTermsCancelCountSuccess = payload => ({
+    type: UPDATE_TERMS_CANCEL_COUNT_SUCCESS,
+    payload,
+});
+
+const updateTermsCancelCountFailure = error => ({
+    type: UPDATE_TERMS_CANCEL_COUNT_FAILURE,
+    error,
+});
+
+export default () => dispatch => {
+    dispatch(updateTermsCancelCountRequest());
+
+    return axios
+        .patch(`${FRONTEND_API_URL}/legal-documents/terms`, getHeaders())
+        .then(res => dispatch(updateTermsCancelCountSuccess(res.data)))
+        .catch(err => dispatch(updateTermsCancelCountFailure(err.message)));
+};
--- app-src/src/reducers/shared/legalDocuments.js
+++ app-src/src/reducers/shared/legalDocuments.js
@@ -4,6 +4,9 @@ import {
     SHARED_FETCH_TERMS_REQUEST,
     SHARED_FETCH_TERMS_SUCCESS,
     SHARED_FETCH_TERMS_FAILURE,
+    UPDATE_TERMS_CANCEL_COUNT_REQUEST,
+    UPDATE_TERMS_CANCEL_COUNT_SUCCESS,
+    UPDATE_TERMS_CANCEL_COUNT_FAILURE,
 } from 'constants/actionTypes/legalDocuments';
 
 export default combineReducers({
@@ -11,6 +14,7 @@ export default combineReducers({
     fetchError: fetchErrorReducer,
     fetchSuccess: fetchSuccessReducer,
     isFetching: isFetchingReducer,
+    isPosting: isPostingReducer,
 });
 
 function docsReducer(state = {}, action) {
@@ -49,6 +53,17 @@ function fetchErrorReducer(state = null, action) {
             return null;
         case SHARED_FETCH_TERMS_FAILURE:
             return action.error;
+        default:
+            return state;
+    }
+}
+function isPostingReducer(state = false, action) {
+    switch (action.type) {
+        case UPDATE_TERMS_CANCEL_COUNT_REQUEST:
+            return true;
+        case UPDATE_TERMS_CANCEL_COUNT_SUCCESS:
+        case UPDATE_TERMS_CANCEL_COUNT_FAILURE:
+            return false;
         default:
             return state;
     }
