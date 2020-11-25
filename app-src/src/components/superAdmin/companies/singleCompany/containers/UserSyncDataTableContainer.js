4d98732d2c37988fcf66d709153b75e8d2232847
Seb Smith <seb@silverchip.com>
2020-11-25 15:11:57 +0000

    Adds error handling

--- app-src/src/components/superAdmin/companies/singleCompany/containers/UserSyncDataTableContainer.js
+++ app-src/src/components/superAdmin/companies/singleCompany/containers/UserSyncDataTableContainer.js
@@ -2,6 +2,9 @@ import React, { useEffect, useCallback } from 'react';
 import { connect } from 'react-redux';
 import UserSyncDataTable from '../presentational/UserSyncDataTable';
 import fetchUserLatestSyncData from 'actions/superAdmin/syncs/async/fetchUserLatestSyncData';
+import { showModal } from 'actions/shared/generic/modals/sync/showModal';
+import { ERROR_MODAL } from 'constants/shared/modalTypes';
+import { usePrevious } from 'helpers/hooks';
 
 const UserSyncDataTableContainer = ({
     id,
@@ -20,6 +23,7 @@ const UserSyncDataTableContainer = ({
         '# Pins Synced',
         '# Pin Histories Synced',
     ];
+    const prevProps = usePrevious({ error });
 
     const getLatestSyncs = useCallback(async () => {
         try {
@@ -32,6 +36,12 @@ const UserSyncDataTableContainer = ({
     useEffect(() => {
         getLatestSyncs();
     }, []);
+
+    useEffect(() => {
+        if (error && !prevProps.error) {
+            return showModal(ERROR_MODAL, { message: error });
+        }
+    }, [error, prevProps.error]);
 
     return <UserSyncDataTable headers={headers} syncData={syncData} isFetching={isFetching} />;
 };
