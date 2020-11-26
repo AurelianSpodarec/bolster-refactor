6c24968d9b5aad854817702b296cc13c7091ce75
Seb Smith <seb@silverchip.com>
2020-11-26 10:32:03 +0000

    Sets up action folders for banner notifications

---
+++ app-src/src/actions/superAdmin/bannerNotifications/async/fetchAllBannerNotifications.js
@@ -0,0 +1,34 @@
+// import axios from 'axios';
+
+// import {
+//     FETCH_ALL_NEW_FEATURES_REQUEST,
+//     FETCH_ALL_NEW_FEATURES_SUCCESS,
+//     FETCH_ALL_NEW_FEATURES_FAILURE,
+// } from 'constants/actionTypes/superAdminNewFeatures';
+// import { ADMIN_API_URL } from 'config';
+// import { getHeaders } from 'helpers/api';
+
+// export const fetchNewFeaturesRequest = () => ({
+//     type: FETCH_ALL_NEW_FEATURES_REQUEST,
+// });
+
+// export const fetchNewFeaturesSuccess = payload => ({
+//     type: FETCH_ALL_NEW_FEATURES_SUCCESS,
+//     payload,
+// });
+
+// export const fetchNewFeaturesFailure = error => ({
+//     type: FETCH_ALL_NEW_FEATURES_FAILURE,
+//     error,
+// });
+
+// export default () => dispatch => {
+//     dispatch(fetchNewFeaturesRequest());
+
+//     return axios
+//         .get(`${ADMIN_API_URL}/recentUpdates`, getHeaders())
+//         .then(({ data }) => dispatch(fetchNewFeaturesSuccess(data)))
+//         .catch(err => dispatch(fetchNewFeaturesFailure(err.message)));
+// };
+
+// Change above code to suit for banner notificaitons
--- app-src/src/components/superAdmin/bannerNotifications/containers/BannerNotificationsContainer.js
+++ app-src/src/components/superAdmin/bannerNotifications/containers/BannerNotificationsContainer.js
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { connect } from 'react-redux';
 
 import BannerNotifications from '../presentational/BannerNotifications';
 
@@ -6,7 +7,14 @@ const BannerNotificationsContainer = ({ fetchBannerNotifications }) => {
     const getBannerNotifications = useCallback(async () => {
         await fetchBannerNotifications();
     }, []);
+
+    // useEffect(() => {
+    //     getBannerNotifications();
+    // }, []);
     return <BannerNotifications />;
 };
 
-export default BannerNotificationsContainer;
+const mapDispatchToProps = {
+    /*fetchBannerNotifications*/
+};
+export default connect(null, mapDispatchToProps)(BannerNotificationsContainer);
