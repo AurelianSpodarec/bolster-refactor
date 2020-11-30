13572d61602400c3ddafe3e5edca3045189cf91f
Seb Smith <seb@silverchip.com>
2020-11-30 20:07:44 +0000

    Ability to close the terms modal
    Former-commit-id: 0f33ef952a26feccc5c92985eaa6d31ac3cbe5fa [formerly 65b14eb3e8f8a2c41bb085125d36cd8f8bb910c8] [formerly b9b1705cdb11e18144471cce5c981fca6b41e063 [formerly 58ca31147b2e3d37b25ac9640870afeeffe1959c]]
    Former-commit-id: 0cbe4a42735642cd54742fd81e2e271e274a1a55 [formerly 6430a35cf38a783a21a04084cf2917ce57103991]
    Former-commit-id: 6a7541532bedab0ecd15f9058df0f8e8e0f89d7a

--- app-src/src/components/companyAdmin/agreeToTerms/AgreeToTermsModal.js
+++ app-src/src/components/companyAdmin/agreeToTerms/AgreeToTermsModal.js
@@ -2,15 +2,23 @@ import React from 'react';
 
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import AgreeToTerms from 'components/companyAdmin/agreeToTerms/AgreeToTerms';
+import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
+import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 
-const AgreeToTermsModal = () => {
+const AgreeToTermsModal = ({ handleClick }) => {
     return (
         <div className="modal-container size-lg-12">
             <div className="bg" />
 
             <div className="modal-block" style={{ width: '80%', maxWidth: '80%' }}>
                 <BlockContainer>
-                    <AgreeToTerms />
+                    <BlockButtonWrapper>
+                        <ButtonContainer handleClick={handleClick}>
+                            <i className="fa fa-times" />
+                            Close
+                        </ButtonContainer>
+                    </BlockButtonWrapper>
+                    <AgreeToTerms handleClick={handleClick} />
                 </BlockContainer>
             </div>
         </div>
