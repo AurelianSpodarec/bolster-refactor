1ccc92a51131929e9513564ff49a91e91c6230bd
Rob Fairclough <rob@silverchip.com>
2020-11-30 13:54:52 +0000

    merge issues
    Former-commit-id: e5c4e33af45ebd79efca7eb755beb9820922736d [formerly 5f5e4b451ebda524a41ad6a3914adb006d3f5f8c] [formerly b4861b5fdb47aa2e672863ed923ec487a325cbd9 [formerly ad46285dc02e9663a8631443b1b9c4874b2ad883]]
    Former-commit-id: bde2deaf339068d758455a0a6f0a1b3ceecd9e38 [formerly 4ce4ecfc3ec04a7fa71ca5bba257f5b0011f82a0]
    Former-commit-id: b579736500c1fe0f69db1013b25151d05cf28771

--- app-src/src/components/companyAdmin/app/app/presentational/CompanyApp.js
+++ app-src/src/components/companyAdmin/app/app/presentational/CompanyApp.js
@@ -6,9 +6,7 @@ import CompanyHeaderContainer from 'components/companyAdmin/layout/header/contai
 import FooterContainer from 'components/companyAdmin/layout/footer/containers/FooterContainer';
 import MenuContainer from 'components/shared/layout/menu/containers/MenuContainer';
 import Routes from 'components/companyAdmin/app/routes/presentational';
-import Helmet from 'components/frontEnd/shared/meta/presentational/Helmet';
 import PageMeta from 'components/frontEnd/shared/meta/presentational/PageMeta';
-import AgreeToTermsCheck from '../containers/AgreeToTermsCheck';
 
 const CompanyApp = () => (
     <div className="dashboard-area">
