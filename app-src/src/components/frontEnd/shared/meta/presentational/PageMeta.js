cbab89b1f1df97ae604f58c6078225b33bd3625b
Rob Fairclough <rob@silverchip.com>
2020-11-30 13:24:16 +0000

    Changes meta titles so it looks better in the tab.
    Former-commit-id: 552eadafef1196aea1cce152db604da3029b1142 [formerly 1310e86b26256cbd18e43bdae36eb6c23c7cfdff] [formerly 760b56d4bd4e46282259555729dc07cd0bfa385e [formerly d7f6a87f8b1c207f1918705b02af271a06be7575]]
    Former-commit-id: 068d931799a6cac1b8913b1dc35e6ce00becef9f [formerly 1f160e2d2f42ed0e42fc754f8ce184a79f922b3d]
    Former-commit-id: fc05ab7c3580533e5af6aec677ddd5eb1ecd8449

--- app-src/src/components/frontEnd/shared/meta/presentational/PageMeta.js
+++ app-src/src/components/frontEnd/shared/meta/presentational/PageMeta.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-const PageMeta = ({ meta: { title, description, canonical, excludeFromSearchEngines } }) => <Helmet>
-    <title>{`${title} | Bolster Systems`}</title>
-    <meta name="description" content={description} />
-    {canonical && <link rel="canonical" href={`https://www.bolstersystems.com${canonical}`}></link>}
-    {excludeFromSearchEngines && <meta name='robots' content='noindex' />}
-</Helmet>;
+const PageMeta = ({ meta: { title, description, canonical, excludeFromSearchEngines } }) => (
+    <Helmet>
+        <title>{title ? `${title} | Bolster Systems` : 'Bolster Systems'}</title>
+        <meta name="description" content={description} />
+        {canonical && (
+            <link rel="canonical" href={`https://www.bolstersystems.com${canonical}`}></link>
+        )}
+        {excludeFromSearchEngines && <meta name="robots" content="noindex" />}
+    </Helmet>
+);
 
 export default PageMeta;
 
--- app-src/src/constants/frontEnd/meta.js
+++ app-src/src/constants/frontEnd/meta.js
@@ -1,48 +1,48 @@
 export const pageMeta = {
     home: {
-        title: 'Compliance documentation software app for construction',
+        title: '',
         description:
             'Bolster Systems is the world leading compliance documentation software, complete with mobile app experience. Streamline and automate your compliance documentation today.',
         canonical: '/',
         excludeFromSearchEngines: false,
     },
     aboutUs: {
-        title: 'About our award winning compliance documentation software',
+        title: 'About Us',
         description:
             'About our industry leading compliance documentation software and team for construction. Automate, streamline and improve your compliance documentation today. Bolster Systems.',
         canonical: '/about-us',
         excludeFromSearchEngines: false,
     },
     ourSystem: {
-        title: 'Bespoke compliance documentation automation app for construction',
+        title: 'Our System',
         description:
             'Learn how our world beating compliance documentation software could help you automate and streamline your compliance documentation today. Bolster Systems.',
         canonical: '/our-system',
         excludeFromSearchEngines: false,
     },
     howItWorks: {
-        title: 'Enterprise compliance documentation software with mobile app',
+        title: 'How It Works',
         description:
             'Enterprise level, cloud based compliance documentation software for the construction industry. Our dynamic system can help automate and streamline your compliance documentation.',
         canonical: '/how-it-works',
         excludeFromSearchEngines: false,
     },
     contact: {
-        title: 'Demo our construction compliance documentation software and app',
+        title: 'Contact',
         description:
             'Contact our expert team to learn more about our enterprise level compliance documentation software. Automate, streamline and improve your compliance documentation today.',
         canonical: '/contact',
         excludeFromSearchEngines: false,
     },
     login: {
-        title: 'Login to our documentation software for construction',
+        title: 'Login',
         description:
             'Login to  Bolster Systems, the world leading enterprise level compliance documentation software, complete with android and iOS mobile apps and fully cloud based.',
         canonical: '/auth/login',
         excludeFromSearchEngines: false,
     },
     register: {
-        title: 'Sign up to our compliance documentation software',
+        title: 'Register',
         description:
             'Register now to Bolster Systems, the world leading enterprise level compliance documentation software, complete with Android and iOS mobile apps and fully cloud based.',
         canonical: '/auth/register',
