a4bcc0c6cec64c9e6affa734660709d609444e07
Rob Fairclough <rob@silverchip.com>
2020-11-30 13:06:11 +0000

    add back missing files
    Former-commit-id: 4c1b0fe3bf54ad933d1b4eb710f52194e123823e

---
+++ app-src/src/components/companyAdmin/app/app/containers/AgreeToTermsCheck.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import fetchTerms from 'actions/shared/legalDocuments/fetchTerms';
+
+import Error from 'components/shared/generic/misc/presentational/Error';
+import Block from 'components/shared/generic/block/presentational/Block';
+import AgreeToTermsModal from 'components/companyAdmin/agreeToTerms/AgreeToTermsModal';
+
+const AgreeToTermsCheck = ({
+    fetchTerms,
+    termsExists,
+    terms,
+    eula,
+    privacy,
+    termsAcceptedOn,
+    fetchSuccess,
+    hasFetchedCompany,
+    fetchError,
+    children,
+}) => {
+    useEffect(() => {
+        fetchTerms();
+    }, []);
+
+    if (fetchError) return <ErrorBlock>{fetchError}</ErrorBlock>;
+    if (!fetchSuccess || !hasFetchedCompany) return null;
+
+    var maxDate = getMaxDate([terms.publishedOn, eula.publishedOn, privacy.publishedOn]);
+    if (termsExists && (!termsAcceptedOn || new Date(maxDate) > new Date(termsAcceptedOn)))
+        return <AgreeToTermsModal terms={terms} eula={eula} privacy={privacy} />;
+
+    return children;
+};
+
+const mapState = ({
+    companyAdmin: { companySettingsReducer },
+    shared: { legalDocumentsReducer },
+}) => ({
+    terms: legalDocumentsReducer.docs.terms || {},
+    eula: legalDocumentsReducer.docs.eula || {},
+    privacy: legalDocumentsReducer.docs.privacy || {},
+    fetchSuccess: legalDocumentsReducer.fetchSuccess,
+    fetchError: legalDocumentsReducer.fetchError,
+    hasFetchedCompany: !!companySettingsReducer.companySettings.id,
+    termsExists: !!legalDocumentsReducer.docs.terms,
+    termsAcceptedOn: companySettingsReducer.companySettings.termsAcceptedOn,
+});
+
+const mapDispatch = { fetchTerms };
+
+function getMaxDate(dates) {
+    return dates.reduce((a, b) => (new Date(a) > new Date(b) ? a : b));
+}
+
+function ErrorBlock({ children }) {
+    return (
+        <Block>
+            <Error>{children}</Error>
+        </Block>
+    );
+}
+
+export default connect(mapState, mapDispatch)(AgreeToTermsCheck);
