3057691dbde3c4b78fb0fc70ce46d7550ccf7ead
Oluseun Adesina <seun@silverchip.com>
2020-12-03 15:59:56 +0000

    added pagination to users table

--- app-src/src/components/superAdmin/companies/singleCompany/containers/CompanyUsersTableContainer.js
+++ app-src/src/components/superAdmin/companies/singleCompany/containers/CompanyUsersTableContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import fetchCompanyUsers from 'actions/superAdmin/users/async/fetchCompanyUsers';
 
 import CompanyUsersTable from '../presentational/CompanyUsersTable';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
@@ -8,12 +9,35 @@ import BlockHeading from 'components/shared/generic/blockHeading/presentational/
 import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 import { showModal } from 'actions/shared/generic/modals/sync/showModal';
 import { ADMIN_CREATE_COMPANY_ADMIN } from 'constants/shared/modalTypes';
+import PageSelector from 'components/shared/pagination/presentational/pageSelector';
 
-const CompanyUsersTableContainer = ({ users, error, isFetching, headers, match, showModal }) => {
+const CompanyUsersTableContainer = ({
+    users,
+    companyUsersInfo,
+    error,
+    isFetching,
+    headers,
+    match,
+    showModal,
+}) => {
+    const dispatch = useDispatch();
+    console.log(users);
+
+    const setPage = nextPage => {
+        const id = match.params.id;
+        dispatch(fetchCompanyUsers(id, nextPage));
+    };
+
     return (
         <BlockContainer>
             <div className="size-lg-6 size-md-12">
-                <BlockHeading title="Users" />
+                <BlockHeading title="Users">
+                    <PageSelector
+                        setPage={setPage}
+                        page={companyUsersInfo.page}
+                        maxPage={companyUsersInfo.maxPage}
+                    />
+                </BlockHeading>
             </div>
             <BlockButtonWrapper additionalClasses="no-margin" sizeClasses="size-lg-6 size-md-12">
                 <button className=" button green" onClick={handleShowAddModal} type="button">
@@ -33,12 +57,13 @@ const CompanyUsersTableContainer = ({ users, error, isFetching, headers, match, 
 const mapStateToProps = (
     {
         superAdmin: {
-            usersReducer: { users, error, isFetching },
+            usersReducer: { companyUsers, companyUsersInfo, error, isFetching },
         },
     },
     { match: { params } },
 ) => ({
-    users: Object.values(users).filter(({ companyID }) => +companyID === +params.id),
+    users: Object.values(companyUsers),
+    companyUsersInfo,
     error,
     isFetching,
     headers: [
