c1bc7e54cbbd2f04ee38d5dbbe7550249d44a799
Mark Baker <mark@silverchip.com>
2020-12-03 16:17:43 +0000

    add templates selection to the edit operative form
    Former-commit-id: d9ac06e6b7af1b3f4fdb3fa6d342563f5cea8b68 [formerly f3df39dd0aacc165029d7610dd97bab0962d1ef9] [formerly 7fe5dd4ffeb49a90ae60caa708a4c8c077eaa349 [formerly 41caad2dbffc298e4cbc2fbd86bbd8c7e7a78bae]]
    Former-commit-id: d2945a4cad865cfcdff0fc962ce7868cc4d4c766 [formerly 6735dcd1469e21e6faafc30f41e2518089c0cac3]
    Former-commit-id: e1fdfc12181f0471e23032ffbf418757bc2ec56c

--- app-src/src/components/companyAdmin/drawings/editDrawingOperative/presentational/EditDrawingOperativeForm.js
+++ app-src/src/components/companyAdmin/drawings/editDrawingOperative/presentational/EditDrawingOperativeForm.js
@@ -1,25 +1,32 @@
 import React from 'react';
 
 import CheckboxListContainer from 'components/shared/generic/form/containers/CheckboxListContainer';
-import Loading from 'components/shared/generic/misc/presentational/Loading';
-import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import Form from 'components/shared/generic/form/containers/Form';
 import Field from 'components/shared/generic/form/presentational/Field';
 import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
+import Block from 'components/shared/generic/block/presentational/Block';
+import CheckboxContainer from 'components/shared/generic/form/containers/CheckboxContainer';
+import Loading from 'components/shared/generic/misc/presentational/Loading';
 
 const EditDrawingOperative = ({
     operative,
     services,
-    isFetching,
+    serviceOptions,
     handleMultiSelect,
     handleSubmit,
     serviceIDs,
     backUrl,
+    isTemplateFilteringEnabled,
+    templateIDs,
+    serviceAreas,
+    getTemplatesForService,
+    isFetching,
 }) => {
     if (!isFetching) {
         const { userFirstName, userLastName } = operative;
+
         return (
             <>
                 <PageHeading
@@ -27,11 +34,11 @@ const EditDrawingOperative = ({
                     title={`Edit Operative: ${userFirstName} ${userLastName}`}
                     withBackButton
                 />
-                <BlockContainer>
+                <Block>
                     <Form onSubmit={handleSubmit}>
                         <Field name="Services" required>
                             <CheckboxListContainer
-                                options={Object.values(services)}
+                                options={serviceOptions}
                                 selectedOptions={serviceIDs}
                                 handleChange={handleMultiSelect}
                                 name="serviceIDs"
@@ -39,6 +46,36 @@ const EditDrawingOperative = ({
                                 hideDisabled
                             />
                         </Field>
+
+                        <Field name="Enable Template Filtering" sizeClasses="size-lg-12">
+                            <CheckboxContainer
+                                name="isTemplateFilteringEnabled"
+                                checked={isTemplateFilteringEnabled}
+                                value={isTemplateFilteringEnabled}
+                                handleChange={handleMultiSelect}
+                            />
+                        </Field>
+
+                        {isTemplateFilteringEnabled && (
+                            <>
+                                {[...serviceAreas].sort().map(service => (
+                                    <Field
+                                        key={service}
+                                        name={services[service].name}
+                                        sizeClasses="size-lg-12"
+                                    >
+                                        <CheckboxListContainer
+                                            required
+                                            name="templateIDs"
+                                            handleChange={handleMultiSelect}
+                                            options={getTemplatesForService(service)}
+                                            hideDisabled
+                                            selectedOptions={templateIDs}
+                                        />
+                                    </Field>
+                                ))}
+                            </>
+                        )}
                     </Form>
                     <BlockButtonWrapper>
                         <button onClick={handleSubmit} className="button green">
@@ -47,10 +84,16 @@ const EditDrawingOperative = ({
                         </button>
                         <ButtonContainer to={backUrl}>Cancel</ButtonContainer>
                     </BlockButtonWrapper>
-                </BlockContainer>
+                </Block>
             </>
         );
-    } else return <Loading />;
+    } else {
+        return (
+            <Block>
+                <Loading />
+            </Block>
+        );
+    }
 };
 
 export default EditDrawingOperative;
