f437d328547e31e672bc2037b851c1333cc38507
Seb Smith <seb@silverchip.com>
2020-12-10 14:29:36 +0000

    Adds form fields to upload image and send up a video link
    Former-commit-id: 519a0fae85d2f2bca2747114608f39b9733b063e [formerly bce9fbbba45483332fd08e1186befd69fb4d5a7e] [formerly a575bf0698cd9fabf6bb9b45dfd730cd7ff0f81b [formerly 42dc0a0eec744a49f281293db6ec16788f41c1d2]]
    Former-commit-id: 92d08b32032daef40fbdbcc3ddc4b252128431c3 [formerly 6fb07654a85713db8047ed600ce7514bc4b19f95]
    Former-commit-id: c742f05b8eec4f1bccc1f88d5424d3dfbbc06c41

--- app-src/src/components/superAdmin/newFeatures/addNewFeatureModal/containers/AddNewFeatureFormContainer.js
+++ app-src/src/components/superAdmin/newFeatures/addNewFeatureModal/containers/AddNewFeatureFormContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { connect } from 'react-redux';
 import moment from 'moment';
@@ -12,17 +12,32 @@ import { ERROR_MODAL } from 'constants/shared/modalTypes';
 import FeatureForm from '../../presentational/FeatureForm';
 import addNewFeature from 'actions/superAdmin/newFeatures/async/addNewFeature';
 
-function AddNewFeatureFormContainer({ addNewFeature, isPosting, postSuccess, error, hideModal }) {
+function AddNewFeatureFormContainer({
+    addNewFeature,
+    isPosting,
+    postSuccess,
+    error,
+    hideModal,
+    filesUploading,
+}) {
     const [formData, handleChange] = useForm({
-        title: '',
-        fullDescription: '',
-        publishDate: '',
+        title: null,
+        fullDescription: null,
+        publishDate: null,
+        image: null,
+        videoLink: null,
     });
+    const [showVideoField, setShowVideoField] = useState(false);
+    const [showImageField, setShowImageField] = useState(false);
+
     const prevProps = usePrevious({ isPosting });
 
     const handleSubmit = e => {
         e.preventDefault();
-        addNewFeature({ ...formData, publishDate: moment.utc(formData.publishDate).format() });
+
+        if (!filesUploading) {
+            addNewFeature({ ...formData, publishDate: moment.utc(formData.publishDate).format() });
+        }
     };
 
     useEffect(() => {
@@ -34,18 +49,40 @@ function AddNewFeatureFormContainer({ addNewFeature, isPosting, postSuccess, err
         }
     }, [isPosting, postSuccess, prevProps.isPosting, error]);
 
-    return <FeatureForm handleChange={handleChange} form={formData} handleSubmit={handleSubmit} />;
+    return (
+        <FeatureForm
+            handleChange={handleChange}
+            form={formData}
+            handleSubmit={handleSubmit}
+            handleCheckboxChange={handleCheckboxChange}
+            showVideoField={showVideoField}
+            showImageField={showImageField}
+        />
+    );
+
+    function handleCheckboxChange(name, value) {
+        if (name === 'videoLink') {
+            setShowVideoField(value);
+        }
+        if (name === 'image') {
+            setShowImageField(value);
+        }
+    }
 }
 
 const mapStateToProps = ({
     superAdmin: {
         newFeaturesReducer: { postSuccess, addedNewFeature, isPosting, error },
     },
+    shared: {
+        filesUploadingReducer: { filesUploading },
+    },
 }) => ({
     postSuccess,
     addedNewFeature,
     isPosting,
     error,
+    filesUploading,
 });
 
 const mapDispatchToProps = {
--- app-src/src/components/superAdmin/newFeatures/presentational/FeatureForm.js
+++ app-src/src/components/superAdmin/newFeatures/presentational/FeatureForm.js
@@ -6,8 +6,17 @@ import TextInputContainer from 'components/shared/generic/form/containers/TextIn
 import DatePickerContainer from 'components/shared/generic/form/containers/DatePickerContainer';
 import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 import SunEditorRichTextContainer from 'components/shared/slateRichText/container/SunEditorRichTextContainer';
+import FileUploadContainer from 'components/shared/generic/form/containers/FileUploadContainer';
+import CheckboxContainer from 'components/shared/generic/form/containers/CheckboxContainer';
 
-const AddNewFeatureForm = ({ handleChange, handleSubmit, form }) => (
+const AddNewFeatureForm = ({
+    handleChange,
+    handleSubmit,
+    form,
+    showVideoField,
+    showImageField,
+    handleCheckboxChange,
+}) => (
     <Form onSubmit={handleSubmit} className="generic-form">
         <Field name="Title" required>
             <TextInputContainer
@@ -27,6 +36,47 @@ const AddNewFeatureForm = ({ handleChange, handleSubmit, form }) => (
                 placeholder="Enter a long description"
             />
         </Field>
+
+        <Field classes="small-height">
+            <CheckboxContainer
+                checked={showVideoField}
+                handleChange={(name, value) => handleCheckboxChange(name, value)}
+                name="videoLink"
+                text="Video link?"
+            />
+        </Field>
+        {showVideoField && (
+            <Field name="Video link" required>
+                <TextInputContainer
+                    placeholder="Enter Youtube or Vimeo link"
+                    name="videoLink"
+                    value={form.videoLink}
+                    handleChange={handleChange}
+                    required
+                />
+            </Field>
+        )}
+        <Field classes="small-height">
+            <CheckboxContainer
+                checked={showImageField}
+                handleChange={handleCheckboxChange}
+                name="image"
+                text="Upload image?"
+            />
+        </Field>
+        {showImageField && (
+            <Field name="Upload image" required>
+                <FileUploadContainer
+                    value={form.image}
+                    required
+                    name="image"
+                    acceptedTypes={['application/pdf', 'image/*']}
+                    handleChange={(name, value) => {
+                        handleChange(name, value, form.name);
+                    }}
+                />
+            </Field>
+        )}
         <Field name="Publish on" sizeClasses="w-dates size-lg-12" required>
             <DatePickerContainer
                 name="publishDate"
