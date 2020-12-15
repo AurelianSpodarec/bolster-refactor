6e325d25a74b39b5b3085992f72899370af04493
Seb Smith <seb@silverchip.com>
2020-12-15 16:22:11 +0000

    Adds React Quill to edit doc version
    Former-commit-id: b37fcdf4270c387f7c3a2852ba2fa5674891e9e2 [formerly 0900a75314edabd6c2e8ca63ee78881c3fe80951] [formerly 516b72d23420156e6bf39e69b84f80d48d00397b [formerly 012e1a5d5fa91800d9f8328c0ed77d36866fcdd5]]
    Former-commit-id: de6521906668c8abe1062728d55f1fb09875e9e4 [formerly 081ecfb312720a00684828faacfc6c6ed55c9a6a]
    Former-commit-id: 378bb031209305ff3f6ba44faa47edba539e7684

--- app-src/src/components/superAdmin/app/routes/presentational/LegalDocumentsRoutes.js
+++ app-src/src/components/superAdmin/app/routes/presentational/LegalDocumentsRoutes.js
@@ -4,15 +4,14 @@ import { Route } from 'react-router-dom';
 import SwitchWith404 from 'components/appRoute/routes/presentational/SwitchWith404';
 import AllLegalDocumentsTableContainer from 'components/superAdmin/legalDocuments/allLegalDocuments/containers/AllLegalDocumentsTableContainer';
 import CreateLegalDocumentContainer from 'components/superAdmin/legalDocuments/createLegalDocument/containers/CreateLegalDocumentContainer';
-import AddLegalDocumentVersionContainer from 'components/superAdmin/legalDocuments/addLegalDocumentVersion/containers/AddLegalDocumentVersionContainer';
-import EditLegalDocumentVersionContainer from 'components/superAdmin/legalDocuments/addLegalDocumentVersion/containers/EditLegalDocumentVersionContainer';
+import UpdateLegalDocumentVersionContainer from 'components/superAdmin/legalDocuments/addLegalDocumentVersion/containers/UpdateLegalDocumentVersion';
 
 const LegalDocumentsRoutes = ({ base = '/admin/legal-documents' }) => (
     <SwitchWith404>
         <Route exact path={base} component={AllLegalDocumentsTableContainer} />
         <Route exact path={`${base}/new`} component={CreateLegalDocumentContainer} />
-        <Route exact path={`${base}/edit/:id`} component={EditLegalDocumentVersionContainer} />
-        <Route exact path={`${base}/update/:id`} component={AddLegalDocumentVersionContainer} />
+        <Route exact path={`${base}/edit/:id`} component={UpdateLegalDocumentVersionContainer} />
+        <Route exact path={`${base}/update/:id`} component={UpdateLegalDocumentVersionContainer} />
     </SwitchWith404>
 );
 
