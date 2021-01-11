b2416c588d4df9278e8063852842c2f90543f4b8
Jake Julich <jake@silverchip.com>
2021-01-11 22:58:16 +1300

    Subscription check fix
    Former-commit-id: c2bb2adba8e7ceebb970c83e97223567269f0800 [formerly 9c74cc9add731d337d41062482d487ceed7c3540] [formerly acc43c029cc465b35e41da1b4d4a1ebc601bb644 [formerly fe8949d0d3bed72d9cc962b8d46b8a4ccf3dd35d]]
    Former-commit-id: f8f5ccbdbb362c9a21a600101fd2ec0cbcfdc82e [formerly 4c8dd46cdc7e8125f2e8ef4e034c0637eec5656d]
    Former-commit-id: a3aa7c54018b7c9bc2aace797f78d78ba832f9e4

--- app-src/src/config/index.js
+++ app-src/src/config/index.js
@@ -1,47 +1,25 @@
-// - Staging
-export const API_URL = 'https://api.company.bolster-staging.com';
-export const CLIENT_API_URL = 'https://api.client.bolster-staging.com';
-export const FILE_API_URL = 'https://api.media.bolster-staging.com/files';
-export const ADMIN_API_URL = 'https://api.admin.bolster-staging.com';
-export const AUTH_API_URL = 'https://api.accounts.bolster-staging.com';
-export const FRONTEND_API_URL = 'https://api.front-end.bolster-staging.com';
-export const FILE_STORAGE_URL = 'https://dizelaxol0ewg.cloudfront.net';
-export const RAW_S3_STORAGE_URL = 'https://s3-eu-west-1.amazonaws.com/bolster-media';
-export const REPORT_VIEWER_URL =
-    'http://bolsterdynamiclive-report-viewer.eu-west-1.elasticbeanstalk.com/1';
-export const STRIPE_PUBLIC_KEY = 'pk_test_QUrrYsRQKFyb1Os4ler16ke1';
-export const INVOICE_GEN_URL =
-    'https://6eixv6n0wf.execute-api.eu-west-1.amazonaws.com/staging/bolster-invoice-gen-staging';
+export const API_URL = 'https://api-company.bolstersystems.com';
+export const CLIENT_API_URL = 'https://api-client.bolstersystems.com';
+export const FILE_API_URL = 'https://api-media.bolstersystems.com/files';
+export const ADMIN_API_URL = 'https://api-admin.bolstersystems.com';
+export const AUTH_API_URL = 'https://api-accounts.bolstersystems.com';
+export const FILE_STORAGE_URL = 'https://dwdo8k5zcfdct.cloudfront.net';
+export const RAW_S3_STORAGE_URL ='https://s3-eu-west-1.amazonaws.com/bolster-media';
+
+export const FRONTEND_API_URL = 'https://api-front-end.bolstersystems.com';
+export const RENEWALS_API_URL = 'https://api-renewals.bolstersystems.com';
+
+export const REPORT_VIEWER_URL = 'http://bolsterdynamiclive-report-viewer.eu-west-1.elasticbeanstalk.com/1';
+
+export const STRIPE_PUBLIC_KEY = 'pk_live_DRdNtr61byBXiuJXYFaIEcdC';
+
+// export const FILE_STORAGE_URL =
+//     'https://s3-eu-west-1.amazonaws.com/bolster-media';
+//     export const RAW_S3_STORAGE_URL =
+// 'https://s3-eu-west-1.amazonaws.com/bolster-media';
+
+// use like {url}/{invoiceGUID}?id={id}
+export const INVOICE_GEN_URL = 'https://6eixv6n0wf.execute-api.eu-west-1.amazonaws.com/live/bolster-invoice-generator';
+
+
 export const GOOGLE_SITE_KEY = '6LeV28wZAAAAAOyyOGzeQiiqtCr_F6WIQGKqXA0a';
-// - Staging
-// export const API_URL = 'https://api.company.bolster-staging.com';
-// export const CLIENT_API_URL = 'https://api.client.bolster-staging.com';
-// export const FILE_API_URL = 'https://api.media.bolster-staging.com/files';
-// export const ADMIN_API_URL = 'https://api.admin.bolster-staging.com';
-// export const AUTH_API_URL = 'https://api.accounts.bolster-staging.com';
-// export const FRONTEND_API_URL = 'https://api.front-end.bolster-staging.com';
-// export const FILE_STORAGE_URL = 'https://dizelaxol0ewg.cloudfront.net';
-// export const RAW_S3_STORAGE_URL = 'https://s3-eu-west-1.amazonaws.com/bolster-media';
-// // LEAVING THIS AS IT IS FOR THE MOMENT BUT IT MAY NOT WORK - IF AREA NEEDED TO WORK ON A TICKET SPEAK TO JAKE
-// export const REPORT_VIEWER_URL =
-// 'http://bolsterdynamiclive-report-viewer.eu-west-1.elasticbeanstalk.com/1';
-// export const STRIPE_PUBLIC_KEY = 'pk_test_QUrrYsRQKFyb1Os4ler16ke1';
-// // LEAVING THIS AS IT IS FOR THE MOMENT BUT IT MAY NOT WORK - IF AREA NEEDED TO WORK ON A TICKET SPEAK TO JAKE
-// export const INVOICE_GEN_URL =
-// 'https://6eixv6n0wf.execute-api.eu-west-1.amazonaws.com/staging/bolster-invoice-gen-staging';
-// - Local Dev
-// export const API_URL = 'https://localhost:5021';
-// export const CLIENT_API_URL = 'https://localhost:5051';
-// export const FILE_API_URL = 'https://localhost:5031/files';
-// export const ADMIN_API_URL = 'https://localhost:5011';
-// export const AUTH_API_URL = 'https://localhost:5001';
-// export const FRONTEND_API_URL = 'https://localhost:5091';
-//
-// export const FILE_STORAGE_URL = 'https://dizelaxol0ewg.cloudfront.net';
-// export const RAW_S3_STORAGE_URL = 'https://s3-eu-west-1.amazonaws.com/bolster-media';
-//
-// export const REPORT_VIEWER_URL =
-// 'https://localhost:6001/1';
-//
-// export const STRIPE_PUBLIC_KEY =
-// 'pk_test_QUrrYsRQKFyb1Os4ler16ke1';
