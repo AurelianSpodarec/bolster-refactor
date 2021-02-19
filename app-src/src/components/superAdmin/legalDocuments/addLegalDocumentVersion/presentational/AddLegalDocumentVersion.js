e55101827f578ef98c4a8a9e14d6f9576bed9e19
Mark Baker <mark@silverchip.com>
2021-02-19 15:51:02 +0000

    update wysiwyg editor for legal documents
    Former-commit-id: ebccbd2286436b741a0a473f82a29d34a595f9bf [formerly b41aabb4182978ff48b375b5806b63f4fa3c9216] [formerly f0f5a1422ad46ec9baf93d62b19d4fa31069ae4c [formerly 5ea08a3468ddff327ca0830a6a0361d8ff80b8da]]
    Former-commit-id: f46aff5edbd25e7a3dc46596af7edf6b1081cdac [formerly d82a8ffceaf76b8eac145751b95df7f9f5b41bc0]
    Former-commit-id: fc511133db4cf75a80e6257bd380fb42675e3ceb

--- app-src/src/components/superAdmin/legalDocuments/addLegalDocumentVersion/presentational/AddLegalDocumentVersion.js
+++ app-src/src/components/superAdmin/legalDocuments/addLegalDocumentVersion/presentational/AddLegalDocumentVersion.js
@@ -7,36 +7,14 @@ import Field from 'components/shared/generic/form/presentational/Field';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
 import DropdownContainer from 'components/shared/generic/form/containers/DropdownContainer';
 import { LEGAL_DOCUMENT_TYPE } from 'constants/superAdmin/enums';
-import ReactQuill from 'react-quill';
 import BlockEditor from 'components/shared/generic/block/presentational/BlockEditor';
+import SunEditorSimpleWysiwyg from 'components/shared/generic/form/presentational/SunEditorSimpleWysiwyg';
+import { legalDocumentsButtons, legalDocumentsFormats } from 'constants/shared/editorFormats';
 
 const options = [
     { text: LEGAL_DOCUMENT_TYPE[10], value: LEGAL_DOCUMENT_TYPE[10] },
     { text: LEGAL_DOCUMENT_TYPE[20], value: LEGAL_DOCUMENT_TYPE[20] },
     { text: LEGAL_DOCUMENT_TYPE[30], value: LEGAL_DOCUMENT_TYPE[30] },
-];
-
-const modules = {
-    toolbar: [
-        [{ header: [1, 2, 3, 4, 5, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-        ['link'],
-        ['clean'],
-    ],
-};
-
-const formats = [
-    'header',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
 ];
 
 const AddLegalDocumentVersion = ({
@@ -74,12 +52,12 @@ const AddLegalDocumentVersion = ({
             </BlockContainer>
 
             <BlockEditor>
-                <ReactQuill
-                    theme="snow"
+                <SunEditorSimpleWysiwyg
+                    name=""
                     value={documentText}
                     onChange={setDocText}
-                    modules={modules}
-                    formats={formats}
+                    buttonOptions={legalDocumentsButtons}
+                    formatOptions={legalDocumentsFormats}
                 />
 
                 <BlockButtonWrapper>
--- app-src/src/components/superAdmin/legalDocuments/createLegalDocument/presentational/CreateLegalDocument.js
+++ app-src/src/components/superAdmin/legalDocuments/createLegalDocument/presentational/CreateLegalDocument.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactQuill from 'react-quill';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
@@ -7,29 +6,8 @@ import ButtonContainer from 'components/shared/generic/button/containers/ButtonC
 import Field from 'components/shared/generic/form/presentational/Field';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
 import BlockEditor from 'components/shared/generic/block/presentational/BlockEditor';
-
-const modules = {
-    toolbar: [
-        [{ header: [1, 2, 3, 4, 5, false] }],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-        ['link'],
-        ['clean'],
-    ],
-};
-
-const formats = [
-    'header',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-];
+import SunEditorSimpleWysiwyg from 'components/shared/generic/form/presentational/SunEditorSimpleWysiwyg';
+import { legalDocumentsButtons, legalDocumentsFormats } from 'constants/shared/editorFormats';
 
 const CreateLegalDocument = ({
     handleSaveDraft,
@@ -54,12 +32,12 @@ const CreateLegalDocument = ({
             </BlockContainer>
 
             <BlockEditor>
-                <ReactQuill
-                    theme="snow"
+                <SunEditorSimpleWysiwyg
+                    name=""
                     value={documentText}
                     onChange={setDocText}
-                    modules={modules}
-                    formats={formats}
+                    buttonOptions={legalDocumentsButtons}
+                    formatOptions={legalDocumentsFormats}
                 />
 
                 <BlockButtonWrapper>
