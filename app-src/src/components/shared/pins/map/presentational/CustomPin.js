18e61ec43b990f6693a54658a71fa72d89c5a245
Oliver <oli@silverchip.com>
2021-03-04 17:06:32 +0000

    fix dynamic imports of pin images

--- app-src/src/components/shared/pins/map/presentational/CustomPin.js
+++ app-src/src/components/shared/pins/map/presentational/CustomPin.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import { pin2xImgs } from '_content/images/map-markers';
+
 const CustomPin = ({ pinColour = 'red', pinCode = '', excluded = false }) => {
-    const imageLocation = require(`_content/images/map-markers/${pinColour}-pin2x.png`);
     const topCode = pinCode.slice(0, 4);
     const bottomCode = pinCode.slice(5);
 
     return (
         <div className="custom-pin" style={{ opacity: excluded ? 0.5 : 1 }}>
-            <img alt={`${pinColour} pin`} src={imageLocation} />
+            <img alt={`${pinColour} pin`} src={pin2xImgs[pinColour]} />
             <div className="code">
                 <p className="code-half">{topCode}</p>
                 <p className="code-half">:{bottomCode}</p>
