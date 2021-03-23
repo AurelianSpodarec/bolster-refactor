cea8a6abd07b383260627f51aa9d653041cb47e8
Mark Baker <mark@silverchip.com>
2021-03-23 11:34:45 +0000

    add users tabs and set for admin

--- app-src/src/components/companyAdmin/userManagement/companyAdmins/allCompanyAdmins/containers/AllCompanyAdminsContainer.js
+++ app-src/src/components/companyAdmin/userManagement/companyAdmins/allCompanyAdmins/containers/AllCompanyAdminsContainer.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { USERS_ADMIN_TABS } from 'constants/shared/tabNames';
+
 import AllCompanyAdmins from '../presentational/AllCompanyAdmins';
 import fetchCompanyUsers from 'actions/companyAdmin/userManagement/async/fetchCompanyUsers';
+import setTabs from 'actions/shared/generic/tabs/sync/setTabs';
 
 export class AllCompanyAdminsContainer extends Component {
     render() {
@@ -10,8 +13,9 @@ export class AllCompanyAdminsContainer extends Component {
     }
 
     componentDidMount = () => {
-        const { fetchAllCompanyUsers } = this.props;
+        const { fetchAllCompanyUsers, setTabs } = this.props;
 
+        setTabs(Object.values(USERS_ADMIN_TABS), USERS_ADMIN_TABS.ACTIVE);
         fetchAllCompanyUsers();
     };
 }
@@ -21,7 +25,8 @@ export class AllCompanyAdminsContainer extends Component {
 const mapDispatchToProps = dispatch => ({
     fetchAllCompanyUsers: () => {
         dispatch(fetchCompanyUsers());
-    }
+    },
+    setTabs: (tabs, selectedTab) => dispatch(setTabs(tabs, selectedTab)),
 });
 
 export default connect(null, mapDispatchToProps)(AllCompanyAdminsContainer);
--- app-src/src/constants/shared/tabNames.js
+++ app-src/src/constants/shared/tabNames.js
@@ -1,32 +1,45 @@
 export const DRAWING_TABS = {
     GENERATE_REPORT: 'Generate Report',
-    GENERAL_OVERVIEW: 'General Overview'
+    GENERAL_OVERVIEW: 'General Overview',
 };
 export const FLOOR_TABS = {
     GENERATE_REPORT: 'Generate Report',
-    GENERAL_OVERVIEW: 'General Overview'
+    GENERAL_OVERVIEW: 'General Overview',
 };
 export const BUILDING_TABS = {
     GENERATE_REPORT: 'Generate Report',
-    GENERAL_OVERVIEW: 'General Overview'
+    GENERAL_OVERVIEW: 'General Overview',
 };
 export const SITE_TABS = {
     GENERATE_REPORT: 'Generate Report',
-    GENERAL_OVERVIEW: 'General Overview'
+    GENERAL_OVERVIEW: 'General Overview',
 };
 
 export const MENU_TABS = {
     SUPER_ADMIN: 'Super Admin',
     COMPANY_USER: 'Admin',
-    CLIENT: 'Client Access'
+    CLIENT: 'Client Access',
 };
 
 export const APPROVED_COMPANIES_TABS = {
     MAP: 'Map',
-    LIST: 'List'
+    LIST: 'List',
 };
 
 export const DASHBOARD_TABS = {
     DRAWINGS: 'Drawings',
-    OPERATIVES: 'Operatives'
+    OPERATIVES: 'Operatives',
+};
+
+export const USERS_ADMIN_TABS = {
+    ACTIVE: 'Active',
+    INVITED: 'Invited',
+    DELETED: 'Deleted',
+};
+
+export const USERS_OPERATIVES_TABS = {
+    ACTIVE: 'Active',
+    INACTIVE: 'Inactive',
+    INVITED: 'Invited',
+    DELETED: 'Deleted',
 };
