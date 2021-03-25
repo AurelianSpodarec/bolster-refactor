58c8af625cb500c7b8972e654db249fc4c5b5f46
Oliver <oli@silverchip.com>
2021-03-25 14:53:58 +0000

    add prereqid onto prereq value options on template questions

--- app-src/src/components/superAdmin/templateBuilder/setQuestion/hocs/withSetQuestion.js
+++ app-src/src/components/superAdmin/templateBuilder/setQuestion/hocs/withSetQuestion.js
@@ -109,31 +109,34 @@ export default function (WrappedComponent) {
             let prereqValueOptions = [];
 
             prereqs.forEach(prereq => {
-                const { questionType, optionType, options } = prereq;
+                const { questionType, optionType, options, name, uuid } = prereq;
+
+                let curPrereqOptions = [];
+
                 if (questionType === STATUS) {
-                    prereqValueOptions = prereqValueOptions.concat(this._getStatusOptions());
+                    curPrereqOptions = (this._getStatusOptions());
                 } else if (questionType === CHECKBOX) {
-                    prereqValueOptions = prereqValueOptions.concat([
+                    curPrereqOptions =([
                         { label: 'True', value: 'true' },
                         { label: 'False', value: 'false' },
                     ]);
                 } else if (optionType) {
-                    prereqValueOptions = prereqValueOptions.concat(
-                        this._getDropownOptionsByType(optionType),
+                    curPrereqOptions = (
+                        this._getDropownOptionsByType(optionType)
                     );
                 } else if (options) {
-                    prereqValueOptions = prereqValueOptions.concat(
+                    curPrereqOptions = (
                         options.map(opt => ({
                             label: opt.text,
                             value: opt.id,
-                        })),
+                        }))
                     );
                 }
+
+                prereqValueOptions = prereqValueOptions.concat(curPrereqOptions.map(({label, value}) => ({label: `${label} (${name})`, value: `${value}#PREREQ_ID_${uuid}`})));
             });
-            const prereqValueOptionsDuplicatesRemoved = prereqValueOptions.filter(
-                (opt, i) => prereqValueOptions.findIndex(({ value }) => value === opt.value) === i,
-            );
-            return prereqValueOptionsDuplicatesRemoved;
+            
+            return prereqValueOptions;
         };
 
         _getStatusOptions = () => {
