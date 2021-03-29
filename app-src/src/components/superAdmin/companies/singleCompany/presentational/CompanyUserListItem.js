1ea8bb4bbbcb5308a06c6bf735a9ccc453d7cda9
Mark Baker <mark@silverchip.com>
2021-03-29 16:51:01 +0100

    add scrollbar to users table on a company (super admin)

--- app-src/src/components/superAdmin/companies/singleCompany/presentational/CompanyUserListItem.js
+++ app-src/src/components/superAdmin/companies/singleCompany/presentational/CompanyUserListItem.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { COMPANY_USER_ROLE_IDS } from 'constants/companyAdmin/enums';
 import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
 
-const CompanyUserListItem = ({ user, handleModalClick }) => (
+const CompanyUserListItem = ({ user, handleModalClick, tableColumnWidths }) => (
     <tr>
-        <td>
+        <td style={{ width: tableColumnWidths[0] }}>
             {user.userFirstName} {user.userLastName} ({user.userEmail})
         </td>
-        {/* <td className="email-field">{user.userEmail}</td> */}
-        <td>{user.userPhoneNumber}</td>
-        <td>{COMPANY_USER_ROLE_IDS[String(user.type)]}</td>
-        <td>{user.formattedOperativeCode}</td>
-        <td className="left-align">{user.linkedDeviceID ? 'Yes' : 'No'}</td>
-        <td>{user.appVersion ? user.appVersion : '-'}</td>
-        <td className="left-align">{user.linkedDeviceName ? user.linkedDeviceName : '-'}</td>
-        <td>
+        <td style={{ width: tableColumnWidths[1] }}>{user.userPhoneNumber}</td>
+        <td style={{ width: tableColumnWidths[2] }}>{COMPANY_USER_ROLE_IDS[String(user.type)]}</td>
+        <td style={{ width: tableColumnWidths[3] }}>{user.formattedOperativeCode}</td>
+        <td style={{ width: tableColumnWidths[4] }} className="left-align">
+            {user.linkedDeviceID ? 'Yes' : 'No'}
+        </td>
+        <td style={{ width: tableColumnWidths[5] }}>{user.appVersion ? user.appVersion : '-'}</td>
+        <td style={{ width: tableColumnWidths[6] }} className="left-align">
+            {user.linkedDeviceName ? user.linkedDeviceName : '-'}
+        </td>
+        <td style={{ width: tableColumnWidths[7] }}>
             <ButtonContainer handleClick={() => handleModalClick()} className="button">
                 View Latest Syncs
             </ButtonContainer>
