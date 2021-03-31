2a75e2264a4e5e9b56e3037038a4c1b2178e0a7e
Rob Fairclough <rob@silverchip.com>
2021-03-31 15:48:42 +0100

    add client area

--- app-src/src/components/companyAdmin/userManagement/clients/allClients/presentational/AllClientsTable.js
+++ app-src/src/components/companyAdmin/userManagement/clients/allClients/presentational/AllClientsTable.js
@@ -4,10 +4,13 @@ import Table from 'components/shared/generic/tables/presentational/Table';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import AllClientsList from './AllClientsList';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
+import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
 
 const AllClientsTable = ({ headers, clients, isFetching, error }) => (
     <BlockContainer>
-        <BlockHeading title="Clients" />
+        <BlockHeading title="Clients">
+            <ButtonContainer to="/company/users-management/clients/create">Add</ButtonContainer>
+        </BlockHeading>
         <Table
             headers={headers}
             isFetching={isFetching}
