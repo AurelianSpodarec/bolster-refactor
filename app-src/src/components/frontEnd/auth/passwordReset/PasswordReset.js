de3bb0316188cf39006b4264608f8c10b395e863
Mark Baker <mark@silverchip.com>
2021-04-09 13:49:08 +0100

    add confirm password field to reset password page

--- app-src/src/components/frontEnd/auth/passwordReset/PasswordReset.js
+++ app-src/src/components/frontEnd/auth/passwordReset/PasswordReset.js
@@ -4,9 +4,13 @@ import ButtonContainer from 'components/shared/generic/button/containers/ButtonC
 import Form from 'components/shared/generic/form/containers/Form';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
 import Field from 'components/shared/generic/form/presentational/Field';
-import React, { useState } from 'react';
+import FrontEndButton from 'components/frontEnd/shared/buttons/presentational/FrontEndButton';
+import { useForm } from 'helpers/hooks';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router';
+import addFieldError from 'actions/shared/generic/fieldErrors/sync/addFieldError';
+import removeFieldError from 'actions/shared/generic/fieldErrors/sync/removeFieldError';
 
 const PasswordReset = () => {
     const location = useLocation();
@@ -17,7 +21,10 @@ const PasswordReset = () => {
 
     const { isPosting, postSuccess, error } = useSelector(requestStateSelector);
 
-    const [password, setPassword] = useState('');
+    const [form, handleChange] = useForm({
+        password: '',
+        confirmPassword: '',
+    });
 
     return (
         <div
@@ -30,18 +37,38 @@ const PasswordReset = () => {
                     <p>Password successfully reset. You may not log in.</p>
                 ) : (
                     <Form onSubmit={handleSubmit}>
-                        <Field name="Enter your new password below." required>
+                        <Field name="Enter new password" sizeClasses="size-lg-6" required>
                             <TextInputContainer
                                 name="password"
-                                value={password}
-                                handleChange={(_, value) => setPassword(value)}
+                                value={form.password}
+                                handleChange={handleChange}
                                 placeholder="Password..."
                                 required
                                 type="password"
+                                validate={validatePassword}
                                 includePasswordStrength
                             />
                         </Field>
-                        <ButtonContainer handleClick={handleSubmit}>Submit</ButtonContainer>
+                        <Field name="Confirm password" sizeClasses="size-lg-6" required>
+                            <TextInputContainer
+                                name="confirmPassword"
+                                value={form.confirmPassword}
+                                handleChange={handleChange}
+                                placeholder="Confirm password..."
+                                required
+                                validate={validateConfirmPassword}
+                                type="password"
+                            />
+                        </Field>
+                        <Field>
+                            <div className="size-lg-12 center-aligned">
+                                <span style={{ display: 'inline-block' }}>
+                                    <FrontEndButton type="submit" disabled={isPosting}>
+                                        Submit
+                                    </FrontEndButton>
+                                </span>
+                            </div>
+                        </Field>
                     </Form>
                 )}
                 {!!error && (
@@ -54,10 +81,28 @@ const PasswordReset = () => {
         </div>
     );
 
+    function validatePassword(password) {
+        const { confirmPassword } = form;
+        if (password !== confirmPassword) {
+            dispatch(addFieldError('confirmPassword', 'Passwords do not match'));
+        } else {
+            dispatch(removeFieldError('confirmPassword'));
+        }
+        return null;
+    }
+
+    function validateConfirmPassword(confirmPassword) {
+        const { password } = form;
+
+        return password !== confirmPassword
+            ? 'Passwords do not match'
+            : dispatch(removeFieldError('confirmPassword'));
+    }
+
     function handleSubmit(e) {
         e.preventDefault();
         if (!isPosting) {
-            dispatch(postPasswordReset({ token, password }));
+            dispatch(postPasswordReset({ token, password: form.password }));
         }
     }
 };
