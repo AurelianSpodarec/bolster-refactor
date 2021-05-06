7078b834bddb2c1b5272d765373bdcce647b6472
Rob Fairclough <rob@silverchip.com>
2021-05-06 13:57:00 +0100

    fix mixed actions

--- app-src/src/actions/shared/auth/async/postConfirmChangeEmail.js
+++ app-src/src/actions/shared/auth/async/postConfirmChangeEmail.js
@@ -2,30 +2,30 @@ import axios from 'axios';
 
 import { getHeaders, handleErrors } from 'helpers/api';
 import {
-    POST_EMAIL_CONFIRMATION_REQUEST,
-    POST_EMAIL_CONFIRMATION_SUCCESS,
-    POST_EMAIL_CONFIRMATION_FAILURE,
+    POST_CHANGE_EMAIL_CONFIRMATION_REQUEST,
+    POST_CHANGE_EMAIL_CONFIRMATION_SUCCESS,
+    POST_CHANGE_EMAIL_CONFIRMATION_FAILURE,
 } from 'constants/actionTypes/auth';
 import { AUTH_API_URL } from 'config';
 
 export const postEmailConfirmationRequest = () => ({
-    type: POST_EMAIL_CONFIRMATION_REQUEST,
+    type: POST_CHANGE_EMAIL_CONFIRMATION_REQUEST,
 });
 
 export const postEmailConfirmationSuccess = () => ({
-    type: POST_EMAIL_CONFIRMATION_SUCCESS,
+    type: POST_CHANGE_EMAIL_CONFIRMATION_SUCCESS,
 });
 
 export const postEmailConfirmationFailure = error => ({
-    type: POST_EMAIL_CONFIRMATION_FAILURE,
+    type: POST_CHANGE_EMAIL_CONFIRMATION_FAILURE,
     error,
 });
 
-export default postBody => dispatch => {
+export default token => dispatch => {
     dispatch(postEmailConfirmationRequest());
 
     return axios
-        .post(`${AUTH_API_URL}/auth/email-confirmation/confirm`, postBody, getHeaders())
+        .post(`${AUTH_API_URL}/auth/change-email/confirm/${token}`, {}, getHeaders())
         .then(res => dispatch(postEmailConfirmationSuccess(res.data)))
         .catch(err => dispatch(handleErrors(postEmailConfirmationFailure)(err)));
 };
