fd5f23d6ffec1c43ab0d34a70cd39c50d64e4795
Mark Baker <mark@silverchip.com>
2021-05-07 13:00:20 +0100

    add asterisk to end of merged pins on merge tool selection

--- app-src/src/components/superAdmin/siteManagement/mergeTool/containers/MergeToolFormContainer.js
+++ app-src/src/components/superAdmin/siteManagement/mergeTool/containers/MergeToolFormContainer.js
@@ -108,10 +108,12 @@ const MergeToolFormContainer = ({
 
     function _getPinsOptionsList() {
         const pinsArr = Object.values(pins).map(pin => {
+            const suffix = pin.isMerged ? '*' : '';
+
             return {
                 value: pin.id,
-                label: pin.pinCode,
-                text: pin.pinCode,
+                label: `${pin.pinCode}${suffix}`,
+                text: `${pin.pinCode}${suffix}`,
             };
         });
 
