bfca11ed5e3a7325c2a3917e983e7e23024dd30c
Mark Baker <mark@silverchip.com>
2021-05-20 10:40:45 +0100

    add red row to super admin users area in companies when needed

--- app-src/src/components/superAdmin/companies/singleCompany/presentational/CompanyUserListItem.js
+++ app-src/src/components/superAdmin/companies/singleCompany/presentational/CompanyUserListItem.js
@@ -3,44 +3,54 @@ import { COMPANY_USER_ROLE_IDS } from 'constants/companyAdmin/enums';
 import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
 import { getStorageString, isLowMemory, isLowStorage } from 'helpers/generic';
 
-const CompanyUserListItem = ({ user, handleModalClick, tableColumnWidths }) => (
-    <tr>
-        <td style={{ width: tableColumnWidths[0] }} className="cell-break-all">
-            {user.userFirstName} {user.userLastName} <br />({user.userEmail})
-        </td>
-        <td style={{ width: tableColumnWidths[1] }} className="cell-break-all">
-            {user.userPhoneNumber}
-        </td>
-        <td style={{ width: tableColumnWidths[2] }}>{COMPANY_USER_ROLE_IDS[String(user.type)]}</td>
-        <td style={{ width: tableColumnWidths[3] }}>{user.formattedOperativeCode}</td>
-        <td style={{ width: tableColumnWidths[4] }} className="left-align">
-            {user.linkedDeviceID ? 'Yes' : 'No'}
-        </td>
-        <td style={{ width: tableColumnWidths[5] }}>{user.appVersion ? user.appVersion : '-'}</td>
-        <td style={{ width: tableColumnWidths[6] }} className="left-align">
-            {user.linkedDeviceName ? user.linkedDeviceName : '-'}
-            {user.deviceRAM && (
-                <>
-                    <br />({getStorageString(user.deviceRAM)} RAM.)
-                    {isLowMemory(user.deviceRAM) &&
-                        'This device has low RAM and may struggle to run the Bolster App.'}
-                </>
-            )}
-            {user.physicalStorageTotal && (
-                <>
-                    <br />({getStorageString(user.physicalStorageAvailable)} /{' '}
-                    {getStorageString(user.physicalStorageTotal)} storage free)
-                    {isLowStorage(user.physicalStorageAvailable) &&
-                        'This device has low storage space. It may struggle to create new pins or down sync new data.'}
-                </>
-            )}
-        </td>
-        <td style={{ width: tableColumnWidths[7] }}>
-            <ButtonContainer handleClick={() => handleModalClick()} className="button">
-                View Latest Syncs
-            </ButtonContainer>
-        </td>
-    </tr>
-);
+const CompanyUserListItem = ({ user, handleModalClick, tableColumnWidths }) => {
+    const isRowRed =
+        (user.deviceRAM && isLowMemory(user.deviceRAM)) ||
+        (user.physicalStorageTotal && isLowStorage(user.physicalStorageAvailable));
+
+    return (
+        <tr className={`${isRowRed ? 'red-row' : ''}`}>
+            <td style={{ width: tableColumnWidths[0] }} className="cell-break-all">
+                {user.userFirstName} {user.userLastName} <br />({user.userEmail})
+            </td>
+            <td style={{ width: tableColumnWidths[1] }} className="cell-break-all">
+                {user.userPhoneNumber}
+            </td>
+            <td style={{ width: tableColumnWidths[2] }}>
+                {COMPANY_USER_ROLE_IDS[String(user.type)]}
+            </td>
+            <td style={{ width: tableColumnWidths[3] }}>{user.formattedOperativeCode}</td>
+            <td style={{ width: tableColumnWidths[4] }} className="left-align">
+                {user.linkedDeviceID ? 'Yes' : 'No'}
+            </td>
+            <td style={{ width: tableColumnWidths[5] }}>
+                {user.appVersion ? user.appVersion : '-'}
+            </td>
+            <td style={{ width: tableColumnWidths[6] }} className="left-align">
+                {user.linkedDeviceName ? user.linkedDeviceName : '-'}
+                {user.deviceRAM && (
+                    <>
+                        <br />({getStorageString(user.deviceRAM)} RAM.)
+                        {isLowMemory(user.deviceRAM) &&
+                            'This device has low RAM and may struggle to run the Bolster App.'}
+                    </>
+                )}
+                {user.physicalStorageTotal && (
+                    <>
+                        <br />({getStorageString(user.physicalStorageAvailable)} /{' '}
+                        {getStorageString(user.physicalStorageTotal)} storage free)
+                        {isLowStorage(user.physicalStorageAvailable) &&
+                            'This device has low storage space. It may struggle to create new pins or down sync new data.'}
+                    </>
+                )}
+            </td>
+            <td style={{ width: tableColumnWidths[7] }}>
+                <ButtonContainer handleClick={() => handleModalClick()} className="button">
+                    View Latest Syncs
+                </ButtonContainer>
+            </td>
+        </tr>
+    );
+};
 
 export default CompanyUserListItem;
