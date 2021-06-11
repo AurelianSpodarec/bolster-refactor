ac654cbe351337a1c18e872ffbb4b9c6f2553df3
Mark Baker <mark@silverchip.com>
2021-06-11 10:54:26 +0100

    add shared text component for showing pin names

--- app-src/src/components/shared/pinSelector/presentational/SelectorPinItem.js
+++ app-src/src/components/shared/pinSelector/presentational/SelectorPinItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+
+import PinSelectionText from './PinSelectionText';
 
 const SelectorPinItem = ({
     pin,
@@ -9,41 +10,19 @@ const SelectorPinItem = ({
     handleMouseUp,
     clicking,
     pinsList,
-}) => {
-    const { drawingID, drawings, pins } = useSelector(mapStateToProps);
-    const fullPin = pins[pin.value] || {};
-    const drawing = drawings[fullPin.drawingID];
-    const showDrawingName = Array.isArray(drawingID) && drawingID.length > 1;
-
-    return (
-        <div
-            // onClick={e => handlePinClick(e, pin.value)}
-            className={`selector-pin ${active ? 'active' : ''}`}
-            onMouseDown={e => {
-                handlePinClick(e, pin.value, pinsList);
-                handleMouseDown();
-            }}
-            onMouseUp={handleMouseUp}
-            onMouseOver={clicking ? e => handlePinClick(e, pin.value) : null}
-        >
-            {showDrawingName ? `${drawing.name} / ` : ''}
-            {pin.text}
-        </div>
-    );
-};
-
-const mapStateToProps = ({
-    companyAdmin: {
-        drawingsReducer: { drawings },
-        pinsReducer: { pins },
-        reportsReducer: {
-            filters: { drawingID },
-        },
-    },
-}) => ({
-    drawingID,
-    drawings,
-    pins,
-});
+}) => (
+    <div
+        // onClick={e => handlePinClick(e, pin.value)}
+        className={`selector-pin ${active ? 'active' : ''}`}
+        onMouseDown={e => {
+            handlePinClick(e, pin.value, pinsList);
+            handleMouseDown();
+        }}
+        onMouseUp={handleMouseUp}
+        onMouseOver={clicking ? e => handlePinClick(e, pin.value) : null}
+    >
+        <PinSelectionText pinID={pin.value} pinCode={pin.text} />
+    </div>
+);
 
 export default SelectorPinItem;
