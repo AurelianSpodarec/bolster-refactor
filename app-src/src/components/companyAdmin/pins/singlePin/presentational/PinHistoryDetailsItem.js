26d75558c96cebf3fa62e5abdfd8f79ebb185492
Seb Smith <seb@silverchip.com>
2021-06-15 15:10:57 +0100

    Plugs in operative code and company name ready for backend

--- app-src/src/components/companyAdmin/pins/singlePin/presentational/PinHistoryDetailsItem.js
+++ app-src/src/components/companyAdmin/pins/singlePin/presentational/PinHistoryDetailsItem.js
@@ -16,7 +16,6 @@ const PinHistoryDetailsItem = ({
     drawingID,
     isDeleteHistory,
     pin,
-    addedByCompany,
     templateName,
 }) => (
     <div className="item">
@@ -36,7 +35,7 @@ const PinHistoryDetailsItem = ({
 
         <FieldOutput
             title="Added by"
-            description={`${history.createdByOperativeFullName} - ${addedByCompany}`}
+            description={`${history.createdByOperativeFullName} - ${history.formattedOperativeCode} (${history.companyName})`}
             sizeClass="size-lg-3 size-md-12"
         />
         <FieldOutput
