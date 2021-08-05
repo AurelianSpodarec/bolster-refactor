d0f25cbf6dcc44d3f83b692e2cfa4ddbecfe3356
Mark Baker <mark@silverchip.com>
2021-08-05 15:59:25 +0100

    add information to user creations table

--- app-src/src/components/superAdmin/userCreations/UserCreations.js
+++ app-src/src/components/superAdmin/userCreations/UserCreations.js
@@ -1,17 +1,30 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import fetchUserCreations from 'actions/superAdmin/users/async/fetchUserCreations';
+
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
-import React from 'react';
+import UserCreationsTable from './UserCreationsTable';
 
-const UserCreations = () => (
-    <>
-        <PageHeading title="User Creations" withBackButton />
-        <BlockContainer>
-            <p>Filter...</p>
-        </BlockContainer>
-        <BlockContainer>
-            <p>Table...</p>
-        </BlockContainer>
-    </>
-);
+const UserCreations = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUserCreations());
+    }, [dispatch]);
+
+    return (
+        <>
+            <PageHeading title="User Creations" withBackButton />
+            <BlockContainer>
+                <p>Filter...</p>
+            </BlockContainer>
+            <BlockContainer>
+                <UserCreationsTable />
+            </BlockContainer>
+        </>
+    );
+};
 
 export default UserCreations;
