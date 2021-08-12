1b02456c4f37a3218111bc99abd3dc63b070f3d0
Oliver <oli@silverchip.com>
2021-08-12 15:09:48 +0100

    mvoe CreateReportReloadOptions into OutputSettingsContainer so it is always active

--- app-src/src/components/companyAdmin/reports/createReport/components/presentational/CreateReport.js
+++ app-src/src/components/companyAdmin/reports/createReport/components/presentational/CreateReport.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import CreateReportForm from '../presentational/CreateReportForm';
-import CreateReportReloadOptions from '../containers/CreateReportReloadOptions';
 
 const CreateReport = () => (
     <>
         <PageHeading title="Create Report" withBackButton />
         <CreateReportForm />
-        <CreateReportReloadOptions/>
     </>
 );
 
