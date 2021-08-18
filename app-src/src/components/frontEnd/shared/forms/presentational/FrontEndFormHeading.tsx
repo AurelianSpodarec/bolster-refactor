7dbff519a5334032ef0bdb84815b646bcbc973cc
Rob Fairclough <rob@silverchip.com>
2021-08-18 13:31:47 +0100

    ts

--- app-src/src/components/frontEnd/shared/forms/presentational/FrontEndFormHeading.tsx
+++ app-src/src/components/frontEnd/shared/forms/presentational/FrontEndFormHeading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const FrontEndFormHeading = ({ title, subtitle = '', classes = '' }) => {
+const FrontEndFormHeading = ({ title, subtitle = '', classes = '' }: FrontEndFormHeadingProps) => {
     return (
         <div className={`auth-form-heading ${classes}`}>
             <h2>{title}</h2>
@@ -8,5 +8,11 @@ const FrontEndFormHeading = ({ title, subtitle = '', classes = '' }) => {
         </div>
     );
 };
+
+interface FrontEndFormHeadingProps {
+    title: string;
+    subtitle?: string;
+    classes?: string;
+}
 
 export default FrontEndFormHeading;
