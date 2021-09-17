f1488066b0d038d468e2ed0b8b1cfb81bd3ac084
Mark Baker <mark@silverchip.com>
2021-09-17 16:00:33 +0100

    generic helper update

--- app-src/src/helpers/generic.ts
+++ app-src/src/helpers/generic.ts
@@ -343,3 +343,28 @@ export const reverseEnum = obj =>
     }, {});
 
 export const boolToYesNo = bool => (bool ? 'Yes' : 'No');
+
+export const getStorageString = (bytes = 0) => {
+    const kb = bytes / 1024;
+    const mb = kb / 1024;
+    const gb = mb / 1024;
+    if (gb >= 1) {
+        return `${gb.toFixed(1)}GB`;
+    }
+    return `${mb.toFixed(1)}MB`;
+};
+
+export const isLowMemory = bytes => {
+    const gbLowerLimit = 3;
+    const kb = bytes / 1024;
+    const mb = kb / 1024;
+    const gb = mb / 1024;
+    return gb < gbLowerLimit;
+};
+
+export const isLowStorage = bytes => {
+    const mbLowerLimit = 500;
+    const kb = bytes / 1024;
+    const mb = kb / 1024;
+    return mb < mbLowerLimit;
+};
