8b78dbb36124282eb9764e20474044901b2eb768
Nathan Rath <nathan@silverchip.com>
2021-10-04 08:37:07 +0100

    moved logic

--- app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/containers/OperativeTimesheetContainer.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/containers/OperativeTimesheetContainer.js
@@ -6,7 +6,35 @@ import { connect } from 'react-redux';
 import OperativeTimesheet from '../presentational/OperativeTimesheet';
 
 const OperativeTimesheetContainer = () => {
-    return <OperativeTimesheet operativeName="##User Name##" />;
+    const thisWeek = moment(new Date()).startOf('week').add(1, 'days').toISOString();
+    const [startDate, setStartDate] = useState(thisWeek);
+
+    const [selectedDate, setSelectedDate] = useState(moment(new Date()).toISOString());
+    const [timePeriod, setTimePeriod] = useState(TIME_PERIOD.DAY);
+
+    const onPrev = () => setStartDate(moment(startDate).subtract(7, 'days').toISOString());
+    const onNext = () => setStartDate(moment(startDate).add(7, 'days').toISOString());
+    const onToday = () => setStartDate(thisWeek);
+
+    const onDaySelect = timestamp => {
+        setTimePeriod(TIME_PERIOD.DAY);
+        setSelectedDate(timestamp);
+    };
+    const onWeekSelect = () => setTimePeriod(TIME_PERIOD.WEEK);
+
+    return (
+        <OperativeTimesheet
+            operativeName="##User Name##"
+            startDate={startDate}
+            selectedDate={selectedDate}
+            timePeriod={timePeriod}
+            onPrev={onPrev}
+            onNext={onNext}
+            onToday={onToday}
+            onDaySelect={onDaySelect}
+            onWeekSelect={onWeekSelect}
+        />
+    );
 };
 
 export default withRouter(connect(null)(OperativeTimesheetContainer));
--- app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/presentational/OperativeTimesheet.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/presentational/OperativeTimesheet.js
@@ -8,23 +8,17 @@ import PageHeading from 'components/shared/generic/pageHeading/presentational/Pa
 import BackButtonContainer from 'components/shared/generic/backButton/containers/BackButtonContainer';
 import TimesheetCalenderContainer from '../timesheetCalender/containers/TimesheetCalenderContainer';
 
-const OperativeTimesheet = ({ operativeName }) => {
-    const thisWeek = moment(new Date()).startOf('week').add(1, 'days').toISOString();
-    const [startDate, setStartDate] = useState(thisWeek);
-
-    const [selectedDate, setSelectedDate] = useState(moment(new Date()).toISOString());
-    const [timePeriod, setTimePeriod] = useState(TIME_PERIOD.DAY);
-
-    const onPrev = () => setStartDate(moment(startDate).subtract(7, 'days').toISOString());
-    const onNext = () => setStartDate(moment(startDate).add(7, 'days').toISOString());
-    const onToday = () => setStartDate(thisWeek);
-
-    const onDaySelect = timestamp => {
-        setTimePeriod(TIME_PERIOD.DAY);
-        setSelectedDate(timestamp);
-    };
-    const onWeekSelect = () => setTimePeriod(TIME_PERIOD.WEEK);
-
+const OperativeTimesheet = ({
+    operativeName,
+    startDate,
+    selectedDate,
+    timePeriod,
+    onPrev,
+    onNext,
+    onToday,
+    onDaySelect,
+    onWeekSelect,
+}) => {
     return (
         <>
             <PageHeading leftChildren={true} title={`Timesheet - ${operativeName}`}>
