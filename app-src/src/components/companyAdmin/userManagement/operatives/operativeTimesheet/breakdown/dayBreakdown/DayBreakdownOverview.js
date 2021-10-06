eb73de0a16f9300056a5d195261130121eb93f8f
Nathan Rath <nathan@silverchip.com>
2021-10-06 16:13:50 +0100

    added pin feed with temp data

--- app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/breakdown/dayBreakdown/DayBreakdownOverview.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/breakdown/dayBreakdown/DayBreakdownOverview.js
@@ -4,19 +4,10 @@ import useDay from '../../hooks/useDay';
 import BreakdownColumns from '../BreakdownColumns';
 import BreakdownDaySummary from '../BreakdownDaySummary';
 import BreakdownNotes from '../BreakdownNotes';
+import DashboardPinFeed from '../../../../../dashboard/presentational/DashboardPinFeed';
 
 const DayBreakdownOverview = ({ selectedDate }) => {
     const { hours, pins, reference, description, notes } = useDay(selectedDate) ?? {};
-
-    const tempData = {
-        statuses: {
-            ActionRequired: 32,
-            Installed: 25,
-            Inspected: 18,
-            NoAction: 1,
-            Other: 2,
-        },
-    };
 
     return (
         <BreakdownColumns
@@ -35,7 +26,10 @@ const DayBreakdownOverview = ({ selectedDate }) => {
             right={
                 <>
                     <div className="breakdown-piechart">
-                        <PieChart stats={tempData} />
+                        <PieChart stats={stats} />
+                    </div>
+                    <div className="breakdown-feed">
+                        <DashboardPinFeed pins={feed} isFetching={false} error={null} />
                     </div>
                 </>
             }
@@ -44,3 +38,76 @@ const DayBreakdownOverview = ({ selectedDate }) => {
 };
 
 export default DayBreakdownOverview;
+
+const stats = {
+    statuses: {
+        ActionRequired: 32,
+        Installed: 25,
+        Inspected: 18,
+        NoAction: 1,
+        Other: 2,
+    },
+};
+
+const feed = [
+    {
+        id: 4439686,
+        status: 10,
+        pinCode: '0007:52',
+        createdBy: 'Tom G',
+        location: 'testing / 475 / floor 1 / drawing 1',
+        createdOn: '2021-10-06T14:31:57.899',
+        syncedOn: '2021-10-06T14:32:02.238',
+        pinID: 3234137,
+    },
+    {
+        id: 4439623,
+        status: 10,
+        pinCode: '0012:57',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite Drawing 3',
+        createdOn: '2021-10-05T10:30:46.156',
+        syncedOn: '2021-10-05T10:43:57.815',
+        pinID: 3234087,
+    },
+    {
+        id: 4439622,
+        status: 10,
+        pinCode: '0001:06',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite Drawing 3',
+        createdOn: '2021-10-05T09:45:39.281',
+        syncedOn: '2021-10-05T09:45:53.003',
+        pinID: 3233857,
+    },
+    {
+        id: 4439621,
+        status: 20,
+        pinCode: '0008:06',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite drawing 4',
+        createdOn: '2021-10-04T16:24:16.27',
+        syncedOn: '2021-10-04T16:24:53.011',
+        pinID: 3234086,
+    },
+    {
+        id: 4439618,
+        status: 20,
+        pinCode: '0008:07',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite drawing 5',
+        createdOn: '2021-09-30T09:02:37.158',
+        syncedOn: '2021-09-30T09:02:58.52',
+        pinID: 3234065,
+    },
+    {
+        id: 4439617,
+        status: 20,
+        pinCode: '0007:07',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite drawing 5',
+        createdOn: '2021-09-30T09:00:47.694',
+        syncedOn: '2021-09-30T09:00:52.542',
+        pinID: 3234064,
+    },
+];
--- app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/breakdown/weekBreakdown/WeekBreakdownOverview.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/breakdown/weekBreakdown/WeekBreakdownOverview.js
@@ -1,3 +1,4 @@
+import DashboardPinFeed from 'components/companyAdmin/dashboard/presentational/DashboardPinFeed';
 import DateTimeContainer from 'components/shared/dateTime/containers/DateTimeContainer';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 import PieChart from 'components/shared/stats/presentational/PieChart';
@@ -9,16 +10,6 @@ import BreakdownDaySummary from '../BreakdownDaySummary';
 
 const WeekBreakdownOverview = ({ selectedDate }) => {
     const week = useWeek(selectedDate);
-
-    const tempData = {
-        statuses: {
-            ActionRequired: 32,
-            Installed: 25,
-            Inspected: 18,
-            NoAction: 1,
-            Other: 2,
-        },
-    };
 
     return (
         <BreakdownColumns
@@ -47,7 +38,10 @@ const WeekBreakdownOverview = ({ selectedDate }) => {
             right={
                 <>
                     <div className="breakdown-piechart">
-                        <PieChart stats={tempData} />
+                        <PieChart stats={stats} />
+                    </div>
+                    <div className="breakdown-feed">
+                        <DashboardPinFeed pins={feed} isFetching={false} error={null} />
                     </div>
                 </>
             }
@@ -56,3 +50,76 @@ const WeekBreakdownOverview = ({ selectedDate }) => {
 };
 
 export default WeekBreakdownOverview;
+
+const stats = {
+    statuses: {
+        ActionRequired: 32,
+        Installed: 25,
+        Inspected: 18,
+        NoAction: 1,
+        Other: 2,
+    },
+};
+
+const feed = [
+    {
+        id: 4439686,
+        status: 10,
+        pinCode: '0007:52',
+        createdBy: 'Tom G',
+        location: 'testing / 475 / floor 1 / drawing 1',
+        createdOn: '2021-10-06T14:31:57.899',
+        syncedOn: '2021-10-06T14:32:02.238',
+        pinID: 3234137,
+    },
+    {
+        id: 4439623,
+        status: 10,
+        pinCode: '0012:57',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite Drawing 3',
+        createdOn: '2021-10-05T10:30:46.156',
+        syncedOn: '2021-10-05T10:43:57.815',
+        pinID: 3234087,
+    },
+    {
+        id: 4439622,
+        status: 10,
+        pinCode: '0001:06',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite Drawing 3',
+        createdOn: '2021-10-05T09:45:39.281',
+        syncedOn: '2021-10-05T09:45:53.003',
+        pinID: 3233857,
+    },
+    {
+        id: 4439621,
+        status: 20,
+        pinCode: '0008:06',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite drawing 4',
+        createdOn: '2021-10-04T16:24:16.27',
+        syncedOn: '2021-10-04T16:24:53.011',
+        pinID: 3234086,
+    },
+    {
+        id: 4439618,
+        status: 20,
+        pinCode: '0008:07',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite drawing 5',
+        createdOn: '2021-09-30T09:02:37.158',
+        syncedOn: '2021-09-30T09:02:58.52',
+        pinID: 3234065,
+    },
+    {
+        id: 4439617,
+        status: 20,
+        pinCode: '0007:07',
+        createdBy: 'Mark Baker',
+        location: 'Elite Site / Elite building 1 / Elite floor 1 / Elite drawing 5',
+        createdOn: '2021-09-30T09:00:47.694',
+        syncedOn: '2021-09-30T09:00:52.542',
+        pinID: 3234064,
+    },
+];
