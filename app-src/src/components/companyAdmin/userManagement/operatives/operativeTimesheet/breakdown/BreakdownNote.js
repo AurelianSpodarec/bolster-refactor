5facb92f3adf6f14c1dd4a4265aa5bd62f0e300d
Nathan Rath <nathan@silverchip.com>
2021-10-11 16:37:12 +0100

    reduced note image size and added zoom modal

--- app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/breakdown/BreakdownNote.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/operativeTimesheet/breakdown/BreakdownNote.js
@@ -1,16 +1,32 @@
+import showModal from 'actions/shared/generic/modals/sync/showModal';
 import DateTimeContainer from 'components/shared/dateTime/containers/DateTimeContainer';
 import { FILE_STORAGE_URL } from 'config';
 import { DATE_TIME_IDS } from 'constants/companyAdmin/enums';
+import { PIN_IMAGE } from 'constants/shared/modalTypes';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 const BreakdownNote = ({ note }) => {
+    const dispatch = useDispatch();
     const { comments, createdOn, imageS3Key } = note;
 
     return (
         <div className="breakdown-note">
             <DateTimeContainer date={new Date(createdOn)} datetime={DATE_TIME_IDS.TIME} />
             <p>{comments}</p>
-            {imageS3Key && <img alt="uploaded" src={`${FILE_STORAGE_URL}/${imageS3Key}`} />}
+            {imageS3Key && (
+                <img
+                    alt="uploaded"
+                    src={`${FILE_STORAGE_URL}/${imageS3Key}?width=200`}
+                    onClick={() =>
+                        dispatch(
+                            showModal(PIN_IMAGE, {
+                                image: `${FILE_STORAGE_URL}/${imageS3Key}?width=1500`,
+                            }),
+                        )
+                    }
+                />
+            )}
         </div>
     );
 };
