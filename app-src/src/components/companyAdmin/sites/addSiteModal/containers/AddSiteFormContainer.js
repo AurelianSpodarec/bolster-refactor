a6962becd3fe32afe43386e0ba213980dfa75524
Seb Smith <seb@silverchip.com>
2021-10-14 09:52:43 +0100

    Fixes crash on add site form

--- app-src/src/components/companyAdmin/sites/addSiteModal/containers/AddSiteFormContainer.js
+++ app-src/src/components/companyAdmin/sites/addSiteModal/containers/AddSiteFormContainer.js
@@ -229,6 +229,7 @@ class AddSiteFormContainer extends Component {
 
         return Object.entries(optionValues).reduce((acc, [manufacturerID, options]) => {
             const formattedOptionValues = formatOptions(Object.values(options));
+            console.log(formattedOptionValues);
             const filteredOptionValues = formattedOptionValues.filter(option =>
                 this.shouldOptionValueBeIncluded(option.serviceIDs),
             );
@@ -239,6 +240,10 @@ class AddSiteFormContainer extends Component {
 
     shouldOptionValueBeIncluded = serviceIDs => {
         const { subscriptionServiceIDs } = this.props;
+
+        if (!serviceIDs) {
+            return true;
+        }
         return serviceIDs.some(id => subscriptionServiceIDs.includes(id));
     };
 
