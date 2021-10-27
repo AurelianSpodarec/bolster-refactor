2473282adb8d4aa25390adb3158ce3126c4d0d79
Oluseun Adesina <seun@silverchip.com>
2021-10-27 12:11:25 +0100

    Fixed formulae for calculating statuses

--- app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/hooks/useTypeAndStatus.js
+++ app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/hooks/useTypeAndStatus.js
@@ -4,11 +4,12 @@ import moment from 'moment';
 const useTypeAndStatus = (isRecurring, actionedOn, dueOn) => {
     const type = isRecurring ? PIN_TASK_RECURRING.RECURRING : PIN_TASK_RECURRING.NON_RECURRING;
     let status = '';
+
     if (actionedOn) {
         if (moment(actionedOn).isAfter(dueOn)) status = PIN_TASK_STATUS.COMPLETE_LATE;
         else status = PIN_TASK_STATUS.COMPLETE;
     } else {
-        if (moment(dueOn).isBefore(new Date())) status = PIN_TASK_STATUS.INCOMPLETE;
+        if (moment(dueOn).isBefore()) status = PIN_TASK_STATUS.INCOMPLETE;
         else status = PIN_TASK_STATUS.DUE_SOON;
     }
 
