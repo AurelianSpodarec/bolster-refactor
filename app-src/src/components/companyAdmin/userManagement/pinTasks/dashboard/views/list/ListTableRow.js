5d0a431cd13787b948e4b06095707dca91679f42
Oluseun Adesina <seun@silverchip.com>
2021-10-28 08:31:00 +0100

    List view displays pin code

--- app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/list/ListTableRow.js
+++ app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/list/ListTableRow.js
@@ -9,14 +9,14 @@ import TaskPill from '../../TaskPill';
 import useTypeAndStatus from '../hooks/useTypeAndStatus';
 
 const ListTableRow = ({ pinTask, startEditPinTask }) => {
-    const { id, companyUserID, pinID, isRecurring, actionedOn, dueOn } = pinTask;
+    const { id, companyUserID, pinCode, isRecurring, actionedOn, dueOn } = pinTask;
     const { type, status } = useTypeAndStatus(isRecurring, actionedOn, dueOn);
 
     return (
         <tr>
             <td>{companyUserID}</td>
             <td></td>
-            <td>{pinID}</td>
+            <td>{pinCode}</td>
             <td>
                 <DateTimeContainer datetime={DATE_TIME_IDS.DATE} date={new Date(dueOn)} />
             </td>
