26a371cdca2ea292af1384b9dc132f73a1f4a4d8
Nick Barnard <nick@silverchip.com>
2021-11-02 17:30:16 +0000

    Tweak clicking behaviour on Grid Items

--- app-src/src/components/companyAdmin/documentLibrary/DocumentsGridItem.js
+++ app-src/src/components/companyAdmin/documentLibrary/DocumentsGridItem.js
@@ -7,27 +7,39 @@ import FileTypeIcon from './FileTypeIcon';
 
 const DocumentsGridItem = ({ item, isSelected, toggleItemSelect }) => {
     return (
-        <a
-            className="grid-item"
-            href={`/company/document-library?prefix=${'#'}`}
-            title={`Open ${item.name}${item.fileExtension ? `.${item.fileExtension}` : ''}`}
-        >
-            <div className="image-container">
+        <div className="grid-item">
+            <a
+                href={
+                    item.type === 100
+                        ? `/company/document-library?s3Key=${item.s3Key}`
+                        : '/document-library'
+                } // WIP
+                title={`Open ${item.name}${item.fileExtension ? `.${item.fileExtension}` : ''}`}
+                className="image-container"
+            >
                 <FileTypeIcon
                     src={item.type === 200 ? getIconFromExt(item.fileExtension) : FolderIcon}
                     alt={`${item.name} icon`}
                     width="auto"
                     height="160"
                 />
-            </div>
-            <div className="details-container">
+            </a>
+            <a
+                className="details-container"
+                href={
+                    item.type === 100
+                        ? `/company/document-library?s3Key=${item.s3Key}`
+                        : '/document-library'
+                } // WIP
+                title={`Open ${item.name}${item.fileExtension ? `.${item.fileExtension}` : ''}`}
+            >
                 {item.type === 200 && <FileTypeIcon src={getIconFromExt(item.fileExtension)} />}
                 <p
                     style={
                         item.type === 200 ? { maxWidth: 'calc(100% - 34px)' } : { maxWidth: '100%' }
                     }
                 >{`${item.name || '-'}${item.type === 200 ? '.' + item.fileExtension : ''}`}</p>
-            </div>
+            </a>
             <p className="meta">
                 Updated{' '}
                 <DateTimeContainer date={new Date(item.createdOn)} datetime={DATE_TIME_IDS.DATE} />
@@ -35,7 +47,7 @@ const DocumentsGridItem = ({ item, isSelected, toggleItemSelect }) => {
             <div className="dl-selection grid" onClick={() => toggleItemSelect(item.id)}>
                 <div className="selection-dot" style={{ opacity: isSelected ? 1 : 0 }} />
             </div>
-        </a>
+        </div>
     );
 };
 
