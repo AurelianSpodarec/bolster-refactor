654ef6a9fac4752224dea4b653c264cc7cff59f1
Nick Barnard <nick@silverchip.com>
2021-11-03 16:13:07 +0000

    Add links to more places

--- app-src/src/components/companyAdmin/documentLibrary/DocumentLibraryBreadcrumb.js
+++ app-src/src/components/companyAdmin/documentLibrary/DocumentLibraryBreadcrumb.js
@@ -1,19 +1,21 @@
+import { stripS3Key } from 'helpers/general';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DocumentLibraryBreadcrumb = ({ s3Key }) => {
     if (!s3Key) return <></>;
-    const s3Arr = s3Key.split('/');
+    const s3Arr = stripS3Key(s3Key).split('/');
     return (
         <div className="dl-breadcrumb">
             {s3Arr.map((item, i) => (
                 <>
-                    <a
+                    <Link
                         href={`/company/document-library/${s3Arr.slice(
                             0,
                             s3Arr.indexOf(item) + 1,
                         )}`}
                         title={item}
-                    >{`${item}${i === s3Arr.length - 1 ? ' / ' : ''}`}</a>
+                    >{`${item}${i === s3Arr.length - 1 ? ' / ' : ''}`}</Link>
                 </>
             ))}
         </div>
