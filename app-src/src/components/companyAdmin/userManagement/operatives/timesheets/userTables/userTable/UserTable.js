d353d558df0a6e64d24bf7e21dd4760df40ceaf0
Nathan Rath <nathan@silverchip.com>
2021-11-08 14:49:28 +0000

    added row limit and expand/hide button

--- app-src/src/components/companyAdmin/userManagement/operatives/timesheets/userTables/userTable/UserTable.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/timesheets/userTables/userTable/UserTable.js
@@ -2,11 +2,13 @@ import DateTimeContainer from 'components/shared/dateTime/containers/DateTimeCon
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 import { DATE_TIME_IDS } from 'constants/companyAdmin/enums';
 import { formatAsHrsMinsSecs } from 'helpers/generic';
-import React from 'react';
+import React, { useState } from 'react';
 import { timesheetUserTableDummyData } from '../UserTablesInner';
 import { Link } from 'react-router-dom';
 
-const UserTable = ({ date, day }) => {
+const UserTable = ({ date, day, initialRows = 3 }) => {
+    const [expanded, setExpanded] = useState(false);
+
     return (
         <>
             <tr>
@@ -26,42 +28,79 @@ const UserTable = ({ date, day }) => {
             </tr>
 
             {timesheetUserTableDummyData.map(
-                ({
-                    id,
-                    firstName,
-                    lastName,
-                    totalHoursWorked,
-                    timeIn,
-                    timeOut,
-                    totalBreakTime,
-                    pinHistoriesCreated,
-                    jobReferences,
-                }) => (
-                    <tr key={id}>
-                        <td>
-                            {firstName} {lastName} - {id}
-                        </td>
-                        <td>{formatAsHrsMinsSecs(totalHoursWorked)}</td>
-                        <td>
-                            <DateTimeContainer date={timeIn} datetime={DATE_TIME_IDS.DATE} />
-                        </td>
-                        <td>
-                            <DateTimeContainer date={timeOut} datetime={DATE_TIME_IDS.DATE} />
-                        </td>
-                        <td>{formatAsHrsMinsSecs(totalBreakTime)}</td>
-                        <td>{pinHistoriesCreated}</td>
-                        <td>{jobReferences.length}</td>
-                        <td>
-                            <Link
-                                to={`/company/users-management/operatives/${id}/timesheet`}
-                                className="button green"
-                            >
-                                <i class="far fa-eye" />
-                                View Timesheet
-                            </Link>
-                        </td>
-                    </tr>
+                (
+                    {
+                        id,
+                        firstName,
+                        lastName,
+                        totalHoursWorked,
+                        timeIn,
+                        timeOut,
+                        totalBreakTime,
+                        pinHistoriesCreated,
+                        jobReferences,
+                    },
+                    i,
+                ) => (
+                    <>
+                        {i > initialRows - 1 && !expanded ? (
+                            i === initialRows ? (
+                                <tr>
+                                    <td className="view-more-row" colSpan={8}>
+                                        <button
+                                            className="button"
+                                            onClick={() => setExpanded(true)}
+                                        >
+                                            <i class="far fa-eye" />
+                                            View All
+                                        </button>
+                                    </td>
+                                </tr>
+                            ) : null
+                        ) : (
+                            <tr key={id}>
+                                <td>
+                                    {firstName} {lastName} - {id}
+                                </td>
+                                <td>{formatAsHrsMinsSecs(totalHoursWorked)}</td>
+                                <td>
+                                    <DateTimeContainer
+                                        date={timeIn}
+                                        datetime={DATE_TIME_IDS.DATE}
+                                    />
+                                </td>
+                                <td>
+                                    <DateTimeContainer
+                                        date={timeOut}
+                                        datetime={DATE_TIME_IDS.DATE}
+                                    />
+                                </td>
+                                <td>{formatAsHrsMinsSecs(totalBreakTime)}</td>
+                                <td>{pinHistoriesCreated}</td>
+                                <td>{jobReferences.length}</td>
+                                <td>
+                                    <Link
+                                        to={`/company/users-management/operatives/${id}/timesheet`}
+                                        className="button green"
+                                    >
+                                        <i class="far fa-eye" />
+                                        View Timesheet
+                                    </Link>
+                                </td>
+                            </tr>
+                        )}
+                    </>
                 ),
+            )}
+            {expanded && (
+                <tr>
+                    <td className="view-more-row" colSpan={8}>
+                        <button className="button" onClick={() => setExpanded(false)}>
+                            <i class="far fa-eye-slash" />
+                            Hide
+                        </button>
+                    </td>
+                </tr>
             )}
         </>
     );
