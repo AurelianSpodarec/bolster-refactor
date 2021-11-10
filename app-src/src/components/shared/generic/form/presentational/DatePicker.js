6bdf54627d055898449c6ef654147563d569e2bf
Seb Smith <seb@silverchip.com>
2021-11-10 11:51:19 +0000

    Adds required fieldErrors for datepicker

--- app-src/src/components/shared/generic/form/presentational/DatePicker.js
+++ app-src/src/components/shared/generic/form/presentational/DatePicker.js
@@ -16,53 +16,62 @@ const DatePickerPresentational = ({
     maxDate,
     showTimeSelect = false,
     isIE10,
-}) =>
-    //currently only works on drawing single page - ie10 fix
-    isIE10 ? (
-        <TextInputContainer
-            type="date"
-            name={name}
-            handleChange={(name, val) => onChange(val)}
-            value={selected}
-            classes={`basic-date-input ${sizeClasses}`}
-            placeholder={placeholderText}
-        />
-    ) : (
-        <div className={`date-picker ${sizeClasses}`}>
-            <DatePicker
-                id={name}
-                selected={selected}
-                onChange={onChange}
-                dateFormat={
-                    showTimeSelect
-                        ? [
-                              'dd/MM/yyyy HH:mm',
-                              'd/M/yyyy HH:mm',
-                              'dd/M/yyyy HH:mm',
-                              'd/MM/yyyy HH:mm',
-                              'd/M/yy HH:mm',
-                              'dd/M/yy HH:mm',
-                              'd/MM/yy HH:mm',
-                          ]
-                        : [
-                              'dd/MM/yyyy',
-                              'd/M/yyyy',
-                              'dd/M/yyyy',
-                              'd/MM/yyyy',
-                              'd/M/yy',
-                              'dd/M/yy',
-                              'd/MM/yy',
-                          ]
-                }
-                placeholderText={placeholderText}
-                required={required}
-                onBlur={onBlur}
-                minDate={minDate}
-                maxDate={maxDate}
-                showTimeSelect={showTimeSelect}
+    error,
+}) => {
+    console.log(error);
+    return (
+        //currently only works on drawing single page - ie10 fix
+        isIE10 ? (
+            <TextInputContainer
+                type="date"
+                name={name}
+                handleChange={(name, val) => onChange(val)}
+                value={selected}
+                classes={`basic-date-input ${sizeClasses}`}
+                placeholder={placeholderText}
             />
-            <i className="far fa-calendar" />
-        </div>
+        ) : (
+            <div className={`date-picker ${sizeClasses}`}>
+                <DatePicker
+                    id={name}
+                    selected={selected}
+                    onChange={onChange}
+                    dateFormat={
+                        showTimeSelect
+                            ? [
+                                  'dd/MM/yyyy HH:mm',
+                                  'd/M/yyyy HH:mm',
+                                  'dd/M/yyyy HH:mm',
+                                  'd/MM/yyyy HH:mm',
+                                  'd/M/yy HH:mm',
+                                  'dd/M/yy HH:mm',
+                                  'd/MM/yy HH:mm',
+                              ]
+                            : [
+                                  'dd/MM/yyyy',
+                                  'd/M/yyyy',
+                                  'dd/M/yyyy',
+                                  'd/MM/yyyy',
+                                  'd/M/yy',
+                                  'dd/M/yy',
+                                  'd/MM/yy',
+                              ]
+                    }
+                    placeholderText={placeholderText}
+                    required={required}
+                    onBlur={onBlur}
+                    minDate={minDate}
+                    maxDate={maxDate}
+                    showTimeSelect={showTimeSelect}
+                />
+                {!!(error && error.length) && (
+                    <p className="error red-text text-accent-4">{error}</p>
+                )}
+
+                <i className="far fa-calendar" />
+            </div>
+        )
     );
+};
 
 export default DatePickerPresentational;
