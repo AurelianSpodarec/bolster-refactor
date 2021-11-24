9c7daf21adec3ec4d72e57ddbce10cb1199882b9
Seb Smith <seb@silverchip.com>
2021-11-24 10:59:33 +0000

    Displays icon in company template details

--- app-src/src/components/companyAdmin/templates/singleTemplate/containers/TemplateDetailsContainer.js
+++ app-src/src/components/companyAdmin/templates/singleTemplate/containers/TemplateDetailsContainer.js
@@ -5,16 +5,19 @@ import BlockContainer from 'components/shared/generic/block/containers/BlockCont
 import { isEmpty } from 'helpers/generic';
 import TemplateDetails from '../presentational/TemplateDetails';
 
-const TemplateDetailsContainer = ({ template, isFetching, error }) => (
-    <BlockContainer isFetching={isFetching} error={error} isEmpty={isEmpty(template)}>
-        <TemplateDetails template={template} />
-    </BlockContainer>
-);
+const TemplateDetailsContainer = ({ template, isFetching, error, service }) => {
+    return (
+        <BlockContainer isFetching={isFetching} error={error} isEmpty={isEmpty(template)}>
+            <TemplateDetails template={template} serviceIcon={service?.pinImageS3Key} />
+        </BlockContainer>
+    );
+};
 
 const mapStateToProps = (
     {
         companyAdmin: {
             templatesReducer: { isFetching, error, templates },
+            servicesReducer: { services },
         },
     },
     ownProps,
@@ -22,6 +25,7 @@ const mapStateToProps = (
     isFetching,
     error,
     template: templates[ownProps.templateID],
+    service: services[templates[ownProps.templateID].serviceID],
 });
 
 export default connect(mapStateToProps)(TemplateDetailsContainer);
