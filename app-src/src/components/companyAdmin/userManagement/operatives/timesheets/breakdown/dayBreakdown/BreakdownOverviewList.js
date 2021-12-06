9c6c7d89af900d948d0b187fec10a4da7154eafd
Oluseun Adesina <seun@silverchip.com>
2021-12-06 10:37:05 +0000

    day overview no data message added

--- app-src/src/components/companyAdmin/userManagement/operatives/timesheets/breakdown/dayBreakdown/BreakdownOverviewList.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/timesheets/breakdown/dayBreakdown/BreakdownOverviewList.js
@@ -13,41 +13,44 @@ const BreakdownOverviewList = ({
     filterDirection,
     filterByHasClockedIn,
 }) => {
-    return timesheets
+    const formattedTimesheets = timesheets
         .filter(timesheetFilter(filterByHasClockedIn, selectedDate))
-        .sort(timesheetSort(filterType, filterDirection, selectedDate))
-        .map(timesheet => {
-            const {
-                companyUserID,
-                firstName,
-                lastName,
-                email,
-                formattedHours,
-                formattedBreakHours,
-                formattedClockedInHours,
-                jobReferences,
-                totalPins,
-                clockIn,
-                clockOut,
-                clockerNotes,
-            } = useDayOverview(timesheet, selectedDate);
+        .sort(timesheetSort(filterType, filterDirection, selectedDate));
 
-            return (
-                <div className="day" key={companyUserID}>
-                    <BreakdownSummary
-                        name={`${firstName} ${lastName} (${email})`}
-                        formattedHours={formattedHours}
-                        formattedBreakHours={formattedBreakHours}
-                        formattedClockedInHours={formattedClockedInHours}
-                        totalPins={totalPins}
-                        clockIn={clockIn}
-                        clockOut={clockOut}
-                        jobReferences={jobReferences}
-                    />
-                    <BreakdownNotes notes={clockerNotes} />
-                </div>
-            );
-        });
+    if (formattedTimesheets.length === 0) return <p>No clock in data to display.</p>;
+
+    return formattedTimesheets.map(timesheet => {
+        const {
+            companyUserID,
+            firstName,
+            lastName,
+            email,
+            formattedHours,
+            formattedBreakHours,
+            formattedClockedInHours,
+            jobReferences,
+            totalPins,
+            clockIn,
+            clockOut,
+            clockerNotes,
+        } = useDayOverview(timesheet, selectedDate);
+
+        return (
+            <div className="day" key={companyUserID}>
+                <BreakdownSummary
+                    name={`${firstName} ${lastName} (${email})`}
+                    formattedHours={formattedHours}
+                    formattedBreakHours={formattedBreakHours}
+                    formattedClockedInHours={formattedClockedInHours}
+                    totalPins={totalPins}
+                    clockIn={clockIn}
+                    clockOut={clockOut}
+                    jobReferences={jobReferences}
+                />
+                <BreakdownNotes notes={clockerNotes} />
+            </div>
+        );
+    });
 };
 
 export default BreakdownOverviewList;
