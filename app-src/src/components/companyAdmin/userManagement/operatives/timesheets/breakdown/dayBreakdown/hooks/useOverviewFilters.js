4c8e491c3885581661fa7ca0ebdb7bfb8af54c0d
Seb Smith <seb@silverchip.com>
2021-12-17 16:55:29 +0000

    Fixes name sort

--- app-src/src/components/companyAdmin/userManagement/operatives/timesheets/breakdown/dayBreakdown/hooks/useOverviewFilters.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/timesheets/breakdown/dayBreakdown/hooks/useOverviewFilters.js
@@ -36,7 +36,7 @@ export const timesheetSort = (filterType, filterDirection, date) => (a, b) => {
 
     switch (filterType) {
         case 'name':
-            return filterDirection > 0 ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA);
+            return filterDirection > 0 ? nameB.localeCompare(nameA) : nameA.localeCompare(nameB);
         case 'hours':
             return filterDirection > 0 ? hoursA - hoursB : hoursB - hoursA;
         default:
