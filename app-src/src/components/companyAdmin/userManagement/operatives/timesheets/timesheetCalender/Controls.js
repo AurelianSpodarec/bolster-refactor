b4efc6e4ecab317818dd8d57e7c9ae10cf5530a4
Oluseun Adesina <seun@silverchip.com>
2021-12-17 11:05:24 +0000

    Added fix for timesheets to show email instead of numbers on main timesheets area

--- app-src/src/components/companyAdmin/userManagement/operatives/timesheets/timesheetCalender/Controls.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/timesheets/timesheetCalender/Controls.js
@@ -49,8 +49,6 @@ const Controls = ({
                         <i className="far fa-calendar-week"></i> Today
                     </ButtonContainer>
 
-                    {console.log('companyUserOptions', companyUserOptions)}
-
                     <div className="end">
                         <MultiSelect
                             options={companyUserOptions}
