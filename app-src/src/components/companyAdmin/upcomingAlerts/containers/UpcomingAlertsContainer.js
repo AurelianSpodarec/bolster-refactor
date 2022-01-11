f49761b689d97c26f2e9ca2710f7c21a6c7b9b27
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-11 08:38:26 +0000

    Auto stash before merge of "BOLSTER-808" and "origin/BOLSTER-808"
    Add fetchAllAlerts

--- app-src/src/components/companyAdmin/upcomingAlerts/containers/UpcomingAlertsContainer.js
+++ app-src/src/components/companyAdmin/upcomingAlerts/containers/UpcomingAlertsContainer.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import UpcomingAlerts from '../presentational/UpcomingAlerts';
-import { alertsIsFetching, selectAlerts } from 'selectors/alerts';
+import { alertsError, alertsIsFetching, selectAlerts } from 'selectors/alerts';
+import { fetchAllAlerts } from 'actions/companyAdmin/alerts/async/fetchAllAlerts';
 
 const UpcomingAlertsContainer = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchAllAlerts());
+    }, []);
+
     const alerts = useSelector(selectAlerts);
-    console.log(alerts);
     const isFetching = useSelector(alertsIsFetching);
+    const error = useSelector(alertsError);
 
     return (
         <div className="upcoming-alerts-container">
-            <UpcomingAlerts alerts={alerts} isFetching={isFetching} />
+            <UpcomingAlerts alerts={alerts} isFetching={isFetching} error={error} />
         </div>
     );
 };
