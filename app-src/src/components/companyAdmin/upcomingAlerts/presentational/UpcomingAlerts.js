328ee9d12143e48f27756da3d87c0b340956a1f9
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-13 08:05:33 +0000

    Clean up

--- app-src/src/components/companyAdmin/upcomingAlerts/presentational/UpcomingAlerts.js
+++ app-src/src/components/companyAdmin/upcomingAlerts/presentational/UpcomingAlerts.js
@@ -1,16 +1,13 @@
 import React from 'react';
+
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
-import Dropdown from 'components/shared/generic/form/presentational/Dropdown';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import Table from 'components/shared/generic/tables/presentational/Table';
-import useUpcomingAlerts from '../hooks/useUpcomingAlerts';
 import AlertItem from './AlertItem';
 import { isEmpty } from 'lodash';
 
 const UpcomingAlerts = ({ error, isFetching, alerts }) => {
-    const { fields, handleChange } = useUpcomingAlerts();
-
     return (
         <>
             <PageHeading title="Upcoming Alerts" withBackButton />
