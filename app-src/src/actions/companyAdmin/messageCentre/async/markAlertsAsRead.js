4c0d354d9e2de78bb84cf30c14143f4f4f3c6cb1
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-13 13:44:30 +0000

    Add messageCentre actions

---
+++ app-src/src/actions/companyAdmin/messageCentre/async/markAlertsAsRead.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+import { API_URL } from 'config';
+import { getHeaders } from 'helpers/api';
+import {
+    POST_ALERTS_REQUEST,
+    POST_ALERTS_SUCCESS,
+    POST_ALERTS_FAILURE,
+} from 'constants/actionTypes/messageCentre';
+
+export const markAlertsAsReadRequest = () => ({
+    type: POST_ALERTS_REQUEST,
+});
+
+export const markAlertsAsReadSuccess = payload => ({
+    type: POST_ALERTS_SUCCESS,
+    payload,
+});
+
+export const markAlertsAsReadFailure = error => ({
+    type: POST_ALERTS_FAILURE,
+    error,
+});
+
+export default postBody => dispatch => {
+    dispatch(markAlertsAsReadRequest());
+
+    return axios
+        .post(`${API_URL}/alertMessages/readall`, postBody, getHeaders())
+        .then(res => dispatch(markAlertsAsReadSuccess(res.data)))
+        .catch(err => dispatch(markAlertsAsReadFailure(err.message)));
+};
