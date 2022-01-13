0ce5205e441b154b28f2cb62d6c3252355890ee7
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-13 08:05:20 +0000

    Add dismiss alert functionality

--- app-src/src/components/companyAdmin/upcomingAlerts/presentational/AlertItem.js
+++ app-src/src/components/companyAdmin/upcomingAlerts/presentational/AlertItem.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Moment from 'react-moment';
@@ -12,9 +12,12 @@ import {
 } from 'constants/companyAdmin/enums';
 import { companyUser } from 'selectors/companyAdmin/companyUser';
 import fetchCompanyUsers from 'actions/companyAdmin/userManagement/async/fetchCompanyUsers';
+import { dismissAlert } from 'actions/companyAdmin/alerts/sync/deleteAlerts';
+import DismissAlertModal from './DismissAlertModal';
 
 const AlertItem = ({
     alert: {
+        id,
         name,
         createdOn,
         frequencyAmount,
@@ -27,6 +30,7 @@ const AlertItem = ({
         createdByCompanyUserID,
     },
 }) => {
+    const [showModal, setShowModal] = useState(false);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -35,36 +39,58 @@ const AlertItem = ({
 
     const user = useSelector(state => companyUser(state, createdByCompanyUserID));
 
+    const handelClick = () => {
+        setShowModal(true);
+    };
+
+    const handelDismissAlert = id => {
+        setShowModal(false);
+        dispatch(dismissAlert(id));
+    };
+
     return (
-        <tr>
-            <td className="left-align">
-                <Moment format={'DD/MM/YYYY'} date={date} />
-            </td>
-            <td className="left-align">
-                <DateTimeContainer date={createdOn} />
-            </td>
-            <td>{lastSendOn ? <DateTimeContainer date={lastSendOn} /> : 'NA'}</td>
-            <td>
-                <Link to={`/company/${HIERARCHY_TYPES[hierarchyType]}s/${hierarchyID}`}>
-                    {`/company/${HIERARCHY_TYPES[hierarchyType]}s/${hierarchyID}`}
-                </Link>
-            </td>
-            <td>{ALERT_METHOD_VALUES[method]}</td>
-            <td>
-                {user.userFirstName} {user.userLastName}/{user.companyName}
-            </td>
-            <td>
-                {frequencyType === 1
-                    ? 'Single'
-                    : frequencyAmount + ' per ' + ALERT_FREQUENCY_SUFFIX_VALUES[frequencyType]}
-            </td>
-            <td className="left-align">{name}</td>
-            <td>
-                <button className="no-background-btn" onClick={() => console.log('dismiss')}>
-                    <i className="fas fa-times-circle close-icon" />
-                </button>
-            </td>
-        </tr>
+        <>
+            <DismissAlertModal
+                id={id}
+                name={name}
+                date={date}
+                showModal={showModal}
+                setShowModal={setShowModal}
+                handelDismissAlert={handelDismissAlert}
+            />
+            <tr>
+                <td className="left-align">
+                    <Moment format={'DD/MM/YYYY'} date={date} />
+                </td>
+                <td className="left-align">
+                    <DateTimeContainer date={createdOn} />
+                </td>
+                <td>{lastSendOn ? <DateTimeContainer date={lastSendOn} /> : 'NA'}</td>
+                <td>
+                    <Link to={`/company/${HIERARCHY_TYPES[hierarchyType]}s/${hierarchyID}`}>
+                        {`/company/${HIERARCHY_TYPES[hierarchyType]}s/${hierarchyID}`}
+                    </Link>
+                </td>
+                <td>{ALERT_METHOD_VALUES[method]}</td>
+                <td>{user && `${user.userFirstName} ${user.userLastName}/${user.companyName}`}</td>
+                <td>
+                    {frequencyType === 1
+                        ? 'Single'
+                        : frequencyAmount + ' per ' + ALERT_FREQUENCY_SUFFIX_VALUES[frequencyType]}
+                </td>
+                <td className="left-align">{name}</td>
+                <td>
+                    <button
+                        className="no-background-btn"
+                        onClick={() => {
+                            handelClick();
+                        }}
+                    >
+                        <i className="fas fa-times-circle close-icon" />
+                    </button>
+                </td>
+            </tr>
+        </>
     );
 };
 
