f7477b0c1d179f9ee82b8e400b56e3f02a4c2e06
Seb Smith <seb@silverchip.com>
2022-01-14 11:32:49 +0000

    Adds createdByUserName to Company alerts

--- app-src/src/components/companyAdmin/messages/messageCentre/listItems/CompanyAlertsListItem.js
+++ app-src/src/components/companyAdmin/messages/messageCentre/listItems/CompanyAlertsListItem.js
@@ -5,14 +5,23 @@ import moment from 'moment';
 import { HIERARCHY_LINK_VALUES } from 'constants/companyAdmin/enums';
 
 const CompanyAlertsListItem = ({
-    message: { id, name, createdOn, description, hierarchyType, hierarchyID, hierarchyName },
+    message: {
+        id,
+        name,
+        createdOn,
+        description,
+        hierarchyType,
+        hierarchyID,
+        hierarchyName,
+        createdByUserName,
+    },
 }) => {
     const hierarchyLink = `/company/${HIERARCHY_LINK_VALUES[hierarchyType]}/${hierarchyID}`;
 
     return (
         <div key={id} className="message-wrapper">
             <div className="title-wrapper">
-                <h3 className="title">NAME SURNAME</h3>
+                <h3 className="title">{createdByUserName}</h3>
 
                 <div className="date-wrapper">
                     <span className="date">{moment(createdOn).format('DD/MM/YY - hh:mm')}</span>
