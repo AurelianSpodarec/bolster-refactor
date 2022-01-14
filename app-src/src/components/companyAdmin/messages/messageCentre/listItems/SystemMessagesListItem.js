0d55bb84e59e6240446a77306cd30dcc079c7e75
Seb Smith <seb@silverchip.com>
2022-01-14 10:42:54 +0000

    Style system messages

--- app-src/src/components/companyAdmin/messages/messageCentre/listItems/SystemMessagesListItem.js
+++ app-src/src/components/companyAdmin/messages/messageCentre/listItems/SystemMessagesListItem.js
@@ -2,18 +2,14 @@ import React from 'react';
 
 import moment from 'moment';
 
-const SystemMessagesListItem = ({
-    message: { id, createdByUserFirstName, createdByUserLastName, createdOn, message },
-}) => {
+const SystemMessagesListItem = ({ message: { id, createdOn, message } }) => {
     return (
         <div key={id} className="message-wrapper">
             <div className="title-wrapper">
-                <h3 className="title">{`${createdByUserFirstName} ${createdByUserLastName}`}</h3>
-
                 <div className="date-wrapper">
                     <span className="date">{moment(createdOn).format('DD/MM/YY - hh:mm')}</span>
-                    <i className="fas fa-times-circle close-icon" />
                 </div>
+                <i className="fas fa-times-circle close-icon" />
             </div>
 
             <div>
