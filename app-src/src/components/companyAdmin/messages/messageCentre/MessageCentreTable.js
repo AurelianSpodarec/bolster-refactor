e94e7af9554689c09e7594835ea39ed628a2001a
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-14 10:38:45 +0000

    Add handelDismiss to messageCentre

--- app-src/src/components/companyAdmin/messages/messageCentre/MessageCentreTable.js
+++ app-src/src/components/companyAdmin/messages/messageCentre/MessageCentreTable.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import MessageCentreTabs from './MessageCentreTabs';
 import useMessageCentreTable from './hooks/useMessageCentreTable';
 import MessagesList from './MessagesList';
 import SearchBar from 'components/companyAdmin/layout/header/presentational/SearchBar';
+import { MESSAGE_CENTRE_TABS } from 'constants/companyAdmin/enums';
+import dismissSystemMessages from 'actions/companyAdmin/messageCentre/async/dismissSystemMessages';
+import dismissCompanyAlert from 'actions/companyAdmin/messageCentre/async/dismissCompanyAlert';
 
 const MessageCentreTable = () => {
+    const dispatch = useDispatch();
     const { selectedTab, setSelectedTab, messages, isFetching, error } = useMessageCentreTable();
+
+    const handelDismiss = () => {
+        if (selectedTab === MESSAGE_CENTRE_TABS.SYSTEM_MESSAGES) {
+            console.log('dismiss messages');
+            // dispatch(dismissSystemMessages);
+        } else if (selectedTab === MESSAGE_CENTRE_TABS.COMPANY_ALERTS) {
+            console.log('dismiss company alert');
+            // dispatch(dismissCompanyAlert);
+        } else if (selectedTab === MESSAGE_CENTRE_TABS.OPERATIVE_ALERTS) {
+            console.log('dismiss operative alert');
+        } else {
+            console.log('dismiss drawing expiry');
+        }
+    };
 
     return (
         <BlockContainer>
@@ -18,10 +37,7 @@ const MessageCentreTable = () => {
                         <SearchBar omitIcon />
                     </div>
                     <div className="button-wrapper">
-                        <button
-                            className="button rounded red"
-                            onClick={() => console.log('Dismiss')}
-                        >
+                        <button className="button rounded red" onClick={() => handelDismiss()}>
                             <i className="fas fa-trash-alt"></i>
                             Dismiss All
                         </button>
