00e01bfe9790450e0ff8c6ba3a31b7e9b0dd1b74
Seb Smith <seb@silverchip.com>
2022-01-14 09:39:10 +0000

    Add correct date format

--- app-src/src/components/companyAdmin/messages/messageCentre/MessagesList.js
+++ app-src/src/components/companyAdmin/messages/messageCentre/MessagesList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import { isEmpty } from 'helpers/generic';
@@ -15,23 +16,26 @@ const MessagesList = ({ messages, isFetching, error, selectedTab }) => {
             noDataMessage={`No ${MESSAGE_CENTRE_NAMES[selectedTab]} To View`}
         >
             <div className="messages-container">
-                {messages.map(message => (
-                    <div key={message.id} className="message-wrapper">
-                        <div className="title-wrapper">
-                            <h3 className="title">{`${message.createdByUserFirstName} ${message.createdByUserLastName}`}</h3>
+                {messages.map(
+                    ({ id, message, createdOn, createdByUserFirstName, createdByUserLastName }) => (
+                        <div key={id} className="message-wrapper">
+                            <div className="title-wrapper">
+                                <h3 className="title">{`${createdByUserFirstName} ${createdByUserLastName}`}</h3>
 
-                            <div className="date-wrapper">
-                                <span className="date">{message.createdOn}</span>
-                                <i className="fas fa-times-circle close-icon" />
+                                <div className="date-wrapper">
+                                    <span className="date">
+                                        {moment(createdOn).format('DD/MM/YY - hh:mm')}
+                                    </span>
+                                    <i className="fas fa-times-circle close-icon" />
+                                </div>
+                            </div>
+
+                            <div>
+                                <p>{message}</p>
                             </div>
                         </div>
-
-                        <div
-                            dangerouslySetInnerHTML={{ __html: message.message }}
-                            className="wysiwyg"
-                        />
-                    </div>
-                ))}
+                    ),
+                )}
             </div>
         </BlockContainer>
     );
