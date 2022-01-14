76575030950fcfe9e048cc4878c0cd87aa8c96e8
Seb Smith <seb@silverchip.com>
2022-01-14 13:27:41 +0000

    Adds metrics back in

--- app-src/src/components/companyAdmin/app/routes/presentational/MessagesRoutes.js
+++ app-src/src/components/companyAdmin/app/routes/presentational/MessagesRoutes.js
@@ -3,10 +3,16 @@ import { Route } from 'react-router-dom';
 
 import SwitchWith404 from 'components/appRoute/routes/presentational/SwitchWith404';
 import MessageCentre from 'components/companyAdmin/messages/messageCentre/MessageCentre';
+import OperativeAlertMetricsContainer from 'components/companyAdmin/operativeAlerts/operativeAlertMetrics/containers/OperativeAlertMetricsContainer';
 
 const MessagesRoutes = ({ base = '/company/message-centre' }) => (
     <SwitchWith404>
         <Route exact path={`${base}`} component={MessageCentre} />
+        <Route
+            exact
+            path={`${base}/operative-alerts/:id/metrics`}
+            component={OperativeAlertMetricsContainer}
+        />
     </SwitchWith404>
 );
 
