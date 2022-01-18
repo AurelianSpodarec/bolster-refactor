18a7f23c553b30f511cd40566d20df1f205f1b6a
Seb Smith <seb@silverchip.com>
2022-01-18 09:13:04 +0000

    Converts if to switch for readability

--- app-src/src/components/shared/generic/modals/hooks/useHierarchyDrawingExpiry.js
+++ app-src/src/components/shared/generic/modals/hooks/useHierarchyDrawingExpiry.js
@@ -9,12 +9,24 @@ const useHierarchyDrawingExpiry = (id, hierarchyID) => {
     const allDrawings = Object.values(useSelector(selectAllDrawings));
 
     const hierarchyDrawings = allDrawings.reduce((acc, drawing) => {
-        if (hierarchyID === HIERARCHY_IDS.SITE && +id === drawing.siteID) {
-            acc.push(drawing);
-        } else if (hierarchyID === HIERARCHY_IDS.BUILDING && drawing.buildingID === +id) {
-            acc.push(drawing);
-        } else if (hierarchyID === HIERARCHY_IDS.FLOOR && drawing.floorID === +id) {
-            acc.push(drawing);
+        switch (hierarchyID) {
+            case HIERARCHY_IDS.SITE:
+                if (drawing.siteID === +id) {
+                    acc.push(drawing);
+                }
+                break;
+            case HIERARCHY_IDS.BUILDING:
+                if (drawing.buildingID === +id) {
+                    acc.push(drawing);
+                }
+                break;
+            case HIERARCHY_IDS.FLOOR:
+                if (drawing.floorID === +id) {
+                    acc.push(drawing);
+                }
+                break;
+            default:
+                break;
         }
 
         return acc;
