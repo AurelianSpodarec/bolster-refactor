f0e2b5e7c41efdad9dd01d42f0238f0342316ecd
Seb Smith <seb@silverchip.com>
2022-01-18 09:27:25 +0000

    Rename selector

--- app-src/src/selectors/companyAdmin/drawings.js
+++ app-src/src/selectors/companyAdmin/drawings.js
@@ -1,3 +1,3 @@
 export const selectDrawingsIsFetching = state => state.companyAdmin.drawingsReducer.isFetching;
-export const selectDrawingsError = state => state.companyAdmin.drawingsReducer.error;
+export const selectDrawingsFetchError = state => state.companyAdmin.drawingsReducer.error;
 export const selectDrawings = state => state.companyAdmin.drawingsReducer.drawings;
