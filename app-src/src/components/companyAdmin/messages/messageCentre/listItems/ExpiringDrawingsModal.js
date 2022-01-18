241f1e86da74fc7dd4dbb2afe7b9b31103972bb1
Seb Smith <seb@silverchip.com>
2022-01-18 13:59:22 +0000

    Sorts drawing expiries

--- app-src/src/components/companyAdmin/messages/messageCentre/listItems/ExpiringDrawingsModal.js
+++ app-src/src/components/companyAdmin/messages/messageCentre/listItems/ExpiringDrawingsModal.js
@@ -22,22 +22,25 @@ const ExpiringDrawingsModal = ({ drawings }) => {
         history.push(`/company/drawings/${drawingID}`);
         closeModal();
     };
-
     return (
         <ModalOuterContainer>
             <BlockHeading title="Drawing Expiry" />
 
-            {drawings.map(({ id, siteName, buildingName, floorName, name, expiresOn }) => (
-                <div key={id} className="mc-drawing-expiry">
-                    <button onClick={e => handleDrawingClick(e, id)}>
-                        {`${siteName} / ${buildingName} / ${floorName} / ${name} - `}
-                        <span className={moment(expiresOn).isAfter(Date.now()) ? 'green' : 'red'}>
-                            <strong>Expiry Date </strong>
-                            {moment(expiresOn).format('DD/MM/YYYY')}
-                        </span>
-                    </button>
-                </div>
-            ))}
+            {drawings
+                .sort((a, b) => (a.sort > b.sort ? b.sort : b.sort))
+                .map(({ id, siteName, buildingName, floorName, name, expiresOn }) => (
+                    <div key={id} className="mc-drawing-expiry">
+                        <button onClick={e => handleDrawingClick(e, id)}>
+                            {`${siteName} / ${buildingName} / ${floorName} / ${name} - `}
+                            <span
+                                className={moment(expiresOn).isAfter(Date.now()) ? 'green' : 'red'}
+                            >
+                                <strong>Expiry Date </strong>
+                                {moment(expiresOn).format('DD/MM/YYYY')}
+                            </span>
+                        </button>
+                    </div>
+                ))}
         </ModalOuterContainer>
     );
 };
