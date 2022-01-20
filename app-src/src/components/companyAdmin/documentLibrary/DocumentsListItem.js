19d4b67f7baa18f88a71e8323c532a3d6d44f819
Rob Fairclough <rob@silverchip.com>
2022-01-20 10:23:36 +0000

    clean documentslistitem

--- app-src/src/components/companyAdmin/documentLibrary/DocumentsListItem.js
+++ app-src/src/components/companyAdmin/documentLibrary/DocumentsListItem.js
@@ -23,119 +23,95 @@ let DocumentsListItem = ({
     const prefix = useQueryParam('prefix') || '';
     let rowClass = 'draggable expandable dl-row';
     if (isDragging) rowClass += ' dragging';
-
+    const isFile = item.type === DOCUMENT_LIBRARY_TYPES.FILE;
+    const isFolder = item.type === DOCUMENT_LIBRARY_TYPES.FOLDER;
+    const isViewApp = (isFile && item.isViewApp) || (isFolder && item.areChildrenViewApp);
+    const isAttachPins = (isFile && item.isAttachPins) || (isFolder && item.areChildrenAttachPins);
     const users = useSelector(mapStateToProps) || {};
 
     return (
         <tr ref={isSorting ? forwardRef : null} className={rowClass}>
-            <>
-                <td className="dl-selection-container">
-                    <div className={'dl-selection'} onClick={() => toggleItemSelect(item.id)}>
-                        <div className="selection-dot" style={{ opacity: isSelected ? 1 : 0 }} />
-                    </div>
-                </td>
-                <td className="hover-anim file-image">
-                    {onMobile && <span className="mobile-table-heading">{headers[1]}</span>}
-                    {item.type === DOCUMENT_LIBRARY_TYPES.FOLDER ? (
-                        !item.isArchived ? (
-                            <Link to={`/company/document-library?prefix=${prefix + item.name}`}>
-                                <FileTypeIcon
-                                    src={
-                                        item.type === DOCUMENT_LIBRARY_TYPES.FILE
-                                            ? getIconFromExt(item.fileExtension)
-                                            : FolderIcon
-                                    }
-                                />
-                            </Link>
-                        ) : (
-                            <p>
-                                <FileTypeIcon
-                                    src={
-                                        item.type === DOCUMENT_LIBRARY_TYPES.FILE
-                                            ? getIconFromExt(item.fileExtension)
-                                            : FolderIcon
-                                    }
-                                />
-                            </p>
-                        )
-                    ) : (
-                        <a
-                            href={`${RAW_S3_STORAGE_URL}/${item.s3Key}`}
-                            title={item.name}
-                            target={item.type === DOCUMENT_LIBRARY_TYPES.FILE && '_blank'}
-                        >
+            <td className="dl-selection-container">
+                <div className={'dl-selection'} onClick={() => toggleItemSelect(item.id)}>
+                    <div className="selection-dot" style={{ opacity: isSelected ? 1 : 0 }} />
+                </div>
+            </td>
+            <td className="hover-anim file-image">
+                {onMobile && <span className="mobile-table-heading">{headers[1]}</span>}
+                {isFile && (
+                    <a
+                        href={`${RAW_S3_STORAGE_URL}/${item.s3Key}`}
+                        title={item.name}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <FileTypeIcon
+                            src={isFile ? getIconFromExt(item.fileExtension) : FolderIcon}
+                        />
+                    </a>
+                )}
+                {isFolder &&
+                    (item.isArchived ? (
+                        <p>
                             <FileTypeIcon
-                                src={
-                                    item.type === DOCUMENT_LIBRARY_TYPES.FILE
-                                        ? getIconFromExt(item.fileExtension)
-                                        : FolderIcon
-                                }
+                                src={isFile ? getIconFromExt(item.fileExtension) : FolderIcon}
                             />
-                        </a>
-                    )}
-                </td>
-                <td className="hover-anim file-name">
-                    {onMobile && <span className="mobile-table-heading">{headers[0]}</span>}
-                    {item.type === DOCUMENT_LIBRARY_TYPES.FOLDER ? (
-                        !item.isArchived ? (
-                            <Link to={`/company/document-library?prefix=${prefix + item.name}`}>
-                                <p>{item.name}</p>
-                            </Link>
-                        ) : (
-                            <p>{item.name}</p>
-                        )
+                        </p>
                     ) : (
-                        <a
-                            href={`${RAW_S3_STORAGE_URL}/${item.s3Key}`}
-                            title={item.name}
-                            target={item.type === DOCUMENT_LIBRARY_TYPES.FILE && '_blank'}
-                        >
+                        <Link to={`/company/document-library?prefix=${prefix + item.name}`}>
+                            <FileTypeIcon
+                                src={isFile ? getIconFromExt(item.fileExtension) : FolderIcon}
+                            />
+                        </Link>
+                    ))}
+            </td>
+            <td className="hover-anim file-name">
+                {onMobile && <span className="mobile-table-heading">{headers[0]}</span>}
+                {isFolder &&
+                    (item.isArchived ? (
+                        <p>{item.name}</p>
+                    ) : (
+                        <Link to={`/company/document-library?prefix=${prefix + item.name}`}>
                             <p>{item.name}</p>
-                        </a>
-                    )}
-                </td>
-                <td>
-                    {onMobile && <span className="mobile-table-heading">{headers[1]}</span>}
-                    {`${
-                        users[item.createdByCompanyUserID]
-                            ? `${users[item.createdByCompanyUserID].userFirstName} ${
-                                  users[item.createdByCompanyUserID].userLastName
-                              }`
-                            : '-'
-                    }`}
-                </td>
-                <td>
-                    {onMobile && <span className="mobile-table-heading">{headers[2]}</span>}
-                    <DateTimeContainer
-                        date={new Date(item.createdOn)}
-                        datetime={DATE_TIME_IDS.DATE}
-                    />
-                </td>
-                <td>
-                    {onMobile && <span className="mobile-table-heading">{headers[3]}</span>}
-                    {item.type == DOCUMENT_LIBRARY_TYPES.FILE
-                        ? item.isViewApp
-                            ? 'Yes'
-                            : 'No'
-                        : item.areChildrenViewApp
-                        ? 'Yes'
-                        : 'No'}
-                </td>
-                <td>
-                    {onMobile && <span className="mobile-table-heading">{headers[4]}</span>}
-                    {item.type == DOCUMENT_LIBRARY_TYPES.FILE
-                        ? item.isAttachPins
-                            ? 'Yes'
-                            : 'No'
-                        : item.areChildrenAttachPins
-                        ? 'Yes'
-                        : 'No'}
-                </td>
-                <td>
-                    {onMobile && <span className="mobile-table-heading">{headers[5]}</span>}
-                    {item.contentLength ? formatBytes(item.contentLength) : ' '}
-                </td>
-            </>
+                        </Link>
+                    ))}
+                {isFile && (
+                    <a
+                        href={`${RAW_S3_STORAGE_URL}/${item.s3Key}`}
+                        title={item.name}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <p>{item.name}</p>
+                    </a>
+                )}
+            </td>
+            <td>
+                {onMobile && <span className="mobile-table-heading">{headers[1]}</span>}
+                {`${
+                    users[item.createdByCompanyUserID]
+                        ? `${users[item.createdByCompanyUserID].userFirstName} ${
+                              users[item.createdByCompanyUserID].userLastName
+                          }`
+                        : '-'
+                }`}
+            </td>
+            <td>
+                {onMobile && <span className="mobile-table-heading">{headers[2]}</span>}
+                <DateTimeContainer date={new Date(item.createdOn)} datetime={DATE_TIME_IDS.DATE} />
+            </td>
+            <td>
+                {onMobile && <span className="mobile-table-heading">{headers[3]}</span>}
+                {isViewApp ? 'Yes' : 'No'}
+            </td>
+            <td>
+                {onMobile && <span className="mobile-table-heading">{headers[4]}</span>}
+                {isAttachPins ? 'Yes' : 'No'}
+            </td>
+            <td>
+                {onMobile && <span className="mobile-table-heading">{headers[5]}</span>}
+                {item.contentLength ? formatBytes(item.contentLength) : ' '}
+            </td>
         </tr>
     );
 };
