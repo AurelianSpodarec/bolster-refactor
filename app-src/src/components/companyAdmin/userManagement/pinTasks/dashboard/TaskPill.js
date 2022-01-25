4a86467e4a4c0fe587081d7ab0b8a23f990b0b91
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-25 10:27:46 +0000

    Make tasks filters clickable

--- app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/TaskPill.js
+++ app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/TaskPill.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const TaskPill = ({ name, title }) => {
     return (
-        <div className="task-pill" key={name}>
-            <div className={`square ${name}`} />
-            <div className="title">{title}</div>
-        </div>
+        <button
+            className="no-background-btn"
+            onClick={() => {
+                console.log('clicked');
+            }}
+        >
+            <div className="task-pill" key={name}>
+                <div className={`square ${name}`} />
+                <div className="title">{title}</div>
+            </div>
+        </button>
     );
 };
 
