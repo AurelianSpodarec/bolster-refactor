ae33688a3c5b9094e947e8ef32fc362a86533066
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-27 13:59:43 +0000

    Add redux for serviceFilters

--- app-src/src/selectors/companyAdmin/services.js
+++ app-src/src/selectors/companyAdmin/services.js
@@ -2,3 +2,4 @@ export const selectServicesIsFetching = state => state.companyAdmin.servicesRedu
 export const selectServicesFetchError = state => state.companyAdmin.servicesReducer.error;
 export const selectServices = state => state.companyAdmin.servicesReducer.services;
 export const selectService = (state, id) => state.companyAdmin.servicesReducer.services[id];
+export const selectServiceFilters = state => state.companyAdmin.servicesReducer.serviceFilters;
