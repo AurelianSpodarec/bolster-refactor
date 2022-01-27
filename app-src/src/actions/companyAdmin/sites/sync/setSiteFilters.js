2b9ffadbdab681bb8662f6baf3657408dabe171a
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-27 14:31:40 +0000

    Add redux for siteFilters

---
+++ app-src/src/actions/companyAdmin/sites/sync/setSiteFilters.js
@@ -0,0 +1,7 @@
+import { SET_SITE_FILTERS } from 'constants/actionTypes/sites ';
+
+export default siteFilterType => dispatch =>
+    dispatch({
+        type: SET_SITE_FILTERS,
+        siteFilterType,
+    });
--- app-src/src/selectors/companyAdmin/sites.js
+++ app-src/src/selectors/companyAdmin/sites.js
@@ -2,3 +2,4 @@ export const selectSitesIsFetching = state => state.companyAdmin.sitesReducer.is
 export const selectSitesFetchError = state => state.companyAdmin.sitesReducer.error;
 export const selectSites = state => state.companyAdmin.sitesReducer.sites;
 export const selectSite = (state, id) => state.companyAdmin.sitesReducer.sites[id];
+export const selectSiteFilters = state => state.companyAdmin.sitesReducer.siteFilters;
