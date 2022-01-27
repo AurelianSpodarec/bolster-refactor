fe8f28fc9898aee849ddca49711ef54c78f59bec
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-27 13:49:29 +0000

    Fix spelling

--- app-src/src/selectors/companyAdmin/pinTasks.js
+++ app-src/src/selectors/companyAdmin/pinTasks.js
@@ -15,4 +15,4 @@ export const selectPinTaskSeries = (state, id) =>
 export const selectPinRecurrenceFilters = state =>
     state.companyAdmin.pinTasksReducer.pinRecurrenceFilters;
 
-export const selectPinRStatusFilters = state => state.companyAdmin.pinTasksReducer.pinStatusFilters;
+export const selectPinStatusFilters = state => state.companyAdmin.pinTasksReducer.pinStatusFilters;
