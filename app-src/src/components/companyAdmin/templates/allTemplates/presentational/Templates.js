7ea8428395d48c97cba3bf774cd1a1035ddf7f71
Viktoriia Petrova <viktoriia@silverchip.com>
2022-01-28 11:26:43 +0000

    Add description for templates and link to download docs

--- app-src/src/components/companyAdmin/templates/allTemplates/presentational/Templates.js
+++ app-src/src/components/companyAdmin/templates/allTemplates/presentational/Templates.js
@@ -1,19 +1,37 @@
 import React from 'react';
+
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import TemplatesTableContainer from '../containers/TemplatesTableContainer';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import PinOptionsTableContainer from '../containers/PinOptionsTableContainer';
 
-const Templates = () => (
-    <>
-        <PageHeading title="My Templates" withBackButton />
-        <BlockContainer>
-            <BlockHeading title="Templates" />
-            <TemplatesTableContainer />
-        </BlockContainer>
-        <PinOptionsTableContainer />
-    </>
-);
+const Templates = () => {
+    return (
+        <>
+            <PageHeading title="My Templates" withBackButton />
+            <BlockContainer>
+                <BlockHeading title="Templates" />
+                <p className="size-lg-12 page-heading">
+                    Below are the current templates your company has set up and access to. If you
+                    would like to make any amendments to an existing template or create a bespoke
+                    template please download our template creation documents{' '}
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={`/Template-guide-and-template-creation.zip`}
+                        className="links"
+                        download={true}
+                    >
+                        here.
+                    </a>
+                </p>
+
+                <TemplatesTableContainer />
+            </BlockContainer>
+            <PinOptionsTableContainer />
+        </>
+    );
+};
 
 export default Templates;
