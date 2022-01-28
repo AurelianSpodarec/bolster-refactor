0ad7d436e79b746a9320d2d3d2665764bd53c4c1
Rob Fairclough <rob@silverchip.com>
2022-01-28 13:59:24 +0000

    show utc start/end dates in picker

--- app-src/src/components/companyAdmin/dashboard/presentational/DashboardStatsFilters.js
+++ app-src/src/components/companyAdmin/dashboard/presentational/DashboardStatsFilters.js
@@ -16,6 +16,8 @@ const DashboardStatsFilters = ({
     selectedMonth,
     handleChange,
 }) => {
+    const start = new Date(selectedStartDate).toUTCString().replace('GMT', '');
+    const end = new Date(selectedEndDate).toUTCString().replace('GMT', '');
     return (
         <form className="multi-filters size-lg-12">
             <div className="flex item ">
@@ -47,7 +49,7 @@ const DashboardStatsFilters = ({
                 <div className="flex">
                     <DateRangePicker
                         onChange={date => handleDateChange('dates', date)}
-                        value={[selectedStartDate, selectedEndDate]}
+                        value={[start, end]}
                         showDoubleView
                         calendarIcon={null}
                         clearIcon={null}
