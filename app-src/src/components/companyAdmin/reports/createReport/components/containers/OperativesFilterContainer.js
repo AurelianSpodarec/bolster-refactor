86516d5cf22d3c2469b484607072eabc3d540ec9
Seb Smith <seb@silverchip.com>
2022-01-31 09:24:53 +0000

    Shows modal for when operatives are not attached to a site

--- app-src/src/components/companyAdmin/reports/createReport/components/containers/OperativesFilterContainer.js
+++ app-src/src/components/companyAdmin/reports/createReport/components/containers/OperativesFilterContainer.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import { withRouter } from 'react-router-dom';
 import withUpdateOnChange from '../hocs/withUpdateOnChange';
 
+import showModal from 'actions/shared/generic/modals/sync/showModal';
+import hideModal from 'actions/shared/generic/modals/sync/hideModal';
+
 import OperativesFilter from '../presentational/OperativesFilter';
+import { SUCCESS_MODAL } from 'constants/shared/modalTypes';
 
 class OperativesFilterContainer extends Component {
     state = { hasSetOp: false };
@@ -48,10 +54,12 @@ class OperativesFilterContainer extends Component {
             customFilters: { operatives },
             filters: { companyUserIDs },
             location: { state: locationState },
+            showModal,
         } = this.props;
         if (operatives.length !== prevOps.length) {
             // remove operative if they're no longer available after filter update
             const opIDs = companyUserIDs.filter(opID => operatives.some(op => opID === op.id));
+
             if (
                 !this.state.hasSetOp &&
                 locationState?.operativeID &&
@@ -60,7 +68,15 @@ class OperativesFilterContainer extends Component {
                 if (!opIDs.some(opID => opID === locationState.operativeID)) {
                     opIDs.push(locationState.operativeID);
                 }
+
                 this.setState({ hasSetOp: true });
+            }
+
+            if (locationState?.operativeID && !opIDs.includes(locationState?.operativeID)) {
+                showModal(SUCCESS_MODAL, {
+                    title: 'Invalid operative',
+                    message: 'The operative you selected has not placed a pin on any sites.',
+                });
             }
 
             handleChange('companyUserIDs', opIDs);
@@ -73,4 +89,11 @@ class OperativesFilterContainer extends Component {
     };
 }
 
-export default withRouter(withUpdateOnChange(OperativesFilterContainer));
+const mapDispatchToProps = dispatch => ({
+    showModal: (type, modalProps) => dispatch(showModal(type, modalProps)),
+    hideModal: () => dispatch(hideModal()),
+});
+
+export default withRouter(
+    connect(null, mapDispatchToProps)(withUpdateOnChange(OperativesFilterContainer)),
+);
