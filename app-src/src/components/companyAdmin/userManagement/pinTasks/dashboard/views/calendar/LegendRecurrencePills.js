60faaeb3005ed7a3dc779bcdf14944e1bde75468
Seb Smith <seb@silverchip.com>
2022-02-01 08:19:32 +0000

    Adds titles to Task views

--- app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/calendar/LegendRecurrencePills.js
+++ app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/calendar/LegendRecurrencePills.js
@@ -3,10 +3,13 @@ import TaskRecurrencePill from '../../TaskRecurrencePill';
 
 const LegendRecurrencePills = ({ stats }) => {
     return (
-        <div className="pills">
-            {stats.map((data, i) => (
-                <TaskRecurrencePill key={i} {...data} />
-            ))}
+        <div className="pills-wrapper">
+            <span>Task type:</span>
+            <div className="pills">
+                {stats.map((data, i) => (
+                    <TaskRecurrencePill key={i} {...data} />
+                ))}
+            </div>
         </div>
     );
 };
--- app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/calendar/LegendStatusPills.js
+++ app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/calendar/LegendStatusPills.js
@@ -3,10 +3,13 @@ import TaskStatusPill from '../../TaskStatusPill';
 
 const LegendStatusPills = ({ stats }) => {
     return (
-        <div className="pills">
-            {stats.map((data, i) => (
-                <TaskStatusPill key={i} {...data} />
-            ))}
+        <div className="pills-wrapper">
+            <span>Task status:</span>
+            <div className="pills">
+                {stats.map((data, i) => (
+                    <TaskStatusPill key={i} {...data} />
+                ))}
+            </div>
         </div>
     );
 };
