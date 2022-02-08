0ed051e8b9a46e41edbda93fd5903f958c585f77
Seb Smith <seb@silverchip.com>
2022-02-08 08:29:43 +0000

    Extracts all pin task filtering logic into shared hook

--- app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/calendar/Calendar.js
+++ app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/calendar/Calendar.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+
 import moment from 'moment';
 
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
 import Table from 'components/shared/generic/tables/presentational/Table';
-import { PIN_TASK_RECURRING, PIN_TASK_STATUS } from 'constants/companyAdmin/enums';
 import { isEmpty } from 'lodash';
-import {
-    selectPinRecurrenceFilters,
-    selectPinStatusFilters,
-} from 'selectors/companyAdmin/pinTasks';
-import CalendarPinTask from './CalendarPinTask';
-import { selectUserFilters } from 'selectors/companyAdmin/companyUsers';
-import { selectServiceFilters } from 'selectors/companyAdmin/services';
-import { selectSiteFilters } from 'selectors/companyAdmin/sites';
 
-const { RECURRING, NON_RECURRING } = PIN_TASK_RECURRING;
-const { COMPLETE_LATE, COMPLETE, INCOMPLETE, DUE_SOON } = PIN_TASK_STATUS;
+import CalendarPinTask from './CalendarPinTask';
 
 const Calendar = ({ startDate, startCreatePinTask, viewTaskNote, days, matrix, isFetching }) => {
-    const selectedRecurrenceFilter = useSelector(selectPinRecurrenceFilters);
-    const selectedStatusFilter = useSelector(selectPinStatusFilters);
-    const selectUserFilter = useSelector(selectUserFilters);
-    const selectServiceFilter = useSelector(selectServiceFilters);
-    const selectSiteFilter = useSelector(selectSiteFilters);
-
     return (
         <BlockContainer contentClass="calendar" isFetching={isFetching} isEmpty={isFetching}>
             <Table headers={days}>
@@ -41,73 +25,13 @@ const Calendar = ({ startDate, startCreatePinTask, viewTaskNote, days, matrix, i
                                         <time>{moment(date).format('DD')}</time>
                                         {!pinTasks || !isEmpty(pinTasks) ? (
                                             <div className="tasks">
-                                                {pinTasks
-                                                    .filter(task => {
-                                                        const recurringName = task.isRecurring
-                                                            ? RECURRING
-                                                            : NON_RECURRING;
-
-                                                        const statusName = task.actionedOn
-                                                            ? moment(task.actionedOn).isAfter(
-                                                                  task.dueOn,
-                                                              )
-                                                                ? COMPLETE_LATE
-                                                                : COMPLETE
-                                                            : moment(task.dueOn).isBefore()
-                                                            ? INCOMPLETE
-                                                            : DUE_SOON;
-
-                                                        if (
-                                                            selectedRecurrenceFilter &&
-                                                            selectedRecurrenceFilter !==
-                                                                recurringName
-                                                        ) {
-                                                            return false;
-                                                        }
-
-                                                        if (
-                                                            selectedStatusFilter.length &&
-                                                            !selectedStatusFilter.includes(
-                                                                statusName,
-                                                            )
-                                                        ) {
-                                                            return false;
-                                                        }
-
-                                                        if (
-                                                            selectUserFilter.length &&
-                                                            !selectUserFilter.includes(
-                                                                task.companyUserID,
-                                                            )
-                                                        ) {
-                                                            return false;
-                                                        }
-
-                                                        if (
-                                                            selectServiceFilter.length &&
-                                                            !selectServiceFilter.includes(
-                                                                task.serviceID,
-                                                            )
-                                                        ) {
-                                                            return false;
-                                                        }
-
-                                                        if (
-                                                            selectSiteFilter.length &&
-                                                            !selectSiteFilter.includes(task.siteID)
-                                                        ) {
-                                                            return false;
-                                                        }
-
-                                                        return true;
-                                                    })
-                                                    .map((pinTask, i) => (
-                                                        <CalendarPinTask
-                                                            {...pinTask}
-                                                            key={i}
-                                                            viewTaskNote={viewTaskNote}
-                                                        />
-                                                    ))}
+                                                {pinTasks.map((pinTask, i) => (
+                                                    <CalendarPinTask
+                                                        {...pinTask}
+                                                        key={i}
+                                                        viewTaskNote={viewTaskNote}
+                                                    />
+                                                ))}
                                             </div>
                                         ) : (
                                             <p className="no-tasks">{!disabled && 'No Tasks'}</p>
--- app-src/src/components/companyAdmin/userManagement/pinTasks/hooks/useFilteredPinTasks.js
+++ app-src/src/components/companyAdmin/userManagement/pinTasks/hooks/useFilteredPinTasks.js
@@ -1,14 +1,45 @@
+import moment from 'moment';
+
 import { useSelector } from 'react-redux';
 import { selectUserFilters } from 'selectors/companyAdmin/companyUsers';
+import {
+    selectPinRecurrenceFilters,
+    selectPinStatusFilters,
+} from 'selectors/companyAdmin/pinTasks';
 import { selectServiceFilters } from 'selectors/companyAdmin/services';
 import { selectSiteFilters } from 'selectors/companyAdmin/sites';
 
+import { PIN_TASK_RECURRING, PIN_TASK_STATUS } from 'constants/companyAdmin/enums';
+
+const { RECURRING, NON_RECURRING } = PIN_TASK_RECURRING;
+const { COMPLETE_LATE, COMPLETE, INCOMPLETE, DUE_SOON } = PIN_TASK_STATUS;
+
 const useFilteredPinTasks = tasks => {
+    const selectedRecurrenceFilter = useSelector(selectPinRecurrenceFilters);
+    const selectedStatusFilter = useSelector(selectPinStatusFilters);
     const selectUserFilter = useSelector(selectUserFilters);
     const selectServiceFilter = useSelector(selectServiceFilters);
     const selectSiteFilter = useSelector(selectSiteFilters);
 
     const pinTasks = Object.values(tasks).filter(task => {
+        const recurringName = task.isRecurring ? RECURRING : NON_RECURRING;
+
+        const statusName = task.actionedOn
+            ? moment(task.actionedOn).isAfter(task.dueOn)
+                ? COMPLETE_LATE
+                : COMPLETE
+            : moment(task.dueOn).isBefore()
+            ? INCOMPLETE
+            : DUE_SOON;
+
+        if (selectedRecurrenceFilter && selectedRecurrenceFilter !== recurringName) {
+            return false;
+        }
+
+        if (selectedStatusFilter.length && !selectedStatusFilter.includes(statusName)) {
+            return false;
+        }
+
         if (selectSiteFilter.length) {
             return selectSiteFilter.includes(task.siteID);
         } else if (selectUserFilter.length) {
