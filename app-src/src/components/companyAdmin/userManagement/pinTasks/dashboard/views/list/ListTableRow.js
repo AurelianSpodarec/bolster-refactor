af724be0387dd949adf5feae90c848720f939b06
Seb Smith <seb@silverchip.com>
2022-02-09 13:10:32 +0000

    Tweaks notes icon

--- app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/list/ListTableRow.js
+++ app-src/src/components/companyAdmin/userManagement/pinTasks/dashboard/views/list/ListTableRow.js
@@ -11,7 +11,7 @@ import {
 import TaskStatusPill from '../../TaskStatusPill';
 import TaskRecurrencePill from '../../TaskRecurrencePill';
 
-import PinTaskNoteIcon from '../../../../../../../_content/images/icons/pinTaskNote.png';
+import PinTaskNoteIcon from '../../../../../../../_content/images/icons/pinTaskNoteWhite.png';
 import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 
 const ListTableRow = ({ pinTask, startEditPinTask, viewTaskNote }) => {
@@ -68,7 +68,7 @@ const ListTableRow = ({ pinTask, startEditPinTask, viewTaskNote }) => {
                     </button>
                     {pinTask.note && (
                         <button
-                            className="button green"
+                            className="button green image-icon"
                             type="button"
                             onClick={() => viewTaskNote(id)}
                         >
