76ac018fa0b928a07305a5ce6bb39432b74c5993
Viktoriia Petrova <viktoriia@silverchip.com>
2022-02-09 18:47:39 +0000

    Add drawingFilters

--- app-src/src/components/companyAdmin/drawings/shared/containers/DrawingTableContainer.js
+++ app-src/src/components/companyAdmin/drawings/shared/containers/DrawingTableContainer.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { connect } from 'react-redux';
 
 import DrawingTable from '../presentational/DrawingTable';
 import { hierarchySort } from 'helpers/generic';
+import { ACCESS_TYPES_VALUES } from 'constants/companyAdmin/enums';
+import { selectDrawingsFilterStatus } from 'selectors/shared/drawings';
 
 const DrawingTableContainer = ({
     isFetching,
@@ -10,16 +13,45 @@ const DrawingTableContainer = ({
     drawings,
     colSpanFirst = false,
     isSorting,
-}) => (
-    <DrawingTable
-        headers={['Drawing name', 'Created on', 'Last updated', 'Expires', 'Permissions', 'Action']}
-        isFetching={isFetching}
-        error={error}
-        drawings={drawings}
-        colSpanFirst={colSpanFirst}
-        isSorting={isSorting}
-    />
-);
+}) => {
+    const filters = useSelector(selectDrawingsFilterStatus);
+
+    const getFilteredDrawings = () => {
+        const { status } = filters;
+
+        if (status === 'active') {
+            return drawings.filter(site => !site.isArchived);
+        }
+
+        if (status === 'read only') {
+            return drawings.filter(site => site.accessType === ACCESS_TYPES_VALUES.READONLY);
+        }
+
+        if (status === 'archived') {
+            return drawings.filter(site => site.isArchived);
+        }
+
+        return drawings;
+    };
+
+    return (
+        <DrawingTable
+            headers={[
+                'Drawing name',
+                'Created on',
+                'Last updated',
+                'Expires',
+                'Permissions',
+                'Action',
+            ]}
+            isFetching={isFetching}
+            error={error}
+            drawings={getFilteredDrawings(drawings)}
+            colSpanFirst={colSpanFirst}
+            isSorting={isSorting}
+        />
+    );
+};
 
 export default connect(({ companyAdmin: { drawingsReducer, hierarchyReducer } }, ownProps) => ({
     isFetching: drawingsReducer.isFetching,
