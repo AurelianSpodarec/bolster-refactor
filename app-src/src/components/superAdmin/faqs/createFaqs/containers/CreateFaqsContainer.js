d5754baefc6a73d8e47fa27ec47654b56fcd0fdd
Seb Smith <seb@silverchip.com>
2022-02-22 13:04:16 +0000

    Fix WYSIWYG input

--- app-src/src/components/superAdmin/faqs/createFaqs/containers/CreateFaqsContainer.js
+++ app-src/src/components/superAdmin/faqs/createFaqs/containers/CreateFaqsContainer.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useAddItem } from 'helpers/hooks';
+import { useAddItem, useForm } from 'helpers/hooks';
 
 import { getKeyByValue } from 'helpers/generic';
 import { FAQS_PAGES } from 'constants/superAdmin/faqs';
@@ -13,13 +13,17 @@ import CreateFaqs from '../presentational/CreateFaqs';
 const CreateFaqsContainer = () => {
     const dispatch = useDispatch();
 
-    const [form, setFormChange] = useState({
-        type: { text: FAQS_PAGES[1], value: FAQS_PAGES[1] },
-        title: 'FAQ Name',
-        content: '',
-        imageS3Key: '',
-        videoLink: '',
-    });
+    const initialForm = useMemo(() => {
+        return {
+            type: { text: FAQS_PAGES[1], value: FAQS_PAGES[1] },
+            title: 'FAQ Name',
+            content: '',
+            imageS3Key: '',
+            videoLink: '',
+        };
+    }, []);
+
+    const [form, handleChange] = useForm(initialForm);
 
     const isPosting = useSelector(selectFaqsIsPosting);
     const postSuccess = useSelector(selectFaqsPostSuccess);
@@ -28,15 +32,9 @@ const CreateFaqsContainer = () => {
 
     const handleFormChange = (name, value) => {
         if (name === 'type') {
-            setFormChange({
-                ...form,
-                [name]: { text: value, value: value },
-            });
+            handleChange(name, { text: value, value: value });
         } else {
-            setFormChange({
-                ...form,
-                [name]: value,
-            });
+            handleChange(name, value);
         }
     };
 
