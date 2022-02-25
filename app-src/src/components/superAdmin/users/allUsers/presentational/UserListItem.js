16a237cf58f57ad9c57f08503bece88943632057
Nick Barnard <nick@silverchip.com>
2022-02-25 15:50:11 +0000

    Fix additional crash

--- app-src/src/components/superAdmin/users/allUsers/presentational/UserListItem.js
+++ app-src/src/components/superAdmin/users/allUsers/presentational/UserListItem.js
@@ -13,63 +13,69 @@ const UserListItem = ({
     handleShowEditUserPasswordModal,
     handleShowConfirmEmailModal,
     handleShowRemoveLockoutModal,
-}) => (
-    <tr>
-        <td>{`${user.firstName} ${user.lastName}`}</td>
-        <td>{user.email}</td>
-        <td>{user.phoneNumber}</td>
-        <Roles user={user} roles={user.roles} />
-        <td>{user.appVersion ? user.appVersion : '-'}</td>
-        <td>
-            {user.clientAccessCreatedByCompanies.length ? (
-                user.clientAccessCreatedByCompanies.map((company, i, arr) => {
-                    const isTheLastCompany = arr.length - 1 === i;
-                    return (
-                        <>
-                            <Link
-                                key={company.companyID}
-                                className="link"
-                                to={`/admin/companies/${company.companyID}`}
-                            >
-                                {company.name}
-                            </Link>
-                            {!isTheLastCompany && <span>, </span>}
-                        </>
-                    );
-                })
-            ) : (
-                <span>N/A</span>
-            )}
-        </td>
-        <td>
-            <DateTimeContainer date={user.createdOn} datetime={DATE_TIME_IDS.DATE} />
-        </td>
-        <td>{boolToYesNo(user.isEmailConfirmed)}</td>
-        <td>{boolToYesNo(user.isDeleted)}</td>
-        <td>
-            <BlockButtonWrapper>
-                <button className="button yellow" onClick={() => handleShowEditUserModal(user)}>
-                    <i className="far fa-pencil" /> Edit
-                </button>
-                {!user.isEmailConfirmed && (
+}) => {
+    console.log(user);
+    return (
+        <tr>
+            <td>{`${user.firstName} ${user.lastName}`}</td>
+            <td>{user.email}</td>
+            <td>{user.phoneNumber}</td>
+            <Roles user={user} roles={user.roles} />
+            <td>{user.appVersion ? user.appVersion : '-'}</td>
+            <td>
+                {user.clientAccessCreatedByCompanies?.length ? (
+                    user.clientAccessCreatedByCompanies.map((company, i, arr) => {
+                        const isTheLastCompany = arr.length - 1 === i;
+                        return (
+                            <>
+                                <Link
+                                    key={company.companyID}
+                                    className="link"
+                                    to={`/admin/companies/${company.companyID}`}
+                                >
+                                    {company.name}
+                                </Link>
+                                {!isTheLastCompany && <span>, </span>}
+                            </>
+                        );
+                    })
+                ) : (
+                    <span>N/A</span>
+                )}
+            </td>
+            <td>
+                <DateTimeContainer date={user.createdOn} datetime={DATE_TIME_IDS.DATE} />
+            </td>
+            <td>{boolToYesNo(user.isEmailConfirmed)}</td>
+            <td>{boolToYesNo(user.isDeleted)}</td>
+            <td>
+                <BlockButtonWrapper>
+                    <button className="button yellow" onClick={() => handleShowEditUserModal(user)}>
+                        <i className="far fa-pencil" /> Edit
+                    </button>
+                    {!user.isEmailConfirmed && (
+                        <button
+                            className="button blue"
+                            onClick={() => handleShowConfirmEmailModal(user)}
+                        >
+                            <i className="far fa-pencil" /> Confirm e-mail
+                        </button>
+                    )}
+                    {user.isLockoutEnabled && (
+                        <button className="button blue" onClick={handleShowRemoveLockoutModal}>
+                            <i className="far fa-lock" /> Remove lockout
+                        </button>
+                    )}
                     <button
-                        className="button blue"
-                        onClick={() => handleShowConfirmEmailModal(user)}
+                        onClick={() => handleShowEditUserPasswordModal(user)}
+                        className="button"
                     >
-                        <i className="far fa-pencil" /> Confirm e-mail
+                        Change password
                     </button>
-                )}
-                {user.isLockoutEnabled && (
-                    <button className="button blue" onClick={handleShowRemoveLockoutModal}>
-                        <i className="far fa-lock" /> Remove lockout
-                    </button>
-                )}
-                <button onClick={() => handleShowEditUserPasswordModal(user)} className="button">
-                    Change password
-                </button>
-            </BlockButtonWrapper>
-        </td>
-    </tr>
-);
+                </BlockButtonWrapper>
+            </td>
+        </tr>
+    );
+};
 
 export default UserListItem;
