4f0be3b8c075baf0726e4b10891d5b1e88415de0
Seb Smith <seb@silverchip.com>
2022-02-28 08:51:06 +0000

    Removes all old nav in replacement of new reusable structure

--- app-src/src/components/shared/layout/menu/presentational/ClientMenu.js
+++ app-src/src/components/shared/layout/menu/presentational/ClientMenu.js
@@ -1,52 +1,14 @@
 import React from 'react';
 
+import { clientNavMenuItems } from '../../../../../constants/client/actionTypes/menuItems';
 import MenuItemContainer from '../containers/MenuItemContainer';
-import MenuHeader from './MenuHeader';
 
 const ClientMenu = ({ dismissMessages, unreadCount, isCompany }) => (
-    <>
-        <div className="menu">
-            <MenuHeader title="My Access" />
-            <MenuItemContainer link="/client/sites">
-                <i className="far fa-building icon fa-fw" />{' '}
-                <span className="menu-text">Sites</span>
-            </MenuItemContainer>
-
-            <MenuHeader title="Reports" />
-            <MenuItemContainer onClick={dismissMessages} link="/client/reports">
-                {/* <MenuItemContainer link="/company/reports"> */}
-                {!!unreadCount && <span className="number">{unreadCount}</span>}
-                <i className="far fa-file-chart-pie fa-fw icon" />
-                <span className={`menu-text ${unreadCount ? 'large' : ''}`}>
-                    My Company Reports
-                </span>
-            </MenuItemContainer>
-            {/* <MenuItemContainer link="/client/reports/create">
-                <i className="far fa-file-edit fa-fw icon" />
-                <span className="menu-text">Create Report</span>
-            </MenuItemContainer> */}
-
-            <MenuHeader title="Settings &amp; Tools" />
-            <MenuItemContainer link="/client/profile">
-                <i className="far fa-user icon fa-fw" />{' '}
-                <span className="menu-text">My Profile</span>
-            </MenuItemContainer>
-            <MenuItemContainer link="/client/companies">
-                <i className="far fa-briefcase icon fa-fw" />{' '}
-                <span className="menu-text">Switch Company</span>
-            </MenuItemContainer>
-            {isCompany && (
-                <MenuItemContainer link="/company">
-                    <i className="fal fa-user-crown icon fa-fw" />{' '}
-                    <span className="menu-text">Company Admin</span>
-                </MenuItemContainer>
-            )}
-            <MenuItemContainer link="#" logout={true}>
-                <i className="far fa-sign-out-alt icon fa-fw" />{' '}
-                <span className="menu-text">Logout</span>
-            </MenuItemContainer>
-        </div>
-    </>
+    <div className="menu">
+        {clientNavMenuItems.map((navItem, index) => {
+            return <MenuItemContainer key={index} item={navItem} />;
+        })}
+    </div>
 );
 
 export default ClientMenu;
