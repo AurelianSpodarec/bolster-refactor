073098f43665c1e241e00e3244c165d05a66b6fb
Viktoriia Petrova <viktoriia@silverchip.com>
2022-02-28 16:29:51 +0000

    Add dynamic styles to Credit button

--- app-src/src/components/companyAdmin/layout/header/presentational/CompanyHeader.js
+++ app-src/src/components/companyAdmin/layout/header/presentational/CompanyHeader.js
@@ -22,74 +22,100 @@ const Header = ({
     shouldRestrictPayments,
     isCompanySelection,
     companyUserID,
-}) => (
-    <header
-        id="page-header"
-        style={{ borderColor: companyUserID ? companyColour : defaultStyles.colourCode }}
-    >
-        <div className="container">
-            <div className="flex-row">
-                {/*** company logo ***/}
-                <div className="logo">
-                    {!companyUserID ? (
-                        <Link to="/company">
-                            <img alt="logo of Bolster Systems" src={defaultStyles.logoFile} />
-                        </Link>
-                    ) : (
-                        <>
-                            {!!company.id && (
-                                <Link to="/company">
-                                    <img
-                                        alt={`logo of ${company.name}`}
-                                        src={
-                                            company.logoFile
-                                                ? `${FILE_STORAGE_URL}/${company.logoFile}`
-                                                : defaultStyles.logoFile
-                                        }
-                                    />
-                                </Link>
-                            )}
-                        </>
+}) => {
+    return (
+        <header
+            id="page-header"
+            style={{ borderColor: companyUserID ? companyColour : defaultStyles.colourCode }}
+        >
+            <div className="container">
+                <div className="flex-row">
+                    {/*** company logo ***/}
+                    <div className="logo">
+                        {!companyUserID ? (
+                            <Link to="/company">
+                                <img alt="logo of Bolster Systems" src={defaultStyles.logoFile} />
+                            </Link>
+                        ) : (
+                            <>
+                                {!!company.id && (
+                                    <Link to="/company">
+                                        <img
+                                            alt={`logo of ${company.name}`}
+                                            src={
+                                                company.logoFile
+                                                    ? `${FILE_STORAGE_URL}/${company.logoFile}`
+                                                    : defaultStyles.logoFile
+                                            }
+                                        />
+                                    </Link>
+                                )}
+                            </>
+                        )}
+                    </div>
+                    {!isCompanySelection && (
+                        <div className="search-area">
+                            <SearchContainer placeholder="Search Sites, Drawings, Operatives" />
+                        </div>
                     )}
                 </div>
-                {!isCompanySelection && (
-                    <div className="search-area">
-                        <SearchContainer placeholder="Search Sites, Drawings, Operatives" />
-                    </div>
-                )}
-            </div>
 
-            <div className="account-area">
-                {!isCompanySelection && (
-                    <div className="notifications">
-                        {!shouldRestrictPayments && (
+                <div className="account-area">
+                    {!isCompanySelection && (
+                        <div className="notifications">
                             <div className="credit-container">
                                 {company.parentalType === PARENTAL_TYPES.NONE && (
-                                    <div className="balance">{totalCredits}</div>
+                                    <div
+                                        className={`${
+                                            shouldRestrictPayments
+                                                ? 'balance'
+                                                : totalCredits > 0
+                                                ? 'balance positive-balance'
+                                                : 'balance negative-balance'
+                                        }`}
+                                    >
+                                        {shouldRestrictPayments
+                                            ? '-'
+                                            : totalCredits > 999999
+                                            ? '<999999'
+                                            : totalCredits}
+                                    </div>
                                 )}
-                                <div className="credit-btn">
+                                <div
+                                    className={`${
+                                        shouldRestrictPayments
+                                            ? 'credit-btn'
+                                            : totalCredits > 0
+                                            ? 'credit-btn positive-balance'
+                                            : 'credit-btn negative-balance'
+                                    }`}
+                                >
                                     <img alt="bank card" className="tools-icon" src={PaymentIcon} />
                                     Buy Credits
                                 </div>
                             </div>
-                        )}
 
-                        <Link to="/company/tools/transfer-requests" className="item">
-                            {!!totalRequests && <span className="notification-dot" />}
-                            <img alt="exchange icon" className="tools-icon" src={ExchangeIcon} />
-                        </Link>
+                            <Link to="/company/tools/transfer-requests" className="item">
+                                {!!totalRequests && <span className="notification-dot" />}
+                                <img
+                                    alt="exchange icon"
+                                    className="tools-icon"
+                                    src={ExchangeIcon}
+                                />
+                            </Link>
 
-                        <Link to="/company/message-centre" className="item">
-                            {!!unreadMessageCount && <span className="notification-dot" />}
-                            <img alt="envelope" className="tools-icon" src={EnvelopeIcon} />
-                        </Link>
-                        <div className="break-line" />
-                        <HeaderProfileContainer isCompanySelection={isCompanySelection} />
-                    </div>
-                )}
+                            <Link to="/company/message-centre" className="item">
+                                {!!unreadMessageCount && <span className="notification-dot" />}
+                                <img alt="envelope" className="tools-icon" src={EnvelopeIcon} />
+                            </Link>
+                            <div className="break-line" />
+                            <HeaderProfileContainer isCompanySelection={isCompanySelection} />
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
-    </header>
-);
+        </header>
+    );
+};
 
 export default Header;
