5ec87337d6f5b525ab172834752387ffb442f8f5
Viktoriia Petrova <viktoriia@silverchip.com>
2022-03-01 16:22:06 +0000

    Auto stash before merge of "BUXUI-2" and "origin/BUXUI-2"

--- app-src/src/components/companyAdmin/layout/header/presentational/HeaderProfile.js
+++ app-src/src/components/companyAdmin/layout/header/presentational/HeaderProfile.js
@@ -2,22 +2,23 @@ import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import { FILE_STORAGE_URL } from 'config';
+import ProfileIcon from '../../../../../_content/images/icons/profile.png';
+import ExchangeIcon from '../../../../../_content/images/icons/exchange-in-user-profile.png';
+import LogoutIcon from '../../../../../_content/images/icons/logout.png';
+import defaultStyles from 'constants/defaultStyles';
 
-const HeaderProfile = ({
-    profile,
-    popupVisible,
-    handleClick,
-    logout,
-    isImpersonating,
-    companyName,
-    isSubscribed,
-    shouldRestrictPayments,
-    isCompanyUserOrSelecting,
-}) => {
+const HeaderProfile = ({ profile, handleClick, logout, companyUserID, companyColour }) => {
     const ref = useRef(null);
     return (
-        <div className="item">
-            <div ref={ref} className="user" onClick={handleClick}>
+        <div className="item profile-container">
+            <div
+                ref={ref}
+                className="user"
+                onClick={handleClick}
+                style={{
+                    backgroundColor: companyUserID ? companyColour : defaultStyles.colourCode,
+                }}
+            >
                 {profile.profileImageS3Key ? (
                     <img alt="profile" src={`${FILE_STORAGE_URL}/${profile.profileImageS3Key}`} />
                 ) : (
@@ -27,20 +28,26 @@ const HeaderProfile = ({
                             profile?.lastName[0]}
                     </div>
                 )}
+                <div className="user-icon-menu-wrapper"></div>
             </div>
 
             <div className="profile-menu">
-                <div className="profile-options">
+                <div
+                    className="profile-options"
+                    // style={{
+                    //     backgroundColor: companyUserID ? companyColour : defaultStyles.colourCode,
+                    // }}
+                >
                     <Link to="/company/profile" className="dropdown-item">
-                        <i className="far fa-user fa-fw icon" />
+                        <img alt="profile icon" src={ProfileIcon} />
                         <span className="item-text">My Profile</span>
                     </Link>
                     <Link to="/company/company-selection" className="dropdown-item">
-                        <i className="icon far fa-exchange fa-fw" />
+                        <img alt="exchange icon" src={ExchangeIcon} />
                         <span className="item-text">Select Company</span>
                     </Link>
                     <Link onClick={logout} to="#" className="dropdown-item">
-                        <i className="icon far fa-sign-out fa-fw" />
+                        <img alt="logout icon" src={LogoutIcon} />
                         <span className="item-text">Logout</span>
                     </Link>
                 </div>
