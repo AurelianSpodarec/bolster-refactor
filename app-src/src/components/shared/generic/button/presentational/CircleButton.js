be85161c8fb99c81dff2c14b798bbaea9110bc14
Viktoriia Petrova <viktoriia@silverchip.com>
2022-03-02 10:56:25 +0000

    Add hover to notifications items

--- app-src/src/components/shared/generic/button/presentational/CircleButton.js
+++ app-src/src/components/shared/generic/button/presentational/CircleButton.js
@@ -5,6 +5,7 @@ const CircleButton = ({ href = '', icon, showNotification }) => (
     <Link to={href} className="circle-button flex-row justify-center align-center">
         {showNotification && <span className="notification-dot" />}
         <img alt="icon" className="icon" src={icon} />
+        <div className="dark-hover"></div>
     </Link>
 );
 
