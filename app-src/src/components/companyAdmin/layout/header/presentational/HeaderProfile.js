fe67646da0b50b172758137b10316f5430833ed6
Mark Baker <mark@silverchip.com>
2022-03-02 11:30:06 +0000

    update styles / icons for header profile when dark logo is enabled

--- app-src/src/components/companyAdmin/layout/header/presentational/HeaderProfile.js
+++ app-src/src/components/companyAdmin/layout/header/presentational/HeaderProfile.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { FILE_STORAGE_URL } from 'config';
 import ProfileIcon from '../../../../../_content/images/icons/profile.png';
+import ProfileIconDark from '../../../../../_content/images/icons/profile-dark.png';
 import ExchangeIcon from '../../../../../_content/images/icons/exchange-in-user-profile.png';
+import ExchangeIconDark from '../../../../../_content/images/icons/exchange-in-user-profile-dark.png';
 import LogoutIcon from '../../../../../_content/images/icons/logout.png';
+import LogoutIconDark from '../../../../../_content/images/icons/logout-dark.png';
+
+import { selectIsBolsterLogoDark } from 'selectors/companyAdmin/companySettings';
 
 import useHeaderProfile from '../hooks/useHeaderProfile';
 import useGetUserInitials from 'hooks/useGetUserInitials';
@@ -13,6 +19,7 @@ const HeaderProfile = () => {
     const { profile, backgroundColor, handleLogout } = useHeaderProfile();
 
     const initials = useGetUserInitials();
+    const isDarkLogoEnabled = useSelector(selectIsBolsterLogoDark);
 
     return (
         <div className="profile-container">
@@ -32,19 +39,31 @@ const HeaderProfile = () => {
             </div>
 
             <div className="profile-menu">
-                <div className="profile-options" style={{ backgroundColor }}>
+                <div
+                    className={`profile-options ${isDarkLogoEnabled ? 'dark' : ''}`}
+                    style={{ backgroundColor }}
+                >
                     <Link to="/company/profile" className="dropdown-item">
-                        <img alt="profile icon" src={ProfileIcon} />
+                        <img
+                            alt="profile icon"
+                            src={isDarkLogoEnabled ? ProfileIconDark : ProfileIcon}
+                        />
                         <span className="item-text">My Profile</span>
                         <div className="dark-hover"></div>
                     </Link>
                     <Link to="/company/company-selection" className="dropdown-item">
-                        <img alt="exchange icon" src={ExchangeIcon} />
+                        <img
+                            alt="exchange icon"
+                            src={isDarkLogoEnabled ? ExchangeIconDark : ExchangeIcon}
+                        />
                         <span className="item-text">Select Company</span>
                         <div className="dark-hover"></div>
                     </Link>
                     <Link onClick={handleLogout} to="#" className="dropdown-item">
-                        <img alt="logout icon" src={LogoutIcon} />
+                        <img
+                            alt="logout icon"
+                            src={isDarkLogoEnabled ? LogoutIconDark : LogoutIcon}
+                        />
                         <span className="item-text">Logout</span>
                         <div className="dark-hover"></div>
                     </Link>
