3f62bea3d0a93e956321f480a15456f654038478
Viktoriia Petrova <viktoriia@silverchip.com>
2022-03-03 18:13:53 +0000

    Add mobile menu to Admin

--- app-src/src/components/superAdmin/layout/header/presentational/AdminHeader.js
+++ app-src/src/components/superAdmin/layout/header/presentational/AdminHeader.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import bolsterLogo from '_content/images/bolster_logo.png';
+import useAdminHeader from '../hooks/useAdminHeader';
 
-const AdminHeader = () => (
-    <header id="page-header" className="basic">
-        <div className="container">
-            <div className="logo">
-                <Link to="/admin">
-                    <img alt="bolster logo" src={bolsterLogo} />
-                </Link>
+const AdminHeader = () => {
+    const { isMobile, toggleMobileMenu } = useAdminHeader();
+
+    return (
+        <header id="page-header" className="flex-row justify-between align-stretch">
+            <div className="flex flex-row align-stretch">
+                <div className="logo flex-row justify-center align-center">
+                    {isMobile && (
+                        <div className="mobile-menu" onClick={() => toggleMobileMenu()}>
+                            <i className="far fa-bars" />
+                        </div>
+                    )}
+
+                    <Link to="/admin">
+                        <img alt="bolster logo" src={bolsterLogo} />
+                    </Link>
+                </div>
             </div>
 
             <div className="clear" />
-        </div>
-    </header>
-);
+        </header>
+    );
+};
 
 export default AdminHeader;
