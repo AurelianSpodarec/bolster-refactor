19002d7154c16b0bf0ac9c74682db744105bf095
Viktoriia Petrova <viktoriia@silverchip.com>
2022-03-03 14:18:59 +0000

    Fix positioning of notification dot

--- app-src/src/components/shared/generic/button/presentational/CircleButton.js
+++ app-src/src/components/shared/generic/button/presentational/CircleButton.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CircleButton = ({ href = '', icon, showNotification }) => (
-    <Link to={href} className="circle-button flex-row justify-center align-center">
+    <div className="circle-button-container">
         {showNotification && <span className="notification-dot" />}
-        <img alt="icon" className="icon" src={icon} />
-        <div className="dark-hover"></div>
-    </Link>
+        <Link to={href} className="circle-button flex-row justify-center align-center">
+            <img alt="icon" className="icon" src={icon} />
+            <div className="dark-hover"></div>
+        </Link>
+    </div>
 );
 
 export default CircleButton;
