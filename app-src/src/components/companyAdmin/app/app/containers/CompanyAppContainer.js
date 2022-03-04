98859a3fbef14418fbd25b3c67166f22e69f1f31
Viktoriia Petrova <viktoriia@silverchip.com>
2022-03-04 16:35:19 +0000

    Fix import

--- app-src/src/components/companyAdmin/app/app/containers/CompanyAppContainer.js
+++ app-src/src/components/companyAdmin/app/app/containers/CompanyAppContainer.js
@@ -23,8 +23,7 @@ import { AUTH_TYPES } from 'constants/shared/auth';
 import fetchMessagesBasic from 'actions/companyAdmin/messages/async/fetchMessagesBasic';
 import fetchRecentUpdates from 'actions/companyAdmin/recentUpdates/async/fetchRecentUpdates';
 import resetFilterOptions from 'actions/companyAdmin/reports/sync/resetFilterOptions';
-<<<<<<< HEAD
-
+import fetchLatestAppVersion from 'actions/companyAdmin/app/async/fetchLatestAppVersion';
 class CompanyAppContainer extends React.PureComponent {
     render() {
         return <CompanyApp />;
