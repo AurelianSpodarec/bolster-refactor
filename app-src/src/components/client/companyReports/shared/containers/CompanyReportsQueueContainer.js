0f55665f99f255365229dc9a1f1da52500ebb377
Seb Smith <seb@silverchip.com>
2022-03-07 15:04:13 +0000

    Fix incorrect action

--- app-src/src/components/client/companyReports/shared/containers/CompanyReportsQueueContainer.js
+++ app-src/src/components/client/companyReports/shared/containers/CompanyReportsQueueContainer.js
@@ -4,16 +4,16 @@ import fetchClientCompanyReports from 'actions/client/reports/queue/async/fetchC
 import CompanyReports from '../presentational/CompanyReports';
 import { getSelectedCompanyForClient } from 'helpers/generic';
 import { MESSAGE_TYPES } from '../../../../../constants/companyAdmin/enums';
-import dismissMessages from '../../../../../actions/companyAdmin/messages/async/dismissMessages';
+import clientDismissMessages from '../../../../../actions/client/messages/async/clientDismissMessages';
 
 class CompanyReportsQueueContainer extends Component {
     render = () => <CompanyReports />;
 
     componentDidMount = () => {
-        const { fetchClientCompanyReports, dismissMessages } = this.props;
+        const { fetchClientCompanyReports, clientDismissMessages } = this.props;
         const selectedCompanyID = getSelectedCompanyForClient();
 
-        dismissMessages();
+        clientDismissMessages();
 
         this._interval = setInterval(() => fetchClientCompanyReports(selectedCompanyID), 5000);
     };
@@ -22,7 +22,7 @@ class CompanyReportsQueueContainer extends Component {
 
 const mapDispatchToProps = dispatch => ({
     fetchClientCompanyReports: companyID => dispatch(fetchClientCompanyReports(companyID)),
-    dismissMessages: () => dispatch(dismissMessages(MESSAGE_TYPES.NOTIFICATION)),
+    clientDismissMessages: () => dispatch(clientDismissMessages(MESSAGE_TYPES.NOTIFICATION)),
 });
 
 export default connect(null, mapDispatchToProps)(CompanyReportsQueueContainer);
