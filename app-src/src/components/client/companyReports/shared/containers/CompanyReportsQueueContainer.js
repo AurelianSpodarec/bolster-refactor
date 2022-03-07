2e08d8fcffe3b71956a55c37c966713ca410c975
Seb Smith <seb@silverchip.com>
2022-03-07 14:59:30 +0000

    Adds notification clear on client reports

--- app-src/src/components/client/companyReports/shared/containers/CompanyReportsQueueContainer.js
+++ app-src/src/components/client/companyReports/shared/containers/CompanyReportsQueueContainer.js
@@ -3,27 +3,26 @@ import { connect } from 'react-redux';
 import fetchClientCompanyReports from 'actions/client/reports/queue/async/fetchClientCompanyReports';
 import CompanyReports from '../presentational/CompanyReports';
 import { getSelectedCompanyForClient } from 'helpers/generic';
+import { MESSAGE_TYPES } from '../../../../../constants/companyAdmin/enums';
+import dismissMessages from '../../../../../actions/companyAdmin/messages/async/dismissMessages';
 
 class CompanyReportsQueueContainer extends Component {
     render = () => <CompanyReports />;
 
     componentDidMount = () => {
-        const { fetchClientCompanyReports } = this.props;
+        const { fetchClientCompanyReports, dismissMessages } = this.props;
         const selectedCompanyID = getSelectedCompanyForClient();
-        this._interval = setInterval(
-            () => fetchClientCompanyReports(selectedCompanyID),
-            5000
-        );
+
+        dismissMessages();
+
+        this._interval = setInterval(() => fetchClientCompanyReports(selectedCompanyID), 5000);
     };
     componentWillUnmount = () => clearInterval(this._interval);
 }
 
 const mapDispatchToProps = dispatch => ({
-    fetchClientCompanyReports: companyID =>
-        dispatch(fetchClientCompanyReports(companyID))
+    fetchClientCompanyReports: companyID => dispatch(fetchClientCompanyReports(companyID)),
+    dismissMessages: () => dispatch(dismissMessages(MESSAGE_TYPES.NOTIFICATION)),
 });
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(CompanyReportsQueueContainer);
+export default connect(null, mapDispatchToProps)(CompanyReportsQueueContainer);
