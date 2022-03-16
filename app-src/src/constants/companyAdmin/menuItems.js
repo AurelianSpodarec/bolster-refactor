6e1a7db34c763c5d244353961f0689de6bf62f7f
Seb Smith <seb@silverchip.com>
2022-03-16 15:12:21 +0000

    Adds upcoming alerts to the navigation

--- app-src/src/constants/companyAdmin/menuItems.js
+++ app-src/src/constants/companyAdmin/menuItems.js
@@ -174,7 +174,13 @@ export const companyNavMenuItems = [
                 paymentRestriction: false,
                 clientAccessRestriction: false,
             },
-
+            {
+                name: 'Upcoming Alerts',
+                link: '/company/message-centre',
+                showWhenNotSubscribed: false,
+                paymentRestriction: false,
+                clientAccessRestriction: false,
+            },
             {
                 name: 'Activity Log',
                 link: '/company/activity-log',
