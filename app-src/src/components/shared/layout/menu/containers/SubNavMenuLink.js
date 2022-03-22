a078eb11c52c183938a27a223936297a95518893
Viktoriia Petrova <viktoriia@silverchip.com>
2022-03-22 18:28:33 +0000

    Trying to fix nav's hover issue

--- app-src/src/components/shared/layout/menu/containers/SubNavMenuLink.js
+++ app-src/src/components/shared/layout/menu/containers/SubNavMenuLink.js
@@ -7,22 +7,30 @@ const SubNavMenuLink = ({
     shouldUseCompanyColours,
 }) => {
     const [hover, setHover] = useState(false);
+    const [clicked, setClicked] = useState('');
 
     const route = location.pathname.toLowerCase();
 
     const isActive = route === link.toLowerCase();
 
     return (
-        <div className="sub-nav-item">
+        <div className={`sub-nav-item ${clicked}`}>
             {!!notificationCount && (
                 <div className="notification-badge">
                     <p>{notificationCount}</p>
                 </div>
             )}
             <Link
-                onClick={onClick}
+                onClick={() => {
+                    console.log(clicked);
+                    setClicked('clicked');
+                    console.log(clicked);
+                    onClick;
+                }}
                 to={link}
-                onMouseEnter={() => setHover(true)}
+                onMouseEnter={() => {
+                    setHover(true);
+                }}
                 onMouseLeave={() => {
                     setHover(false);
                 }}
