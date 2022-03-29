0aed4b66b64a3cc165cfe76c31a2098d78ae8340
Nick Barnard <nick@silverchip.com>
2022-03-29 16:01:41 +0100

    Add Redux for confirm dark theme

--- app-src/src/reducers/shared/colourTheme.js
+++ app-src/src/reducers/shared/colourTheme.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 
-import { TOGGLE_COLOUR_THEME } from '../../constants/shared/colourTheme';
+import { TOGGLE_COLOUR_THEME, CONFIRM_DARK_THEME } from '../../constants/shared/colourTheme';
 import { FETCH_PROFILE_SUCCESS } from '../../constants/actionTypes/profile';
 
 export default combineReducers({
     isDarkModeEnabled: colourThemeReducer,
+    confirmDarkTheme: confirmDarkThemeReducer,
 });
 
 function colourThemeReducer(state = null, action) {
@@ -13,6 +14,15 @@ function colourThemeReducer(state = null, action) {
             return action.value;
         case FETCH_PROFILE_SUCCESS:
             return action.payload.isDarkModeEnabled;
+        default:
+            return state;
+    }
+}
+
+function confirmDarkThemeReducer(state = false, action) {
+    switch (action.type) {
+        case CONFIRM_DARK_THEME:
+            return true;
         default:
             return state;
     }
