61c0ae8825228a1cbb00bfbb717166ae853c2dcf
Nick Barnard <nick@silverchip.com>
2022-03-31 11:26:05 +0100

    Revert "Add new buttons to Services table" (to be done on another branch)
    This reverts commit 1df383b7c32cbabe82ac4df601dde2e81dce1bde.

--- app-src/src/components/companyAdmin/subscription/activeServices/presentational/AddServiceItem.js
+++ app-src/src/components/companyAdmin/subscription/activeServices/presentational/AddServiceItem.js
@@ -3,56 +3,49 @@ import React from 'react';
 import { ADD_MULTIPLE_SERVICES_TO_SUBSCRIPTION } from 'constants/shared/modalTypes';
 import StatusIcon from 'components/shared/generic/statusIcon/presentationl/StatusIcon';
 import TooltipContainer from 'components/shared/generic/tooltip/containers/TooltipContainer';
-import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
-import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 
 const AddServiceItem = ({ service, showModal, canEdit }) => {
     if (canEdit)
         return (
             <div className="subscription-item add-service size-lg-6 size-md-12">
-                <div className="field-name size-lg-12 flex-row justify-space-between">
+                <div className="field-name size-lg-6 size-md-8">
                     <StatusIcon classes="add" iconClass="fa fa-plus" />
-                    <label style={{ flex: 1 }}>{service.name}</label>
-                    <ButtonWrapper alignment="right" inline>
-                        <ActionButton
-                            onClick={e => {
-                                e.preventDefault();
-                                showModal(ADD_MULTIPLE_SERVICES_TO_SUBSCRIPTION, {
-                                    serviceID: service.id,
-                                });
-                            }}
-                            text="Add"
-                            size="small"
-                            ambient="positive"
-                        />
-                    </ButtonWrapper>
+                    <label>{service.name}</label>
                 </div>
+
+                <button
+                    className="button green"
+                    onClick={e => {
+                        e.preventDefault();
+                        showModal(ADD_MULTIPLE_SERVICES_TO_SUBSCRIPTION, { serviceID: service.id });
+                    }}
+                >
+                    Add
+                </button>
             </div>
         );
 
     return (
         <div className="subscription-item add-service size-lg-6 size-md-12">
-            <div className="field-name size-lg-12 flex-row justify-space-between">
+            <div className="field-name size-lg-6 size-md-8">
                 <StatusIcon classes="add" iconClass="fa fa-plus" />
-                <label style={{ flex: 1 }}>{service.name}</label>
-                <TooltipContainer
-                    side="top"
-                    text="You can add new services when your new subsciption begins."
-                >
-                    <ButtonWrapper alignment="right" inline>
-                        <ActionButton
-                            onClick={e => {
-                                e.preventDefault();
-                            }}
-                            text="Add"
-                            size="small"
-                            ambient="positive"
-                            disabled
-                            extraClasses=""
-                        />
-                    </ButtonWrapper>
-                </TooltipContainer>
+                <label>{service.name}</label>
             </div>
+            <TooltipContainer
+                side="top"
+                text="You can add new services when your new subsciption begins."
+            >
+                <button
+                    type="button"
+                    style={{ opacity: 0.5, cursor: 'default' }}
+                    className="button green"
+                    onClick={e => {
+                        e.preventDefault();
+                    }}
+                >
+                    Add
+                </button>
+            </TooltipContainer>
         </div>
     );
 };
