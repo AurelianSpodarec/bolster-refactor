987880bcfc5f542c02d459e8a98cc4fb127ab659
Nick Barnard <nick@silverchip.com>
2022-03-31 11:24:03 +0100

    Add inline & extraClasses props to ButtonWrapper

--- app-src/src/components/shared/generic/button/presentational/ButtonWrapper.js
+++ app-src/src/components/shared/generic/button/presentational/ButtonWrapper.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const ButtonWrapper = ({ children, alignment = 'left' }) => {
+const ButtonWrapper = ({ children, alignment = 'left', inline = false, extraClasses = '' }) => {
     const justifyPosition =
         alignment === 'right' ? 'end' : alignment === 'center' ? 'center' : 'start';
 
     return (
         <div
-            className={`button-wrapper flex-row align-center justify-${justifyPosition} ${alignment}`}
+            className={`button-wrapper ${
+                inline ? 'inline' : ''
+            } flex-row align-center justify-${justifyPosition} ${alignment} ${extraClasses}`}
         >
             {children}
         </div>
