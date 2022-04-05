bd63fb914a8556e3d37f354034dcc84409cf128d
Mark Baker <mark@silverchip.com>
2022-04-05 13:34:05 +0100

    add 'sets' components

--- app-src/src/constants/shared/blockTabs.ts
+++ app-src/src/constants/shared/blockTabs.ts
@@ -1,18 +1,18 @@
 import FRRatings from 'components/companyAdmin/pinOptions/frRatings/FRRatings';
-import InstallationTypes from 'components/companyAdmin/pinOptions/installationTypes/InstallationTypes';
-import ItemTypes from 'components/companyAdmin/pinOptions/itemTypes/ItemTypes';
+import InstallationTypeSets from 'components/companyAdmin/pinOptions/installationTypes/InstallationTypeSets';
+import ItemTypeSets from 'components/companyAdmin/pinOptions/itemTypes/ItemTypeSets';
 import Prelims from 'components/companyAdmin/pinOptions/prelims/Prelims';
 
 export const PIN_OPTIONS_TABS = [
     {
         id: 1,
         name: 'Installation Types',
-        component: InstallationTypes,
+        component: InstallationTypeSets,
     },
     {
         id: 2,
         name: 'Item Types',
-        component: ItemTypes,
+        component: ItemTypeSets,
     },
     {
         id: 3,
