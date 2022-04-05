29b4270d4c3b685fe885d7040dae86d6e935e6b7
Mark Baker <mark@silverchip.com>
2022-04-05 11:37:53 +0100

    add initial searches

--- app-src/src/components/companyAdmin/pinOptions/itemTypes/ItemTypes.js
+++ app-src/src/components/companyAdmin/pinOptions/itemTypes/ItemTypes.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import useSearch from 'hooks/useSearch';
+
+import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
+
 const ItemTypes = () => {
+    const { searchTerm, handleUpdateSearch } = useSearch();
+
     return (
         <>
-            <p className="size-lg-12">Item Types</p>
+            <div className="flex-row align-center width-12">
+                <TextInputContainer
+                    name="search"
+                    value={searchTerm}
+                    handleChange={handleUpdateSearch}
+                    placeholder="Search"
+                />
+
+                <button className="button green">Add</button>
+            </div>
         </>
     );
 };
