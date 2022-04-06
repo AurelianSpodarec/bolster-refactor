01ffcae13dae520a28fc576e0d0bd802c014c268
Mark Baker <mark@silverchip.com>
2022-04-06 11:32:00 +0100

    add fetch for pin option types

--- app-src/src/components/companyAdmin/pinOptions/PinOptions.js
+++ app-src/src/components/companyAdmin/pinOptions/PinOptions.js
@@ -5,6 +5,7 @@ import { isEmpty } from 'helpers/generic';
 
 import useBlockTabs from 'components/shared/tabs/hooks/useBlockTabs';
 import useFetchServices from 'components/companyAdmin/hooks/useFetchServices';
+import useFetchPinOptionTypes from '../hooks/useFetchPinOptionTypes';
 
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
@@ -14,15 +15,20 @@ const PinOptions = () => {
     const { selectedTabID, setSelectedTabID, SpecificComponent } = useBlockTabs(PIN_OPTIONS_TABS);
 
     const { services, isFetchingServices, servicesError } = useFetchServices();
+    const { pinOptionTypes, isFetchingPinOptionTypes, pinOptionTypesError } =
+        useFetchPinOptionTypes();
 
     return (
         <>
             <PageHeading title="Pin Options" />
 
             <BlockContainer
-                isEmpty={isEmpty(services) && isFetchingServices}
-                isFetching={isFetchingServices}
-                error={servicesError}
+                isEmpty={
+                    (isEmpty(services) && isFetchingServices) ||
+                    (isEmpty(pinOptionTypes) && isFetchingPinOptionTypes)
+                }
+                isFetching={isFetchingServices || isFetchingPinOptionTypes}
+                error={servicesError || pinOptionTypesError}
             >
                 <BlockTabs
                     tabs={PIN_OPTIONS_TABS}
