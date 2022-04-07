349b56ed3109a63414f32d2eda189af12408ed92
Mark Baker <mark@silverchip.com>
2022-04-07 14:30:48 +0100

    add type validation for documents page

--- app-src/src/components/companyAdmin/pinOptions/optionDocuments/OptionDocuments.js
+++ app-src/src/components/companyAdmin/pinOptions/optionDocuments/OptionDocuments.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+
+import { PIN_OPTION_TYPES_LOOKUP } from 'constants/companyAdmin/enums';
 
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
 import PageHeading from 'components/shared/generic/pageHeading/presentational/PageHeading';
 
 const OptionDocuments = () => {
+    const { optionID, setID, type } = useParams();
+
+    const typeLink = PIN_OPTION_TYPES_LOOKUP[type];
+
+    if (!typeLink) return <Redirect to="/company/pin-options" />;
+
     return (
         <>
             <PageHeading title={'##document name##'} withBackButton />
