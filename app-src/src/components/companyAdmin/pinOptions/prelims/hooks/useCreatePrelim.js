37bbe23aa22e6a9a756a6569fc015ec0cb21be68
Viktoriia Petrova <viktoriia@silverchip.com>
2022-04-08 16:25:20 +0100

    Hide modal on success

--- app-src/src/components/companyAdmin/pinOptions/prelims/hooks/useCreatePrelim.js
+++ app-src/src/components/companyAdmin/pinOptions/prelims/hooks/useCreatePrelim.js
@@ -5,13 +5,19 @@ import { ERROR_MODAL } from 'constants/shared/modalTypes';
 import { useForm } from 'helpers/hooks';
 
 import showModal from 'actions/shared/generic/modals/sync/showModal';
-import { selectPrelimIsPosting, selectPrelimPostError } from 'selectors/companyAdmin/prelims';
+import {
+    selectPrelimIsPosting,
+    selectPrelimPostError,
+    selectPrelimPostSuccess,
+} from 'selectors/companyAdmin/prelims';
 import createPrelim from 'actions/companyAdmin/prelims/async/createPrelim';
+import hideModal from 'actions/shared/generic/modals/sync/hideModal';
 
 const useCreatePrelim = () => {
     const dispatch = useDispatch();
     const isPosting = useSelector(selectPrelimIsPosting);
     const postError = useSelector(selectPrelimPostError);
+    const postSuccess = useSelector(selectPrelimPostSuccess);
 
     const [form, handleChange] = useForm({
         name: '',
@@ -30,6 +36,10 @@ const useCreatePrelim = () => {
     useEffect(() => {
         if (postError) dispatch(showModal(ERROR_MODAL));
     }, [postError]);
+
+    useEffect(() => {
+        if (postSuccess) dispatch(hideModal());
+    }, [postSuccess]);
 
     return { form, handleChange, handleSubmit, isPosting };
 };
