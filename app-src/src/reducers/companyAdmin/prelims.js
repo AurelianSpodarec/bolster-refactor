51135e3a25cd56c2c03532c0c8385d4e3ca50922
Viktoriia Petrova <viktoriia@silverchip.com>
2022-04-08 17:00:27 +0100

    Add delete and edit reducers for Prelims

--- app-src/src/reducers/companyAdmin/prelims.js
+++ app-src/src/reducers/companyAdmin/prelims.js
@@ -1,9 +1,15 @@
 import { combineReducers } from 'redux';
-import { convertArrToObj } from 'helpers/generic';
+import { convertArrToObj, updateObj } from 'helpers/generic';
 import {
     CREATE_PRELIM_FAILURE,
     CREATE_PRELIM_REQUEST,
     CREATE_PRELIM_SUCCESS,
+    DELETE_PRELIM_FAILURE,
+    DELETE_PRELIM_REQUEST,
+    DELETE_PRELIM_SUCCESS,
+    EDIT_PRELIM_FAILURE,
+    EDIT_PRELIM_REQUEST,
+    EDIT_PRELIM_SUCCESS,
     FETCH_ALL_PRELIMS_FAILURE,
     FETCH_ALL_PRELIMS_REQUEST,
     FETCH_ALL_PRELIMS_SUCCESS,
@@ -44,9 +50,11 @@ function isPostingReducer(state = false, action) {
 function postSuccessReducer(state = false, action) {
     switch (action.type) {
         case CREATE_PRELIM_SUCCESS:
+        case DELETE_PRELIM_REQUEST:
             return true;
         case CREATE_PRELIM_REQUEST:
         case CREATE_PRELIM_FAILURE:
+        case DELETE_PRELIM_SUCCESS:
             return false;
         default:
             return state;
@@ -56,8 +64,12 @@ function postSuccessReducer(state = false, action) {
 function errorReducer(state = null, action) {
     switch (action.type) {
         case FETCH_ALL_PRELIMS_REQUEST:
+        case EDIT_PRELIM_REQUEST:
+        case DELETE_PRELIM_REQUEST:
             return null;
         case FETCH_ALL_PRELIMS_FAILURE:
+        case EDIT_PRELIM_FAILURE:
+        case DELETE_PRELIM_FAILURE:
             return action.error;
         default:
             return state;
@@ -68,6 +80,8 @@ function prelimsReducer(state = {}, action) {
     switch (action.type) {
         case FETCH_ALL_PRELIMS_SUCCESS:
             return convertArrToObj(action.payload);
+        case EDIT_PRELIM_SUCCESS:
+            return updateObj(state, action.payload.prelim.id, action.payload.prelim);
         default:
             return state;
     }
