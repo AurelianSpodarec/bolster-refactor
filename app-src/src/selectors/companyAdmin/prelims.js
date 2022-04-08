7acb3dee9cc3220d8fdae4201034f9943d7bea78
Viktoriia Petrova <viktoriia@silverchip.com>
2022-04-08 09:21:09 +0100

    Add prelims selectors

--- app-src/src/selectors/companyAdmin/prelims.js
+++ app-src/src/selectors/companyAdmin/prelims.js
@@ -2,3 +2,7 @@ export const selectPrelimsArr = state =>
     Object.values(state.companyAdmin.prelimsReducer.prelims || {});
 export const selectPrelimsIsFetching = state => state.companyAdmin.prelimsReducer.isFetching;
 export const selectPrelimsFetchError = state => state.companyAdmin.prelimsReducer.error;
+
+export const selectPrelimIsPosting = state => state.companyAdmin.prelimsReducer.isPosting;
+export const selectPrelimPostError = state => state.companyAdmin.prelimsReducer.postError;
+export const selectPrelimPostSuccess = state => state.companyAdmin.prelimsReducer.postSuccess;
