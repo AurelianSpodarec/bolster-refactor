74ed8b7324c91c72c6453d2deb31188e2fea3764
Mark Baker <mark@silverchip.com>
2022-04-08 14:43:20 +0100

    add flex wrapper component

---
+++ app-src/src/components/shared/generic/flexWrapper/FlexWrapper.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const FlexWrapper = ({
+    children,
+    direction = 'row', // row, row-reverse, column, column-reverse
+    wrap = 'nowrap', // wrap, nowrap
+    justify = 'start', // start, end, center, around, between, evenly
+    align = 'stretch', // start, end, center, stretch, baseline
+    gap = 0,
+    width = 12, // number ranging from 1 to 12
+    extraClasses = '',
+}) => {
+    const flexDirection = `flex-${direction}`;
+    const flexWrap = `flex-${wrap}`;
+    const flexJustify = `justify-${justify}`;
+    const flexAlign = `align-${align}`;
+    const blockWidth = `width-${width}`;
+
+    return (
+        <div
+            className={`${flexDirection} ${flexWrap} ${flexJustify} ${flexAlign} ${blockWidth} ${extraClasses}`}
+            style={{ gap }}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default FlexWrapper;
