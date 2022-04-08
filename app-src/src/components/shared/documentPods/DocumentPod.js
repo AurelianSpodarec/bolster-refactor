181b5d0c52f4af2f6d68a83c26f4d9b61268903f
Mark Baker <mark@silverchip.com>
2022-04-08 15:38:22 +0100

    add initial styles for document pods

--- app-src/src/components/shared/documentPods/DocumentPod.js
+++ app-src/src/components/shared/documentPods/DocumentPod.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import MockImage from '_content/images/previews/light-theme-preview.jpg';
+
 const DocumentPod = () => {
-    return <div>Document pod...</div>;
+    return (
+        <a className="document-pod" href="#">
+            <div className="image-wrapper">
+                <img alt="Document preview" src={MockImage} />
+            </div>
+
+            <div className="info-wrapper">
+                <p className="title">## Name of document ##</p>
+                <p className="last-updated">Updated ## 12/01/2021 - 14:14 ##</p>
+            </div>
+        </a>
+    );
 };
 
 export default DocumentPod;
