5f05f069bad6ac81ad34cbdca9774d481d9eca6a
Viktoriia Petrova <viktoriia@silverchip.com>
2022-04-12 16:40:54 +0100

    Refactor

--- app-src/src/components/companyAdmin/pinOptions/optionSets/hooks/useEditOptionSet.js
+++ app-src/src/components/companyAdmin/pinOptions/optionSets/hooks/useEditOptionSet.js
@@ -1,30 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { ERROR_MODAL } from 'constants/shared/modalTypes';
-import { useForm, usePrevious } from 'helpers/hooks';
+import { useForm } from 'helpers/hooks';
 import { formatCheckboxListOptions } from 'helpers/generic';
-
 import editPinOptionSet from 'actions/companyAdmin/pinOptions/async/editPinOptionSet';
-import showModal from 'actions/shared/generic/modals/sync/showModal';
-import hideModal from 'actions/shared/generic/modals/sync/hideModal';
-import {
-    selectPinOptionSetsIsPosting,
-    selectPinOptionSetsPostError,
-    selectPinOptionSetsPostSuccess,
-} from 'selectors/companyAdmin/pinOptionSets';
+import { selectPinOptionSetsIsPosting } from 'selectors/companyAdmin/pinOptionSets';
 import { selectServicesArr } from 'selectors/companyAdmin/services';
 
 const useEditOptionSet = set => {
     const dispatch = useDispatch();
     const isPosting = useSelector(selectPinOptionSetsIsPosting);
-    const postError = useSelector(selectPinOptionSetsPostError);
-    const postSuccess = useSelector(selectPinOptionSetsPostSuccess);
-
     const services = useSelector(selectServicesArr);
     const serviceOptions = formatCheckboxListOptions(services);
-
-    const prevProps = usePrevious({ postError, postSuccess });
 
     const [form, handleChange] = useForm({
         name: set.name,
@@ -34,14 +21,6 @@ const useEditOptionSet = set => {
     const handleSubmit = () => {
         dispatch(editPinOptionSet(set.id, form));
     };
-
-    useEffect(() => {
-        if (postError && !prevProps.postError) dispatch(showModal(ERROR_MODAL));
-    }, [postError, prevProps.postError]);
-
-    useEffect(() => {
-        if (postSuccess && !prevProps.postSuccess) dispatch(hideModal());
-    }, [postSuccess, prevProps.postSuccess]);
 
     return { form, handleChange, handleSubmit, isPosting, serviceOptions };
 };
