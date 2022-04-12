571c2056a8f41c9f77003f31eaf5a96ebc2cdd6e
Mark Baker <mark@silverchip.com>
2022-04-12 12:24:03 +0100

    update enable / disable functionality to be a switch on sets

--- app-src/src/components/companyAdmin/pinOptions/optionSets/OptionSetsListItem.js
+++ app-src/src/components/companyAdmin/pinOptions/optionSets/OptionSetsListItem.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import ActionMenu from 'components/shared/actionMenu/ActionMenu';
 import ActionMenuActionButton from 'components/shared/actionMenu/ActionMenuActionButton';
 import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
+import CheckboxContainer from 'components/shared/generic/form/containers/CheckboxContainer';
+import FlexWrapper from 'components/shared/generic/flexWrapper/FlexWrapper';
 
 const OptionSetsListItem = ({
     set,
@@ -15,24 +17,27 @@ const OptionSetsListItem = ({
     disableOptionSet,
 }) => (
     <tr>
-        <td className="row-link">
-            <Link to={`/company/pin-options/${setLink}/${id}`}>{name}</Link>
+        <td className="row-link w-checkbox">
+            <FlexWrapper justify="start" align="center">
+                <CheckboxContainer
+                    text=""
+                    name={`pin-set-checkbox-${id}`}
+                    checked={!isDisabled}
+                    handleChange={(_, value) => {
+                        if (value) {
+                            enableOptionSet(set);
+                        } else {
+                            disableOptionSet(set);
+                        }
+                    }}
+                />
+                <Link to={`/company/pin-options/${setLink}/${id}`}>{name}</Link>
+            </FlexWrapper>
         </td>
         <td>
             <ButtonWrapper alignment="right">
                 <ActionMenu>
                     <ActionMenuActionButton text="Edit" onClick={() => showEditModal(set)} />
-                    <ActionMenuActionButton
-                        text={isDisabled ? 'Enable' : 'Disable'}
-                        onClick={() => {
-                            if (isDisabled) {
-                                enableOptionSet(set);
-                                return;
-                            }
-
-                            disableOptionSet(set);
-                        }}
-                    />
                     <ActionMenuActionButton
                         text="Delete"
                         onClick={() => showDeleteModal(set)}
