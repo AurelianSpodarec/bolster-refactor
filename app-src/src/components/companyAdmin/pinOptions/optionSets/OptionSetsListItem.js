2f903e54dbfb2b039e78a9f813a5982222ebc323
Viktoriia Petrova <viktoriia@silverchip.com>
2022-04-13 17:05:19 +0100

    Add set as default button

--- app-src/src/components/companyAdmin/pinOptions/optionSets/OptionSetsListItem.js
+++ app-src/src/components/companyAdmin/pinOptions/optionSets/OptionSetsListItem.js
@@ -6,6 +6,8 @@ import ActionMenuActionButton from 'components/shared/actionMenu/ActionMenuActio
 import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 import CheckboxContainer from 'components/shared/generic/form/containers/CheckboxContainer';
 import FlexWrapper from 'components/shared/generic/flexWrapper/FlexWrapper';
+import setOptionSetAsDefault from 'actions/companyAdmin/pinOptions/async/setOptionSetAsDefault';
+import { useDispatch } from 'react-redux';
 
 const OptionSetsListItem = ({
     set,
@@ -15,38 +17,46 @@ const OptionSetsListItem = ({
     showDeleteModal,
     enableOptionSet,
     disableOptionSet,
-}) => (
-    <tr>
-        <td className="row-link w-checkbox">
-            <FlexWrapper justify="start" align="center">
-                <CheckboxContainer
-                    text=""
-                    name={`pin-set-checkbox-${id}`}
-                    checked={!isDisabled}
-                    handleChange={(_, value) => {
-                        if (value) {
-                            enableOptionSet(set);
-                        } else {
-                            disableOptionSet(set);
-                        }
-                    }}
-                />
-                <Link to={`/company/pin-options/${setLink}/${id}`}>{name}</Link>
-            </FlexWrapper>
-        </td>
-        <td>
-            <ButtonWrapper alignment="right">
-                <ActionMenu>
-                    <ActionMenuActionButton text="Edit" onClick={() => showEditModal(set)} />
-                    <ActionMenuActionButton
-                        text="Delete"
-                        onClick={() => showDeleteModal(set)}
-                        isNegative
+}) => {
+    const dispatch = useDispatch();
+    console.log(set);
+    return (
+        <tr>
+            <td className="row-link w-checkbox">
+                <FlexWrapper justify="start" align="center">
+                    <CheckboxContainer
+                        text=""
+                        name={`pin-set-checkbox-${id}`}
+                        checked={!isDisabled}
+                        handleChange={(_, value) => {
+                            if (value) {
+                                enableOptionSet(set);
+                            } else {
+                                disableOptionSet(set);
+                            }
+                        }}
                     />
-                </ActionMenu>
-            </ButtonWrapper>
-        </td>
-    </tr>
-);
+                    <Link to={`/company/pin-options/${setLink}/${id}`}>{name}</Link>
+                </FlexWrapper>
+            </td>
+            <td>
+                <ButtonWrapper alignment="right">
+                    <ActionMenu>
+                        <ActionMenuActionButton text="Edit" onClick={() => showEditModal(set)} />
+                        <ActionMenuActionButton
+                            text="Delete"
+                            onClick={() => showDeleteModal(set)}
+                            isNegative
+                        />
+                        <ActionMenuActionButton
+                            text="Set as default"
+                            onClick={() => dispatch(setOptionSetAsDefault(set))}
+                        />
+                    </ActionMenu>
+                </ButtonWrapper>
+            </td>
+        </tr>
+    );
+};
 
 export default OptionSetsListItem;
