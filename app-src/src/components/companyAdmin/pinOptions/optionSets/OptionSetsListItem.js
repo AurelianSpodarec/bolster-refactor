6bd81caa914c7e99279c182839725b489b977702
Mark Baker <mark@silverchip.com>
2022-04-14 15:34:45 +0100

    clean up

--- app-src/src/components/companyAdmin/pinOptions/optionSets/OptionSetsListItem.js
+++ app-src/src/components/companyAdmin/pinOptions/optionSets/OptionSetsListItem.js
@@ -8,27 +8,6 @@ import ButtonWrapper from 'components/shared/generic/button/presentational/Butto
 import CheckboxContainer from 'components/shared/generic/form/containers/CheckboxContainer';
 import FlexWrapper from 'components/shared/generic/flexWrapper/FlexWrapper';
 import setOptionSetAsDefault from 'actions/companyAdmin/pinOptions/async/setOptionSetAsDefault';
-import ButtonMultiDropdown from 'components/shared/filters/ButtonMultiDropdown';
-
-const options = [
-    {
-        text: 'opt 1',
-        value: 1,
-        isDisabled: false,
-    },
-    {
-        text: 'opt 2',
-        value: 2,
-        isDisabled: false,
-    },
-    {
-        text: 'opt 3',
-        value: 3,
-        isDisabled: false,
-    },
-];
-
-const selectedOptions = [];
 
 const OptionSetsListItem = ({
     set,
