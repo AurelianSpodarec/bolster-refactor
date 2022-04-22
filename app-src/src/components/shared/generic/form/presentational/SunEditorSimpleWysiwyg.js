99d7b87a8f25ca8dac997574a6b58f452e56c798
Seb Smith <seb@silverchip.com>
2022-04-22 08:58:05 +0100

    Fixes wysiwyg formatting from word

--- app-src/src/components/shared/generic/form/presentational/SunEditorSimpleWysiwyg.js
+++ app-src/src/components/shared/generic/form/presentational/SunEditorSimpleWysiwyg.js
@@ -10,16 +10,30 @@ const SunEditorSimpleWysiwyg = ({
     buttonOptions = [['formatBlock'], ['bold', 'italic', 'underline'], ['link', 'list']],
     formatOptions = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote'],
 }) => {
+    const cleanPaste = html => {
+        html = html.replace(
+            /<(\/)*(\\?xml:|meta|link|span|font|del|ins|st1:|[ovwxp]:)((.|\s)*?)>/gi,
+            '',
+        );
+        html = html.replace(/(class|style|type|start)=("(.*?)"|(\w*))/gi, '');
+        html = html.replace(/<style(.*?)style>/gi, '');
+        html = html.replace(/<script(.*?)script>/gi, '');
+        html = html.replace(/<!--(.*?)-->/gi, '');
+
+        return html;
+    };
+
     return (
         <SunEditor
             name={name}
             setContents={value}
-            onChange={onChange}
+            onChange={val => onChange(val)}
             setOptions={{
                 height: 400,
                 buttonList: buttonOptions,
                 formats: formatOptions,
             }}
+            onPaste={(_, html) => cleanPaste(html)}
         />
     );
 };
