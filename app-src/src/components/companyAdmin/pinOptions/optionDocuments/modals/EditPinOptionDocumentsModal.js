ee5f91bdad1ff9f9bda787819a095b515fd46862
Mark Baker <mark@silverchip.com>
2022-04-26 15:37:17 +0100

    update text in edit modal

--- app-src/src/components/companyAdmin/pinOptions/optionDocuments/modals/EditPinOptionDocumentsModal.js
+++ app-src/src/components/companyAdmin/pinOptions/optionDocuments/modals/EditPinOptionDocumentsModal.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import ModalOuterContainer from 'components/shared/generic/modals/containers/ModalOuterContainer';
+import useEditPinOptionDocument from '../hooks/useEditPinOptionDocument';
 
+import ModalOuterContainer from 'components/shared/generic/modals/containers/ModalOuterContainer';
 import Form from 'components/shared/generic/form/containers/Form';
 import Field from 'components/shared/generic/form/presentational/Field';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
@@ -9,22 +10,18 @@ import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/pr
 import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
 import ModalHeading from 'components/shared/generic/modals/presentational/ModalHeading';
-import useEditPinOptionDocument from '../hooks/useEditPinOptionDocument';
 import FileUploadContainer from 'components/shared/generic/form/containers/FileUploadContainer';
-import hideModal from 'actions/shared/generic/modals/sync/hideModal';
-import { useDispatch } from 'react-redux';
 
-const EditPinOptionDocumentsModal = ({ documentsVersion }) => {
-    const dispatch = useDispatch();
+const EditPinOptionDocumentsModal = ({ hideModal, documentsVersion }) => {
     const { form, handleChange, isPosting, handleSubmit } =
         useEditPinOptionDocument(documentsVersion);
 
     return (
         <ModalOuterContainer hideCloseButton>
-            <ModalHeading title="" />
+            <ModalHeading title="Edit File" />
 
             <Form onSubmit={handleSubmit} className="generic-form size-lg-12">
-                <Field name="Upload Files" required>
+                <Field name="" required>
                     <FileUploadContainer
                         value={form.documentS3Key}
                         name="documentS3Key"
@@ -49,15 +46,12 @@ const EditPinOptionDocumentsModal = ({ documentsVersion }) => {
                     <ButtonWrapper alignment="right">
                         <ActionButton
                             text="Cancel"
-                            onClick={() => dispatch(hideModal())}
+                            onClick={hideModal}
                             source="secondary"
                             size="medium"
                         />
                         <ActionButton
                             text="Confirm"
-                            icon={isPosting ? 'spinner' : 'save'}
-                            iconSpin={isPosting}
-                            ambient="positive"
                             size="medium"
                             disabled={isPosting}
                             type="submit"
