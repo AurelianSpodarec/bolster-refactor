c8e48a08800b5f4defeda9dd9b4d461e62f1c38a
Nick Barnard <nick@silverchip.com>
2022-04-26 10:13:20 +0100

    Add C&E Summary item

--- app-src/src/components/companyAdmin/costingAndEstimating/CostingCart.js
+++ app-src/src/components/companyAdmin/costingAndEstimating/CostingCart.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import BlockContainer from '../../shared/generic/block/containers/BlockContainer';
+import CostingCartSummaryItem from './CostingCartSummaryItem';
 
 const CostingCart = () => {
     return (
         <div className="costing-cart">
             <BlockContainer>
                 <h2>Costing cart</h2>
+
+                <CostingCartSummaryItem />
 
                 <div className="grand-total">
                     <h3>Total exc VAT:</h3>
--- app-src/src/components/companyAdmin/costingAndEstimating/CostingCartSummaryItem.js
+++ app-src/src/components/companyAdmin/costingAndEstimating/CostingCartSummaryItem.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import AccordionButton from 'components/shared/generic/button/presentational/AccordionButton';
+import FlexWrapper from 'components/shared/generic/flexWrapper/FlexWrapper';
+import React, { useState } from 'react';
 
 const CostingCartSummaryItem = () => {
-    return <div className="summary-item"></div>;
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    return (
+        <div className="summary-item">
+            <FlexWrapper direction="row" justify="between" align="center">
+                <h4>Title</h4>
+                <div>
+                    <AccordionButton
+                        active={isExpanded}
+                        onClick={() => setIsExpanded(!isExpanded)}
+                    />
+                </div>
+            </FlexWrapper>
+        </div>
+    );
 };
 
 export default CostingCartSummaryItem;
