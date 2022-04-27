32ca7fc5553e7d798b9ef84062ead98e7496d162
Seb Smith <seb@silverchip.com>
2022-04-27 11:38:49 +0100

    Styles calendar button

--- app-src/src/components/companyAdmin/costingAndEstimating/CostingAndEstimatingGraph.js
+++ app-src/src/components/companyAdmin/costingAndEstimating/CostingAndEstimatingGraph.js
@@ -5,6 +5,7 @@ import { Line } from 'react-chartjs-2';
 import BlockContainer from '../../shared/generic/block/containers/BlockContainer';
 import useCostingAndEstimatingGraph from './_hooks/useCostingAndEstimatingGraph';
 import DateRangePicker from '../../shared/generic/form/presentational/DateRangePicker';
+import FlexWrapper from '../../shared/generic/flexWrapper/FlexWrapper';
 
 const CostingAndEstimatingGraph = ({ graph, filterFormData, onChange }) => {
     const { data, options } = useCostingAndEstimatingGraph(graph);
@@ -13,9 +14,17 @@ const CostingAndEstimatingGraph = ({ graph, filterFormData, onChange }) => {
     return (
         <div className="graph-wrapper">
             <BlockContainer contentClass="border">
-                <h3>Graph</h3>
+                <FlexWrapper extraClasses="graph-filters">
+                    <div className="calendar-select border">
+                        <DateRangePicker
+                            name="dateRange"
+                            value={dateRange}
+                            onChange={onChange}
+                            text="Calendar"
+                        />
+                    </div>
+                </FlexWrapper>
 
-                <DateRangePicker name="dateRange" value={dateRange} onChange={onChange} />
                 <Line data={data} options={options} />
                 <div className="graph-total">
                     <h3>Total:</h3>
