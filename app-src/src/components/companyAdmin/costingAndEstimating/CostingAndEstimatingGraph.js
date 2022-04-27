442b675720123dc06101fdf6398aa910d2d8dcc7
Nick Barnard <nick@silverchip.com>
2022-04-27 10:16:20 +0100

    Add DateRangePicker component

--- app-src/src/components/companyAdmin/costingAndEstimating/CostingAndEstimatingGraph.js
+++ app-src/src/components/companyAdmin/costingAndEstimating/CostingAndEstimatingGraph.js
@@ -1,16 +1,29 @@
 import { formatCurrency } from 'helpers/generic';
-import React from 'react';
+import moment from 'moment';
+import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import BlockContainer from '../../shared/generic/block/containers/BlockContainer';
 import useCostingAndEstimatingGraph from './_hooks/useCostingAndEstimatingGraph';
+import DateRangePicker from '../../shared/generic/form/presentational/DateRangePicker';
 
 const CostingAndEstimatingGraph = ({ graph }) => {
     const { data, options } = useCostingAndEstimatingGraph(graph);
+
+    const [dateRange, setDateRange] = useState({
+        startDate: moment().subtract(7, 'days').toDate(),
+        endDate: moment().toDate(),
+    });
+
+    const _onChange = (name, value) => {
+        setDateRange(value);
+    };
 
     return (
         <div className="graph-wrapper">
             <BlockContainer contentClass="border">
                 <h3>Graph</h3>
+
+                <DateRangePicker name="daterange" value={dateRange} onChange={_onChange} />
                 <Line data={data} options={options} />
                 <div className="graph-total">
                     <h3>Total:</h3>
