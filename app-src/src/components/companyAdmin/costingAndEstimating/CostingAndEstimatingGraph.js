7874043442c66a6531ea20e7aca22de990b4587b
Seb Smith <seb@silverchip.com>
2022-04-27 12:55:12 +0100

    Adds more controls

--- app-src/src/components/companyAdmin/costingAndEstimating/CostingAndEstimatingGraph.js
+++ app-src/src/components/companyAdmin/costingAndEstimating/CostingAndEstimatingGraph.js
@@ -10,19 +10,36 @@ import FlexWrapper from '../../shared/generic/flexWrapper/FlexWrapper';
 const CostingAndEstimatingGraph = ({ graph, filterFormData, onChange }) => {
     const { data, options } = useCostingAndEstimatingGraph(graph);
     const { dateRange } = filterFormData;
-
+    console.log(dateRange);
     return (
         <div className="graph-wrapper">
             <BlockContainer contentClass="border">
                 <FlexWrapper extraClasses="graph-filters">
-                    <div className="calendar-select border">
-                        <DateRangePicker
-                            name="dateRange"
-                            value={dateRange}
-                            onChange={onChange}
-                            text="Calendar"
-                        />
-                    </div>
+                    <FlexWrapper align="center" justify="between" width={4}>
+                        <div className="date-period-buttons">
+                            <button>
+                                <i className="far fa-chevron-left" />
+                            </button>
+                            <button>
+                                <i className="far fa-chevron-right" />
+                            </button>
+                        </div>
+
+                        <p>last 7 days</p>
+
+                        <div className="calendar-select border">
+                            <DateRangePicker
+                                name="dateRange"
+                                value={dateRange}
+                                onChange={onChange}
+                                text="Calendar"
+                            />
+                        </div>
+                    </FlexWrapper>
+
+                    <FlexWrapper align="center" justify="end" width={8}>
+                        <p>Filter</p>
+                    </FlexWrapper>
                 </FlexWrapper>
 
                 <Line data={data} options={options} />
