3da5c00570e0891710c0a39ddb49294897ba8ab9
Nick Barnard <nick@silverchip.com>
2022-04-27 11:10:45 +0100

    Plug filters into Graph

--- app-src/src/components/companyAdmin/costingAndEstimating/_hooks/useCostingAndEstimatingFilters.js
+++ app-src/src/components/companyAdmin/costingAndEstimating/_hooks/useCostingAndEstimatingFilters.js
@@ -1,3 +1,16 @@
-const useCostingAndEstimatingFilters = () => {};
+import { useForm } from 'helpers/hooks';
+import moment from 'moment';
+
+const useCostingAndEstimatingFilters = () => {
+    const initialFormData = {
+        dateRange: {
+            startDate: moment().subtract(7, 'days').toDate(),
+            endDate: moment().toDate(),
+        },
+    };
+    const [formData, onChange] = useForm(initialFormData);
+
+    return { filterFormData: formData, onChange };
+};
 
 export default useCostingAndEstimatingFilters;
