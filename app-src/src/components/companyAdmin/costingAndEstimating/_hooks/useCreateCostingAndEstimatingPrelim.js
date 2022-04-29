29727d6773a4c7c43254baf030ac1bafbfdf3a10
Viktoriia Petrova <viktoriia@silverchip.com>
2022-04-29 14:35:56 +0100

    Add CreateCostingAndEstimatingPrelim

---
+++ app-src/src/components/companyAdmin/costingAndEstimating/_hooks/useCreateCostingAndEstimatingPrelim.js
@@ -0,0 +1,25 @@
+import { useForm } from 'helpers/hooks';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const useCreateCostingAndEstimatingPrelim = () => {
+    const dispatch = useDispatch();
+
+    const isPosting = false;
+    const [form, handleChange] = useForm({
+        description: '',
+        amount: null,
+    });
+
+    const handleSubmit = () => {
+        const postBody = {
+            ...form,
+        };
+    };
+
+    useEffect(() => {}, []);
+
+    return { form, handleChange, handleSubmit, isPosting };
+};
+
+export default useCreateCostingAndEstimatingPrelim;
