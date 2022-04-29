ff3b48f53eb21975cc54021890c2cc72e5a27ad7
Rob Fairclough <rob@silverchip.com>
2022-04-29 16:22:30 +0100

    temp styles for measurement unit secltro

--- app-src/src/actions/companyAdmin/drawings/sync/updateDrawingDropdownOptions.js
+++
@@ -1,7 +0,0 @@
-import { UPDATE_DRAWING_PIN_OPTION_TYPES } from 'constants/actionTypes/drawings';
-
-export default options => dispatch =>
-    dispatch({
-        type: UPDATE_DRAWING_PIN_OPTION_TYPES,
-        options,
-    });
