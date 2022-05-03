e58aa4c5cf727366a2c89d10f9d52f1e14b2844f
Seb Smith <seb@silverchip.com>
2022-05-03 14:10:42 +0100

    Adds error handling to option pods

---
+++ app-src/src/components/shared/generic/form/containers/OptionPodContainer.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import OptionPod from '../presentational/OptionPod';
+import { useDispatch } from 'react-redux';
+import addFieldError from '../../../../../actions/shared/generic/fieldErrors/sync/addFieldError';
+import removeFieldError from '../../../../../actions/shared/generic/fieldErrors/sync/removeFieldError';
+import { usePrevious } from '../../../../../helpers/hooks';
+
+const OptionPodContainer = ({
+    name,
+    checked,
+    onChange,
+    icon = '',
+    svgIconComponent: SvgIconComponent,
+    pathStroke = false,
+    error,
+    errorsVisible = true,
+    required,
+}) => {
+    const dispatch = useDispatch();
+    const prevChecked = usePrevious(checked);
+
+    const errorMessage = errorsVisible ? error : null;
+
+    useEffect(() => {
+        validate();
+    }, []);
+
+    useEffect(() => {
+        if (checked !== prevChecked) validate();
+    }, []);
+
+    const validate = () => {
+        if (required && !checked) {
+            dispatch(addFieldError(name, 'This is a required field.'));
+        } else if (error) {
+            dispatch(removeFieldError(name));
+        }
+    };
+
+    return (
+        <OptionPod
+            checked={checked}
+            onChange={onChange}
+            name={name}
+            icon={icon}
+            pathStroke={pathStroke}
+            error={errorMessage}
+            errorsVisible={errorsVisible}
+            svgIconComponent={SvgIconComponent}
+        />
+    );
+};
+
+export default OptionPodContainer;
