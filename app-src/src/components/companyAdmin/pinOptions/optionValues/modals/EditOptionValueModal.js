f7c8e8ab294df067079299c8f3811ac6fafd92d0
Rob Fairclough <rob@silverchip.com>
2022-05-03 18:58:10 +0100

    allow edit cost measurement type

--- app-src/src/components/companyAdmin/pinOptions/optionValues/modals/EditOptionValueModal.js
+++ app-src/src/components/companyAdmin/pinOptions/optionValues/modals/EditOptionValueModal.js
@@ -91,7 +91,6 @@ const EditOptionValueModal = ({ option }) => {
                                         measurementDropdownOptions[form.costMeasurementType]
                                     }
                                     handleChange={handleChange}
-                                    withoutPlaceholder
                                     required
                                 />
                             </Field>
