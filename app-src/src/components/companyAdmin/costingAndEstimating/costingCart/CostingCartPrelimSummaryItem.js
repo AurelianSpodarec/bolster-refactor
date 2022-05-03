fb684608b95e08368b44ade4f155b5772e6e1f32
Viktoriia Petrova <viktoriia@silverchip.com>
2022-05-03 11:36:37 +0100

    Add ActionMenu to prelims

--- app-src/src/components/companyAdmin/costingAndEstimating/costingCart/CostingCartPrelimSummaryItem.js
+++ app-src/src/components/companyAdmin/costingAndEstimating/costingCart/CostingCartPrelimSummaryItem.js
@@ -1,3 +1,5 @@
+import ActionMenu from 'components/shared/actionMenu/ActionMenu';
+import ActionMenuActionButton from 'components/shared/actionMenu/ActionMenuActionButton';
 import FlexWrapper from 'components/shared/generic/flexWrapper/FlexWrapper';
 import { formatCurrency } from 'helpers/generic';
 import React from 'react';
@@ -6,9 +8,15 @@ const CostingCartPrelimSummaryItem = ({ prelim }) => {
     const { name, markup, cost } = prelim;
 
     return (
-        <FlexWrapper direction="row" justify="between" align="center">
+        <FlexWrapper direction="row" justify="between" align="center" width="12">
             <span>{name}</span>
-            <span>{cost ? `£${formatCurrency(cost)}` : `${markup}%`}</span>
+            <FlexWrapper direction="row" justify="end" align="center" width="3">
+                <span>{cost ? `£${formatCurrency(cost)}` : `${markup}%`}</span>
+                <ActionMenu>
+                    <ActionMenuActionButton text="Edit" onClick={() => {}} />
+                    <ActionMenuActionButton text="Delete" onClick={() => {}} isNegative />
+                </ActionMenu>
+            </FlexWrapper>
         </FlexWrapper>
     );
 };
