5dc2c08d9ef94174c2b10f23a22e5c21fac0b5fe
Viktoriia Petrova <viktoriia@silverchip.com>
2022-05-04 08:51:57 +0100

    pull

---
+++ app-src/src/components/companyAdmin/costingAndEstimating/_hooks/useAddExistingPrelim.js
@@ -0,0 +1,75 @@
+import { useForm, usePrevious } from 'helpers/hooks';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import useCurrentHierarchyID from './useCurrentHierarchyID';
+import useCurrentHierarchyType from './useCurrentHierarchyType';
+
+import fetchAllPrelims from 'actions/companyAdmin/prelims/async/fetchAllPrelims';
+import linkPrelim from '../../../../actions/companyAdmin/costingAndEstimating/linkPrelim';
+
+import { selectPrelimsArr } from 'selectors/companyAdmin/prelims';
+
+import { PRELIMS_ENUM } from 'constants/companyAdmin/enums';
+import { convertArrToObj, formatCurrency } from 'helpers/generic';
+import { selectJWTData } from '../../../../selectors/shared/decodeJWT';
+import { hideModal } from '../../../../actions/shared/generic/modals/sync/hideModal';
+import { selectCostingAndEstimatingPostSuccess } from 'selectors/companyAdmin/costingAndEstimating';
+import fetchCostingAndEstimatingCart from 'actions/companyAdmin/costingAndEstimating/fetchCostingAndEstimatingCart';
+
+const useAddExistingPrelim = ({ cAndEPostBody }) => {
+    const dispatch = useDispatch();
+
+    const hierarchyID = useCurrentHierarchyID();
+    const hierarchyType = useCurrentHierarchyType();
+
+    const { companyID } = useSelector(selectJWTData);
+    const postSuccess = useSelector(selectCostingAndEstimatingPostSuccess);
+    const prevPostSuccess = usePrevious(postSuccess);
+
+    const formatArrForDropdown = arr => {
+        const options = arr
+            .filter(val => val)
+            .map(({ name, id, type, value }) => ({
+                value: id,
+                text: `${name} - (${
+                    PRELIMS_ENUM[type] === 'Percent' ? value + '%' : '£' + formatCurrency(value)
+                })`,
+            }));
+
+        return convertArrToObj(options, 'value');
+    };
+
+    const allPrelims = useSelector(selectPrelimsArr);
+    const prelimsOptions = formatArrForDropdown(allPrelims);
+    const isPosting = false;
+    const [form, handleChange] = useForm({
+        prelimID: '',
+    });
+
+    const handleSubmit = () => {
+        const postBody = { ...form, hierarchyID, hierarchyType, companyID };
+
+        dispatch(linkPrelim(postBody));
+    };
+
+    useEffect(() => {
+        dispatch(fetchAllPrelims());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (postSuccess && !prevPostSuccess) {
+            dispatch(hideModal());
+            dispatch(fetchCostingAndEstimatingCart(cAndEPostBody));
+        }
+    }, [postSuccess, prevPostSuccess]);
+
+    return {
+        form,
+        handleChange,
+        handleSubmit,
+        isPosting,
+        prelimsOptions,
+    };
+};
+
+export default useAddExistingPrelim;
--- app-src/src/components/companyAdmin/costingAndEstimating/_hooks/useCreateCostingAndEstimatingPrelim.js
+++ app-src/src/components/companyAdmin/costingAndEstimating/_hooks/useCreateCostingAndEstimatingPrelim.js
@@ -19,7 +19,7 @@ import { ERROR_MODAL } from 'constants/shared/modalTypes';
 import hideModal from 'actions/shared/generic/modals/sync/hideModal';
 import { selectPrelimPostSuccess } from '../../../../selectors/companyAdmin/prelims';
 
-const useCreateCostingAndEstimatingPrelim = () => {
+const useCreateCostingAndEstimatingPrelim = cAndEPostBody => {
     const dispatch = useDispatch();
     const prelimsOptions = convertEnumToDropdownOptions(PRELIMS_ENUM);
     const hierarchyID = useCurrentHierarchyID();
