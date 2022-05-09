011423ceca35dc016a67938becae4396730fd546
Viktoriia Petrova <viktoriia@silverchip.com>
2022-05-09 18:42:28 +0100

    Style pinPage View button

--- app-src/src/components/companyAdmin/pins/singlePin/presentational/PinInspectionLogsListItem.js
+++ app-src/src/components/companyAdmin/pins/singlePin/presentational/PinInspectionLogsListItem.js
@@ -3,8 +3,9 @@ import {
     PIN_STATUS_COLOURS as COLOURS,
     PIN_STATUS_TYPES as TYPES,
 } from 'constants/companyAdmin/enums';
-import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
 import { pin2xImgs } from '_content/images/map-markers';
+import LinkButton from 'components/shared/generic/button/presentational/LinkButton';
+import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 
 const PinInspectionLogsListItem = ({ pin, onMobile, headers }) => {
     const pinColour = COLOURS[pin.latestStatus] || 'red';
@@ -23,7 +24,15 @@ const PinInspectionLogsListItem = ({ pin, onMobile, headers }) => {
             </td>
             <td>
                 {onMobile && <span className="mobile-table-heading">{headers[2]}</span>}
-                <ButtonContainer to={`/company/pins/${pin.id}`}>View</ButtonContainer>
+                <ButtonWrapper alignment="right">
+                    <LinkButton
+                        href={`/company/pins/${pin.id}`}
+                        text="View"
+                        size="small"
+                        source="secondary"
+                        ambient="positive"
+                    />
+                </ButtonWrapper>
             </td>
         </tr>
     );
