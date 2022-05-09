a67b04e70f688d4f124e4fafe68534db88c7dffd
Viktoriia Petrova <viktoriia@silverchip.com>
2022-05-09 19:33:15 +0100

    Style View buttons

--- app-src/src/components/client/drawings/singleDrawing/presentational/DrawingInspectionLogsListItem.js
+++ app-src/src/components/client/drawings/singleDrawing/presentational/DrawingInspectionLogsListItem.js
@@ -3,8 +3,10 @@ import {
     PIN_STATUS_COLOURS as COLOURS,
     PIN_STATUS_TYPES as TYPES,
 } from 'constants/companyAdmin/enums';
-import ButtonContainer from 'components/shared/generic/button/containers/ButtonContainer';
+
 import { pin2xImgs } from '_content/images/map-markers';
+import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
+import LinkButton from 'components/shared/generic/button/presentational/LinkButton';
 
 const DrawingInspectionLogsListItem = ({ pin }) => {
     const pinColour = COLOURS[pin.latestStatus] || 'red';
@@ -17,7 +19,15 @@ const DrawingInspectionLogsListItem = ({ pin }) => {
             </td>
             <td>{TYPES[pin.latestStatus]}</td>
             <td>
-                <ButtonContainer to={`/client/pins/${pin.id}`}>View</ButtonContainer>
+                <ButtonWrapper alignment="right">
+                    <LinkButton
+                        href={`/client/pins/${pin.id}`}
+                        text="View"
+                        size="small"
+                        source="secondary"
+                        ambient="positive"
+                    />
+                </ButtonWrapper>
             </td>
         </tr>
     );
