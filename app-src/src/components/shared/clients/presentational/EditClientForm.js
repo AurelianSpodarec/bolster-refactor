cc57aba7ad1a65d862b9d4d08c19736614c74c8d
Sewb <seb@silverchip.com>
2022-05-10 10:29:36 +0100

    Adds updates buttons to client access

--- app-src/src/components/shared/clients/presentational/EditClientForm.js
+++ app-src/src/components/shared/clients/presentational/EditClientForm.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
 import Form from 'components/shared/generic/form/containers/Form';
-import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 import Field from 'components/shared/generic/form/presentational/Field';
 import CheckboxListContainer from 'components/shared/generic/form/containers/CheckboxListContainer';
+import ButtonWrapper from '../../generic/button/presentational/ButtonWrapper';
+import ActionButton from '../../generic/button/presentational/ActionButton';
 
 const EditClientForm = ({
     handleSubmit,
@@ -26,24 +27,27 @@ const EditClientForm = ({
             />
         </Field>
 
-        <BlockButtonWrapper>
-            {showMoreServicesMesssage && (
-                <p className="generic-text size-lg-12 switched">
-                    Looking for more service types? Check your{' '}
-                    <Link to="/company/subscription" className="switched">
-                        subscriptions
-                    </Link>{' '}
-                    to add more!
-                </p>
-            )}
-
-            <button type="submit" className="button green">
-                Confirm
-            </button>
-            <button type="button" className="button" onClick={() => goBack()}>
-                Cancel
-            </button>
-        </BlockButtonWrapper>
+        {showMoreServicesMesssage && (
+            <p className="generic-text size-lg-12 switched">
+                Looking for more service types? Check your{' '}
+                <Link to="/company/subscription" className="switched">
+                    subscriptions
+                </Link>{' '}
+                to add more!
+            </p>
+        )}
+        <div className="size-lg-12">
+            <ButtonWrapper alignment="right">
+                <ActionButton
+                    text="Cancel"
+                    icon="times"
+                    onClick={goBack}
+                    size="small"
+                    ambient="negative"
+                />
+                <ActionButton text="Confirm" icon="save" size="small" type="submit" />
+            </ButtonWrapper>
+        </div>
     </Form>
 );
 
