e92586a4349471224f958fdaaed10eb53d063a16
Viktoriia Petrova <viktoriia@silverchip.com>
2022-05-10 21:23:27 +0100

    Move Create alerts and View alerts to the top

--- app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/SingleDrawing.js
+++ app-src/src/components/companyAdmin/drawings/singleDrawing/presentational/SingleDrawing.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import SingleDrawingHeaderContainer from '../containers/SingleDrawingHeaderContainer';
 import SingleDrawingRouteContainer from '../containers/SingleDrawingRouteContainer';
+import CreateAndViewPinButtons from './CreateAndViewPinButtons';
 
 const SingleDrawing = () => (
     <>
         <SingleDrawingHeaderContainer />
+        <CreateAndViewPinButtons />
         <SingleDrawingRouteContainer />
     </>
 );
