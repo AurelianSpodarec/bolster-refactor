fcf3ac2a7ab1c178dd03882d10a861f329181eba
Rob Fairclough <rob@silverchip.com>
2022-05-17 08:40:34 +0100

    bug fixing in costing and estimating component

---
+++ app-src/src/components/companyAdmin/costingAndEstimating/Costing.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import CostingAndEstimating from './CostingAndEstimating';
+
+const Costing = () => <CostingAndEstimating />;
+export default Costing;
--- app-src/src/components/companyAdmin/sites/singleSite/containers/SingleSiteRouteContainer.js
+++ app-src/src/components/companyAdmin/sites/singleSite/containers/SingleSiteRouteContainer.js
@@ -4,12 +4,14 @@ import { SITE_TABS } from 'constants/shared/tabNames';
 import SingleSiteGeneralOverview from '../presentational/SingleSiteGeneralOverview';
 import HierarchyAdvancedReport from 'components/companyAdmin/reports/createReport/components/presentational/HierarchyAdvancedReport';
 import CostingAndEstimating from '../../../costingAndEstimating/CostingAndEstimating';
+import Costing from '../../../costingAndEstimating/Costing';
+import Estimating from '../../../costingAndEstimating/Estimating';
 
 const SiteRouteContainer = ({ selectedTab }) => {
     const contentOptions = {
         [SITE_TABS.GENERAL_OVERVIEW]: SingleSiteGeneralOverview,
-        [SITE_TABS.ESTIMATING]: CostingAndEstimating,
-        [SITE_TABS.COSTING]: CostingAndEstimating,
+        [SITE_TABS.ESTIMATING]: Costing,
+        [SITE_TABS.COSTING]: Estimating,
         [SITE_TABS.GENERATE_REPORT]: HierarchyAdvancedReport,
     };
 
