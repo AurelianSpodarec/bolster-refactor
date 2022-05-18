e01bd9e3d66cc3b378288a175c256be84f4bdeb7
Mark Baker <mark@silverchip.com>
2022-05-18 13:48:58 +0100

    update prelim modals

--- app-src/src/components/companyAdmin/pinOptions/prelims/modals/EditPrelimModal.js
+++ app-src/src/components/companyAdmin/pinOptions/prelims/modals/EditPrelimModal.js
@@ -1,72 +1,70 @@
 import React from 'react';
 
-import ModalOuterContainer from 'components/shared/generic/modals/containers/ModalOuterContainer';
 import useEditPrelim from '../hooks/useEditPrelim';
+
 import Form from 'components/shared/generic/form/containers/Form';
 import Field from 'components/shared/generic/form/presentational/Field';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
-import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 import DropdownContainer from 'components/shared/generic/form/containers/DropdownContainer';
 import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
-import ModalHeading from 'components/shared/generic/modals/presentational/ModalHeading';
+import FlexModalOuter from 'components/shared/generic/modals/presentational/FlexModalOuter';
 
 const EditPrelimModal = ({ set }) => {
     const { form, handleChange, isPosting, handleSubmit, prelimsOptions } = useEditPrelim(set);
 
     return (
-        <ModalOuterContainer hideCloseButton>
-            <ModalHeading title={`Edit ${set.name}`} />
+        <FlexModalOuter title={`Edit ${set.name}`}>
+            <Form onSubmit={handleSubmit} className="generic-form flex-content-wrapper size-lg-12">
+                <div className="flex-content">
+                    <div className="form-fields-container">
+                        <Field name="Name" required>
+                            <TextInputContainer
+                                name="name"
+                                value={form.name}
+                                handleChange={handleChange}
+                                placeholder="Type name"
+                                required
+                            />
+                        </Field>
 
-            <Form onSubmit={handleSubmit} className="generic-form size-lg-12">
-                <Field name="Name" required>
-                    <TextInputContainer
-                        name="name"
-                        value={form.name}
-                        handleChange={handleChange}
-                        placeholder="Type name"
-                        required
+                        <Field name="Type" required>
+                            <DropdownContainer
+                                placeholder="Choose prelim type"
+                                name="type"
+                                options={Object.values(prelimsOptions)}
+                                value={prelimsOptions[form.type]}
+                                selectedOption={prelimsOptions[form.type]}
+                                handleChange={handleChange}
+                                required
+                            />
+                        </Field>
+
+                        <Field name="Amount" required>
+                            <TextInputContainer
+                                name="value"
+                                value={form.value}
+                                handleChange={handleChange}
+                                placeholder="Type amount"
+                                type="number"
+                                required
+                            />
+                        </Field>
+                    </div>
+                </div>
+
+                <ButtonWrapper alignment="right" extraClasses="flex-modal-footer">
+                    <ActionButton
+                        text="Save"
+                        icon={isPosting ? 'spinner' : 'save'}
+                        iconSpin={isPosting}
+                        ambient="positive"
+                        disabled={isPosting}
+                        type="submit"
                     />
-                </Field>
-
-                <Field name="Type" required>
-                    <DropdownContainer
-                        placeholder="Choose prelim type"
-                        name="type"
-                        options={Object.values(prelimsOptions)}
-                        value={prelimsOptions[form.type]}
-                        selectedOption={prelimsOptions[form.type]}
-                        handleChange={handleChange}
-                        required
-                    />
-                </Field>
-
-                <Field name="Amount" required>
-                    <TextInputContainer
-                        name="value"
-                        value={form.value}
-                        handleChange={handleChange}
-                        placeholder="Type amount"
-                        type="number"
-                        required
-                    />
-                </Field>
-
-                <BlockButtonWrapper>
-                    <ButtonWrapper alignment="right">
-                        <ActionButton
-                            text="Save"
-                            icon={isPosting ? 'spinner' : 'save'}
-                            iconSpin={isPosting}
-                            ambient="positive"
-                            size="medium"
-                            disabled={isPosting}
-                            type="submit"
-                        />
-                    </ButtonWrapper>
-                </BlockButtonWrapper>
+                </ButtonWrapper>
             </Form>
-        </ModalOuterContainer>
+        </FlexModalOuter>
     );
 };
 
