ac07854620a526b5dff169db157fe35bee1e8045
Mark Baker <mark@silverchip.com>
2022-05-18 12:03:07 +0100

    update duplicate value modal

--- app-src/src/components/companyAdmin/pinOptions/optionValues/modals/DuplicateOptionValueModal.js
+++ app-src/src/components/companyAdmin/pinOptions/optionValues/modals/DuplicateOptionValueModal.js
@@ -2,60 +2,58 @@ import React from 'react';
 
 import useDuplicateOptionValue from '../hooks/useDuplicateOptionValue';
 
-import ModalOuterContainer from 'components/shared/generic/modals/containers/ModalOuterContainer';
 import Form from 'components/shared/generic/form/containers/Form';
 import Field from 'components/shared/generic/form/presentational/Field';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
-import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
-import ModalHeading from 'components/shared/generic/modals/presentational/ModalHeading';
+import FlexModalOuter from 'components/shared/generic/modals/presentational/FlexModalOuter';
 
 const DuplicateOptionValueModal = ({ option, hideModal }) => {
     const { form, handleChange, handleSubmit, isPosting } = useDuplicateOptionValue(option);
 
     return (
-        <ModalOuterContainer hideCloseButton>
-            <ModalHeading title={`Duplicate ${option.name}?`} />
+        <FlexModalOuter title={`Duplicate ${option.name}?`}>
+            <Form onSubmit={handleSubmit} className="generic-form flex-content-wrapper size-lg-12">
+                <div className="flex-content">
+                    <div className="form-fields-container">
+                        <Field name="Name" required>
+                            <TextInputContainer
+                                name="name"
+                                value={form.name}
+                                handleChange={handleChange}
+                                placeholder="Type name"
+                                required
+                            />
+                        </Field>
 
-            <Form onSubmit={handleSubmit} className="generic-form size-lg-12">
-                <Field name="Name" required>
-                    <TextInputContainer
-                        name="name"
-                        value={form.name}
-                        handleChange={handleChange}
-                        placeholder="Type name"
-                        required
+                        <Field name="Short Name" labelClasses="small-margin">
+                            <p className="generic-text size-lg-12">
+                                This is how the pin option will output through the app.
+                            </p>
+
+                            <TextInputContainer
+                                name="shortName"
+                                value={form.shortName}
+                                handleChange={handleChange}
+                                placeholder="Type short name"
+                            />
+                        </Field>
+                    </div>
+                </div>
+
+                <ButtonWrapper alignment="right" extraClasses="flex-modal-footer">
+                    <ActionButton text="Cancel" source="secondary" onClick={hideModal} />
+                    <ActionButton
+                        text="Duplicate"
+                        icon={isPosting ? 'spinner' : 'copy'}
+                        iconSpin={isPosting}
+                        disabled={isPosting}
+                        type="submit"
                     />
-                </Field>
-
-                <Field name="Short Name" labelClasses="small-margin">
-                    <p className="generic-text size-lg-12">
-                        This is how the pin option will output through the app.
-                    </p>
-
-                    <TextInputContainer
-                        name="shortName"
-                        value={form.shortName}
-                        handleChange={handleChange}
-                        placeholder="Type short name"
-                    />
-                </Field>
-
-                <BlockButtonWrapper>
-                    <ButtonWrapper alignment="right">
-                        <ActionButton text="Cancel" source="secondary" onClick={hideModal} />
-                        <ActionButton
-                            text="Duplicate"
-                            icon={isPosting ? 'spinner' : 'copy'}
-                            iconSpin={isPosting}
-                            disabled={isPosting}
-                            type="submit"
-                        />
-                    </ButtonWrapper>
-                </BlockButtonWrapper>
+                </ButtonWrapper>
             </Form>
-        </ModalOuterContainer>
+        </FlexModalOuter>
     );
 };
 
