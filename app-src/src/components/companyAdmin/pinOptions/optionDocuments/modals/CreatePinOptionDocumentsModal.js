abefa4a4718ed63b3d1489fd1f8162741a367198
Mark Baker <mark@silverchip.com>
2022-05-18 12:10:28 +0100

    update option document modals

--- app-src/src/components/companyAdmin/pinOptions/optionDocuments/modals/CreatePinOptionDocumentsModal.js
+++ app-src/src/components/companyAdmin/pinOptions/optionDocuments/modals/CreatePinOptionDocumentsModal.js
@@ -2,63 +2,56 @@ import React from 'react';
 
 import useCreatePinOptionDocument from '../hooks/useCreatePinOptionDocument';
 
-import ModalOuterContainer from 'components/shared/generic/modals/containers/ModalOuterContainer';
 import Form from 'components/shared/generic/form/containers/Form';
 import Field from 'components/shared/generic/form/presentational/Field';
 import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
-import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
-import ModalHeading from 'components/shared/generic/modals/presentational/ModalHeading';
 import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
 import FileUploadContainer from 'components/shared/generic/form/containers/FileUploadContainer';
+import FlexModalOuter from 'components/shared/generic/modals/presentational/FlexModalOuter';
 
 const CreatePinOptionDocumentsModal = ({ hideModal, optionID }) => {
     const { form, handleChange, handleSubmit, isPosting } = useCreatePinOptionDocument(optionID);
 
     return (
-        <ModalOuterContainer hideCloseButton>
-            <ModalHeading title="Upload file" />
+        <FlexModalOuter title="Upload file">
+            <Form onSubmit={handleSubmit} className="generic-form flex-content-wrapper size-lg-12">
+                <div className="flex-content">
+                    <div className="form-fields-container">
+                        <Field name="" required>
+                            <FileUploadContainer
+                                value={form.documentS3Key}
+                                name="documentS3Key"
+                                acceptedTypes={[
+                                    'application/pdf',
+                                    'image/jpg',
+                                    'image/png',
+                                    'image/jpeg',
+                                ]}
+                                handleChange={handleChange}
+                                maxFiles={1}
+                                required
+                            />
+                        </Field>
 
-            <Form onSubmit={handleSubmit} className="generic-form size-lg-12">
-                <Field name="" required>
-                    <FileUploadContainer
-                        value={form.documentS3Key}
-                        name="documentS3Key"
-                        acceptedTypes={['application/pdf', 'image/jpg', 'image/png', 'image/jpeg']}
-                        handleChange={handleChange}
-                        maxFiles={1}
-                        required
-                    />
-                </Field>
+                        <Field name="Product Name" required>
+                            <TextInputContainer
+                                name="name"
+                                value={form.name}
+                                handleChange={handleChange}
+                                placeholder="Enter Text Here..."
+                                required
+                            />
+                        </Field>
+                    </div>
+                </div>
 
-                <Field name="Product Name" required>
-                    <TextInputContainer
-                        name="name"
-                        value={form.name}
-                        handleChange={handleChange}
-                        placeholder="Enter Text Here..."
-                        required
-                    />
-                </Field>
-
-                <BlockButtonWrapper>
-                    <ButtonWrapper alignment="right">
-                        <ActionButton
-                            text="Cancel"
-                            onClick={hideModal}
-                            source="secondary"
-                            size="medium"
-                        />
-                        <ActionButton
-                            text="Confirm"
-                            size="medium"
-                            disabled={isPosting}
-                            type="submit"
-                        />
-                    </ButtonWrapper>
-                </BlockButtonWrapper>
+                <ButtonWrapper alignment="right" extraClasses="flex-modal-footer">
+                    <ActionButton text="Cancel" onClick={hideModal} source="secondary" />
+                    <ActionButton text="Confirm" disabled={isPosting} type="submit" />
+                </ButtonWrapper>
             </Form>
-        </ModalOuterContainer>
+        </FlexModalOuter>
     );
 };
 
