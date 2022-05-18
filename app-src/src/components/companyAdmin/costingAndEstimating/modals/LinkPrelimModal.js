0908fa9ca77db92a6faea079c141156f260d03a2
Mark Baker <mark@silverchip.com>
2022-05-18 13:11:56 +0100

    update costing / estimating tab modals

--- app-src/src/components/companyAdmin/costingAndEstimating/modals/LinkPrelimModal.js
+++ app-src/src/components/companyAdmin/costingAndEstimating/modals/LinkPrelimModal.js
@@ -2,54 +2,47 @@ import React from 'react';
 
 import useLinkPrelim from '../_hooks/useLinkPrelim';
 
-import ModalOuterContainer from 'components/shared/generic/modals/containers/ModalOuterContainer';
 import Form from 'components/shared/generic/form/containers/Form';
-import BlockButtonWrapper from 'components/shared/generic/blockButtonWrappers/presentational/BlockButtonWrapper';
 import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
-import ModalHeading from 'components/shared/generic/modals/presentational/ModalHeading';
 import Field from 'components/shared/generic/form/presentational/Field';
 import DropdownContainer from 'components/shared/generic/form/containers/DropdownContainer';
+import FlexModalOuter from 'components/shared/generic/modals/presentational/FlexModalOuter';
 
 const LinkPrelimModal = () => {
     const { form, handleChange, handleSubmit, isPosting, prelimsOptions, closeModal } =
         useLinkPrelim();
 
     return (
-        <ModalOuterContainer hideCloseButton>
-            <ModalHeading title={'Add prelim'} />
+        <FlexModalOuter title="Add existing prelim">
+            <Form onSubmit={handleSubmit} className="generic-form flex-content-wrapper size-lg-12">
+                <div className="flex-content">
+                    <div className="form-fields-container">
+                        <Field name="Select prelim" required>
+                            <DropdownContainer
+                                name="prelimID"
+                                options={Object.values(prelimsOptions)}
+                                value={prelimsOptions[form.prelimID]}
+                                selectedOption={prelimsOptions[form.prelimID]}
+                                handleChange={handleChange}
+                                required
+                            />
+                        </Field>
+                    </div>
+                </div>
 
-            <Form onSubmit={handleSubmit} className="generic-form size-lg-12">
-                <Field name="" required>
-                    <DropdownContainer
-                        name="prelimID"
-                        options={Object.values(prelimsOptions)}
-                        value={prelimsOptions[form.prelimID]}
-                        selectedOption={prelimsOptions[form.prelimID]}
-                        handleChange={handleChange}
-                        required
+                <ButtonWrapper alignment="right" extraClasses="flex-modal-footer">
+                    <ActionButton text="Cancel" onClick={closeModal} source="secondary" />
+                    <ActionButton
+                        text="Save"
+                        icon={isPosting ? 'spinner' : 'save'}
+                        iconSpin={isPosting}
+                        disabled={isPosting}
+                        type="submit"
                     />
-                </Field>
-                <BlockButtonWrapper>
-                    <ButtonWrapper alignment="right">
-                        <ActionButton
-                            text="Cancel"
-                            onClick={closeModal}
-                            source="secondary"
-                            size="small"
-                        />
-                        <ActionButton
-                            text="Save"
-                            icon={isPosting ? 'spinner' : 'save'}
-                            iconSpin={isPosting}
-                            size="small"
-                            disabled={isPosting}
-                            type="submit"
-                        />
-                    </ButtonWrapper>
-                </BlockButtonWrapper>
+                </ButtonWrapper>
             </Form>
-        </ModalOuterContainer>
+        </FlexModalOuter>
     );
 };
 
