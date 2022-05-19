7a0aadc3e11eed4635fcc562193f65f8c9385f4e
Viktoriia Petrova <viktoriia@silverchip.com>
2022-05-19 17:55:36 +0100

    Add useFilterDocuments

---
+++ app-src/src/components/superAdmin/pinOptions/optionDocuments/hooks/useFilterDocuments.js
@@ -0,0 +1,9 @@
+const useFilterDocuments = (allDocuments, optionID) => {
+    const documents = allDocuments.filter(
+        document => document.pinOptionID === parseInt(optionID) && !document.isDeleted,
+    );
+
+    return { documents };
+};
+
+export default useFilterDocuments;
