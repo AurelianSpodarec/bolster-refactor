7f7bd7274d0aa31efbc042e205be3e07a9a1464d
Mark Baker <mark@silverchip.com>
2022-05-19 10:48:54 +0100

    update drawing expiry modal

--- app-src/src/components/shared/generic/modals/presentational/DrawingExpiryModal.js
+++ app-src/src/components/shared/generic/modals/presentational/DrawingExpiryModal.js
@@ -3,39 +3,48 @@ import moment from 'moment';
 
 import useHierarchyDrawingExpiry from '../hooks/useHierarchyDrawingExpiry';
 
-import BlockHeading from '../../blockHeading/presentational/BlockHeading';
 import BlockContainer from '../../block/containers/BlockContainer';
-import ModalOuterContainer from '../containers/ModalOuterContainer';
+import FlexModalOuter from './FlexModalOuter';
+import ButtonWrapper from '../../button/presentational/ButtonWrapper';
+import ActionButton from '../../button/presentational/ActionButton';
 
 const DrawingExpiryModal = ({ hideModal, id, hierarchyID }) => {
     const { hierarchyDrawings, isFetching, error } = useHierarchyDrawingExpiry(id, hierarchyID);
 
     return (
-        <ModalOuterContainer handleClose={hideModal}>
-            <BlockHeading title={'Drawing Expiry'} />
+        <FlexModalOuter title="Drawing Expiry">
+            <div className="flex-content-wrapper">
+                <div className="flex-content">
+                    <BlockContainer
+                        isFetching={isFetching}
+                        isEmpty={!hierarchyDrawings.length}
+                        error={error}
+                        noDataMessage="There are no drawings on this hierarchy"
+                        contentClass="no-padding"
+                    >
+                        {hierarchyDrawings.map(
+                            ({ id, siteName, buildingName, floorName, name, expiresOn }) => (
+                                <p key={id} className="drawing-expiry-item">
+                                    {`${siteName} / ${buildingName} / ${floorName} / ${name} - `}
+                                    <span
+                                        className={
+                                            moment(expiresOn).isAfter(Date.now()) ? 'green' : 'red'
+                                        }
+                                    >
+                                        <span className="bold-text">Expiry Date </span>
+                                        {moment(expiresOn).format('DD/MM/YYYY')}
+                                    </span>
+                                </p>
+                            ),
+                        )}
+                    </BlockContainer>
+                </div>
+            </div>
 
-            <BlockContainer
-                isFetching={isFetching}
-                isEmpty={!hierarchyDrawings.length}
-                error={error}
-                noDataMessage="There are no drawings on this hierarchy"
-                contentClass="no-padding"
-            >
-                {hierarchyDrawings.map(
-                    ({ id, siteName, buildingName, floorName, name, expiresOn }) => (
-                        <p key={id} className="drawing-expiry-item">
-                            {`${siteName} / ${buildingName} / ${floorName} / ${name} - `}
-                            <span
-                                className={moment(expiresOn).isAfter(Date.now()) ? 'green' : 'red'}
-                            >
-                                <span className="bold-text">Expiry Date </span>
-                                {moment(expiresOn).format('DD/MM/YYYY')}
-                            </span>
-                        </p>
-                    ),
-                )}
-            </BlockContainer>
-        </ModalOuterContainer>
+            <ButtonWrapper alignment="right" extraClasses="flex-modal-footer">
+                <ActionButton text="Close" onClick={hideModal} />
+            </ButtonWrapper>
+        </FlexModalOuter>
     );
 };
 
