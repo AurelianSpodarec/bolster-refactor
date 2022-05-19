20b079599fa37c618a8ad3c926cc167e2475721d
Mark Baker <mark@silverchip.com>
2022-05-19 16:09:09 +0100

    update photo modal

--- app-src/src/components/shared/generic/modals/presentational/PinPhotoModal.js
+++ app-src/src/components/shared/generic/modals/presentational/PinPhotoModal.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-import ModalOuterContainer from '../containers/ModalOuterContainer';
+import ActionButton from '../../button/presentational/ActionButton';
+import ButtonWrapper from '../../button/presentational/ButtonWrapper';
+
 import Loading from '../../misc/presentational/Loading';
+import FlexModalOuter from './FlexModalOuter';
 
 class PinPhotoModal extends Component {
     state = { isLoading: true };
-    render = () => (
-        <ModalOuterContainer>
-            {this.state.isLoading && <Loading message="Loading photo..." />}
-            <img
-                alt="pin"
-                src={this.props.image}
-                // set the loading variable to false on load or error
-                onLoad={() => this.setState({ isLoading: false })}
-                onError={() => this.setState({ isLoading: false })}
-                style={{
-                    padding: '1em',
-                }}
-            />
-        </ModalOuterContainer>
-    );
+
+    render = () => {
+        const { hideModal } = this.props;
+
+        return (
+            <FlexModalOuter title="View Photo">
+                <div className="flex-content-wrapper">
+                    <div className="flex-content">
+                        {this.state.isLoading && <Loading message="Loading photo..." />}
+                        <img
+                            alt="pin"
+                            src={this.props.image}
+                            // set the loading variable to false on load or error
+                            onLoad={() => this.setState({ isLoading: false })}
+                            onError={() => this.setState({ isLoading: false })}
+                        />
+                    </div>
+
+                    <ButtonWrapper alignment="right" extraClasses="flex-modal-footer">
+                        <ActionButton text="Close" onClick={hideModal} />
+                    </ButtonWrapper>
+                </div>
+            </FlexModalOuter>
+        );
+    };
 }
 
 export default PinPhotoModal;
