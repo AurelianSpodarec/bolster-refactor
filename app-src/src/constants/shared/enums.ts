668998fa162f7c03516f512c676756b3fec0bd15
Mark Baker <mark@silverchip.com>
2022-06-01 09:21:39 +0100

    add enums for push notifications

---
+++ app-src/src/constants/shared/enums.ts
@@ -0,0 +1,45 @@
+export const PUSH_NOTIFICATION_TARGET_VALUES = {
+    ALL: 1,
+    SITE: 2,
+    USER: 3,
+};
+
+export const PUSH_NOTIFICATION_TARGET_NAMES = {
+    1: 'All',
+    2: 'Site',
+    3: 'User',
+};
+
+export const PUSH_NOTIFICATION_FREQUENCY_VALUES = {
+    ONCE: 0,
+    DAILY: 1,
+    WEEKLY: 2,
+    MONTHLY: 3,
+};
+
+export const PUSH_NOTIFICATION_FREQUENCY_NAMES = {
+    0: 'Once',
+    1: 'Daily',
+    2: 'Weekly',
+    3: 'Monthly',
+};
+
+export const RECURRENCE_DAYS_VALUES = {
+    MONDAY: 1,
+    TUESDAY: 2,
+    WEDNESDAY: 4,
+    THURSDAY: 8,
+    FRIDAY: 16,
+    SATURDAY: 32,
+    SUNDAY: 64,
+};
+
+export const RECURRENCE_DAYS_NAMES = {
+    1: 'Monday',
+    2: 'Tuesday',
+    4: 'Wednesday',
+    8: 'Thursday',
+    16: 'Friday',
+    32: 'Saturday',
+    64: 'Sunday',
+};
