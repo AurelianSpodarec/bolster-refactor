1fa99e50de4ef21ce7df9356f23703c0cbf3827c
Viktoriia Petrova <viktoriia@silverchip.com>
2022-06-08 17:09:27 +0100

    Add auto-renewal checkbox

--- app-src/src/components/companyAdmin/subscription/bolsterPlus/BolsterPlusPod.js
+++ app-src/src/components/companyAdmin/subscription/bolsterPlus/BolsterPlusPod.js
@@ -5,19 +5,38 @@ import BlockHeading from 'components/shared/generic/blockHeading/presentational/
 import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
 import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 import FlexWrapper from 'components/shared/generic/flexWrapper/FlexWrapper';
+import CheckboxContainer from 'components/shared/generic/form/containers/CheckboxContainer';
+import useAutoRenew from './hooks/useAutoRenew';
 
 const BolsterPlusPod = () => {
-    const isBolsterPlusActivated = true;
+    const isBolsterPlusActivated = false;
+
+    const { handleAutoRenewChange, isAutoRenew } = useAutoRenew(isAutoRenew);
+
     return (
         <BlockContainer>
-            <BlockHeading title="Bolster plus" />
+            <FlexWrapper>
+                <BlockHeading title="Bolster plus" />
+
+                {isBolsterPlusActivated && (
+                    <FlexWrapper justify="end" width="6">
+                        <CheckboxContainer
+                            name={'isAutoRenew'}
+                            text="Auto-Renewal"
+                            value={isAutoRenew}
+                            handleChange={handleAutoRenewChange}
+                            checked={!!isAutoRenew}
+                        />
+                    </FlexWrapper>
+                )}
+            </FlexWrapper>
             <p className="size-lg-12">
                 Utilise the best features of Bolster Systems to improve your business workflows
             </p>
-            <p className="size-lg-12 heading">£3000</p>
-
-            {isBolsterPlusActivated && (
+            {!isBolsterPlusActivated && (
                 <>
+                    <p className="size-lg-12 heading">£3000</p>
+
                     <p className="size-lg-12">Highlights</p>
                     <h1 className="size-lg-12">
                         Costing &amp; Estimating (price sites automatically from your schedule of
