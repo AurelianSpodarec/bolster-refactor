3bface125388c5ec43119f9b49fbfba88e782ce8
Viktoriia Petrova <viktoriia@silverchip.com>
2022-06-09 09:59:41 +0100

    Clean up

--- app-src/src/components/companyAdmin/subscription/bolsterPlus/modals/AddBolsterPlusModal.js
+++ app-src/src/components/companyAdmin/subscription/bolsterPlus/modals/AddBolsterPlusModal.js
@@ -1,43 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import Form from 'components/shared/generic/form/containers/Form';
-import RadioButton from 'components/shared/generic/form/presentational/RadioButton';
-import { PAYMENT_IDS, PAYMENT_TYPES } from 'constants/companyAdmin/enums';
-import Field from 'components/shared/generic/form/presentational/Field';
-import TextInputContainer from 'components/shared/generic/form/containers/TextInputContainer';
-import { formatNumber } from 'helpers/generic';
-import CheckboxContainer from 'components/shared/generic/form/containers/CheckboxContainer';
-import AddCardFormContainer from '../../cardManagement/addCardModal/containers/AddCardFormContainer';
-import Select from 'components/shared/generic/form/presentational/Select';
-import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
-import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
 import FlexModalOuter from 'components/shared/generic/modals/presentational/FlexModalOuter';
-import FieldOutput from 'components/shared/generic/fieldOutput/presentational/FieldOutput';
 
-const AddBolsterPlusModal = ({
-    hideModal,
-    handleChange,
-    handleSubmit,
-    paymentType,
-    creditsToBuy = 0,
-    cards,
-    selectedCard,
-    handleCreditsChange,
-    costWithVAT,
-    costWithoutVAT,
-    noCards,
-    addCardVisible,
-    showAddCard,
-    hideAddCard,
-    handleAddCardSuccess,
-    termsAgreed,
-    isPosting,
-    shouldReceiveFreeCredit,
-}) => {
-    if (addCardVisible)
-        return <AddCardFormContainer close={hideAddCard} onSuccess={handleAddCardSuccess} />;
-
+const AddBolsterPlusModal = () => {
     return (
         <FlexModalOuter title="Add Bolster Plus Subscription">
             <div className="flex-content">
