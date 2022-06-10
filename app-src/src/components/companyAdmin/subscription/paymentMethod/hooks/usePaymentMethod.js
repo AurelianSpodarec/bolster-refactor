fc0edc2a058da876921e1005b8369d6f168e053c
Viktoriia Petrova <viktoriia@silverchip.com>
2022-06-10 14:44:05 +0100

    Add payment functionality

--- app-src/src/components/companyAdmin/subscription/paymentMethod/hooks/usePaymentMethod.js
+++ app-src/src/components/companyAdmin/subscription/paymentMethod/hooks/usePaymentMethod.js
@@ -1,11 +1,25 @@
+import fetchAllCards from 'actions/companyAdmin/cards/async/fetchAllCards';
+import showModal from 'actions/shared/generic/modals/sync/showModal';
 import { PAYMENT_IDS } from 'constants/companyAdmin/enums';
-import { useForm } from 'helpers/hooks';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { selectCards } from 'selectors/companyAdmin/cards';
+import { PAYMENT_ERROR, PAYMENT_SUCCESS } from 'constants/shared/modalTypes';
+import { useForm, usePrevious } from 'helpers/hooks';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    selectCards,
+    selectCardsIsFetching,
+    selectCardsPostError,
+    selectCardsPostSuccess,
+} from 'selectors/companyAdmin/cards';
 
 const usePaymentMethod = () => {
+    const dispatch = useDispatch();
     const cards = useSelector(selectCards);
+    const isFetching = useSelector(selectCardsIsFetching);
+    const postSuccess = useSelector(selectCardsPostSuccess);
+    const postError = useSelector(selectCardsPostError);
+    const prevProps = usePrevious({ postSuccess, postError, isFetching });
+
     const [addCardVisible, setCardVisible] = useState(false);
     const [form, handleChange] = useForm({
         paymentType: null,
@@ -42,14 +56,51 @@ const usePaymentMethod = () => {
         setCardVisible(true);
     };
 
+    const hideAddCard = () => {
+        setCardVisible(false);
+    };
+
+    useEffect(() => {
+        dispatch(fetchAllCards());
+    }, [dispatch]);
+
+    useEffect(() => {
+        const { paymentType } = form;
+
+        if (!isFetching && prevProps.isFetching) {
+            const primaryCard = cards.find(({ isPrimary }) => isPrimary);
+            handleChange('stripeCardID', primaryCard ? primaryCard.id : null);
+        }
+
+        if (postSuccess && !prevProps.postSuccess) {
+            // fetch();
+            showModal(PAYMENT_SUCCESS, {
+                message: `Your order has been successfully placed and your Bolster Plus upgrade ${
+                    +paymentType === PAYMENT_IDS.CARD
+                        ? 'have been added.'
+                        : 'will be available once the invoice has been paid'
+                }`,
+            });
+        }
+
+        if (postError && !prevProps.postError) {
+            // fetch();
+            showModal(PAYMENT_ERROR, {
+                message:
+                    'There was an error while purchasing Bolster Plus upgrade. Please try again.',
+                resubmit: this.handleSubmit,
+            });
+        }
+    }, [isFetching, postSuccess, postError, prevProps]);
+
     return {
         form,
         handleChange,
         handleSubmit,
-        cardOptions,
+        cards: cardOptions,
         showAddCard,
+        hideAddCard,
         addCardVisible,
-        cards,
         handleAddCardSuccess,
     };
 };
