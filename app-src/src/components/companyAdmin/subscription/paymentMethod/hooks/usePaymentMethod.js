a6af09b8f6d8c95c1587b70acf90cc743b5f6ed2
Viktoriia Petrova <viktoriia@silverchip.com>
2022-06-10 11:44:26 +0100

    Add payment functionality to AddBolsterPlusModal

--- app-src/src/components/companyAdmin/subscription/paymentMethod/hooks/usePaymentMethod.js
+++ app-src/src/components/companyAdmin/subscription/paymentMethod/hooks/usePaymentMethod.js
@@ -1,7 +1,57 @@
-const usePaymentMethod = () => {
-    const handleChange = () => {};
+import { PAYMENT_IDS } from 'constants/companyAdmin/enums';
+import { useForm } from 'helpers/hooks';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectCards } from 'selectors/companyAdmin/cards';
 
-    return { handleChange };
+const usePaymentMethod = () => {
+    const cards = useSelector(selectCards);
+    const [addCardVisible, setCardVisible] = useState(false);
+    const [form, handleChange] = useForm({
+        paymentType: null,
+        stripeCardID: null,
+        idempotencyKey: null,
+    });
+
+    const cardOptions = cards.map(card => ({
+        label: `${card.nickname || card.name} - ${card.lastFour}`,
+        value: card.id,
+    }));
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const { paymentType, stripeCardID, idempotencyKey } = form;
+
+        // if (isPosting) return;
+        const postBody = {
+            paymentType,
+            stripeCardID: +paymentType === PAYMENT_IDS.CARD ? stripeCardID : null,
+            idempotencyKey,
+        };
+
+        // action(postBody);
+    };
+
+    const handleAddCardSuccess = card => {
+        handleChange('stripleCardID', card.id);
+        setCardVisible(false);
+    };
+
+    const showAddCard = () => {
+        setCardVisible(true);
+    };
+
+    return {
+        form,
+        handleChange,
+        handleSubmit,
+        cardOptions,
+        showAddCard,
+        addCardVisible,
+        cards,
+        handleAddCardSuccess,
+    };
 };
 
 export default usePaymentMethod;
---
+++ app-src/src/selectors/companyAdmin/cards.js
@@ -0,0 +1,1 @@
+export const selectCards = state => Object.values(state.companyAdmin.cardsReducer.cards);
