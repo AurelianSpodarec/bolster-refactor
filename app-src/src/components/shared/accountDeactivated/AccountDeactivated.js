9d2305280b2617606ac230780ae83f4a47e10089
Viktoriia Petrova <viktoriia@silverchip.com>
2022-06-13 15:13:38 +0100

    Style AccountDeactivated page

--- app-src/src/components/shared/accountDeactivated/AccountDeactivated.js
+++ app-src/src/components/shared/accountDeactivated/AccountDeactivated.js
@@ -1,22 +1,54 @@
 import React from 'react';
 
 import InvoicesTableContainer from 'components/companyAdmin/invoices/shared/invoiceListTable/containers/InvoicesTableContainer';
-import BlockContainer from '../generic/block/containers/BlockContainer';
+
 import Block from '../generic/block/presentational/Block';
 import useAccountDeactivated from './hooks/useAccountDeactivated';
+import PageHeading from '../generic/pageHeading/presentational/PageHeading';
+import BlockContainer from '../generic/block/containers/BlockContainer';
+import FlexWrapper from '../generic/flexWrapper/FlexWrapper';
+import BolsterLogo from '../../../_content/images/bolster_logo_no_background.png';
 
 const AccountDeactivated = () => {
     const { invoices } = useAccountDeactivated();
     return (
         <>
-            <BlockContainer heading="Pending Invoices">
+            <PageHeading title="Company Invoices" />
+
+            <BlockContainer>
                 <InvoicesTableContainer invoices={invoices} />
             </BlockContainer>
             <Block>
-                <p>
-                    Due to an outstanding invoice your account has been deactivated.Please contact
-                    Bolster Systems.
-                </p>
+                <FlexWrapper justify="center" align="center" direction="column">
+                    <p className="large-text">
+                        Due to an outstanding invoice your account has been deactivated.Please
+                        contact Bolster Systems.
+                    </p>
+                    <br />
+                    <img src={BolsterLogo} alt="Bolster Logo" style={{ width: '400px' }} />
+                    <FlexWrapper justify="center" align="center">
+                        <p className="with-horizontal-margin large-text">
+                            <br />
+                            <i
+                                className="fa fa-phone"
+                                style={{ transform: 'rotate(90deg)', marginRight: '10px' }}
+                            />
+                            <a href="tel:0161 873 7679" className="link-without-decoration">
+                                0161 873 7679
+                            </a>
+                        </p>
+                        <p className="with-horizontal-margin large-text">
+                            <br />
+                            <i className="fa fa-envelope" style={{ marginRight: '10px' }} />
+                            <a
+                                href="mailto:info@bolstersystems.com"
+                                className="link-without-decoration"
+                            >
+                                support@bolstersystems.com
+                            </a>
+                        </p>
+                    </FlexWrapper>
+                </FlexWrapper>
             </Block>
         </>
     );
