ef1e8d283c977fef93f8c730a42c0d588089713f
Nick <nick@silverchip.com>
2022-06-16 11:08:47 +0100

    WIP timesheet shift pods

--- app-src/src/components/companyAdmin/userManagement/operatives/timesheets/breakdown/dayBreakdown/BreakdownOverviewList.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/timesheets/breakdown/dayBreakdown/BreakdownOverviewList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { selectFilterByHasClockedIn } from 'selectors/companyAdmin/timesheets';
@@ -9,66 +9,107 @@ import BreakdownSummary from '../BreakdownSummary';
 
 import { timesheetFilter, timesheetSort } from './hooks/useOverviewFilters';
 import { timesheetSelectedCompanyIDs } from 'selectors/companyAdmin/timesheets';
+import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
+import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
+import moment from 'moment';
+import getShiftPodData from '../../helpers/getShiftPodData';
+import { selectCompanyUsers } from 'selectors/companyAdmin/companyUsers';
 
 const BreakdownOverviewList = ({ timesheets, selectedDate, filterType, filterDirection }) => {
     const selectedUserIDs = useSelector(timesheetSelectedCompanyIDs);
 
     const filterByHasClockedIn = useSelector(selectFilterByHasClockedIn);
 
-    let formattedTimesheets = [];
+    const users = useSelector(selectCompanyUsers);
 
-    if (filterByHasClockedIn && selectedUserIDs.length === 0) {
-        formattedTimesheets = timesheets
-            .filter(timesheetFilter(filterByHasClockedIn, selectedDate))
-            .sort(timesheetSort(filterType, filterDirection, selectedDate));
-    }
+    const selectedDay = useMemo(() => {
+        const getDayMatch = day => moment(day.date).isSame(selectedDate, 'day');
+        const thisDay = timesheets.reduce((acc, curr) => {
+            return curr.days.find(getDayMatch);
+        }, undefined);
+        return thisDay;
+    }, [selectedDate, timesheets]);
 
-    if (!filterByHasClockedIn && selectedUserIDs.length === 0) {
-        formattedTimesheets = timesheets.sort(
-            timesheetSort(filterType, filterDirection, selectedDate),
-        );
-    }
+    // let formattedTimesheets = [];
 
-    if (selectedUserIDs.length) {
-        formattedTimesheets = timesheets
-            .filter(({ companyUserID }) => selectedUserIDs.includes(companyUserID))
-            .sort(timesheetSort(filterType, filterDirection, selectedDate));
-    }
+    // if (filterByHasClockedIn && selectedUserIDs.length === 0) {
+    //     formattedTimesheets = timesheets
+    //         .filter(timesheetFilter(filterByHasClockedIn, selectedDate))
+    //         .sort(timesheetSort(filterType, filterDirection, selectedDate));
+    // }
 
-    if (formattedTimesheets.length === 0) return <p>No clock in data to display.</p>;
+    // if (!filterByHasClockedIn && selectedUserIDs.length === 0) {
+    //     formattedTimesheets = timesheets.sort(
+    //         timesheetSort(filterType, filterDirection, selectedDate),
+    //     );
+    // }
 
-    return formattedTimesheets.map(timesheet => {
+    // if (selectedUserIDs.length) {
+    //     formattedTimesheets = timesheets
+    //         .filter(({ companyUserID }) => selectedUserIDs.includes(companyUserID))
+    //         .sort(timesheetSort(filterType, filterDirection, selectedDate));
+    // }
+
+    if (!selectedDay || !selectedDay?.shifts?.length) return <p>No clock in data to display.</p>;
+
+    console.log({ timesheets, selectedDate, selectedDay, users });
+
+    return selectedDay.shifts.map(shift => {
+        // const {
+        //     companyUserID,
+        //     firstName,
+        //     lastName,
+        //     email,
+        //     formattedHours,
+        //     formattedBreakHours,
+        //     formattedClockedInHours,
+        //     jobReferenceIDs,
+        //     totalPins,
+        //     clockIn,
+        //     clockOut,
+        //     clockerNotes,
+        // } = useDayOverview(timesheet, selectedDate);
+
+        const user = users[shift.companyUserID] || {};
+
         const {
-            companyUserID,
-            firstName,
-            lastName,
-            email,
-            formattedHours,
-            formattedBreakHours,
-            formattedClockedInHours,
-            jobReferenceIDs,
-            totalPins,
-            clockIn,
-            clockOut,
-            clockerNotes,
-        } = useDayOverview(timesheet, selectedDate);
+            timeIn,
+            timeOut,
+            breakTime,
+            noOfHistories,
+            jobReferences, // { jobRef, hoursWorked, wageSplit }
+            jobReferencesTotalHours,
+            jobReferencesTotalCost,
+            expenses, // { name, cost }
+            expensesTotal,
+            shiftTotal,
+            notes,
+            status,
+            groupUID,
+        } = getShiftPodData(shift);
 
         return (
-            <div className="day" key={companyUserID}>
-                <BreakdownSummary
-                    name={`${firstName} ${lastName} (${email})`}
-                    formattedHours={formattedHours}
-                    formattedBreakHours={formattedBreakHours}
-                    formattedClockedInHours={formattedClockedInHours}
-                    totalPins={totalPins}
-                    clockIn={clockIn}
-                    clockOut={clockOut}
-                    jobReferenceIDs={jobReferenceIDs}
+            <BlockContainer key={groupUID} containerClass={`shift-pod`}>
+                <BlockHeading
+                    title={`${user.userFirstName} ${user.userLastName} (${user.userEmail})`}
                 />
-                <BreakdownNotes notes={clockerNotes} />
-            </div>
+            </BlockContainer>
         );
     });
 };
+
+// <div className="day" key={companyUserID}>
+//     <BreakdownSummary
+//         name={`${firstName} ${lastName} (${email})`}
+//         formattedHours={formattedHours}
+//         formattedBreakHours={formattedBreakHours}
+//         formattedClockedInHours={formattedClockedInHours}
+//         totalPins={totalPins}
+//         clockIn={clockIn}
+//         clockOut={clockOut}
+//         jobReferenceIDs={jobReferenceIDs}
+//     />
+//     <BreakdownNotes notes={clockerNotes} />
+// </div>
 
 export default BreakdownOverviewList;
---
+++ app-src/src/components/companyAdmin/userManagement/operatives/timesheets/helpers/getShiftPodData.js
@@ -0,0 +1,38 @@
+const getShiftPodData = shift => {
+    const {
+        clockerEntries,
+        companyUserID,
+        endOn,
+        formattedBreakHours,
+        formattedClockedInHours,
+        formattedHours,
+        groupUID,
+        id,
+        lastClockedOutTime,
+        overriddenByCompanyUserID,
+        overriddenOn,
+        overrideShiftTime,
+        overrideWage,
+        startOn,
+        status,
+        totalPins,
+    } = shift;
+
+    return {
+        timeIn: startOn,
+        timeOut: endOn,
+        breakTime: formattedBreakHours,
+        noOfHistories: 0,
+        jobReferences: [], // { jobRef, hoursWorked, wageSplit }
+        jobReferencesTotalHours: formattedClockedInHours,
+        jobReferencesTotalCost: 0,
+        expenses: [], // { name, cost }
+        expensesTotal: 0,
+        shiftTotal: 0,
+        notes: [],
+        status,
+        groupUID,
+    };
+};
+
+export default getShiftPodData;
