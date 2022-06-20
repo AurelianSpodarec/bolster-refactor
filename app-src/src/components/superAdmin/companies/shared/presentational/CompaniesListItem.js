efd948a1b54ec8d36f606ec23e898d8e05041b13
Mark Baker <mark@silverchip.com>
2022-06-20 16:09:21 +0100

    add enable / disable to single company page

--- app-src/src/components/superAdmin/companies/shared/presentational/CompaniesListItem.js
+++ app-src/src/components/superAdmin/companies/shared/presentational/CompaniesListItem.js
@@ -4,9 +4,7 @@ import { withRouter } from 'react-router-dom';
 import DateTimeContainer from 'components/shared/dateTime/containers/DateTimeContainer';
 import { DATE_TIME_IDS, COMPANY_TYPES, getEnumKey } from 'constants/companyAdmin/enums';
 import { capitaliseWords } from 'helpers/generic';
-import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
 import LinkButton from 'components/shared/generic/button/presentational/LinkButton';
-import useCompanyActions from '../hooks/useCompanyActions';
 
 const CompaniesListItem = ({
     company: {
@@ -18,48 +16,35 @@ const CompaniesListItem = ({
         companyType,
         hideOnClientList,
         creditValue,
-        isDisabled,
     },
     match: { url },
-    company,
-}) => {
-    const { showEnableCompanyModal, showDisableCompanyModal } = useCompanyActions(company);
-
-    return (
-        <tr>
-            <td>{name}</td>
-            <td>{telephone || '-'}</td>
-            <td>{address || '-'}</td>
-            <td>
-                {nextSubscriptionExpiryDate ? (
-                    <DateTimeContainer
-                        date={nextSubscriptionExpiryDate}
-                        datetime={DATE_TIME_IDS.DATE}
-                    />
-                ) : (
-                    '-'
-                )}
-            </td>
-            <td>{capitaliseWords(getEnumKey(COMPANY_TYPES, companyType))}</td>
-            <td>{creditValue || 0}</td>
-            <td>{!hideOnClientList ? 'Yes' : 'No'}</td>
-            <td>
-                <LinkButton
-                    text="More info"
-                    href={`${url}/${id}`}
-                    source="secondary"
-                    ambient="positive"
+}) => (
+    <tr>
+        <td>{name}</td>
+        <td>{telephone || '-'}</td>
+        <td>{address || '-'}</td>
+        <td>
+            {nextSubscriptionExpiryDate ? (
+                <DateTimeContainer
+                    date={nextSubscriptionExpiryDate}
+                    datetime={DATE_TIME_IDS.DATE}
                 />
-            </td>
-            <td>
-                {isDisabled ? (
-                    <ActionButton text="Enable" onClick={showEnableCompanyModal} />
-                ) : (
-                    <ActionButton text="Disable" onClick={showDisableCompanyModal} />
-                )}
-            </td>
-        </tr>
-    );
-};
+            ) : (
+                '-'
+            )}
+        </td>
+        <td>{capitaliseWords(getEnumKey(COMPANY_TYPES, companyType))}</td>
+        <td>{creditValue || 0}</td>
+        <td>{!hideOnClientList ? 'Yes' : 'No'}</td>
+        <td>
+            <LinkButton
+                text="More info"
+                href={`${url}/${id}`}
+                source="secondary"
+                ambient="positive"
+            />
+        </td>
+    </tr>
+);
 
 export default withRouter(CompaniesListItem);
