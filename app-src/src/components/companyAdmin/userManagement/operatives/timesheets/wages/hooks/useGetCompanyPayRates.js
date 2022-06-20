2f86b825f1101aa7f0eeef5acb408f9e07bec718
Seb Smith <seb@silverchip.com>
2022-06-20 13:07:09 +0100

    Refactors hook to me more usable

--- app-src/src/components/companyAdmin/userManagement/operatives/timesheets/wages/hooks/useGetCompanyPayRates.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/timesheets/wages/hooks/useGetCompanyPayRates.js
@@ -1,39 +1,26 @@
-import { useMemo, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { showModal } from 'actions/shared/generic/modals/sync/showModal';
 import fetchPayRates from 'actions/companyAdmin/payRates/fetchPayRates';
 
-import { selectPayRates } from 'selectors/companyAdmin/payRates';
-
-import { isEmpty } from 'helpers/generic';
-
-import { PAY_RATES_MODAL } from 'constants/shared/modalTypes';
+import {
+    selectPayRates,
+    selectPayRatesError,
+    selectPayRatesIsFetching,
+} from 'selectors/companyAdmin/payRates';
 
 const useGetCompanyPayRates = () => {
     const dispatch = useDispatch();
 
     const companyPayRates = useSelector(selectPayRates);
+    const isFetching = useSelector(selectPayRatesIsFetching);
+    const error = useSelector(selectPayRatesError);
 
     useEffect(() => {
         dispatch(fetchPayRates());
     }, []);
 
-    const companyPayRateOptions = useMemo(() => {
-        if (isEmpty(companyPayRates)) return [];
-
-        const options = companyPayRates?.map(payRate => ({
-            value: payRate.id,
-            label: payRate.name,
-        }));
-
-        return [
-            ...options,
-            { value: 0, label: 'Create New', onClick: () => dispatch(showModal(PAY_RATES_MODAL)) },
-        ];
-    }, [companyPayRates]);
-
-    return { companyPayRateOptions };
+    return { companyPayRates, isFetching, error };
 };
 
 export default useGetCompanyPayRates;
