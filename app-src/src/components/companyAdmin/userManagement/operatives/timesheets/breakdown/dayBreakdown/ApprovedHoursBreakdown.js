39859f38043197057ce086c0697e825e1f0567fc
Nick <nick@silverchip.com>
2022-06-22 10:59:45 +0100

    Add Approved Hours pod

---
+++ app-src/src/components/companyAdmin/userManagement/operatives/timesheets/breakdown/dayBreakdown/ApprovedHoursBreakdown.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
+import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
+import Table from 'components/shared/generic/tables/presentational/Table';
+import moment from 'moment';
+
+const ApprovedHoursBreakdown = ({ currencySymbol = '£' }) => {
+    return (
+        <BlockContainer contentClass="inner-pod">
+            <BlockHeading title="Approved Hours Breakdown" />
+            <div className="divider" />
+            <div className="table-container">
+                <Table
+                    headers={['Job References', 'Hours Worked', 'Wage Split']}
+                    isFetching={false}
+                    error={null}
+                    noData={false}
+                    noDataMessage="No hours to display."
+                >
+                    {[].map((item, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>###</td>
+                                <td>###</td>
+                                <td>0.00</td>
+                            </tr>
+                        );
+                    })}
+                    <tr className="total-row">
+                        <td>Total</td>
+                        <td>###</td>
+                        <td>###</td>
+                    </tr>
+                </Table>
+            </div>
+        </BlockContainer>
+    );
+};
+
+export default ApprovedHoursBreakdown;
