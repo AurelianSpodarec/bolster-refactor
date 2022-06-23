05736f6012288af9025725b4cb2595f5c9371fe0
Nick <nick@silverchip.com>
2022-06-23 13:24:28 +0100

    Plug CSV button into modal

--- app-src/src/components/companyAdmin/userManagement/operatives/timesheets/GenerateTimesheetsCSVButton.js
+++ app-src/src/components/companyAdmin/userManagement/operatives/timesheets/GenerateTimesheetsCSVButton.js
@@ -1,17 +1,33 @@
-import postGenerateTimesheetsCSV from 'actions/companyAdmin/timesheets/async/postGenerateTimesheetsCSV';
-import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
 import React from 'react';
+import showModal from 'actions/shared/generic/modals/sync/showModal';
+import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
 import { useDispatch } from 'react-redux';
+import { GENERATE_TIMESHEET_REPORT } from 'constants/shared/modalTypes';
 
 const GenerateTimesheetsCSVButton = () => {
     const dispatch = useDispatch();
 
-    const handleSubmit = () => {
-        const postBody = {};
-        dispatch(postGenerateTimesheetsCSV(postBody));
+    const handlePress = () => {
+        dispatch(
+            showModal(GENERATE_TIMESHEET_REPORT, {
+                fromDateInclusive: null,
+                toDateInclusive: null,
+                serviceID: null,
+                hierarchyID: null,
+                pinIDs: null,
+            }),
+        );
     };
 
-    return <ActionButton size="medium" onClick={handleSubmit} text="Export CSV" icon="file-csv" />;
+    return (
+        <ActionButton
+            size="medium"
+            onClick={handlePress}
+            text="Export CSV"
+            icon="file-csv"
+            isPosting={false}
+        />
+    );
 };
 
 export default GenerateTimesheetsCSVButton;
