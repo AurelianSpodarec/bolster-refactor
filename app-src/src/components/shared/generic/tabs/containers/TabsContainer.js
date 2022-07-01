46d49f3505ff500516e7b3897b25780ad93d21b8
Seb Smith <seb@silverchip.com>
2022-07-01 14:40:01 +0100

    Hides costing and estimating tabs

--- app-src/src/components/shared/generic/tabs/containers/TabsContainer.js
+++ app-src/src/components/shared/generic/tabs/containers/TabsContainer.js
@@ -4,19 +4,36 @@ import { connect } from 'react-redux';
 import selectTab from 'actions/shared/generic/tabs/sync/selectTab';
 
 import Tabs from '../presentational/Tabs';
+import { COMPANY_USER_ROLE_TYPES } from '../../../../../constants/companyAdmin/enums';
 
-const TabsContainer = ({ dispatch, tabs, selectedTab, classes }) => (
-    <Tabs
-        tabs={tabs}
-        selectedTab={selectedTab}
-        selectTab={(e, tab) => {
-            e.preventDefault();
-            dispatch(selectTab(tab));
-        }}
-        classes={classes}
-    />
-);
+const TabsContainer = ({ dispatch, tabs, selectedTab, classes, companyUserType }) => {
+    const isAdminPlus = companyUserType > COMPANY_USER_ROLE_TYPES.ADMIN;
+    const filteredTabs = tabs.filter(tab =>
+        !isAdminPlus
+            ? !tab.toLowerCase().includes('costing') && !tab.toLowerCase().includes('estimating')
+            : true,
+    );
 
-const mapStateToProps = ({ shared: { tabsReducer } }) => tabsReducer;
+    return (
+        <Tabs
+            tabs={filteredTabs}
+            selectedTab={selectedTab}
+            selectTab={(e, tab) => {
+                e.preventDefault();
+                dispatch(selectTab(tab));
+            }}
+            classes={classes}
+        />
+    );
+};
+
+const mapStateToProps = ({
+    shared: {
+        tabsReducer: { selectedTab, tabs },
+        decodeJWTReducer: {
+            jwtData: { companyUserType },
+        },
+    },
+}) => ({ tabs, selectedTab, companyUserType });
 
 export default connect(mapStateToProps)(TabsContainer);
