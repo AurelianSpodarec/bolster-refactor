628ecf55fc90c00861620568035b6aaaec416dce
Viktoriia Petrova <viktoriia@silverchip.com>
2022-07-05 14:30:15 +0100

    Fix goBack

--- app-src/src/components/shared/generic/modals/presentational/BolsterPlusUpgradeModal.js
+++ app-src/src/components/shared/generic/modals/presentational/BolsterPlusUpgradeModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
@@ -16,7 +16,7 @@ import LinkButton from '../../button/presentational/LinkButton';
 import FlexWrapper from '../../flexWrapper/FlexWrapper';
 
 // handleClose will be used for any extra actions that might be needed when closing the modal (e.g. change tab)
-const BolsterPlusUpgradeModal = ({ handleClose, hideModal, handleSwitchTab }) => {
+const BolsterPlusUpgradeModal = ({ shouldGoBack, handleClose, hideModal, handleSwitchTab }) => {
     const dispatch = useDispatch();
     const history = useHistory();
 
@@ -46,7 +46,9 @@ const BolsterPlusUpgradeModal = ({ handleClose, hideModal, handleSwitchTab }) =>
                             onClick={() => {
                                 if (handleClose) {
                                     handleClose();
-                                } else {
+                                }
+
+                                if (shouldGoBack) {
                                     history.goBack();
                                 }
 
