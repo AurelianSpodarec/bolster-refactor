fca6180fa2df4f0a9034131ff9cd14be1e468562
Viktoriia Petrova <viktoriia@silverchip.com>
2022-07-12 12:10:05 +0100

    Change to pie chart if tab was changed

--- app-src/src/components/companyAdmin/dashboard/containers/DashboardPinHistoryCharts.js
+++ app-src/src/components/companyAdmin/dashboard/containers/DashboardPinHistoryCharts.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { connect, useSelector } from 'react-redux';
 import { isIE } from 'react-device-detect';
 
 import BlockContainer from 'components/shared/generic/block/containers/BlockContainer';
@@ -9,9 +9,20 @@ import DashboardBarChartContainer from './DashboardBarChartContainer';
 import BlockHeading from 'components/shared/generic/blockHeading/presentational/BlockHeading';
 import ButtonWrapper from 'components/shared/generic/button/presentational/ButtonWrapper';
 import ActionButton from 'components/shared/generic/button/presentational/ActionButton';
+import { usePrevious } from 'helpers/hooks';
+import { selectUpdateDashboardFilters } from '../../../../selectors/companyAdmin/dashboard';
 
 const DashboardPinHistoryCharts = ({ isFetching, error, datasets, showLineGraph }) => {
     const [selectedTab, setSelectedTab] = useState('pie');
+    const updateDashboardFilters = useSelector(selectUpdateDashboardFilters);
+    const prevProp = usePrevious(updateDashboardFilters.status);
+
+    useEffect(() => {
+        if (updateDashboardFilters.status !== prevProp) {
+            setSelectedTab('pie');
+        }
+    }, [updateDashboardFilters, prevProp]);
+
     return !isIE ? (
         <BlockContainer
             isFetching={isFetching}
