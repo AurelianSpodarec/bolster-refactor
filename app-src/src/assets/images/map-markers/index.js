4b283013dd6e32379b310507cf09b70e91d13ef8
Aurelian Spodarec <aurelian.spod@gmail.com>
2022-09-09 23:07:20 +0100

    chore/(images) - Re-added images

---
+++ app-src/src/assets/images/map-markers/index.js
@@ -0,0 +1,41 @@
+import blue from './blue-pin.png';
+import green from './green-pin.png';
+import purple from './purple-pin.png';
+import red from './red-pin.png';
+import yellow from './yellow-pin.png';
+
+import blue2x from './blue-pin2x.png';
+import green2x from './green-pin2x.png';
+import purple2x from './purple-pin2x.png';
+import red2x from './red-pin2x.png';
+import yellow2x from './yellow-pin2x.png';
+
+import iconBlue from './icon-pin-blue.png';
+import iconGreen from './icon-pin-green.png';
+import iconPurple from './icon-pin-purple.png';
+import iconRed from './icon-pin-red.png';
+import iconYellow from './icon-pin-yellow.png';
+
+export const pinImgs = {
+    blue: blue,
+    green: green,
+    purple: purple,
+    red: red,
+    yellow: yellow,
+};
+
+export const pin2xImgs = {
+    blue: blue2x,
+    green: green2x,
+    purple: purple2x,
+    red: red2x,
+    yellow: yellow2x,
+};
+
+export const iconPinImgs = {
+    blue: iconBlue,
+    green: iconGreen,
+    purple: iconPurple,
+    red: iconRed,
+    yellow: iconYellow,
+};
